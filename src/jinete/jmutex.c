afb2d3f502723ac20f0b922b47b125341e5d83ab
David Capello <davidcapello@gmail.com>
2008-01-23 16:18:58 +0000

    Added JMutex, lock_current_sprite, and locked & mutex fields for Sprite. Added readable/writable buttons in the Film Editor. Added forbidden cursor. Fixed some bugs in editors_draw_sprite_tiled and cmd_configure_tools.

---
+++ src/jinete/jmutex.c
@@ -0,0 +1,67 @@
+/* Jinete - a GUI library
+ * Copyright (C) 2003, 2004, 2005, 2007, 2008 David A. Capello.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *   * Neither the name of the Jinete nor the names of its contributors may
+ *     be used to endorse or promote products derived from this software
+ *     without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <allegro.h>
+#include <assert.h>
+
+#include "jinete/jmutex.h"
+
+JMutex jmutex_new(void)
+{
+  assert(system_driver != NULL);
+  assert(system_driver->create_mutex != NULL);
+
+  return (JMutex)system_driver->create_mutex();
+}
+
+void jmutex_free(JMutex mutex)
+{
+  assert(system_driver != NULL);
+  assert(system_driver->destroy_mutex != NULL);
+
+  return system_driver->destroy_mutex(mutex);
+}
+
+void jmutex_lock(JMutex mutex)
+{
+  assert(system_driver != NULL);
+  assert(system_driver->lock_mutex != NULL);
+
+  system_driver->lock_mutex(mutex);
+}
+
+void jmutex_unlock(JMutex mutex)
+{
+  assert(system_driver != NULL);
+  assert(system_driver->unlock_mutex != NULL);
+
+  system_driver->unlock_mutex(mutex);
+}
