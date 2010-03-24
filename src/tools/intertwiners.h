65ae558291f5a5ab85bf8c39ddb146fae90deaba
David Capello <davidcapello@gmail.com>
2010-03-24 00:07:48 -0300

    Preview of a filled polygon is a closed outline now.

--- src/tools/intertwiners.h
+++ src/tools/intertwiners.h
@@ -54,6 +54,16 @@ public:
 
 	algo_line(x1, y1, x2, y2, loop, (AlgoPixel)doPointshapePoint);
       }
+
+      // Closed shape
+      if (loop->getFilled()) {
+	int x1 = points[points.size()-1].x;
+	int y1 = points[points.size()-1].y;
+	int x2 = points[0].x;
+	int y2 = points[0].y;
+
+	algo_line(x1, y1, x2, y2, loop, (AlgoPixel)doPointshapePoint);
+      }
     }
   }
 
