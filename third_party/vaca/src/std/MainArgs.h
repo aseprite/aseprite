69bad938de3167578a65506a120d630e2e92bd85
David Capello <davidcapello@gmail.com>
2010-05-30 10:47:16 -0300

    Removed unnecessary #include <allegro.h>

--- third_party/vaca/src/std/MainArgs.h
+++ third_party/vaca/src/std/MainArgs.h
@@ -29,7 +29,6 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 // OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include <allegro.h>
 #include "Vaca/String.h"
 
 void Vaca::details::MainArgs::setArgs(int argc, char* argv[])
