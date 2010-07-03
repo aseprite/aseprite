3d89a21e2ff58802a3edce090137041b325bc065
David Capello <davidcapello@gmail.com>
2010-07-03 13:34:17 -0300

    Added "Quick Reference" command. - Added Launcher class to open files and URLs.

---
+++ src/commands/cmd_quick_reference.cpp
@@ -0,0 +1,57 @@
+/* ASE - Allegro Sprite Editor
+ * Copyright (C) 2001-2010  David Capello
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "config.h"
+
+#include "commands/command.h"
+#include "launcher.h"
+
+//////////////////////////////////////////////////////////////////////
+// about
+
+class QuickReferenceCommand : public Command
+{
+public:
+  QuickReferenceCommand();
+  Command* clone() const { return new QuickReferenceCommand(*this); }
+
+protected:
+  void execute(Context* context);
+};
+
+QuickReferenceCommand::QuickReferenceCommand()
+  : Command("quick_reference",
+	    "Quick Reference",
+	    CmdUIOnlyFlag)
+{
+}
+
+void QuickReferenceCommand::execute(Context* context)
+{
+  std::string path = "docs/quickref.pdf";
+
+  Launcher::openFile(path);
+}
+
+//////////////////////////////////////////////////////////////////////
+// CommandFactory
+
+Command* CommandFactory::create_quick_reference_command()
+{
+  return new QuickReferenceCommand;
+}
