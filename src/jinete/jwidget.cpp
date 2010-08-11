aseprite/aseprite

commit 01c811e7266cbd5abc8f452b3529007a59dac271

Add PreferredSizeEvent and Widget::onPreferredSize to replace JM_REQSIZE message. There are a lot of widgets in C code, so JM_REQSIZE cannot be removed until all widgets are converted to C++ classes.

src/jinete/jwidget.cpp
@@ -49,6 +49,7 @@
 
 #include "jinete/jinete.h"
 #include "jinete/jintern.h"
+#include "Vaca/PreferredSizeEvent.h"
 
 int ji_register_widget_type()
 {
@@ -95,6 +96,8 @@ Widget::Widget(int type)
   this->user_data[1] = NULL;
   this->user_data[2] = NULL;
   this->user_data[3] = NULL;
+
+  m_preferredSize = NULL;
 }
 
 void jwidget_free(JWidget widget)
@@ -143,6 +146,9 @@ Widget::~Widget()
   JI_LIST_FOR_EACH(this->hooks, link)
     jhook_free(reinterpret_cast<JHook>(link->data));
   jlist_free(this->hooks);
+
+  // Delete the preferred size
+  delete m_preferredSize;
 
   /* low level free */
   _ji_remove_widget(this);
@@ -702,19 +708,6 @@ void Widget::setBounds(const Rect& rc)
 {
   jrect jrc = { rc.x, rc.y, rc.x+rc.w, rc.y+rc.h };
   jwidget_set_rect(this, &jrc);
-}
-
-void jwidget_request_size(JWidget widget, int *w, int *h)
-{
-  JMessage msg;
-
-  assert_valid_widget(widget);
-
-  msg = jmessage_new(JM_REQSIZE);
-  jwidget_send_message(widget, msg);
-  *w = MID(widget->min_w, msg->reqsize.w, widget->max_w);
-  *h = MID(widget->min_h, msg->reqsize.h, widget->max_h);
-  jmessage_free(msg);
 }
 
 void jwidget_relayout(JWidget widget)
@@ -1309,6 +1302,76 @@ void Widget::closeWindow()
 }
 
 // ===============================================================
+// SIZE & POSITION
+// ===============================================================
+
+/**
+   Returns the preferred size of the Widget.
+
+   It checks if the preferred size is static (it means when it was
+   set through #setPreferredSize before) or if it is dynamic (this is
+   the default and is when the #onPreferredSize is used to determined
+   the preferred size).
+
+   In another words, if you do not use #setPreferredSize to set a
+   <em>static preferred size</em> for the widget then #onPreferredSize
+   will be used to calculate it.
+
+   @see setPreferredSize, onPreferredSize, #getPreferredSize(const Size &)
+*/
+Size Widget::getPreferredSize()
+{
+  if (m_preferredSize != NULL)
+    return *m_preferredSize;
+  else {
+    PreferredSizeEvent ev(this, Size(0, 0));
+    onPreferredSize(ev);
+    return ev.getPreferredSize();
+  }
+}
+
+/**
+   Returns the preferred size trying to fit in the specified size.
+   Remember that if you use #setPreferredSize this routine will
+   return the static size which you specified manually.
+
+   @param fitIn
+       This can have both attributes (width and height) in
+       zero, which means that it'll behave same as #getPreferredSize().
+       If the width is great than zero the #onPreferredSize will try to
+       fit in that width (this is useful to fit @link Vaca::Label Label@endlink
+       or @link Vaca::Edit Edit@endlink controls in a specified width and
+       calculate the height it could occupy).
+
+   @see getPreferredSize
+*/
+Size Widget::getPreferredSize(const Size& fitIn)
+{
+  if (m_preferredSize != NULL)
+    return *m_preferredSize;
+  else {
+    PreferredSizeEvent ev(this, fitIn);
+    onPreferredSize(ev);
+    return ev.getPreferredSize();
+  }
+}
+
+/**
+   Sets a fixed preferred size specified by the user.
+   Widget::getPreferredSize() will return this value if it's setted.
+*/
+void Widget::setPreferredSize(const Size& fixedSize)
+{
+  delete m_preferredSize;
+  m_preferredSize = new Size(fixedSize);
+}
+
+void Widget::setPreferredSize(int fixedWidth, int fixedHeight)
+{
+  setPreferredSize(Size(fixedWidth, fixedHeight));
+}
+
+// ===============================================================
 // FOCUS & MOUSE
 // ===============================================================
 
@@ -1502,4 +1565,30 @@ bool Widget::onProcessMessage(JMessage msg)
   }
 
   return false;
+}
+
+// ===============================================================
+// EVENTS
+// ===============================================================
+
+/**
+   Calculates the preferred size for the widget.
+
+   The default implementation get the preferred size of the current
+   layout manager. Also, if there exists layout-free widgets inside
+   this parent (like a StatusBar), they preferred-sizes are
+   accumulated.
+
+   @see Layout#getPreferredSize,
+*/
+void Widget::onPreferredSize(PreferredSizeEvent& ev)
+{
+  JMessage msg = jmessage_new(JM_REQSIZE);
+  jwidget_send_message(this, msg);
+  Size sz(msg->reqsize.w, msg->reqsize.h);
+  sz.w = MID(this->min_w, sz.w, this->max_w);
+  sz.h = MID(this->min_h, sz.h, this->max_h);
+  jmessage_free(msg);
+
+  ev.setPreferredSize(sz);
 }

makefile.lst
@@ -510,9 +510,11 @@ VACA_SOURCES =					\
 	$(VACA_DIR)/src/Application.cpp		\
 	$(VACA_DIR)/src/Component.cpp		\
 	$(VACA_DIR)/src/Debug.cpp		\
+	$(VACA_DIR)/src/Event.cpp		\
 	$(VACA_DIR)/src/Exception.cpp		\
 	$(VACA_DIR)/src/Mutex.cpp		\
 	$(VACA_DIR)/src/Point.cpp		\
+	$(VACA_DIR)/src/PreferredSizeEvent.cpp	\
 	$(VACA_DIR)/src/Property.cpp		\
 	$(VACA_DIR)/src/Rect.cpp		\
 	$(VACA_DIR)/src/Referenceable.cpp	\

src/commands/cmd_palette_editor.cpp
@@ -25,6 +25,7 @@
 #include <vector>
 
 #include "Vaca/Bind.h"
+#include "Vaca/Size.h"
 #include "jinete/jinete.h"
 
 #include "app.h"
@@ -49,6 +50,8 @@
 #include "widgets/statebar.h"
 #include "sprite_wrappers.h"
 #include "ui_context.h"
+
+using Vaca::Size;
 
 static Frame* window = NULL;
 static int redraw_timer_id = -1;
@@ -997,20 +1000,20 @@ static bool select_hsv_hook(JWidget widget, void *data)
 
 static bool expand_button_select_hook(JWidget widget, void *data)
 {
-  int req_w, req_h;
+  Size reqSize;
 
   if (more_options->isVisible()) {
     set_config_bool("PaletteEditor", "ShowMoreOptions", false);
     more_options->setVisible(false);
 
     // Get the required size of the "More options" panel
-    jwidget_request_size(more_options, &req_w, &req_h);
-    req_h += 4;
+    reqSize = more_options->getPreferredSize();
+    reqSize.h += 4;
 
     // Remove the space occupied by the "More options" panel
     {
       JRect rect = jrect_new(window->rc->x1, window->rc->y1,
-			     window->rc->x2, window->rc->y2 - req_h);
+			     window->rc->x2, window->rc->y2 - reqSize.h);
       window->move_window(rect);
       jrect_free(rect);
     }
@@ -1020,12 +1023,12 @@ static bool expand_button_select_hook(JWidget widget, void *data)
     more_options->setVisible(true);
 
     // Get the required size of the whole window
-    jwidget_request_size(window, &req_w, &req_h);
+    reqSize = window->getPreferredSize();
 
     // Add space for the "more_options" panel
-    if (jrect_h(window->rc) < req_h) {
+    if (jrect_h(window->rc) < reqSize.h) {
       JRect rect = jrect_new(window->rc->x1, window->rc->y1,
-			     window->rc->x2, window->rc->y1 + req_h);
+			     window->rc->x2, window->rc->y1 + reqSize.h);
 
       // Show the expanded area inside the screen
       if (rect->y2 > JI_SCREEN_H)

src/jinete/jbox.cpp
@@ -38,6 +38,7 @@
 #include "jinete/jrect.h"
 #include "jinete/jwidget.h"
 #include "jinete/jtheme.h"
+#include "Vaca/Size.h"
 
 static bool box_msg_proc(JWidget widget, JMessage msg);
 static void box_request_size(JWidget widget, int *w, int *h);
@@ -80,11 +81,11 @@ static void box_request_size(JWidget widget, int *w, int *h)
 #define GET_CHILD_SIZE(w, h)			\
   {						\
     if (widget->getAlign() & JI_HOMOGENEOUS)	\
-      *w = MAX(*w, req_##w);			\
+      *w = MAX(*w, reqSize.w);			\
     else					\
-      *w += req_##w;				\
+      *w += reqSize.w;				\
 						\
-    *h = MAX(*h, req_##h);			\
+    *h = MAX(*h, reqSize.h);			\
   }
 
 #define FINAL_SIZE(w)					\
@@ -96,7 +97,6 @@ static void box_request_size(JWidget widget, int *w, int *h)
   }
 
   int nvis_children;
-  int req_w, req_h;
   JWidget child;
   JLink link;
 
@@ -115,7 +115,7 @@ static void box_request_size(JWidget widget, int *w, int *h)
     if (child->flags & JI_HIDDEN)
       continue;
 
-    jwidget_request_size(child, &req_w, &req_h);
+    Size reqSize = child->getPreferredSize();
 
     if (widget->getAlign() & JI_HORIZONTAL) {
       GET_CHILD_SIZE(w, h);
@@ -151,7 +151,7 @@ static void box_set_position(JWidget widget, JRect rect)
 	extra = width / nvis_children;					\
       }									\
       else if (nexpand_children > 0) {					\
-	width = jrect_##w(widget->rc) - req_##w;			\
+	width = jrect_##w(widget->rc) - reqSize.w;			\
 	extra = width / nexpand_children;				\
       }									\
       else {								\
@@ -179,9 +179,9 @@ static void box_set_position(JWidget widget, JRect rect)
 	    width -= extra;						\
 	  }								\
 	  else {							\
-	    jwidget_request_size(child, &req_w, &req_h);		\
+	    reqSize = child->getPreferredSize();			\
 									\
-	    child_width = req_##w;					\
+	    child_width = reqSize.w;					\
 									\
 	    if (jwidget_is_expansive(child)) {				\
 	      if (nexpand_children == 1)				\
@@ -213,7 +213,6 @@ static void box_set_position(JWidget widget, JRect rect)
   JWidget child;
   int nvis_children = 0;
   int nexpand_children = 0;
-  int req_w, req_h;
   int child_width;
   JLink link;
   int width;
@@ -232,7 +231,7 @@ static void box_set_position(JWidget widget, JRect rect)
     }
   }
 
-  jwidget_request_size(widget, &req_w, &req_h);
+  Size reqSize = widget->getPreferredSize();
 
   if (widget->getAlign() & JI_HORIZONTAL) {
     FIXUP(x, y, w, h, l, t, r, b);

src/jinete/jcombobox.cpp
@@ -34,6 +34,8 @@
 #include <allegro.h>
 
 #include "jinete/jinete.h"
+#include "Vaca/Size.h"
+#include "Vaca/PreferredSizeEvent.h"
 
 struct ComboBox::Item
 {
@@ -267,47 +269,16 @@ bool ComboBox::onProcessMessage(JMessage msg)
       closeListBox();
       break;
 
-    case JM_REQSIZE: {
-      int w, h;
-
-      msg->reqsize.w = 0;
-      msg->reqsize.h = 0;
-
-      jwidget_request_size(m_entry, &w, &h);
-
-      // Get the text-length of every item and put in 'w' the maximum value
-      std::vector<Item*>::iterator it, end = m_items.end();
-      for (it = m_items.begin(); it != end; ++it) {
-	int item_w =
-	  2*jguiscale()+
-	  text_length(this->getFont(), (*it)->text.c_str())+
-	  10*jguiscale();
-
-	w = MAX(w, item_w);
-      }
-
-      msg->reqsize.w += w;
-      msg->reqsize.h += h;
-
-      jwidget_request_size(m_button, &w, &h);
-      msg->reqsize.w += w;
-      msg->reqsize.h = MAX(msg->reqsize.h, h);
-
-      return true;
-    }
-
     case JM_SETPOS: {
       JRect cbox = jrect_new_copy(&msg->setpos.rect);
-      int w, h;
-
       jrect_copy(this->rc, cbox);
 
-      /* button */
-      jwidget_request_size(m_button, &w, &h);
-      cbox->x1 = msg->setpos.rect.x2 - w;
+      // Button
+      Size buttonSize = m_button->getPreferredSize();
+      cbox->x1 = msg->setpos.rect.x2 - buttonSize.w;
       jwidget_set_rect(m_button, cbox);
 
-      /* entry */
+      // Entry
       cbox->x2 = cbox->x1;
       cbox->x1 = msg->setpos.rect.x1;
       jwidget_set_rect(m_entry, cbox);
@@ -336,6 +307,31 @@ bool ComboBox::onProcessMessage(JMessage msg)
   }
 
   return Widget::onProcessMessage(msg);
+}
+
+void ComboBox::onPreferredSize(PreferredSizeEvent& ev)
+{
+  Size reqSize(0, 0);
+  Size entrySize = m_entry->getPreferredSize();
+
+  // Get the text-length of every item and put in 'w' the maximum value
+  std::vector<Item*>::iterator it, end = m_items.end();
+  for (it = m_items.begin(); it != end; ++it) {
+    int item_w =
+      2*jguiscale()+
+      text_length(this->getFont(), (*it)->text.c_str())+
+      10*jguiscale();
+
+    reqSize.w = MAX(reqSize.w, item_w);
+  }
+
+  reqSize.w += entrySize.w;
+  reqSize.h += entrySize.h;
+
+  Size buttonSize = m_button->getPreferredSize();
+  reqSize.w += buttonSize.w;
+  reqSize.h = MAX(reqSize.h, buttonSize.h);
+  ev.setPreferredSize(reqSize);
 }
 
 static bool combobox_entry_msg_proc(JWidget widget, JMessage msg)

src/jinete/jcombobox.h
@@ -79,6 +79,7 @@ public:
 
 protected:
   bool onProcessMessage(JMessage msg);
+  void onPreferredSize(PreferredSizeEvent& ev);
 
 private:
   struct Item;

src/jinete/jgrid.cpp
@@ -41,6 +41,7 @@
 #include "jinete/jrect.h"
 #include "jinete/jwidget.h"
 #include "jinete/jtheme.h"
+#include "Vaca/Size.h"
 
 struct Cell
 {
@@ -214,7 +215,7 @@ static void grid_set_position(JWidget widget, JRect rect)
   Grid* grid = reinterpret_cast<Grid*>(jwidget_get_data(widget, JI_GRID));
   JRect cpos = jrect_new(0, 0, 0, 0);
   int pos_x, pos_y;
-  int req_w, req_h;
+  Size reqSize;
   int x, y, w, h;
   int col, row;
   int i, j;
@@ -251,30 +252,30 @@ static void grid_set_position(JWidget widget, JRect rect)
 	CALCULATE_CELL_SIZE(col, hspan, colstrip, w);
 	CALCULATE_CELL_SIZE(row, vspan, rowstrip, h);
 
-	jwidget_request_size(cell->child, &req_w, &req_h);
+	reqSize = cell->child->getPreferredSize();
 
 	if (cell->align & JI_LEFT) {
-	  w = req_w;
+	  w = reqSize.w;
 	}
 	else if (cell->align & JI_CENTER) {
-	  x += w/2 - req_w/2;
-	  w = req_w;
+	  x += w/2 - reqSize.w/2;
+	  w = reqSize.w;
 	}
 	else if (cell->align & JI_RIGHT) {
-	  x += w - req_w;
-	  w = req_w;
+	  x += w - reqSize.w;
+	  w = reqSize.w;
 	}
 
 	if (cell->align & JI_TOP) {
-	  h = req_h;
+	  h = reqSize.h;
 	}
 	else if (cell->align & JI_MIDDLE) {
-	  y += h/2 - req_h/2;
-	  h = req_h;
+	  y += h/2 - reqSize.h/2;
+	  h = reqSize.h;
 	}
 	else if (cell->align & JI_BOTTOM) {
-	  y += h - req_h;
-	  h = req_h;
+	  y += h - reqSize.h;
+	  h = reqSize.h;
 	}
 
 	jrect_replace(cpos, x, y, x+w, y+h);
@@ -299,7 +300,7 @@ static void grid_calculate_size(JWidget widget)
 {
   Grid* grid = reinterpret_cast<Grid*>(jwidget_get_data(widget, JI_GRID));
   int row, col, size;
-  int req_w, req_h;
+  Size reqSize;
   int i, expand;
   int expand_count;
   int last_expand;
@@ -320,15 +321,15 @@ static void grid_calculate_size(JWidget widget)
     for (p_row=0; p_row<grid->p_rows; ++p_row) {			\
       /* for each cell */						\
       cell = grid->cells[row]+col;					\
-      req_w = req_h = 0;						\
+      reqSize.w = reqSize.h = 0;					\
 									\
       if (cell->child != NULL) {					\
 	if (cell->parent == NULL) {					\
 	  /* if the widget isn't hidden then we can request its size */ \
 	  if (!(cell->child->flags & JI_HIDDEN)) {			\
-	    jwidget_request_size(cell->child, &req_w, &req_h);		\
-	    cell->w = req_w - (cell->hspan-1) * widget->child_spacing;	\
-	    cell->h = req_h - (cell->vspan-1) * widget->child_spacing;	\
+	    reqSize = cell->child->getPreferredSize();			\
+	    cell->w = reqSize.w - (cell->hspan-1) * widget->child_spacing;	\
+	    cell->h = reqSize.h - (cell->vspan-1) * widget->child_spacing;	\
 	    if ((cell->align & p_align) == p_align)			\
 	      ++expand_count;						\
 	  }								\
@@ -397,8 +398,8 @@ static void grid_calculate_size(JWidget widget)
     ++current_span;							\
   } while (more_span);
   
-  CALCULATE_STRIPS(col, cols, row, rows, vspan, req_w, colstrip, JI_HORIZONTAL);
-  CALCULATE_STRIPS(row, rows, col, cols, hspan, req_h, rowstrip, JI_VERTICAL);
+  CALCULATE_STRIPS(col, cols, row, rows, vspan, reqSize.w, colstrip, JI_HORIZONTAL);
+  CALCULATE_STRIPS(row, rows, col, cols, hspan, reqSize.h, rowstrip, JI_VERTICAL);
   EXPAND_STRIPS(col, cols, row, rows, hspan, w, colstrip, grid_inc_col_size);
   EXPAND_STRIPS(row, rows, col, cols, vspan, h, rowstrip, grid_inc_row_size);
 

src/jinete/jlistbox.cpp
@@ -41,6 +41,7 @@
 #include "jinete/jtheme.h"
 #include "jinete/jview.h"
 #include "jinete/jwidget.h"
+#include "Vaca/Size.h"
 
 static bool listbox_msg_proc(JWidget widget, JMessage msg);
 static void listbox_request_size(JWidget widget, int *w, int *h);
@@ -326,16 +327,16 @@ static bool listbox_msg_proc(JWidget widget, JMessage msg)
 
 static void listbox_request_size(JWidget widget, int *w, int *h)
 {
-  int req_w, req_h;
+  Size reqSize;
   JLink link;
 
   *w = *h = 0;
 
   JI_LIST_FOR_EACH(widget->children, link) {
-    jwidget_request_size(reinterpret_cast<JWidget>(link->data), &req_w, &req_h);
+    reqSize = reinterpret_cast<Widget*>(link->data)->getPreferredSize();
 
-    *w = MAX(*w, req_w);
-    *h += req_h + ((link->next)? widget->child_spacing: 0);
+    *w = MAX(*w, reqSize.w);
+    *h += reqSize.h + ((link->next)? widget->child_spacing: 0);
   }
 
   *w += widget->border_width.l + widget->border_width.r;
@@ -344,7 +345,7 @@ static void listbox_request_size(JWidget widget, int *w, int *h)
 
 static void listbox_set_position(JWidget widget, JRect rect)
 {
-  int req_w, req_h;
+  Size reqSize;
   JWidget child;
   JRect cpos;
   JLink link;
@@ -355,9 +356,9 @@ static void listbox_set_position(JWidget widget, JRect rect)
   JI_LIST_FOR_EACH(widget->children, link) {
     child = (JWidget)link->data;
 
-    jwidget_request_size(child, &req_w, &req_h);
+    reqSize = child->getPreferredSize();
 
-    cpos->y2 = cpos->y1+req_h;
+    cpos->y2 = cpos->y1+reqSize.h;
     jwidget_set_rect(child, cpos);
 
     cpos->y1 += jrect_h(child->rc) + widget->child_spacing;
@@ -427,24 +428,24 @@ static bool listitem_msg_proc(JWidget widget, JMessage msg)
 
 static void listitem_request_size(JWidget widget, int *w, int *h)
 {
-  int max_w, max_h;
-  int req_w, req_h;
+  Size maxSize;
+  Size reqSize;
   JLink link;
 
   if (widget->hasText()) {
-    max_w = jwidget_get_text_length(widget);
-    max_h = jwidget_get_text_height(widget);
+    maxSize.w = jwidget_get_text_length(widget);
+    maxSize.h = jwidget_get_text_height(widget);
   }
   else
-    max_w = max_h = 0;
+    maxSize.w = maxSize.h = 0;
 
   JI_LIST_FOR_EACH(widget->children, link) {
-    jwidget_request_size(reinterpret_cast<JWidget>(link->data), &req_w, &req_h);
+    reqSize = reinterpret_cast<Widget*>(link->data)->getPreferredSize();
 
-    max_w = MAX(max_w, req_w);
-    max_h = MAX(max_h, req_h);
+    maxSize.w = MAX(maxSize.w, reqSize.w);
+    maxSize.h = MAX(maxSize.h, reqSize.h);
   }
 
-  *w = widget->border_width.l + max_w + widget->border_width.r;
-  *h = widget->border_width.t + max_h + widget->border_width.b;
+  *w = widget->border_width.l + maxSize.w + widget->border_width.r;
+  *h = widget->border_width.t + maxSize.h + widget->border_width.b;
 }

commit 58a34476981bad52e8d958afe1718d1506a6657f

Fix problem in Widget::getPreferredSize() introduced in commit 01c811e7266cbd5abc8f452b3529007a59dac271. Min/max sizes are taken into account when getPreferredSize() calls a customized onPreferredSize() method.

src/jinete/jwidget.cpp
@@ -1325,7 +1325,11 @@ Size Widget::getPreferredSize()
   else {
     PreferredSizeEvent ev(this, Size(0, 0));
     onPreferredSize(ev);
-    return ev.getPreferredSize();
+
+    Size sz(ev.getPreferredSize());
+    sz.w = MID(this->min_w, sz.w, this->max_w);
+    sz.h = MID(this->min_h, sz.h, this->max_h);
+    return sz;
   }
 }
 
@@ -1351,7 +1355,11 @@ Size Widget::getPreferredSize(const Size& fitIn)
   else {
     PreferredSizeEvent ev(this, fitIn);
     onPreferredSize(ev);
-    return ev.getPreferredSize();
+
+    Size sz(ev.getPreferredSize());
+    sz.w = MID(this->min_w, sz.w, this->max_w);
+    sz.h = MID(this->min_h, sz.h, this->max_h);
+    return sz;
   }
 }
 
@@ -1585,8 +1593,6 @@ void Widget::onPreferredSize(PreferredSizeEvent& ev)
   JMessage msg = jmessage_new(JM_REQSIZE);
   jwidget_send_message(this, msg);
   Size sz(msg->reqsize.w, msg->reqsize.h);
-  sz.w = MID(this->min_w, sz.w, this->max_w);
-  sz.h = MID(this->min_h, sz.h, this->max_h);
   jmessage_free(msg);
 
   ev.setPreferredSize(sz);