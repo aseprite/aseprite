56c8d3423742991f8af46da2cd47d8e754f52498
David Capello <davidcapello@gmail.com>
2011-02-02 00:42:19 -0300

    Minor changes in font.h (format).

--- src/gui/font.h
+++ src/gui/font.h
@@ -9,28 +9,27 @@
 
 #include "gui/base.h"
 
-struct BITMAP;
 struct FONT;
 
-struct FONT *ji_font_load(const char *filepathname);
-struct FONT *ji_font_load_bmp(const char *filepathname);
-struct FONT *ji_font_load_ttf(const char *filepathname);
+FONT* ji_font_load(const char* filepathname);
+FONT* ji_font_load_bmp(const char* filepathname);
+FONT* ji_font_load_ttf(const char* filepathname);
 
-int ji_font_get_size(struct FONT *f);
-int ji_font_set_size(struct FONT *f, int height);
+int ji_font_get_size(FONT* f);
+int ji_font_set_size(FONT* f, int height);
 
-int ji_font_get_aa_mode(struct FONT *f);
-int ji_font_set_aa_mode(struct FONT *f, int mode);
+int ji_font_get_aa_mode(FONT* f);
+int ji_font_set_aa_mode(FONT* f, int mode);
 
-bool ji_font_is_fixed(struct FONT *f);
-bool ji_font_is_scalable(struct FONT *f);
+bool ji_font_is_fixed(FONT* f);
+bool ji_font_is_scalable(FONT* f);
 
-const int *ji_font_get_available_fixed_sizes(struct FONT *f, int *n);
+const int *ji_font_get_available_fixed_sizes(FONT* f, int* n);
 
-int ji_font_get_char_extra_spacing(struct FONT *f);
-void ji_font_set_char_extra_spacing(struct FONT *f, int spacing);
+int ji_font_get_char_extra_spacing(FONT* f);
+void ji_font_set_char_extra_spacing(FONT* f, int spacing);
 
-int ji_font_char_len(struct FONT *f, int chr);
-int ji_font_text_len(struct FONT *f, const char *text);
+int ji_font_char_len(FONT* f, int chr);
+int ji_font_text_len(FONT* f, const char* text);
 
 #endif
