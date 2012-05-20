b208750112218090a604e7f4ff44cb78673398bf
David Capello <davidcapello@gmail.com>
2012-05-20 12:07:23 -0300

    Add script to upload packages.

---
+++ misc/upload_packages.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+
+version=$(cat ../config.h | grep VERSION | sed -e 's/.*\"\(.*\)\"/\1/g')
+python=c:/Python27/python.exe
+
+echo "Uploading $version..."
+
+echo -n "Google Code username? "
+read username
+
+echo -n "Google Code password? "
+read password
+
+$python googlecode_upload.py \
+    --summary="ASEPRITE $version Source Code without 3rd party code" \
+    --project=aseprite \
+    --user=$username \
+    --password=$password \
+    --labels="Type-Source,OpSys-Linux" \
+    aseprite-$version.tar.xz
+
+$python googlecode_upload.py \
+    --summary="ASEPRITE $version Source Code" \
+    --project=aseprite \
+    --user=$username \
+    --password=$password \
+    --labels="Featured,OpSys-All,Type-Source" \
+    aseprite-$version.zip
+
+$python googlecode_upload.py \
+    --summary="ASEPRITE $version for Windows" \
+    --project=aseprite \
+    --user=$username \
+    --password=$password \
+    --labels="Featured,OpSys-Windows,Type-Archive" \
+    aseprite-$version-win32.zip
