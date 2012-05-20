ba58d839df02a04ea15ce304bdbd78415c03e553
David Capello <davidcapello@gmail.com>
2012-05-20 20:54:34 -0300

    Add create_dmg.sh script to create .dmg packages.

---
+++ misc/create_dmg.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+dir="`pwd`"
+version=$(cat ../config.h | grep VERSION | sed -e 's/.*\"\(.*\)\"/\1/g')
+distdir=ASEPRITE_$version
+
+if [ ! -f $distdir.dmg ] ; then
+    cd "$dir/.."
+    mkdir "$dir/$distdir"
+    mkdir "$dir/$distdir/aseprite.app"
+    mkdir "$dir/$distdir/aseprite.app/Contents"
+    mkdir "$dir/$distdir/aseprite.app/Contents/MacOS"
+    mkdir "$dir/$distdir/aseprite.app/Contents/Resources"
+    cp -R LICENSE.txt docs "$dir/$distdir"
+    cp -R aseprite "$dir/$distdir/aseprite.app/Contents/MacOS"
+    cp -R "$dir/macosx/Info.plist" "$dir/$distdir/aseprite.app/Contents"
+    cp -R data "$dir/macosx/aseprite.icns" "$dir/$distdir/aseprite.app/Contents/Resources"
+    cd "$dir"
+
+    hdiutil create "$distdir.dmg" -srcfolder "$distdir"
+    rm -fr $distdir
+fi
