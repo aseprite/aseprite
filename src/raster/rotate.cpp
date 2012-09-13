da9e9fd062bfc14e5e675bd2f49716be23144a69
David Capello <davidcapello@gmail.com>
2012-09-13 19:12:40 -0300

    Fix algofill.cpp and rotate.cpp files to refer to the correct license.

--- src/raster/rotate.cpp
+++ src/raster/rotate.cpp
@@ -1,27 +1,11 @@
-/* ASEPRITE
- * Copyright (C) 2001-2012  David Capello
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-
-/* Most code come from original Allegro rotation code:
- *      By Shawn Hargreaves.
- *      Flipping routines by Andrew Geers.
- *      Optimized by Sven Sandberg.
- *      To C++ templates by David Capello
- */
+// Most code come from original Allegro rotation code:
+// By Shawn Hargreaves.
+// Flipping routines by Andrew Geers.
+// Optimized by Sven Sandberg.
+// To C++ templates by David Capello
+//
+// This source file is ditributed under a Allegro license, please
+// read allegro4-LICENSE.txt for more information.
 
 #include "config.h"
 
