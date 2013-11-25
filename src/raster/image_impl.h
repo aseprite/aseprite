06e0e5c8b34858ecffa18a51d0ca0eb0820fae36
David Capello <davidcapello@gmail.com>
2013-11-25 10:42:38 -0300

    Add missing inline modifier for ImageImpl<>::clear() specializations

--- src/raster/image_impl.h
+++ src/raster/image_impl.h
@@ -248,12 +248,12 @@ namespace raster {
   // Specializations
 
   template<>
-  void ImageImpl<IndexedTraits>::clear(color_t color) OVERRIDE {
+  inline void ImageImpl<IndexedTraits>::clear(color_t color) OVERRIDE {
     memset(m_bits, color, getWidth()*getHeight());
   }
 
   template<>
-  void ImageImpl<BitmapTraits>::clear(color_t color) OVERRIDE {
+  inline void ImageImpl<BitmapTraits>::clear(color_t color) OVERRIDE {
     memset(m_bits, (color ? 0xff: 0x00),
            BitmapTraits::getRowStrideBytes(getWidth()) * getHeight());
   }
