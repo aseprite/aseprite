f9e0be9e56ce2279180da91b2abd8e0c3c0441cf
David Capello <davidcapello@gmail.com>
2014-06-04 22:11:58 -0300

    Add distribution type to create_release.sh script

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -6,6 +6,9 @@ read srcdir
 echo -n "What version to release (e.g. 0.9.1-beta) ? "
 read version
 
+echo -n "What kind of distribution (e.g. production, trial) ? "
+read distribution
+
 destdir=aseprite-release-$version
 
 # --------------------------
@@ -31,13 +34,24 @@ cd ..
 if [ ! -d build ] ; then
     mkdir build
     cd build
-    cmake \
-        -D "CMAKE_BUILD_TYPE:STRING=RelWithDebInfo" \
-        -D "CMAKE_CXX_FLAGS_RELWITHDEBINFO:STRING = /Zi /MT /O2 /Ob1 /D NDEBUG" \
-        -D "CMAKE_C_FLAGS_RELWITHDEBINFO:STRING = /Zi /MT /O2 /Ob1 /D NDEBUG" \
-        -D "CMAKE_EXE_LINKER_FLAGS:STRING = /MACHINE:X86 /SUBSYSTEM:WINDOWS,5.01" \
-        -G "Ninja" \
-        ..
+    if [ "$distribution" == "production" ]; then
+        echo cmake \
+            -D "CMAKE_BUILD_TYPE:STRING=RelWithDebInfo" \
+            -D "CMAKE_CXX_FLAGS_RELWITHDEBINFO:STRING = /Zi /MT /O2 /Ob1 /D NDEBUG" \
+            -D "CMAKE_C_FLAGS_RELWITHDEBINFO:STRING = /Zi /MT /O2 /Ob1 /D NDEBUG" \
+            -D "CMAKE_EXE_LINKER_FLAGS:STRING = /MACHINE:X86 /SUBSYSTEM:WINDOWS,5.01" \
+            -G "Ninja" \
+            ..
+    elif [ "$distribution" == "trial" ]; then
+        cmake \
+            -D "CMAKE_BUILD_TYPE:STRING=RelWithDebInfo" \
+            -D "CMAKE_CXX_FLAGS_RELWITHDEBINFO:STRING = /Zi /MT /O2 /Ob1 /D NDEBUG" \
+            -D "CMAKE_C_FLAGS_RELWITHDEBINFO:STRING = /Zi /MT /O2 /Ob1 /D NDEBUG" \
+            -D "CMAKE_EXE_LINKER_FLAGS:STRING = /MACHINE:X86 /SUBSYSTEM:WINDOWS,5.01" \
+            -D "ENABLE_TRIAL_MODE:BOOL=ON" \
+            -G "Ninja" \
+            ..
+    fi
     cd ..
 fi
 
