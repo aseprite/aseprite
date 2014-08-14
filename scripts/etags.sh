ff93400c4e01f8d39d084cdff9052655f2d61b82
David Capello <davidcapello@gmail.com>
2014-08-14 01:23:35 -0300

    Remove scripts/ subdirectory

--- scripts/etags.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-find src third_party \
-   \( -name '*.[ch]' -o -name '*.cpp' \) -print | \
-    sed -e "/_old/D" | \
-    etags -
-
-ebrowse $(find src \( -name '*.h' -o -name '*.cpp' \) -print)
