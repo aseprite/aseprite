16be1718b580eb1a9f046886d4ae9049abc79dae
David Capello <davidcapello@gmail.com>
2014-11-10 22:48:40 -0300

    Fix clang compiler error: missing <stdexcept> to use std::runtime_error

--- src/app/context.cpp
+++ src/app/context.cpp
@@ -1,5 +1,5 @@
 /* Aseprite
- * Copyright (C) 2001-2013  David Capello
+ * Copyright (C) 2001-2014  David Capello
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -30,6 +30,7 @@
 #include "app/settings/settings.h"
 
 #include <algorithm>
+#include <stdexcept>
 
 namespace app {
 
