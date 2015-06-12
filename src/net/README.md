dfa5e561dab4ced03f13e3f4c24a8220ef63d70d
David Capello <davidcapello@gmail.com>
2015-06-12 11:17:26 -0300

    Change net library license to MIT

---
+++ src/net/README.md
@@ -0,0 +1,6 @@
+# Aseprite Network Library
+*Copyright (C) 2001-2015 David Capello*
+
+> Distributed under [MIT license](LICENSE.txt)
+
+Simple curl wrapper.
--- src/net/http_response.h
+++ src/net/http_response.h
@@ -1,9 +1,8 @@
-// Aseprite
-// Copyright (C) 2001-2015  David Capello
+// Aseprite Network Library
+// Copyright (c) 2001-2015 David Capello
 //
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License version 2 as
-// published by the Free Software Foundation.
+// This file is released under the terms of the MIT license.
+// Read LICENSE.txt for more information.
 
 #ifndef NET_HTTP_RESPONSE_H_INCLUDED
 #define NET_HTTP_RESPONSE_H_INCLUDED
