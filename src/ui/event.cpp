80a62f8ed55017f9464a50debcd18000dbca3017
David Capello <davidcapello@gmail.com>
2015-06-23 14:40:54 -0300

    Include config.h in ui/event.cpp

--- src/ui/event.cpp
+++ src/ui/event.cpp
@@ -1,8 +1,12 @@
 // Aseprite UI Library
-// Copyright (C) 2001-2013  David Capello
+// Copyright (C) 2001-2013, 2015  David Capello
 //
 // This file is released under the terms of the MIT license.
 // Read LICENSE.txt for more information.
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 
 #include "ui/event.h"
 
