aseprite/aseprite

commit 35229e99a6196d71b90ef2762abb32b0a164f594

Add opaque/transparent mode in context bar for transformations (fix #546)
With this change now we add a "mask" image/parameter in rotation
functions. In this way we can identify which specific pixels are
inside the original mask/selection, and in opaque mode we can
include/scale/rotate all those pixels inside the mask, whatever
value they are, even if they are the mask color.

Fixes #730

src/doc/algorithm/rotsprite.cpp
@@ -161,15 +161,15 @@ static void image_scale2x(Image* dst, const Image* src, int src_w, int src_h)
   }
 }
 
-void rotsprite_image(Image* bmp, Image* spr,
+void rotsprite_image(Image* bmp, const Image* spr, const Image* mask,
   int x1, int y1, int x2, int y2,
   int x3, int y3, int x4, int y4)
 {
-  static ImageBufferPtr buf1, buf2, buf3; // TODO non-thread safe
+  static ImageBufferPtr buf[3]; // TODO non-thread safe
 
-  if (!buf1) buf1.reset(new ImageBuffer(1));
-  if (!buf2) buf2.reset(new ImageBuffer(1));
-  if (!buf3) buf3.reset(new ImageBuffer(1));
+  for (int i=0; i<3; ++i)
+    if (!buf[i])
+      buf[i].reset(new ImageBuffer(1));
 
   int xmin = MIN(x1, MIN(x2, MIN(x3, x4)));
   int xmax = MAX(x1, MAX(x2, MAX(x3, x4)));
@@ -179,9 +179,10 @@ void rotsprite_image(Image* bmp, Image* spr,
   int rot_height = ymax - ymin + 1;
 
   int scale = 8;
-  base::UniquePtr<Image> bmp_copy(Image::create(bmp->pixelFormat(), rot_width*scale, rot_height*scale, buf1));
-  base::UniquePtr<Image> tmp_copy(Image::create(spr->pixelFormat(), spr->width()*scale, spr->height()*scale, buf2));
-  base::UniquePtr<Image> spr_copy(Image::create(spr->pixelFormat(), spr->width()*scale, spr->height()*scale, buf3));
+  base::UniquePtr<Image> bmp_copy(Image::create(bmp->pixelFormat(), rot_width*scale, rot_height*scale, buf[0]));
+  base::UniquePtr<Image> tmp_copy(Image::create(spr->pixelFormat(), spr->width()*scale, spr->height()*scale, buf[1]));
+  base::UniquePtr<Image> spr_copy(Image::create(spr->pixelFormat(), spr->width()*scale, spr->height()*scale, buf[2]));
+  base::UniquePtr<Image> msk_copy;
 
   color_t maskColor = spr->maskColor();
 
@@ -189,23 +190,38 @@ void rotsprite_image(Image* bmp, Image* spr,
   tmp_copy->setMaskColor(maskColor);
   spr_copy->setMaskColor(maskColor);
 
-  bmp_copy->clear(bmp->maskColor());
   spr_copy->clear(maskColor);
   spr_copy->copy(spr, gfx::Clip(spr->bounds()));
 
   for (int i=0; i<3; ++i) {
-    clear_image(tmp_copy, maskColor);
+    // clear_image(tmp_copy, maskColor);
     image_scale2x(tmp_copy, spr_copy, spr->width()*(1<<i), spr->height()*(1<<i));
     spr_copy->copy(tmp_copy, gfx::Clip(tmp_copy->bounds()));
   }
 
-  doc::algorithm::parallelogram(
-    bmp_copy, spr_copy,
+  if (mask) {
+    // Same ImageBuffer than tmp_copy
+    msk_copy.reset(Image::create(IMAGE_BITMAP, mask->width()*scale, mask->height()*scale, buf[1]));
+    clear_image(msk_copy, 0);
+    scale_image(msk_copy, mask,
+                0, 0, msk_copy->width(), msk_copy->height(),
+                0, 0, mask->width(), mask->height());
+  }
+
+  clear_image(bmp_copy, maskColor);
+  clear_image(bmp_copy, 0);
+  scale_image(bmp_copy, bmp,
+              0, 0, bmp_copy->width(), bmp_copy->height(),
+              xmin, ymin, rot_width, rot_height);
+
+  parallelogram(
+    bmp_copy, spr_copy, msk_copy.get(),
     (x1-xmin)*scale, (y1-ymin)*scale, (x2-xmin)*scale, (y2-ymin)*scale,
     (x3-xmin)*scale, (y3-ymin)*scale, (x4-xmin)*scale, (y4-ymin)*scale);
 
-  doc::algorithm::scale_image(bmp, bmp_copy,
-    xmin, ymin, bmp_copy->width()/scale, bmp_copy->height()/scale);
+  scale_image(bmp, bmp_copy,
+              xmin, ymin, rot_width, rot_height,
+              0, 0, bmp_copy->width(), bmp_copy->height());
 }
 
 } // namespace algorithm

data/pref.xml
@@ -110,6 +110,7 @@
     </section>
     <section id="selection">
       <option id="mode" type="app::tools::SelectionMode" default="app::tools::SelectionMode::DEFAULT" />
+      <option id="opaque" type="bool" default="false" />
       <option id="transparent_color" type="app::Color" />
       <option id="rotation_algorithm" type="app::tools::RotationAlgorithm" default="app::tools::RotationAlgorithm::DEFAULT" />
     </section>

data/skins/default/skin.xml
@@ -375,6 +375,8 @@
     <part id="ink_default"                      x="144" y="144" w="16" h="16" />
     <part id="ink_composite"                    x="160" y="144" w="16" h="16" />
     <part id="ink_lock_alpha"                   x="176" y="144" w="16" h="16" />
+    <part id="selection_opaque"                 x="208" y="176" w="16" h="10" />
+    <part id="selection_masked"                 x="224" y="176" w="16" h="10" />
   </parts>
 
   <stylesheet>

src/app/thumbnail_generator.cpp
@@ -90,7 +90,9 @@ private:
         // Stretch the 'image'
         m_thumbnail.reset(Image::create(image->pixelFormat(), thumb_w, thumb_h));
         clear_image(m_thumbnail, 0);
-        algorithm::scale_image(m_thumbnail, image, 0, 0, thumb_w, thumb_h);
+        algorithm::scale_image(m_thumbnail, image,
+                               0, 0, thumb_w, thumb_h,
+                               0, 0, image->width(), image->height());
       }
 
       // Close file

src/app/ui/context_bar.cpp
@@ -477,18 +477,72 @@ protected:
   }
 };
 
-
-class ContextBar::TransparentColorField : public ColorButton
-{
+class ContextBar::TransparentColorField : public HBox {
 public:
-  TransparentColorField() : ColorButton(app::Color::fromMask(), IMAGE_RGB) {
-    Change.connect(Bind<void>(&TransparentColorField::onChange, this));
+  TransparentColorField(ContextBar* owner)
+    : m_icon(1)
+    , m_maskColor(app::Color::fromMask(), IMAGE_RGB)
+    , m_owner(owner) {
+    addChild(&m_icon);
+    addChild(&m_maskColor);
+
+    m_icon.addItem(static_cast<SkinTheme*>(getTheme())->get_part(PART_SELECTION_OPAQUE));
+    gfx::Size sz = m_icon.getItem(0)->getPreferredSize();
+    sz.w += 2*guiscale();
+    m_icon.getItem(0)->setMinSize(sz);
+    setOpaque(Preferences::instance().selection.opaque());
+
+    m_icon.ItemChange.connect(Bind<void>(&TransparentColorField::onPopup, this));
+    m_maskColor.Change.connect(Bind<void>(&TransparentColorField::onChangeColor, this));
   }
 
-protected:
-  void onChange() {
-    Preferences::instance().selection.transparentColor(getColor());
+private:
+
+  void onPopup() {
+    gfx::Rect bounds = getBounds();
+
+    Menu menu;
+    MenuItem
+      opaque("Opaque"),
+      masked("Transparent");
+    menu.addChild(&opaque);
+    menu.addChild(&masked);
+
+    if (Preferences::instance().selection.opaque())
+      opaque.setSelected(true);
+    else
+      masked.setSelected(true);
+
+    opaque.Click.connect(Bind<void>(&TransparentColorField::setOpaque, this, true));
+    masked.Click.connect(Bind<void>(&TransparentColorField::setOpaque, this, false));
+
+    menu.showPopup(gfx::Point(bounds.x, bounds.y+bounds.h));
   }
+
+  void onChangeColor() {
+    Preferences::instance().selection.transparentColor(
+      m_maskColor.getColor());
+  }
+
+  void setOpaque(bool opaque) {
+    int part = (opaque ? PART_SELECTION_OPAQUE: PART_SELECTION_MASKED);
+    m_icon.getItem(0)->setIcon(
+      static_cast<SkinTheme*>(getTheme())->get_part(part));
+
+    m_maskColor.setVisible(!opaque);
+    Preferences::instance().selection.opaque(opaque);
+    if (!opaque) {
+      Preferences::instance().selection.transparentColor(
+        m_maskColor.getColor());
+    }
+
+    if (m_owner)
+      m_owner->layout();
+  }
+
+  ButtonSet m_icon;
+  ColorButton m_maskColor;
+  ContextBar* m_owner;
 };
 
 class ContextBar::RotAlgorithmField : public ComboBox
@@ -860,7 +914,7 @@ ContextBar::ContextBar()
   addChild(m_selectionOptionsBox = new HBox());
   m_selectionOptionsBox->addChild(m_dropPixels = new DropPixelsField());
   m_selectionOptionsBox->addChild(m_selectionMode = new SelectionModeField);
-  m_selectionOptionsBox->addChild(m_transparentColor = new TransparentColorField);
+  m_selectionOptionsBox->addChild(m_transparentColor = new TransparentColorField(this));
   m_selectionOptionsBox->addChild(m_rotAlgo = new RotAlgorithmField());
 
   addChild(m_brushType = new BrushTypeField(this));

src/app/ui/editor/editor.cpp
@@ -1417,8 +1417,11 @@ void Editor::setZoomAndCenterInMouse(Zoom zoom,
   }
 }
 
-void Editor::pasteImage(const Image* image, const gfx::Point& pos)
+void Editor::pasteImage(const Image* image, const Mask* mask)
 {
+  ASSERT(image);
+  ASSERT(mask);
+
   // Change to a selection tool: it's necessary for PixelsMovement
   // which will use the extra cel for transformation preview, and is
   // not compatible with the drawing cursor preview which overwrite
@@ -1433,8 +1436,8 @@ void Editor::pasteImage(const Image* image, const gfx::Point& pos)
   Sprite* sprite = this->sprite();
 
   // Check bounds where the image will be pasted.
-  int x = pos.x;
-  int y = pos.y;
+  int x = mask->bounds().x;
+  int y = mask->bounds().y;
   {
     // Then we check if the image will be visible by the user.
     Rect visibleBounds = getVisibleSpriteBounds().shrink(4*ui::guiscale());
@@ -1450,12 +1453,12 @@ void Editor::pasteImage(const Image* image, const gfx::Point& pos)
   // pasted image.
   m_brushPreview.hide();
 
+  Mask mask2(*mask);
+  mask2.setOrigin(x, y);
+
   PixelsMovementPtr pixelsMovement(
     new PixelsMovement(UIContext::instance(),
-      getSite(), image, gfx::Point(x, y), "Paste"));
-
-  // Select the pasted image so the user can move it and transform it.
-  pixelsMovement->maskImage(image);
+                       getSite(), image, &mask2, "Paste"));
 
   setState(EditorStatePtr(new MovingPixelsState(this, NULL, pixelsMovement, NoHandle)));
 }

src/app/ui/editor/editor.h
@@ -178,7 +178,7 @@ namespace app {
     void setZoomAndCenterInMouse(render::Zoom zoom,
       const gfx::Point& mousePos, ZoomBehavior zoomBehavior);
 
-    void pasteImage(const Image* image, const gfx::Point& pos);
+    void pasteImage(const Image* image, const Mask* mask);
 
     void startSelectionTransformation(const gfx::Point& move);
 

commit 58db0fba09471b0ebbde27c08a90b16447393b64

Fix bug clearing an Image with color=0 in rotsprite
Introduced in commit 35229e99a6196d71b90ef2762abb32b0a164f594

src/doc/algorithm/rotsprite.cpp
@@ -209,7 +209,6 @@ void rotsprite_image(Image* bmp, const Image* spr, const Image* mask,
   }
 
   clear_image(bmp_copy, maskColor);
-  clear_image(bmp_copy, 0);
   scale_image(bmp_copy, bmp,
               0, 0, bmp_copy->width(), bmp_copy->height(),
               xmin, ymin, rot_width, rot_height);