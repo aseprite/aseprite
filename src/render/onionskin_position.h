1b217f257c57619f951382774aa28c5fbc11d00f
David Capello <davidcapello@gmail.com>
2015-07-31 16:07:24 -0300

    Add missing src/render/onionskin_position.h file

---
+++ src/render/onionskin_position.h
@@ -0,0 +1,20 @@
+// Aseprite Render Library
+// Copyright (c) 2001-2015 David Capello
+//
+// This file is released under the terms of the MIT license.
+// Read LICENSE.txt for more information.
+
+#ifndef RENDER_ONIONSKIN_POSITION_H_INCLUDED
+#define RENDER_ONIONSKIN_POSITION_H_INCLUDED
+#pragma once
+
+namespace render {
+
+  enum class OnionskinPosition {
+    BEHIND,
+    INFRONT,
+  };
+
+} // namespace render
+
+#endif
