aseprite/aseprite

commit e0020d36f20be650b0c26f4bdbc943585151468b

Fix bug undoing/redoing pixel movements with several mask movements
If we call updateDocumentMask() in this point, we'll leave a
sequence of cmds: SetMask(old mask, new mask) -> ClearMask(new mask)
And we need the opposite: ClearMask(old mask) -> SetMask(old mask, new mask)

This bug was introduced in 35229e99a6196d71b90ef2762abb32b0a164f594

src/app/ui/editor/pixels_movement.cpp
@@ -88,9 +88,7 @@ PixelsMovement::PixelsMovement(
   // and its owner could destroy our new "extra cel".
   ASSERT(!m_document->getExtraCel());
   redrawExtraImage();
-
   redrawCurrentMask();
-  updateDocumentMask();
 }
 
 PixelsMovement::~PixelsMovement()

commit 43606935f9443c89813e14427af0ca24881a3c41

Fix bug regenerating selection boundaries after Paste
This bug was introduced in e0020d36f20be650b0c26f4bdbc943585151468b

src/app/ui/editor/pixels_movement.cpp
@@ -89,6 +89,34 @@ PixelsMovement::PixelsMovement(
   ASSERT(!m_document->getExtraCel());
   redrawExtraImage();
   redrawCurrentMask();
+
+  // If the mask isn't in the document (e.g. it's from Paste command),
+  // we've to replace the document mask and generate its boundaries.
+  //
+  // This is really tricky. PixelsMovement is used in two situations:
+  // 1) When the current selection is transformed, and
+  // 2) when the user pastes the clipboard content.
+  //
+  // In the first case, the current document selection is used. And a
+  // cutMask() command could be called after PixelsMovement ctor. We
+  // need the following stack of Cmd instances in the Transaction:
+  // - cmd::ClearMask: clears the old mask)
+  // - cmd::SetMask (m_setMaskCmd): replaces the old mask with a new mask
+  // The new mask in m_setMaskCmd is replaced each time the mask is modified.
+  //
+  // In the second case, the mask isn't in the document, is a new mask
+  // used to paste the pixels, so we've to replace the document mask.
+  // The Transaction contains just a:
+  // - cmd::SetMask
+  //
+  // The main point here is that cmd::SetMask must be the last item in
+  // the Transaction using the mask (because we use cmd::SetMask::setNewMask()).
+  //
+  // TODO Simplify this code in some way or make explicit both usages
+  if (mask != m_document->mask()) {
+    updateDocumentMask();
+    update_screen_for_document(m_document);
+  }
 }
 
 PixelsMovement::~PixelsMovement()