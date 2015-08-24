aseprite/aseprite

commit d1799c037abc011e79663892f52e0e2e0b5c5334

Fix hanging when switching focus between different editable comboboxes

src/ui/widget.cpp
@@ -1119,6 +1119,31 @@ GraphicsPtr Widget::getGraphics(const gfx::Rect& clip)
 bool Widget::sendMessage(Message* msg)
 {
   ASSERT(msg != NULL);
+
+  // Filter out-of-sync messages
+  switch (msg->type()) {
+
+    case kFocusEnterMessage:
+      if (!hasFocus())
+        return false;
+      break;
+
+    case kFocusLeaveMessage:
+      if (hasFocus())
+        return false;
+      break;
+
+    case kMouseEnterMessage:
+      if (!hasMouse())
+        return false;
+      break;
+
+    case kMouseLeaveMessage:
+      if (hasMouse())
+        return false;
+      break;
+  }
+
   return onProcessMessage(msg);
 }
 

commit 549404c2166e2c78e841836efd6c543b0a8439ca

Fix tooltips visibility (fix #770)
TooltipManager uses filters to intercept a kMouseEnterMessage, if we
remove this kind of message for the TooltipManager itself (in sendMessage),
we never get that message so we never show the tooltip.

This problem was introduced in d1799c037abc011e79663892f52e0e2e0b5c5334
to fix a problem with comboboxes.

src/ui/widget.cpp
@@ -1118,32 +1118,7 @@ GraphicsPtr Widget::getGraphics(const gfx::Rect& clip)
 
 bool Widget::sendMessage(Message* msg)
 {
-  ASSERT(msg != NULL);
-
-  // Filter out-of-sync messages
-  switch (msg->type()) {
-
-    case kFocusEnterMessage:
-      if (!hasFocus())
-        return false;
-      break;
-
-    case kFocusLeaveMessage:
-      if (hasFocus())
-        return false;
-      break;
-
-    case kMouseEnterMessage:
-      if (!hasMouse())
-        return false;
-      break;
-
-    case kMouseLeaveMessage:
-      if (hasMouse())
-        return false;
-      break;
-  }
-
+  ASSERT(msg);
   return onProcessMessage(msg);
 }
 