d55017cd17689f347b70e1e65308ec26015da96d
David Capello <davidcapello@gmail.com>
2015-09-22 17:27:47 -0300

    Use advanced_mode.xml.h in cmd_advanced_mode.cpp

--- src/app/commands/cmd_advanced_mode.cpp
+++ src/app/commands/cmd_advanced_mode.cpp
@@ -18,6 +18,8 @@
 #include "app/ui/main_window.h"
 #include "ui/ui.h"
 
+#include "advanced_mode.xml.h"
+
 #include <cstdio>
 
 namespace app {
@@ -65,16 +67,14 @@ void AdvancedModeCommand::onExecute(Context* context)
       get_config_bool("AdvancedMode", "Warning", true)) {
     Key* key = KeyboardShortcuts::instance()->command(this->id().c_str());
     if (!key->accels().empty()) {
-      base::UniquePtr<Window> window(app::load_widget<Window>("advanced_mode.xml", "advanced_mode_warning"));
-      Widget* warning_label = app::find_widget<Widget>(window, "warning_label");
-      Widget* donot_show = app::find_widget<Widget>(window, "donot_show");
+      app::gen::AdvancedMode window;
 
-      warning_label->setTextf("You can go back pressing \"%s\" key.",
+      window.warningLabel()->setTextf("You can go back pressing \"%s\" key.",
         key->accels().front().toString().c_str());
 
-      window->openWindowInForeground();
+      window.openWindowInForeground();
 
-      set_config_bool("AdvancedMode", "Warning", !donot_show->isSelected());
+      set_config_bool("AdvancedMode", "Warning", !window.donotShow()->isSelected());
     }
   }
 }
