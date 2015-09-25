aseprite/aseprite

commit 5e3ba8237a7216b0eba3f3be269d201786ed09c9

Fix from_allegro() auxiliary function to get correct alpha values

src/she/alleg4/alleg_surface.cpp
@@ -72,7 +72,8 @@ inline gfx::Color from_allegro(int color_depth, int color)
   return gfx::rgba(
     getr_depth(color_depth, color),
     getg_depth(color_depth, color),
-    getb_depth(color_depth, color));
+    getb_depth(color_depth, color),
+    geta_depth(color_depth, color));
 }
 
 Alleg4Surface::Alleg4Surface(BITMAP* bmp, DestroyFlag destroy)

commit 4ebefd3f13d79f5a953a1e9acd75bb6001d461d0

Fix problem getting pixels in non-32bpp Allegro BITMAPs
Regression introduced in 5e3ba8237a7216b0eba3f3be269d201786ed09c9
This changed was needed to load certain .png fonts correctly in the
SkinTheme.

src/allegro/src/color.c
@@ -211,6 +211,8 @@ int geta_depth(int color_depth, int c)
    if (color_depth == 32)
       return geta32(c);
 
+   /* dacap: This should return 255, anyway as the Debian port uses
+      the official Allegro library, we cannot depend on this function. */
    return 0;
 }
 

src/she/alleg4/alleg_surface.cpp
@@ -73,7 +73,8 @@ inline gfx::Color from_allegro(int color_depth, int color)
     getr_depth(color_depth, color),
     getg_depth(color_depth, color),
     getb_depth(color_depth, color),
-    geta_depth(color_depth, color));
+    // This condition is here because geta_depth() returns 0 if color depth != 32
+    (color_depth == 32 ? geta32(color): 255));
 }
 
 Alleg4Surface::Alleg4Surface(BITMAP* bmp, DestroyFlag destroy)