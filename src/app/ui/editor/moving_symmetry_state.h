ad9b7ce645de2da1e06cb54875db6312a8bd8a33
David Capello <davidcapello@gmail.com>
2016-02-11 12:21:37 -0300

    Remove unused variable from MovingSymmetryState

--- src/app/ui/editor/moving_symmetry_state.h
+++ src/app/ui/editor/moving_symmetry_state.h
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2015  David Capello
+// Copyright (C) 2015-2016  David Capello
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License version 2 as
@@ -32,7 +32,6 @@ namespace app {
     app::gen::SymmetryMode m_symmetryMode;
     Option<int>& m_symmetryAxis;
     int m_symmetryAxisStart;
-    int m_symmetryAxisNew;
     gfx::Point m_mouseStart;
   };
 
