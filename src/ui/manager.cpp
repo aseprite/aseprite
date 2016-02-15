aseprite/aseprite

commit b0650f6afea1b860a8593fd4614ca272f8edbfac

Generalize View::setViewScroll() to blit valid/scrollable region automatically
With this change we are removing specific code to scroll the Editor widget.
Now if we use Editor::setEditorScroll() to scroll it should work as
View::setViewScroll(). In this way we remove the ugly "blitValidRegion"
parameter from setEditorScroll().

Changes:
* Removed Widget::scrollRegion() because each widget must control
  valid/invalid regions after a ui::move_region() in a very specific way
  (see View::setViewScroll() or Window::moveWindow)
* Invalidate the whole widget on Widget::setBoundsQuietly()
* Fixed problems blitting invalid regions/not yet updated/painted: using
  the new ui::Manager::m_invalidRegion.
* Added View::onSetViewScroll() and View::onScrollRegion()
* Added FileListView to avoid moving the thumbnail region when we scroll

src/ui/manager.cpp
@@ -1058,6 +1058,9 @@ void Manager::onResize(ResizeEvent& ev)
   gfx::Rect new_pos = ev.bounds();
   setBoundsQuietly(new_pos);
 
+  // The whole manager area is invalid now.
+  m_invalidRegion = gfx::Region(new_pos);
+
   int dx = new_pos.x - old_pos.x;
   int dy = new_pos.y - old_pos.y;
   int dw = new_pos.w - old_pos.w;
@@ -1266,6 +1269,10 @@ void Manager::pumpQueue()
             // Restore clip region for paint messages.
             surface->setClipBounds(oldClip);
           }
+
+          // As this kPaintMessage's rectangle was updated, we can
+          // remove it from "m_invalidRegion".
+          m_invalidRegion -= gfx::Region(paintMsg->rect());
         }
       }
       else {

src/ui/manager.h
@@ -1,5 +1,5 @@
 // Aseprite UI Library
-// Copyright (C) 2001-2013, 2015  David Capello
+// Copyright (C) 2001-2016  David Capello
 //
 // This file is released under the terms of the MIT license.
 // Read LICENSE.txt for more information.
@@ -86,6 +86,19 @@ namespace ui {
 
     bool isFocusMovementKey(Message* msg);
 
+    // Returns the invalid region in the screen to being updated with
+    // PaintMessages. This region is cleared when each widget receives
+    // a paint message.
+    const gfx::Region& getInvalidRegion() const {
+      return m_invalidRegion;
+    }
+
+    void addInvalidRegion(const gfx::Region& b) {
+      m_invalidRegion |= b;
+    }
+
+    // Mark the given rectangle as a area to be flipped to the real
+    // screen
     void dirtyRect(const gfx::Rect& bounds);
 
     void _openWindow(Window* window);
@@ -130,6 +143,7 @@ namespace ui {
     she::Display* m_display;
     she::Clipboard* m_clipboard;
     she::EventQueue* m_eventQueue;
+    gfx::Region m_invalidRegion;  // Invalid region (we didn't receive paint messages yet for this).
 
     // This member is used to make freeWidget() a no-op when we
     // restack a window if the user clicks on it.

src/ui/view.cpp
@@ -1,8 +1,10 @@
 // Aseprite UI Library
-// Copyright (C) 2001-2013, 2015  David Capello
+// Copyright (C) 2001-2016  David Capello
 //
 // This file is released under the terms of the MIT license.
 // Read LICENSE.txt for more information.
+
+// #define DEBUG_SCROLL_EVENTS
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -10,14 +12,25 @@
 
 #include "gfx/size.h"
 #include "ui/intern.h"
+#include "ui/manager.h"
 #include "ui/message.h"
-#include "ui/size_hint_event.h"
+#include "ui/move_region.h"
 #include "ui/resize_event.h"
 #include "ui/scroll_helper.h"
+#include "ui/scroll_region_event.h"
+#include "ui/size_hint_event.h"
 #include "ui/system.h"
 #include "ui/theme.h"
 #include "ui/view.h"
 #include "ui/widget.h"
+
+#ifdef DEBUG_SCROLL_EVENTS
+#include "base/thread.h"
+#include "she/display.h"
+#include "she/scoped_surface_lock.h"
+#endif
+
+#include <queue>
 
 #define HBAR_SIZE (m_scrollbar_h.getBarWidth())
 #define VBAR_SIZE (m_scrollbar_v.getBarWidth())
@@ -33,7 +46,7 @@ View::View()
 {
   m_hasBars = true;
 
-  this->setFocusStop(true);
+  setFocusStop(true);
   addChild(&m_viewport);
   setScrollableSize(Size(0, 0));
 
@@ -128,21 +141,7 @@ Point View::viewScroll() const
 
 void View::setViewScroll(const Point& pt)
 {
-  Point oldScroll = viewScroll();
-  Size maxsize = getScrollableSize();
-  Size visible = visibleSize();
-  Point newScroll(MID(0, pt.x, MAX(0, maxsize.w - visible.w)),
-                  MID(0, pt.y, MAX(0, maxsize.h - visible.h)));
-
-  if (newScroll == oldScroll)
-    return;
-
-  m_scrollbar_h.setPos(newScroll.x);
-  m_scrollbar_v.setPos(newScroll.y);
-
-  m_viewport.layout();
-
-  onScrollChange();
+  onSetViewScroll(pt);
 }
 
 void View::updateView()
@@ -226,6 +225,137 @@ void View::onSizeHint(SizeHintEvent& ev)
 void View::onPaint(PaintEvent& ev)
 {
   theme()->paintView(ev);
+}
+
+void View::onSetViewScroll(const gfx::Point& pt)
+{
+  Point oldScroll = viewScroll();
+  Size maxsize = getScrollableSize();
+  Size visible = visibleSize();
+  Point newScroll(MID(0, pt.x, MAX(0, maxsize.w - visible.w)),
+                  MID(0, pt.y, MAX(0, maxsize.h - visible.h)));
+
+  if (newScroll == oldScroll)
+    return;
+
+  // This is the movement for the scrolled region (which is inverse to
+  // the scroll position delta/movement).
+  Point delta = oldScroll - newScroll;
+
+  // Visible viewport region that is not overlapped by windows
+  Region drawableRegion;
+  m_viewport.getDrawableRegion(
+    drawableRegion, DrawableRegionFlags(kCutTopWindows | kUseChildArea));
+
+  // Start the region to scroll equal to the drawable viewport region.
+  Rect cpos = m_viewport.childrenBounds();
+  Region validRegion(cpos);
+  validRegion &= drawableRegion;
+
+  // Remove all children invalid regions from this "validRegion"
+  {
+    std::queue<Widget*> items;
+    items.push(&m_viewport);
+    while (!items.empty()) {
+      Widget* item = items.front();
+      items.pop();
+      for (Widget* child : item->children())
+        items.push(child);
+
+      if (item->isVisible())
+        validRegion -= item->getUpdateRegion();
+    }
+  }
+
+  // Remove invalid region in the screen (areas that weren't
+  // re-painted yet)
+  Manager* manager = this->manager();
+  if (manager)
+    validRegion -= manager->getInvalidRegion();
+
+  // Add extra regions that cannot be scrolled (this can be customized
+  // by subclassing ui::View). We use two ScrollRegionEvent, this
+  // first one with the old scroll position. And the next one with the
+  // new scroll position.
+  {
+    ScrollRegionEvent ev(this, validRegion);
+    onScrollRegion(ev);
+  }
+
+  // Move viewport children
+  cpos.offset(-newScroll);
+  for (auto child : m_viewport.children()) {
+    Size reqSize = child->sizeHint();
+    cpos.w = MAX(reqSize.w, cpos.w);
+    cpos.h = MAX(reqSize.h, cpos.h);
+    if (cpos.w != child->bounds().w ||
+        cpos.h != child->bounds().h)
+      child->setBounds(cpos);
+    else
+      child->offsetWidgets(cpos.x - child->bounds().x,
+                           cpos.y - child->bounds().y);
+  }
+
+  // Change scroll bar positions
+  m_scrollbar_h.setPos(newScroll.x);
+  m_scrollbar_v.setPos(newScroll.y);
+
+  // Region to invalidate (new visible children/child parts)
+  Region invalidRegion(cpos);
+  invalidRegion &= drawableRegion;
+
+  // Move the valid screen region.
+  {
+    // The movable region includes the given "validRegion"
+    // intersecting itself when it's in the new position, so we don't
+    // overlap regions outside the "validRegion".
+    Region movable = validRegion;
+    movable.offset(delta);
+    movable &= validRegion;
+    invalidRegion -= movable;   // Remove the moved region as invalid
+    movable.offset(-delta);
+
+    ui::move_region(manager, movable, delta.x, delta.y);
+  }
+
+#ifdef DEBUG_SCROLL_EVENTS
+  // Paint invalid region with red fill
+  {
+    auto display = manager->getDisplay();
+    if (display)
+      display->flip(gfx::Rect(0, 0, display_w(), display_h()));
+    base::this_thread::sleep_for(0.002);
+    {
+      she::ScopedSurfaceLock lock(display->getSurface());
+      for (const auto& rc : invalidRegion)
+        lock->fillRect(gfx::rgba(255, 0, 0), rc);
+    }
+    if (display)
+      display->flip(gfx::Rect(0, 0, display_w(), display_h()));
+    base::this_thread::sleep_for(0.002);
+  }
+#endif
+
+  // Don't re-invalidate the already invalid region.
+  if (manager)
+    invalidRegion -= manager->getInvalidRegion();
+
+  // Invalidate viewport's children regions
+  m_viewport.invalidateRegion(invalidRegion);
+
+  // Notify about the new scroll position
+  onScrollChange();
+
+  // Generate PaintMessages right now when the invalid region is too
+  // disaggregated. This is useful to avoid a lot of PaintMessage with
+  // small rectangles.
+  if (manager->getInvalidRegion().size() > 4)
+    flushRedraw();
+}
+
+void View::onScrollRegion(ScrollRegionEvent& ev)
+{
+  // Do nothing
 }
 
 void View::onScrollChange()

src/ui/widget.cpp
@@ -1,5 +1,5 @@
 // Aseprite UI Library
-// Copyright (C) 2001-2015  David Capello
+// Copyright (C) 2001-2016  David Capello
 //
 // This file is released under the terms of the MIT license.
 // Read LICENSE.txt for more information.
@@ -617,8 +617,8 @@ void Widget::setBounds(const Rect& rc)
 
 void Widget::setBoundsQuietly(const gfx::Rect& rc)
 {
-  m_updateRegion.offset(rc.x - m_bounds.x, rc.y - m_bounds.y);
   m_bounds = rc;
+  invalidate();
 }
 
 void Widget::setBorder(const Border& br)
@@ -874,6 +874,9 @@ void Widget::flushRedraw()
     processing.push(this);
   }
 
+  Manager* manager = this->manager();
+  ASSERT(manager);
+
   while (!processing.empty()) {
     Widget* widget = processing.front();
     processing.pop();
@@ -910,9 +913,10 @@ void Widget::flushRedraw()
         msg->addRecipient(widget);
 
         // Enqueue the draw message
-        manager()->enqueueMessage(msg);
+        manager->enqueueMessage(msg);
       }
 
+      manager->addInvalidRegion(widget->m_updateRegion);
       widget->m_updateRegion.clear();
     }
   }
@@ -1034,35 +1038,6 @@ void Widget::invalidateRect(const gfx::Rect& rect)
 void Widget::invalidateRegion(const Region& region)
 {
   onInvalidateRegion(region);
-}
-
-void Widget::scrollRegion(const Region& region, const Point& delta)
-{
-  if (delta.x == 0 && delta.y == 0)
-    return;
-
-  // The movable region includes the given region in the "region"
-  // parameter without the invalid widget region (i.e. m_updateRegion,
-  // as we cannot move invalid/non-updated screen areas), and
-  // intersecting with the moved "region" area (so we don't overlap
-  // regions outside the "region" parameters)
-  Region movable = region;
-  movable.createSubtraction(movable, m_updateRegion);
-  movable.offset(delta);
-  movable.createIntersection(movable, region);
-
-  // Now we invalidate the given "region" without the moved region
-  // ("movable" variable).
-  m_updateRegion.createUnion(m_updateRegion, region);
-  m_updateRegion.createSubtraction(m_updateRegion, movable);
-  mark_dirty_flag(this);
-
-  // Move screen pixels
-  movable.offset(-delta);
-  ui::move_region(manager(), movable, delta.x, delta.y);
-
-  // Generate the kPaintMessage messages for the widget's m_updateRegion
-  flushRedraw();
 }
 
 class DeleteGraphicsAndSurface {

data/widgets/file_selector.xml
@@ -1,5 +1,5 @@
 <!-- ASEPRITE -->
-<!-- Copyright (C) 2001-2013, 2015 by David Capello -->
+<!-- Copyright (C) 2001-2016 by David Capello -->
 <gui>
   <window id="file_selector" text="">
   <vbox id="main">
@@ -12,7 +12,7 @@
       <button text="" id="new_folder_button" tooltip="New folder" />
       <combobox id="location" expansive="true" />
     </box>
-    <view id="file_view" expansive="true" />
+    <vbox id="file_view_placeholder" expansive="true" />
     <grid columns="2">
       <label text="File name:" />
       <box id="file_name_placeholder" cell_align="horizontal" />

src/app/CMakeLists.txt
@@ -1,5 +1,5 @@
 # Aseprite
-# Copyright (C) 2001-2015  David Capello
+# Copyright (C) 2001-2016  David Capello
 
 # Generate a ui::Widget for each widget in a XML file
 file(GLOB widget_files ${CMAKE_SOURCE_DIR}/data/widgets/*.xml)
@@ -366,6 +366,7 @@ add_library(app-lib
   ui/editor/transform_handles.cpp
   ui/editor/zooming_state.cpp
   ui/file_list.cpp
+  ui/file_list_view.cpp
   ui/file_selector.cpp
   ui/font_popup.cpp
   ui/frame_tag_window.cpp

src/app/commands/cmd_scroll.cpp
@@ -129,7 +129,7 @@ void ScrollCommand::onExecute(Context* context)
     case Down:  delta.y = +m_quantity * pixels; break;
   }
 
-  current_editor->setEditorScroll(scroll+delta, true);
+  current_editor->setEditorScroll(scroll+delta);
 }
 
 std::string ScrollCommand::onGetFriendlyName() const

src/app/ui/editor/drawing_state.cpp
@@ -169,7 +169,7 @@ bool DrawingState::onMouseMove(Editor* editor, MouseMessage* msg)
   HideBrushPreview hide(editor->brushPreview());
 
   // Infinite scroll
-  gfx::Point mousePos = editor->autoScroll(msg, AutoScroll::MouseDir, true);
+  gfx::Point mousePos = editor->autoScroll(msg, AutoScroll::MouseDir);
   tools::ToolLoopManager::Pointer pointer(editor->screenToEditor(mousePos),
                                           button_from_msg(msg));
 

commit 1588e834c3838ffda630abc21bb70c2d4fd75d4d

Fix artifacts when scrolling on widgets with sub-children (fix #963)
This bug was introduced in b0650f6afea1b860a8593fd4614ca272f8edbfac
To fix this issue we've to remove kPaintMessages from the message queue
for widgets that were moved. Those pending paint messages aren't valid
anymore because the widget was invalidated again.

src/ui/manager.cpp
@@ -835,21 +835,27 @@ void Manager::freeWidget(Widget* widget)
 
 void Manager::removeMessage(Message* msg)
 {
-  Messages::iterator it = std::find(msg_queue.begin(), msg_queue.end(), msg);
+  auto it = std::find(msg_queue.begin(), msg_queue.end(), msg);
   ASSERT(it != msg_queue.end());
   msg_queue.erase(it);
 }
 
 void Manager::removeMessagesFor(Widget* widget)
 {
-  for (Messages::iterator it=msg_queue.begin(), end=msg_queue.end();
-       it != end; ++it)
-    removeWidgetFromRecipients(widget, *it);
+  for (Message* msg : msg_queue)
+    removeWidgetFromRecipients(widget, msg);
+}
+
+void Manager::removeMessagesFor(Widget* widget, MessageType type)
+{
+  for (Message* msg : msg_queue)
+    if (msg->type() == type)
+      removeWidgetFromRecipients(widget, msg);
 }
 
 void Manager::removeMessagesForTimer(Timer* timer)
 {
-  for (Messages::iterator it=msg_queue.begin(); it != msg_queue.end(); ) {
+  for (auto it=msg_queue.begin(); it != msg_queue.end(); ) {
     Message* msg = *it;
 
     if (!msg->isUsed() &&
@@ -1269,11 +1275,11 @@ void Manager::pumpQueue()
             // Restore clip region for paint messages.
             surface->setClipBounds(oldClip);
           }
-
-          // As this kPaintMessage's rectangle was updated, we can
-          // remove it from "m_invalidRegion".
-          m_invalidRegion -= gfx::Region(paintMsg->rect());
         }
+
+        // As this kPaintMessage's rectangle was updated, we can
+        // remove it from "m_invalidRegion".
+        m_invalidRegion -= gfx::Region(paintMsg->rect());
       }
       else {
         // Call the message handler

src/ui/manager.h
@@ -74,6 +74,7 @@ namespace ui {
     void freeWidget(Widget* widget);
     void removeMessage(Message* msg);
     void removeMessagesFor(Widget* widget);
+    void removeMessagesFor(Widget* widget, MessageType type);
     void removeMessagesForTimer(Timer* timer);
 
     void addMessageFilter(int message, Widget* widget);

src/ui/view.cpp
@@ -336,21 +336,14 @@ void View::onSetViewScroll(const gfx::Point& pt)
   }
 #endif
 
-  // Don't re-invalidate the already invalid region.
-  if (manager)
-    invalidRegion -= manager->getInvalidRegion();
-
   // Invalidate viewport's children regions
   m_viewport.invalidateRegion(invalidRegion);
 
   // Notify about the new scroll position
   onScrollChange();
 
-  // Generate PaintMessages right now when the invalid region is too
-  // disaggregated. This is useful to avoid a lot of PaintMessage with
-  // small rectangles.
-  if (manager->getInvalidRegion().size() > 4)
-    flushRedraw();
+  // Generate PaintMessages right now.
+  flushRedraw();
 }
 
 void View::onScrollRegion(ScrollRegionEvent& ev)

src/ui/widget.cpp
@@ -618,6 +618,11 @@ void Widget::setBounds(const Rect& rc)
 void Widget::setBoundsQuietly(const gfx::Rect& rc)
 {
   m_bounds = rc;
+
+  // Remove all paint messages for this widget.
+  if (Manager* manager = this->manager())
+    manager->removeMessagesFor(this, kPaintMessage);
+
   invalidate();
 }
 
@@ -1456,6 +1461,10 @@ void Widget::offsetWidgets(int dx, int dy)
 {
   m_updateRegion.offset(dx, dy);
   m_bounds.offset(dx, dy);
+
+  // Remove all paint messages for this widget.
+  if (Manager* manager = this->manager())
+    manager->removeMessagesFor(this, kPaintMessage);
 
   for (auto child : m_children)
     child->offsetWidgets(dx, dy);