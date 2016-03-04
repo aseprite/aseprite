0a5e108ce1993b86646382c01d67b1d85fd55faf
David Capello <davidcapello@gmail.com>
2016-03-03 22:30:13 -0300

    Include base/ints.h in median_filter.h because it's needed for uint8_t

--- src/filters/median_filter.h
+++ src/filters/median_filter.h
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2001-2015  David Capello
+// Copyright (C) 2001-2016  David Capello
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License version 2 as
@@ -9,6 +9,7 @@
 #define FILTERS_MEDIAN_FILTER_PROCESS_H_INCLUDED
 #pragma once
 
+#include "base/ints.h"
 #include "filters/filter.h"
 #include "filters/tiled_mode.h"
 
