039d926b17b79a75e3db555b1b3c11536c1aceb9
Gabriel Rauter <rauter.gabriel@gmail.com>
2016-04-05 23:09:02 +0200

    switch to dual licence GPL2/MIT

--- desktop/kde/aseprite_thumb_creator.h
+++ desktop/kde/aseprite_thumb_creator.h
@@ -1,9 +1,12 @@
 // Aseprite Desktop Integration Module
 // Copyright (C) 2016  Gabriel Rauter
 //
+// Dual licensed under the MIT and GPL 2 licenses.
+//
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License version 2 as
-// published by the Free Software Foundation.
+// published by the Free Software Foundation or the MIT License
+// (https://opensource.org/licenses/MIT).
 
 #ifndef _ASEPRITE_THUMBCREATOR_H_
 #define _ASEPRITE_THUMBCREATOR_H_
