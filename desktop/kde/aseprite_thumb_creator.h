f9d13e94ed1a8b8d60fafb7564c33a7f65ba9684
Gabriel Rauter <rauter.gabriel@gmail.com>
2016-04-05 14:49:19 +0200

    add missing licence info

--- desktop/kde/aseprite_thumb_creator.h
+++ desktop/kde/aseprite_thumb_creator.h
@@ -1,3 +1,10 @@
+// Aseprite Desktop Integration Module
+// Copyright (C) 2016  Gabriel Rauter
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+
 #ifndef _ASEPRITE_THUMBCREATOR_H_
 #define _ASEPRITE_THUMBCREATOR_H_
 #pragma once
