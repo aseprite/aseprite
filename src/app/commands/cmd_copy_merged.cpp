0f3252a80befc77aed785929ae09ece749c88266
David Capello <davidcapello@gmail.com>
2016-05-17 12:59:48 -0300

    Add CopyMerged command (Ctrl+Shift+C) (fix #824)

---
+++ src/app/commands/cmd_copy_merged.cpp
@@ -0,0 +1,51 @@
+// Aseprite
+// Copyright (C) 2016  David Capello
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "app/commands/command.h"
+#include "app/context_access.h"
+#include "app/util/clipboard.h"
+
+namespace app {
+
+class CopyMergedCommand : public Command {
+public:
+  CopyMergedCommand();
+  Command* clone() const override { return new CopyMergedCommand(*this); }
+
+protected:
+  bool onEnabled(Context* ctx) override;
+  void onExecute(Context* ctx) override;
+};
+
+CopyMergedCommand::CopyMergedCommand()
+  : Command("CopyMerged",
+            "Copy Merged",
+            CmdUIOnlyFlag)
+{
+}
+
+bool CopyMergedCommand::onEnabled(Context* ctx)
+{
+  return ctx->checkFlags(ContextFlags::ActiveDocumentIsReadable);
+}
+
+void CopyMergedCommand::onExecute(Context* ctx)
+{
+  ContextReader reader(ctx);
+  clipboard::copy_merged(reader);
+}
+
+Command* CommandFactory::createCopyMergedCommand()
+{
+  return new CopyMergedCommand;
+}
+
+} // namespace app
--- src/app/util/new_image_from_mask.cpp
+++ src/app/util/new_image_from_mask.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2001-2015  David Capello
+// Copyright (C) 2001-2016  David Capello
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License version 2 as
@@ -15,7 +15,9 @@
 #include "base/unique_ptr.h"
 #include "doc/image_impl.h"
 #include "doc/mask.h"
+#include "doc/primitives.h"
 #include "doc/site.h"
+#include "render/render.h"
 
 namespace app {
 
@@ -27,44 +29,68 @@ Image* new_image_from_mask(const Site& site)
   return new_image_from_mask(site, srcMask);
 }
 
-doc::Image* new_image_from_mask(const doc::Site& site, const doc::Mask* srcMask)
+doc::Image* new_image_from_mask(const doc::Site& site,
+                                const doc::Mask* srcMask,
+                                bool merged)
 {
   const Sprite* srcSprite = site.sprite();
-  const Image* srcMaskBitmap = srcMask->bitmap();
-  const gfx::Rect& srcBounds = srcMask->bounds();
+  const Image* srcMaskBitmap = (srcMask ? srcMask->bitmap(): nullptr);
+  gfx::Rect srcBounds = (srcMask ? srcMask->bounds(): srcSprite->bounds());
   int x, y, u, v, getx, gety;
-  const Image *src = site.image(&x, &y);
 
   ASSERT(srcSprite);
-  ASSERT(srcMask);
-  ASSERT(srcMaskBitmap);
 
   base::UniquePtr<Image> dst(Image::create(srcSprite->pixelFormat(), srcBounds.w, srcBounds.h));
   if (!dst)
     return nullptr;
 
   // Clear the new image
-  dst->setMaskColor(src ? src->maskColor(): srcSprite->transparentColor());
+  dst->setMaskColor(srcSprite->transparentColor());
   clear_image(dst, dst->maskColor());
+
+  const Image* src = nullptr;
+  if (merged) {
+    render::Render render;
+    render.renderSprite(dst, srcSprite, site.frame(),
+                        gfx::Clip(0, 0, srcBounds));
+
+    src = dst;
+  }
+  else {
+    src = site.image(&x, &y);
+  }
 
   // Copy the masked zones
   if (src) {
-    const LockImageBits<BitmapTraits> maskBits(srcMaskBitmap, gfx::Rect(0, 0, srcBounds.w, srcBounds.h));
-    LockImageBits<BitmapTraits>::const_iterator mask_it = maskBits.begin();
+    if (srcMaskBitmap) {
+      // Copy active layer with mask
+      const LockImageBits<BitmapTraits> maskBits(srcMaskBitmap, gfx::Rect(0, 0, srcBounds.w, srcBounds.h));
+      LockImageBits<BitmapTraits>::const_iterator mask_it = maskBits.begin();
 
-    for (v=0; v<srcBounds.h; ++v) {
-      for (u=0; u<srcBounds.w; ++u, ++mask_it) {
-        ASSERT(mask_it != maskBits.end());
+      for (v=0; v<srcBounds.h; ++v) {
+        for (u=0; u<srcBounds.w; ++u, ++mask_it) {
+          ASSERT(mask_it != maskBits.end());
 
-        if (*mask_it) {
-          getx = u+srcBounds.x-x;
-          gety = v+srcBounds.y-y;
+          if (src != dst) {
+            if (*mask_it) {
+              getx = u+srcBounds.x-x;
+              gety = v+srcBounds.y-y;
 
-          if ((getx >= 0) && (getx < src->width()) &&
-              (gety >= 0) && (gety < src->height()))
-            dst->putPixel(u, v, src->getPixel(getx, gety));
+              if ((getx >= 0) && (getx < src->width()) &&
+                  (gety >= 0) && (gety < src->height()))
+                dst->putPixel(u, v, src->getPixel(getx, gety));
+            }
+          }
+          else {
+            if (!*mask_it) {
+              dst->putPixel(u, v, dst->maskColor());
+            }
+          }
         }
       }
+    }
+    else if (src != dst) {
+      copy_image(dst, src, -srcBounds.x, -srcBounds.y);
     }
   }
 
