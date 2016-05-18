4929efbd9c4b32b5dede0654f142c8d4fd1082cb
David Capello <davidcapello@gmail.com>
2016-05-18 11:55:39 -0300

    Remove "Moving Pixels" label in status bar

--- src/app/ui/editor/moving_pixels_state.cpp
+++ src/app/ui/editor/moving_pixels_state.cpp
@@ -435,7 +435,7 @@ bool MovingPixelsState::onUpdateStatusBar(Editor* editor)
   gfx::Size imageSize = m_pixelsMovement->getInitialImageSize();
 
   StatusBar::instance()->setStatusText
-    (100, "Moving Pixels: :pos: %d %d :size: %3d %3d :selsize: %d %d [%.02f%% %.02f%%] :angle: %.1f",
+    (100, ":pos: %d %d :size: %3d %3d :selsize: %d %d [%.02f%% %.02f%%] :angle: %.1f",
      transform.bounds().x, transform.bounds().y,
      imageSize.w, imageSize.h,
      transform.bounds().w, transform.bounds().h,
