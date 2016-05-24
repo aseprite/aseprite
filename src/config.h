0b8e3968df8bc768dc4d3285a72ce733d63d2509
David Capello <davidcapello@gmail.com>
2016-05-24 12:58:06 -0300

    Update version to 1.2-dev

--- src/config.h
+++ src/config.h
@@ -24,7 +24,7 @@
 
 // General information
 #define PACKAGE "Aseprite"
-#define VERSION "1.1.6-dev"
+#define VERSION "1.2-dev"
 
 #ifdef CUSTOM_WEBSITE_URL
 #define WEBSITE                 CUSTOM_WEBSITE_URL // To test web server
