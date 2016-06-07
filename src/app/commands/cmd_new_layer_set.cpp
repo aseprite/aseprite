21afd4ecb50047007d8f24e31c743652bce5b4bc
David Capello <davidcapello@gmail.com>
2016-06-07 19:06:36 -0300

    Add new menu option to create a layer group

--- src/app/commands/cmd_new_layer_set.cpp
+++ src/app/commands/cmd_new_layer_set.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2001-2015  David Capello
+// Copyright (C) 2001-2016  David Capello
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License version 2 as
@@ -67,7 +67,7 @@ void NewLayerSetCommand::onExecute(Context* context)
   std::string name = window->findChild("name")->text();
   Layer* layer;
   {
-    Transaction transaction(writer.context(), "New Layer");
+    Transaction transaction(writer.context(), "New Group");
     layer = document->getApi(transaction).newLayerFolder(sprite);
     transaction.commit();
   }
