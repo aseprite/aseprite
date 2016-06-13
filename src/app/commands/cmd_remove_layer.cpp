9307c12174d69ca8f5095a87e8b0b8410f00f619
David Capello <davidcapello@gmail.com>
2016-06-13 14:53:02 -0300

    Fix RemoveLayer when we remove groups/sublayers/etc

--- src/app/commands/cmd_remove_layer.cpp
+++ src/app/commands/cmd_remove_layer.cpp
@@ -15,7 +15,6 @@
 #include "app/document_api.h"
 #include "app/modules/gui.h"
 #include "app/ui/status_bar.h"
-#include "app/ui/timeline.h"
 #include "app/transaction.h"
 #include "doc/layer.h"
 #include "doc/sprite.h"
@@ -52,25 +51,33 @@ bool RemoveLayerCommand::onEnabled(Context* context)
 
 void RemoveLayerCommand::onExecute(Context* context)
 {
-  std::string layer_name;
+  std::string layerName;
   ContextWriter writer(context);
   Document* document(writer.document());
   Sprite* sprite(writer.sprite());
-  Layer* layer(writer.layer());
   {
     Transaction transaction(writer.context(), "Remove Layer");
     DocumentApi api = document->getApi(transaction);
 
-    // TODO the range of selected layer should be in doc::Site.
-    auto range = App::instance()->timeline()->range();
-    if (range.enabled()) {
-      if (range.layers() == sprite->countLayers()) {
+    const Site* site = writer.site();
+    if (site->focus() == Site::OnTimeline &&
+        !site->selectedLayers().empty()) {
+      SelectedLayers selLayers = site->selectedLayers();
+      remove_children_if_parent_is_selected(selLayers);
+
+      int deletedTopLevelLayers = 0;
+      for (Layer* layer : selLayers) {
+        if (layer->parent() == sprite->root())
+          ++deletedTopLevelLayers;
+      }
+
+      if (deletedTopLevelLayers == sprite->countLayers()) {
         ui::Alert::show("Error<<You cannot delete all layers.||&OK");
         return;
       }
 
-      for (LayerIndex layer = range.layerEnd(); layer >= range.layerBegin(); --layer) {
-        api.removeLayer(sprite->indexToLayer(layer));
+      for (Layer* layer : selLayers) {
+        api.removeLayer(layer);
       }
     }
     else {
@@ -79,7 +86,8 @@ void RemoveLayerCommand::onExecute(Context* context)
         return;
       }
 
-      layer_name = layer->name();
+      Layer* layer = writer.layer();
+      layerName = layer->name();
       api.removeLayer(layer);
     }
 
@@ -88,8 +96,8 @@ void RemoveLayerCommand::onExecute(Context* context)
   update_screen_for_document(document);
 
   StatusBar::instance()->invalidate();
-  if (!layer_name.empty())
-    StatusBar::instance()->showTip(1000, "Layer `%s' removed", layer_name.c_str());
+  if (!layerName.empty())
+    StatusBar::instance()->showTip(1000, "Layer '%s' removed", layerName.c_str());
   else
     StatusBar::instance()->showTip(1000, "Layers removed");
 }
