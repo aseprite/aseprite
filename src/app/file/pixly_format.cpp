888a528321cd853e145fa3e3d19482ec0839fd64
Carlo 'zED' Caputo <carlo.caputo@gmail.com>
2016-07-08 08:22:09 -0300

    pixly: load_document instead of png load

--- src/app/file/pixly_format.cpp
+++ src/app/file/pixly_format.cpp
@@ -90,130 +90,11 @@ template<typename Number> static Number check_number(const char* c_str) {
 
 bool PixlyFormat::onLoad(FileOp* fop)
 {
-  png_uint_32 width, height, y;
-  unsigned int sig_read = 0;
-  png_structp png_ptr;
-  png_infop info_ptr;
-  int bit_depth, color_type, interlace_type;
-  int pass, number_passes;
-  png_bytepp rows_pointer;
-  PixelFormat pixelFormat;
-
-  FileHandle handle(open_file_with_exception(base::replace_extension(fop->filename(),"png"), "rb"));
-  FILE* fp = handle.get();
-
-  /* Create and initialize the png_struct with the desired error handler
-   * functions.  If you want to use the default stderr and longjump method,
-   * you can supply NULL for the last three parameters.  We also supply the
-   * the compiler header file version, so that we know if the application
-   * was compiled with a compatible version of the library
-   */
-  png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, (png_voidp)fop,
-                                   report_png_error, report_png_error);
-  if (png_ptr == NULL) {
-    fop->setError("png_create_read_struct\n");
-    return false;
-  }
-
-  /* Allocate/initialize the memory for image information. */
-  info_ptr = png_create_info_struct(png_ptr);
-  if (info_ptr == NULL) {
-    fop->setError("png_create_info_struct\n");
-    png_destroy_read_struct(&png_ptr, NULL, NULL);
-    return false;
-  }
-
-  /* Set error handling if you are using the setjmp/longjmp method (this is
-   * the normal method of doing things with libpng).
-   */
-  if (setjmp(png_jmpbuf(png_ptr))) {
-    fop->setError("Error reading PNG file\n");
-    /* Free all of the memory associated with the png_ptr and info_ptr */
-    png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-    /* If we get here, we had a problem reading the file */
-    return false;
-  }
-
-  /* Set up the input control if you are using standard C streams */
-  png_init_io(png_ptr, fp);
-
-  /* If we have already read some of the signature */
-  png_set_sig_bytes(png_ptr, sig_read);
-
-  /* The call to png_read_info() gives us all of the information from the
-   * PNG file before the first IDAT (image data chunk).
-   */
-  png_read_info(png_ptr, info_ptr);
-
-  png_get_IHDR(png_ptr, info_ptr, &width, &height, &bit_depth, &color_type,
-               &interlace_type, NULL, NULL);
-
-
-  /* Set up the data transformations you want.  Note that these are all
-   * optional.  Only call them if you want/need them.  Many of the
-   * transformations only work on specific types of images, and many
-   * are mutually exclusive.
-   */
-
-  /* tell libpng to strip 16 bit/color files down to 8 bits/color */
-  png_set_strip_16(png_ptr);
-
-  /* Extract multiple pixels with bit depths of 1, 2, and 4 from a single
-   * byte into separate bytes (useful for paletted and grayscale images).
-   */
-  png_set_packing(png_ptr);
-
-  /* Turn on interlace handling.  REQUIRED if you are not using
-   * png_read_image().  To see how to handle interlacing passes,
-   * see the png_read_row() method below:
-   */
-  number_passes = png_set_interlace_handling(png_ptr);
-
-  /* Optional call to gamma correct and add the background to the palette
-   * and update info structure.
-   */
-  png_read_update_info(png_ptr, info_ptr);
-
-  /* create the output image */
-  switch (png_get_color_type(png_ptr, info_ptr)) {
-
-    case PNG_COLOR_TYPE_RGB_ALPHA:
-      fop->sequenceSetHasAlpha(true);
-      pixelFormat = IMAGE_RGB;
-      break;
-
-    default:
-      fop->setError("Pixly loader requires a RGBA PNG\n)");
-      png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-      return false;
-  }
-
-  int imageWidth = png_get_image_width(png_ptr, info_ptr);
-  int imageHeight = png_get_image_height(png_ptr, info_ptr);
-
-  // Allocate the memory to hold the image using the fields of info_ptr.
-  rows_pointer = (png_bytepp)png_malloc(png_ptr, sizeof(png_bytep) * height);
-  for (y = 0; y < height; y++)
-    rows_pointer[y] = (png_bytep)png_malloc(png_ptr, png_get_rowbytes(png_ptr, info_ptr));
-
-  for (pass = 0; pass < number_passes; pass++) {
-    for (y = 0; y < height; y++) {
-      png_read_rows(png_ptr, rows_pointer+y, nullptr, 1);
-
-      fop->setProgress(
-        0.5 * ((double)((double)pass + (double)(y+1) / (double)(height))
-        / (double)number_passes));
-
-      if (fop->isStop())
-        break;
-    }
-  }
-
-  bool success = true;
   try {
+    // load XML metadata
     XmlDocumentRef doc = open_xml(fop->filename());
     TiXmlHandle xml(doc.get());
-    fop->setProgress(0.75);
+    fop->setProgress(0.25);
 
     TiXmlElement* xmlAnim = check(xml.FirstChild("PixlyAnimation").ToElement());
     double version = check_number<double>(xmlAnim->Attribute("version"));
@@ -244,6 +125,20 @@ bool PixlyFormat::onLoad(FileOp* fop)
       sprite->folder()->addLayer(new LayerImage(sprite));
     }
 
+    // load image sheet
+    Document* png = load_document(nullptr, base::replace_extension(fop->filename(),"png").c_str());
+    fop->setProgress(0.5);
+
+    Image* sheet = check(check(check(check(check(png)->sprite())->layer(0))->cel(0))->image()); // do I need to check it all?
+
+    if(sheet->pixelFormat() != IMAGE_RGB) {
+      throw("Pixly loader requires a RGBA PNG");
+    }
+
+    int sheetWidth = sheet->width();
+    int sheetHeight = sheet->height();
+
+    // slice cels from sheet
     std::vector<int> visible(layerCount, 0);
 
     TiXmlElement* xmlFrame = check(xmlFrames->FirstChild("Frame"))->ToElement();
@@ -264,34 +159,28 @@ bool PixlyFormat::onLoad(FileOp* fop)
       visible[(int)layer_index] += (int)(std::string(check(xmlFrame->Attribute("visible"),"false")) == "true");
 
       int x0 = check_number<int>(xmlRegion->Attribute("x"));
-      int y0 = check_number<int>(xmlRegion->Attribute("y")); // inverted
+      int y0_up = check_number<int>(xmlRegion->Attribute("y")); // inverted
 
-      if(y0 < 0 || y0 + frameHeight > imageHeight || x0 < 0 || x0 + frameWidth > imageWidth) {
+      if(y0_up < 0 || y0_up + frameHeight > sheetHeight || x0 < 0 || x0 + frameWidth > sheetWidth) {
         throw Exception("looking for cels outside the bounds of the PNG");
       }
 
-      // TODO use Document* tmpDoc = load_document(nullptr, pngfilename) and save_document(nullptr, tmpDoc)
       // TODO link identical neighbors
       // TODO if all transparent make empty
       // TODO trim cel image
 
       base::UniquePtr<Cel> cel;
-      ImageRef image(Image::create(pixelFormat, frameWidth, frameHeight));
+      ImageRef image(Image::create(IMAGE_RGB, frameWidth, frameHeight));
 
       // Convert rows_pointer into the doc::Image
       for (int y = 0; y < frameHeight; y++) {
         // RGB_ALPHA
-        uint8_t* src_address = rows_pointer[imageHeight-1 - y0 - (frameHeight-1) + y] + (x0 * 4);
-        uint32_t* dst_address = (uint32_t*)image->getPixelAddress(0, y);
-        unsigned int r, g, b, a;
+        int y0_down = sheetHeight-1 - y0_up - (frameHeight-1) + y;
+        uint32_t* src_begin = (uint32_t*)sheet->getPixelAddress(x0           , y0_down);
+        uint32_t* src_end   = (uint32_t*)sheet->getPixelAddress(x0+frameWidth, y0_down);
+        uint32_t* dst_begin = (uint32_t*)image->getPixelAddress(0, y);
 
-        for (int x=0; x<frameWidth; x++) {
-          r = *(src_address++);
-          g = *(src_address++);
-          b = *(src_address++);
-          a = *(src_address++);
-          *(dst_address++) = rgba(r, g, b, a);
-        }
+        std::copy(src_begin, src_end, dst_begin);
       }
 
       cel.reset(new Cel(frame, image));
@@ -299,7 +188,7 @@ bool PixlyFormat::onLoad(FileOp* fop)
       cel.release();
 
       xmlFrame = xmlFrame->NextSiblingElement();
-      fop->setProgress(0.75 + 0.25 * ((float)(index+1) / (float)imageCount));
+      fop->setProgress(0.5 + 0.5 * ((float)(index+1) / (float)imageCount));
     }
 
     for(int i=0; i<layerCount; i++) {
@@ -313,17 +202,10 @@ bool PixlyFormat::onLoad(FileOp* fop)
   }
   catch(Exception &e) {
     fop->setError((std::string("Pixly file format: ")+std::string(e.what())+"\n").c_str());
-    success = false;
+    return false;
   }
 
-  for (y = 0; y < height; y++) {
-    png_free(png_ptr, rows_pointer[y]);
-  }
-  png_free(png_ptr, rows_pointer);
-
-  // Clean up after the read, and free any memory allocated
-  png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-  return success;
+  return true;
 }
 
 #ifdef ENABLE_SAVE
@@ -346,6 +228,8 @@ bool PixlyFormat::onSave(FileOp* fop)
   png_infop info_ptr;
   png_bytepp rows_pointer;
   int color_type = 0;
+
+  // TODO use save_document(nullptr, tmpDoc)
 
   /* open the file */
   FileHandle xml_handle(open_file_with_exception(fop->filename(), "wb"));
