1d21bc935f9b60defc1e83d01323623c14a9619c
Carlo 'zED' Caputo <carlo.caputo@gmail.com>
2016-07-14 15:42:56 -0300

    copyright fix

--- src/app/thumbnails.h
+++ src/app/thumbnails.h
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2016  Carlo "zED" Caputo
+// Copyright (C) 2016  Carlo Caputo
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License version 2 as
