aseprite/aseprite

commit 43a225afe0217c698896a92e20f1cb627e718bcf

Avoid focus on a hidden widget in RGB/HSB text fields

src/app/ui/color_popup.cpp
@@ -260,6 +260,9 @@ void ColorPopup::selectColorType(app::Color::Type type)
     case app::Color::MaskType:  m_colorType.setSelectedItem(MASK_MODE); break;
   }
 
+  // Remove focus from some RGB/HSB text entry
+  manager()->freeFocus();
+
   m_vbox.layout();
   m_vbox.invalidate();
 }

commit 52030e83f3e440fd4c057e9ef49dc539e5a5d8af

Don't release focus from color popup text fields on key down
This bug was introduced in 43a225afe0217c698896a92e20f1cb627e718bcf

src/app/ui/color_popup.cpp
@@ -270,8 +270,13 @@ void ColorPopup::selectColorType(app::Color::Type type)
     case app::Color::MaskType:  m_colorType.setSelectedItem(MASK_MODE); break;
   }
 
-  // Remove focus from some RGB/HSB text entry
-  manager()->freeFocus();
+  // Remove focus from hidden RGB/HSB text entries
+  auto widget = manager()->getFocus();
+  if (widget && !widget->isVisible()) {
+    auto window = widget->window();
+    if (window && window == this)
+      widget->releaseFocus();
+  }
 
   m_vbox.layout();
   m_vbox.invalidate();