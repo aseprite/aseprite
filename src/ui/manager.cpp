aseprite/aseprite

commit 66e9cee3e6647c764b6ff34ed089c00218ad9a91

Fix crash on UI tests
Generating paint messages without a she::System available.

src/ui/manager.cpp
@@ -610,12 +610,14 @@ void Manager::handleWindowZOrder()
 void Manager::dispatchMessages()
 {
   // Send messages in the queue (mouse/key/timer/etc. events) This
-  // might change the state of widgets, etc.
-  pumpQueue();
-
-  // Generate and send only kPaintMessages with the latest UI state.
-  flushRedraw();
-  pumpQueue();
+  // might change the state of widgets, etc. In case pumpQueue()
+  // returns a number greater than 0, it means that we've processed
+  // some messages, so we've to redraw the screen.
+  if (pumpQueue() > 0) {
+    // Generate and send just kPaintMessages with the latest UI state.
+    flushRedraw();
+    pumpQueue();
+  }
 
   // Flip the back-buffer to the real display.
   flipDisplay();
@@ -1219,12 +1221,13 @@ void Manager::onSizeHint(SizeHintEvent& ev)
   ev.setSizeHint(gfx::Size(w, h));
 }
 
-void Manager::pumpQueue()
+int Manager::pumpQueue()
 {
 #ifdef LIMIT_DISPATCH_TIME
   base::tick_t t = base::current_tick();
 #endif
 
+  int count = 0;                // Number of processed messages
   auto it = msg_queue.begin();
   while (it != msg_queue.end()) {
 #ifdef LIMIT_DISPATCH_TIME
@@ -1289,7 +1292,10 @@ void Manager::pumpQueue()
 
     // Destroy the message
     delete first_msg;
+    ++count;
   }
+
+  return count;
 }
 
 bool Manager::sendMessageToWidget(Message* msg, Widget* widget)

src/ui/manager.h
@@ -1,5 +1,5 @@
 // Aseprite UI Library
-// Copyright (C) 2001-2016  David Capello
+// Copyright (C) 2001-2017  David Capello
 //
 // This file is released under the terms of the MIT license.
 // Read LICENSE.txt for more information.
@@ -149,7 +149,7 @@ namespace ui {
                             const double magnification);
     void handleWindowZOrder();
 
-    void pumpQueue();
+    int pumpQueue();
     bool sendMessageToWidget(Message* msg, Widget* widget);
 
     static void removeWidgetFromRecipients(Widget* widget, Message* msg);

src/ui/message.cpp
@@ -1,5 +1,5 @@
 // Aseprite UI Library
-// Copyright (C) 2001-2016  David Capello
+// Copyright (C) 2001-2017  David Capello
 //
 // This file is released under the terms of the MIT license.
 // Read LICENSE.txt for more information.
@@ -24,7 +24,7 @@ Message::Message(MessageType type, KeyModifiers modifiers)
   , m_used(false)
   , m_fromFilter(false)
 {
-  if (modifiers == kKeyUninitializedModifier)
+  if (modifiers == kKeyUninitializedModifier && she::instance())
     m_modifiers = she::instance()->keyModifiers();
   else
     m_modifiers = modifiers;

commit 8fa5d2dff1cb39d8415474e462ac103f0a65d6f6

Remove the "new_windows" var from ui::Manager impl
Fix regression bug introduced in
66e9cee3e6647c764b6ff34ed089c00218ad9a91 where new opened windows
weren't painted.

src/ui/manager.cpp
@@ -62,7 +62,6 @@ typedef std::list<Filter*> Filters;
 Manager* Manager::m_defaultManager = NULL;
 gfx::Region Manager::m_dirtyRegion;
 
-static WidgetsList new_windows; // Windows that we should show
 static WidgetsList mouse_widgets_list; // List of widgets to send mouse events
 static Messages msg_queue;             // Messages queue
 static Filters msg_filters[NFILTERS]; // Filters for every enqueued message
@@ -124,7 +123,6 @@ Manager::Manager()
   if (!m_defaultManager) {
     // Empty lists
     ASSERT(msg_queue.empty());
-    ASSERT(new_windows.empty());
     mouse_widgets_list.clear();
 
     // Reset variables
@@ -168,7 +166,6 @@ Manager::~Manager()
 
     // Shutdown system
     ASSERT(msg_queue.empty());
-    ASSERT(new_windows.empty());
     mouse_widgets_list.clear();
   }
 }
@@ -229,30 +226,6 @@ bool Manager::generateMessages()
   // First check: there are windows to manage?
   if (children().empty())
     return false;
-
-  // New windows to show?
-  if (!new_windows.empty()) {
-    for (auto window : new_windows) {
-      // Relayout
-      window->layout();
-
-      // Dirty the entire window and show it
-      window->setVisible(true);
-      window->invalidate();
-
-      // Attract the focus to the magnetic widget...
-      // 1) get the magnetic widget
-      Widget* magnet = findMagneticWidget(window->window());
-      // 2) if magnetic widget exists and it doesn't have the focus
-      if (magnet && !magnet->hasFocus())
-        setFocus(magnet);
-      // 3) if not, put the focus in the first child
-      else if (static_cast<Window*>(window)->isWantFocus())
-        focusFirstChild(window);
-    }
-
-    new_windows.clear();
-  }
 
   generateMessagesFromSheEvents();
 
@@ -998,12 +971,27 @@ void Manager::_openWindow(Window* window)
   insertChild(0, window);
 
   // Broadcast the open message.
-  Message* msg = new Message(kOpenMessage);
-  msg->addRecipient(window);
-  enqueueMessage(msg);
+  {
+    base::UniquePtr<Message> msg(new Message(kOpenMessage));
+    window->sendMessage(msg);
+  }
 
-  // Update the new windows list to show.
-  new_windows.push_back(window);
+  // Relayout
+  window->layout();
+
+  // Dirty the entire window and show it
+  window->setVisible(true);
+  window->invalidate();
+
+  // Attract the focus to the magnetic widget...
+  // 1) get the magnetic widget
+  Widget* magnet = findMagneticWidget(window);
+  // 2) if magnetic widget exists and it doesn't have the focus
+  if (magnet && !magnet->hasFocus())
+    setFocus(magnet);
+  // 3) if not, put the focus in the first child
+  else if (window->isWantFocus())
+    focusFirstChild(window);
 
   // Update mouse widget (as it can be a widget below the
   // recently opened window).
@@ -1051,21 +1039,16 @@ void Manager::_closeWindow(Window* window, bool redraw_background)
   window->setVisible(false);
 
   // Close message.
-  Message* msg = new Message(kCloseMessage);
-  msg->addRecipient(window);
-  enqueueMessage(msg);
+  {
+    base::UniquePtr<Message> msg(new Message(kCloseMessage));
+    window->sendMessage(msg);
+  }
 
   // Update manager list stuff.
   removeChild(window);
 
   // Redraw background.
   invalidateRegion(reg1);
-
-  // Maybe the window is in the "new_windows" list.
-  WidgetsList::iterator it =
-    std::find(new_windows.begin(), new_windows.end(), window);
-  if (it != new_windows.end())
-    new_windows.erase(it);
 
   // Update mouse widget (as it can be a widget below the
   // recently closed window).