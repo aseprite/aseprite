e83f9c18aa0a7c2948fac907d2d7b2b0904cb2f0
David Capello <davidcapello@gmail.com>
2017-03-07 14:52:45 -0300

    Add support to load fonts w/Unicode filenames

--- src/ft/lib.h
+++ src/ft/lib.h
@@ -1,5 +1,5 @@
 // Aseprite FreeType Wrapper
-// Copyright (c) 2016 David Capello
+// Copyright (c) 2016-2017 David Capello
 //
 // This file is released under the terms of the MIT license.
 // Read LICENSE.txt for more information.
@@ -17,31 +17,12 @@ namespace ft {
 
   class Lib {
   public:
-    Lib() : m_ft(nullptr) {
-      FT_Init_FreeType(&m_ft);
-    }
+    Lib();
+    ~Lib();
 
-    ~Lib() {
-      if (m_ft)
-        FT_Done_FreeType(m_ft);
-    }
+    operator FT_Library() { return m_ft; }
 
-    operator FT_Library() {
-      return m_ft;
-    }
-
-    FT_Face open(const std::string& filename) {
-      FT_Open_Args args;
-      memset(&args, 0, sizeof(args));
-      args.flags = FT_OPEN_PATHNAME;
-      args.pathname = (FT_String*)filename.c_str();
-
-      FT_Face face = nullptr;
-      FT_Error err = FT_Open_Face(m_ft, &args, 0, &face);
-      if (!err)
-        FT_Select_Charmap(face, FT_ENCODING_UNICODE);
-      return face;
-    }
+    FT_Face open(const std::string& filename);
 
   private:
     FT_Library m_ft;
