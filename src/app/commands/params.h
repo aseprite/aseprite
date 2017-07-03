4f5ce4bf57a1668a8140c759713961fce8d0061c
David Capello <davidcapello@gmail.com>
2017-07-03 17:31:56 -0300

    Don't modify Params on const member functions

--- src/app/commands/params.h
+++ src/app/commands/params.h
@@ -24,13 +24,9 @@ namespace app {
     const_iterator begin() const { return m_params.begin(); }
     const_iterator end() const { return m_params.end(); }
 
-    bool empty() const {
-      return m_params.empty();
-    }
-
-    void clear() {
-      return m_params.clear();
-    }
+    bool empty() const { return m_params.empty(); }
+    int size() const { return int(m_params.size()); }
+    void clear() { return m_params.clear(); }
 
     bool has_param(const char* name) const {
       return m_params.find(name) != m_params.end();
@@ -56,21 +52,24 @@ namespace app {
         return std::string();
     }
 
-    void operator|=(const Params& params) const {
+    void operator|=(const Params& params) {
       for (const auto& p : params)
         m_params[p.first] = p.second;
     }
 
     template<typename T>
     const T get_as(const char* name) const {
-      std::istringstream stream(m_params[name]);
       T value = T();
-      stream >> value;
+      auto it = m_params.find(name);
+      if (it != m_params.end()) {
+        std::istringstream stream(it->second);
+        stream >> value;
+      }
       return value;
     }
 
   private:
-    mutable Map m_params;
+    Map m_params;
   };
 
 } // namespace app
