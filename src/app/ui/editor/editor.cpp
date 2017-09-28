aseprite/aseprite

commit ec2a1f229d48c76f1d8124102e7c3a69f236f32f

Possibility to Add/Subtract selection inside the selection
https://community.aseprite.org/t/470

src/app/ui/editor/editor.cpp
@@ -1891,6 +1891,16 @@ bool Editor::isInsideSelection()
     m_document->mask()->containsPoint(spritePos.x, spritePos.y);
 }
 
+bool Editor::canStartMovingSelectionPixels()
+{
+  return
+    (isInsideSelection()) &&
+    // In this way we cannot move the selection when add/subtract
+    // modes are enabled (we prefer to modify the selection on those
+    // modes instead of moving pixels).
+    (int(m_toolLoopModifiers) & int(tools::ToolLoopModifiers::kReplaceSelection));
+}
+
 EditorHit Editor::calcHit(const gfx::Point& mouseScreenPos)
 {
   tools::Ink* ink = getCurrentEditorInk();

src/app/ui/editor/editor.h
@@ -199,6 +199,11 @@ namespace app {
     // Returns true if the cursor is inside the active mask/selection.
     bool isInsideSelection();
 
+    // Returns true if the cursor is inside the selection and the
+    // selection mode is the default one which prioritizes and easy
+    // way to move the selection.
+    bool canStartMovingSelectionPixels();
+
     // Returns the element that will be modified if the mouse is used
     // in the given position.
     EditorHit calcHit(const gfx::Point& mouseScreenPos);

src/app/ui/editor/standby_state.cpp
@@ -283,7 +283,7 @@ bool StandbyState::onMouseDown(Editor* editor, MouseMessage* msg)
     }
 
     // Move selected pixels
-    if (layer && editor->isInsideSelection() && msg->left()) {
+    if (layer && editor->canStartMovingSelectionPixels() && msg->left()) {
       if (!layer->isEditableHierarchy()) {
         StatusBar::instance()->showTip(1000,
           "Layer '%s' is locked", layer->name().c_str());
@@ -411,7 +411,7 @@ bool StandbyState::onSetCursor(Editor* editor, const gfx::Point& mouseScreenPos)
       }
 
       // Move pixels
-      if (editor->isInsideSelection()) {
+      if (editor->canStartMovingSelectionPixels()) {
         EditorCustomizationDelegate* customization = editor->getCustomizationDelegate();
         if ((customization) &&
             int(customization->getPressedKeyAction(KeyContext::TranslatingSelection) & KeyAction::CopySelection))

commit 331e09bdbf1465d638e9b8d224e58089f05cd7ad

Fix Ctrl+drag and drop to copy the selection content on Windows
Regression introduced in ec2a1f229d48c76f1d8124102e7c3a69f236f32f

Bug reports: https://community.aseprite.org/t/503

src/app/ui/editor/editor.cpp
@@ -1894,11 +1894,14 @@ bool Editor::isInsideSelection()
 bool Editor::canStartMovingSelectionPixels()
 {
   return
-    (isInsideSelection()) &&
-    // In this way we cannot move the selection when add/subtract
-    // modes are enabled (we prefer to modify the selection on those
-    // modes instead of moving pixels).
-    (int(m_toolLoopModifiers) & int(tools::ToolLoopModifiers::kReplaceSelection));
+    isInsideSelection() &&
+    // We cannot move the selection when add/subtract modes are
+    // enabled (we prefer to modify the selection on those modes
+    // instead of moving pixels).
+    ((int(m_toolLoopModifiers) & int(tools::ToolLoopModifiers::kReplaceSelection)) ||
+     // We can move the selection when the Copy selection key (Ctrl) is pressed.
+     (m_customizationDelegate &&
+      int(m_customizationDelegate->getPressedKeyAction(KeyContext::TranslatingSelection) & KeyAction::CopySelection)));
 }
 
 EditorHit Editor::calcHit(const gfx::Point& mouseScreenPos)