aseprite/aseprite

commit 86c8bc57a5c5e2093194abf10ad6331de50cfd33

Avoid redrawing the whole PaletteView when we move the mouse between color entries

src/app/ui/palette_view.cpp
@@ -413,8 +413,13 @@ bool PaletteView::onProcessMessage(Message* msg)
       MouseMessage* mouseMsg = static_cast<MouseMessage*>(msg);
       Hit hit = hitTest(mouseMsg->position() - bounds().origin());
       if (hit != m_hot) {
+        // Redraw only when we put the mouse in other part of the
+        // widget (e.g. if we move from color to color, we don't want
+        // to redraw the whole widget).
+        if (hit.part != m_hot.part)
+          invalidate();
+
         m_hot = hit;
-        invalidate();
       }
       setCursor();
       return true;

commit 27f0182bdb9cd53a5d60a66bc10f9e4821a5634f

Fix redrawing PaletteView when dragging colors (fix #1616)
Regression introduced in 86c8bc57a5c5e2093194abf10ad6331de50cfd33

src/app/ui/palette_view.cpp
@@ -415,10 +415,11 @@ bool PaletteView::onProcessMessage(Message* msg)
       if (hit != m_hot) {
         // Redraw only when we put the mouse in other part of the
         // widget (e.g. if we move from color to color, we don't want
-        // to redraw the whole widget).
-        if (hit.part != m_hot.part)
+        // to redraw the whole widget if we're on WAITING state).
+        if ((m_state == State::WAITING && hit.part != m_hot.part) ||
+            (m_state != State::WAITING && hit != m_hot)) {
           invalidate();
-
+        }
         m_hot = hit;
       }
       setCursor();