aseprite/aseprite

commit 245285f84e427749ae3d6b3b29515f8198c7f80a

Filters: Use the selected cels range instead of target buttons
With this commit we've definitely removed the target buttons to apply a
filter in current cel/layer/frame (which never make too much sense to
the end-user), and now we can just select the range of
layers/frames/cels where we want to apply the filter (like Flip and
Rotate commands that were already working in this way).

src/app/commands/filters/filter_manager_impl.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2001-2017  David Capello
+// Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
 // the End-User License Agreement for Aseprite.
@@ -10,6 +10,7 @@
 
 #include "app/commands/filters/filter_manager_impl.h"
 
+#include "app/app.h"
 #include "app/cmd/copy_region.h"
 #include "app/cmd/patch_cel.h"
 #include "app/cmd/set_palette.h"
@@ -23,10 +24,12 @@
 #include "app/ui/color_bar.h"
 #include "app/ui/editor/editor.h"
 #include "app/ui/palette_view.h"
+#include "app/ui/timeline/timeline.h"
+#include "app/util/range_utils.h"
 #include "doc/algorithm/shrink_bounds.h"
 #include "doc/cel.h"
+#include "doc/cel.h"
 #include "doc/image.h"
-#include "doc/images_collector.h"
 #include "doc/layer.h"
 #include "doc/mask.h"
 #include "doc/site.h"
@@ -228,13 +231,17 @@ void FilterManagerImpl::applyToTarget()
   const bool paletteChange = paletteHasChanged();
   bool cancelled = false;
 
-  ImagesCollector images((m_target & TARGET_ALL_LAYERS ?
-                          m_site.sprite()->root():
-                          m_site.layer()),
-                         m_site.frame(),
-                         (m_target & TARGET_ALL_FRAMES) == TARGET_ALL_FRAMES,
-                         true); // we will write in each image
-  if (images.empty() && !paletteChange) {
+  CelList cels;
+  auto range = App::instance()->timeline()->range();
+  if (range.enabled())
+    cels = get_unlocked_unique_cels(m_site.sprite(), range);
+  else if (m_site.cel() &&
+           m_site.layer() &&
+           m_site.layer()->isEditable()) {
+    cels.push_back(m_site.cel());
+  }
+
+  if (cels.empty() && !paletteChange) {
     // We don't have images/palette changes to do (there will not be a
     // transaction).
     return;
@@ -246,7 +253,7 @@ void FilterManagerImpl::applyToTarget()
   m_transaction.reset(new Transaction(writer.context(), m_filter->getName(), ModifyDocument));
 
   m_progressBase = 0.0f;
-  m_progressWidth = 1.0f / images.size();
+  m_progressWidth = (cels.size() > 0 ? 1.0f / cels.size(): 1.0f);
 
   std::set<ObjectId> visited;
 
@@ -260,15 +267,15 @@ void FilterManagerImpl::applyToTarget()
   }
 
   // For each target image
-  for (auto it = images.begin();
-       it != images.end() && !cancelled;
+  for (auto it = cels.begin();
+       it != cels.end() && !cancelled;
        ++it) {
-    Image* image = it->image();
+    Image* image = (*it)->image();
 
     // Avoid applying the filter two times to the same image
     if (visited.find(image->id()) == visited.end()) {
       visited.insert(image->id());
-      applyToCel(it->cel());
+      applyToCel(*it);
     }
 
     // Is there a delegate to know if the process was cancelled by the user?

src/app/commands/filters/filter_target_buttons.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2001-2017  David Capello
+// Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
 // the End-User License Agreement for Aseprite.
@@ -37,7 +37,6 @@ FilterTargetButtons::FilterTargetButtons(int imgtype, bool withChannels)
   , m_alpha(nullptr)
   , m_gray(nullptr)
   , m_index(nullptr)
-  , m_cels(nullptr)
 {
   setMultipleSelection(true);
   addChild(&m_tooltips);
@@ -62,17 +61,10 @@ FilterTargetButtons::FilterTargetButtons(int imgtype, bool withChannels)
         break;
     }
   }
-
-  // Create the button to select which cels will be modified by the
-  // filter.
-  m_cels = addItem(getCelsIcon(), 4, 1);
 }
 
 void FilterTargetButtons::setTarget(int target)
 {
-  m_target &= (TARGET_ALL_FRAMES | TARGET_ALL_LAYERS);
-  m_target |= (target & ~(TARGET_ALL_FRAMES | TARGET_ALL_LAYERS));
-
   selectTargetButton(m_red,   TARGET_RED_CHANNEL);
   selectTargetButton(m_green, TARGET_GREEN_CHANNEL);
   selectTargetButton(m_blue,  TARGET_BLUE_CHANNEL);
@@ -91,32 +83,12 @@ void FilterTargetButtons::selectTargetButton(Item* item, Target specificTarget)
 
 void FilterTargetButtons::updateFromTarget()
 {
-  m_cels->setIcon(getCelsIcon());
-
   updateComponentTooltip(m_red, "Red", BOTTOM);
   updateComponentTooltip(m_green, "Green", BOTTOM);
   updateComponentTooltip(m_blue, "Blue", BOTTOM);
   updateComponentTooltip(m_gray, "Gray", BOTTOM);
   updateComponentTooltip(m_alpha, "Alpha", BOTTOM);
   updateComponentTooltip(m_index, "Index", LEFT);
-
-  const char* celsTooltip = "";
-  switch (m_target & (TARGET_ALL_FRAMES | TARGET_ALL_LAYERS)) {
-    case 0:
-      celsTooltip = "Apply to the active frame/layer (the active cel)";
-      break;
-    case TARGET_ALL_FRAMES:
-      celsTooltip = "Apply to all frames in the active layer";
-      break;
-    case TARGET_ALL_LAYERS:
-      celsTooltip = "Apply to all layers in the active frame";
-      break;
-    case TARGET_ALL_FRAMES | TARGET_ALL_LAYERS:
-      celsTooltip = "Apply to all cels in the sprite";
-      break;
-  }
-
-  m_tooltips.addTooltipFor(m_cels, celsTooltip, LEFT);
 }
 
 void FilterTargetButtons::updateComponentTooltip(Item* item, const char* channelName, int align)
@@ -133,7 +105,7 @@ void FilterTargetButtons::updateComponentTooltip(Item* item, const char* channel
 void FilterTargetButtons::onItemChange(Item* item)
 {
   ButtonSet::onItemChange(item);
-  Target flags = (m_target & (TARGET_ALL_FRAMES | TARGET_ALL_LAYERS));
+  Target flags = m_target;
 
   if (m_index && item && item->isSelected()) {
     if (item == m_index) {
@@ -157,43 +129,10 @@ void FilterTargetButtons::onItemChange(Item* item)
   if (m_index && m_index->isSelected()) flags |= TARGET_INDEX_CHANNEL;
   if (m_alpha && m_alpha->isSelected()) flags |= TARGET_ALPHA_CHANNEL;
 
-  if (m_cels->isSelected()) {
-    m_cels->setSelected(false);
-
-    // Rotate cels target
-    if (flags & TARGET_ALL_FRAMES) {
-      flags &= ~TARGET_ALL_FRAMES;
-
-      if (flags & TARGET_ALL_LAYERS)
-        flags &= ~TARGET_ALL_LAYERS;
-      else
-        flags |= TARGET_ALL_LAYERS;
-    }
-    else {
-      flags |= TARGET_ALL_FRAMES;
-    }
-  }
-
   if (m_target != flags) {
     m_target = flags;
     updateFromTarget();
     TargetChange();
-  }
-}
-
-SkinPartPtr FilterTargetButtons::getCelsIcon() const
-{
-  SkinTheme* theme = SkinTheme::instance();
-
-  if (m_target & TARGET_ALL_FRAMES) {
-    return (m_target & TARGET_ALL_LAYERS) ?
-      theme->parts.targetFramesLayers():
-      theme->parts.targetFrames();
-  }
-  else {
-    return (m_target & TARGET_ALL_LAYERS) ?
-      theme->parts.targetLayers():
-      theme->parts.targetOne();
   }
 }
 

src/app/commands/filters/filter_target_buttons.h
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2001-2016  David Capello
+// Copyright (C) 2001-2016, 2018  David Capello
 //
 // This program is distributed under the terms of
 // the End-User License Agreement for Aseprite.
@@ -41,7 +41,6 @@ namespace app {
     void selectTargetButton(Item* item, Target specificTarget);
     void updateFromTarget();
     void updateComponentTooltip(Item* item, const char* channelName, int align);
-    skin::SkinPartPtr getCelsIcon() const;
 
     Target m_target;
     Item* m_red;
@@ -50,7 +49,6 @@ namespace app {
     Item* m_alpha;
     Item* m_gray;
     Item* m_index;
-    Item* m_cels;
     ui::TooltipManager m_tooltips;
   };
 

data/extensions/aseprite-theme/theme.xml
@@ -240,10 +240,6 @@
         <part id="pal_presets" x="160" y="200" w="5" h="5" />
         <part id="pal_options" x="168" y="200" w="5" h="5" />
         <part id="pal_resize" x="176" y="200" w="5" h="5" />
-        <part id="target_one" x="144" y="224" w="32" h="16" />
-        <part id="target_frames" x="176" y="224" w="32" h="16" />
-        <part id="target_layers" x="208" y="224" w="32" h="16" />
-        <part id="target_frames_layers" x="240" y="224" w="32" h="16" />
         <part id="selection_replace" x="176" y="160" w="7" h="7" />
         <part id="selection_add" x="184" y="160" w="7" h="7" />
         <part id="selection_subtract" x="192" y="160" w="7" h="7" />

src/doc/CMakeLists.txt
@@ -1,5 +1,5 @@
 # Aseprite Document Library
-# Copyright (C) 2001-2017 David Capello
+# Copyright (C) 2001-2018 David Capello
 
 if(WIN32)
   add_definitions(-D_CRT_SECURE_NO_WARNINGS)
@@ -41,7 +41,6 @@ add_library(doc-lib
   image.cpp
   image_impl.cpp
   image_io.cpp
-  images_collector.cpp
   layer.cpp
   layer_io.cpp
   layer_list.cpp

src/doc/images_collector.cpp
@@ -1,70 +1,0 @@
-// Aseprite Document Library
-// Copyright (c) 2001-2016 David Capello
-//
-// This file is released under the terms of the MIT license.
-// Read LICENSE.txt for more information.
-
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
-
-#include "doc/cel.h"
-#include "doc/image.h"
-#include "doc/images_collector.h"
-#include "doc/layer.h"
-#include "doc/mask.h"
-#include "doc/sprite.h"
-
-namespace doc {
-
-ImagesCollector::ImagesCollector(Layer* layer,
-                                 frame_t frame,
-                                 bool allFrames,
-                                 bool forEdit)
-  : m_allFrames(allFrames)
-  , m_forEdit(forEdit)
-{
-  collectFromLayer(layer, frame);
-}
-
-void ImagesCollector::collectFromLayer(Layer* layer, frame_t frame)
-{
-  const Sprite* sprite = layer->sprite();
-
-  if (!layer->isVisible())
-    return;
-
-  if (m_forEdit && !layer->isEditable())
-    return;
-
-  switch (layer->type()) {
-
-    case ObjectType::LayerImage: {
-      if (m_allFrames) {
-        for (frame_t frame(0); frame<sprite->totalFrames(); ++frame) {
-          if (Cel* cel = layer->cel(frame))
-            collectImage(layer, cel);
-        }
-      }
-      else {
-        if (Cel* cel = layer->cel(frame))
-          collectImage(layer, cel);
-      }
-      break;
-    }
-
-    case ObjectType::LayerGroup: {
-      for (Layer* child : static_cast<LayerGroup*>(layer)->layers())
-        collectFromLayer(child, frame);
-      break;
-    }
-
-  }
-}
-
-void ImagesCollector::collectImage(Layer* layer, Cel* cel)
-{
-  m_items.push_back(Item(layer, cel, cel->image()));
-}
-
-} // namespace doc

src/doc/images_collector.h
@@ -1,74 +1,0 @@
-// Aseprite Document Library
-// Copyright (c) 2001-2016 David Capello
-//
-// This file is released under the terms of the MIT license.
-// Read LICENSE.txt for more information.
-
-#ifndef DOC_IMAGES_REF_H_INCLUDED
-#define DOC_IMAGES_REF_H_INCLUDED
-#pragma once
-
-#include "doc/frame.h"
-
-#include <list>
-
-namespace doc {
-
-  class Sprite;
-  class Image;
-  class Layer;
-  class Cel;
-
-  // Collects images from a sprite
-  class ImagesCollector {
-  public:
-    class Item {
-    public:
-      Item(Layer* layer, Cel* cel, Image* image)
-        : m_layer(layer)
-        , m_cel(cel)
-        , m_image(image)
-      { }
-
-      Layer* layer() const { return m_layer; }
-      Cel*   cel()   const { return m_cel; }
-      Image* image() const { return m_image; }
-
-    private:
-      Layer* m_layer;
-      Cel*   m_cel;
-      Image* m_image;
-    };
-
-    typedef std::list<Item> Items;
-    typedef std::list<Item>::iterator ItemsIterator;
-
-    // Creates a collection of images from the specified sprite.
-    // Parameters:
-    // * allFrames: True if you want to collect images from all frames
-    //              or false if you need images from the given "frame" param.
-    // * forEdit:   True if you will modify the images (it is used to avoid
-    //              returning images from layers which are read-only/write-locked).
-    ImagesCollector(Layer* layer,
-                    frame_t frame,
-                    bool allFrames,
-                    bool forEdit);
-
-    ItemsIterator begin() { return m_items.begin(); }
-    ItemsIterator end() { return m_items.end(); }
-
-    std::size_t size() const { return m_items.size(); }
-    bool empty() const { return m_items.empty(); }
-
-  private:
-    void collectFromLayer(Layer* layer, frame_t frame);
-    void collectImage(Layer* layer, Cel* cel);
-
-    Items m_items;
-    bool m_allFrames;
-    bool m_forEdit;
-  };
-
-} // namespace doc
-
-#endif

commit 58252946ea742f28ad427ddae41994b68fe38316

Put back a simpler vesion of the target button for cels
Now we have a button to apply filters to the selected region
in the timeline, or to all cels. Regression introduced in 245285f84e427749ae3d6b3b29515f8198c7f80a

Discussion:
http://steamcommunity.com/app/431730/discussions/0/343785574514723475/

data/pref.xml
@@ -288,6 +288,9 @@
     <section id="hue_saturation">
       <option id="mode" type="HueSaturationMode" default="HueSaturationMode::HSL" />
     </section>
+    <section id="filters">
+      <option id="cels_target" type="CelsTarget" default="CelsTarget::Selected" />
+    </section>
   </global>
 
   <tool>

data/strings/en.ini
@@ -507,6 +507,12 @@ new_folder_button_tooltip = New folder
 file_name = File name:
 file_type = File type:
 
+[filters]
+selected_cels = Selected
+selected_cels_tooltip = Apply to the active selection in the timeline
+all_cels = All
+all_cels_tooltip = Apply to all cels in the sprite
+
 [font_popup]
 load = Load
 

src/app/commands/filters/cels_target.h
@@ -1,0 +1,20 @@
+// Aseprite
+// Copyright (C) 2018  David Capello
+//
+// This program is distributed under the terms of
+// the End-User License Agreement for Aseprite.
+
+#ifndef APP_COMMANDS_FILTERS_CELS_TARGET_H_INCLUDED
+#define APP_COMMANDS_FILTERS_CELS_TARGET_H_INCLUDED
+#pragma once
+
+namespace app {
+
+  enum class CelsTarget {
+    Selected,                 // Selected cels in the timeline
+    All                       // All cels in the sprite
+  };
+
+} // namespace app
+
+#endif

src/app/commands/filters/filter_manager_impl.cpp
@@ -28,7 +28,7 @@
 #include "app/util/range_utils.h"
 #include "doc/algorithm/shrink_bounds.h"
 #include "doc/cel.h"
-#include "doc/cel.h"
+#include "doc/cels_range.h"
 #include "doc/image.h"
 #include "doc/layer.h"
 #include "doc/mask.h"
@@ -55,15 +55,15 @@ FilterManagerImpl::FilterManagerImpl(Context* context, Filter* filter)
   , m_cel(nullptr)
   , m_src(nullptr)
   , m_dst(nullptr)
+  , m_row(0)
   , m_mask(nullptr)
   , m_previewMask(nullptr)
+  , m_targetOrig(TARGET_ALL_CHANNELS)
+  , m_target(TARGET_ALL_CHANNELS)
+  , m_celsTarget(CelsTarget::Selected)
   , m_oldPalette(nullptr)
   , m_progressDelegate(NULL)
 {
-  m_row = 0;
-  m_targetOrig = TARGET_ALL_CHANNELS;
-  m_target = TARGET_ALL_CHANNELS;
-
   int x, y;
   Image* image = m_site.image(&x, &y);
   if (!image)
@@ -104,6 +104,11 @@ void FilterManagerImpl::setTarget(int target)
   if (m_site.layer() &&
       m_site.layer()->isBackground())
     m_target &= ~TARGET_ALPHA_CHANNEL;
+}
+
+void FilterManagerImpl::setCelsTarget(CelsTarget celsTarget)
+{
+  m_celsTarget = celsTarget;
 }
 
 void FilterManagerImpl::begin()
@@ -232,13 +237,28 @@ void FilterManagerImpl::applyToTarget()
   bool cancelled = false;
 
   CelList cels;
-  auto range = App::instance()->timeline()->range();
-  if (range.enabled())
-    cels = get_unlocked_unique_cels(m_site.sprite(), range);
-  else if (m_site.cel() &&
-           m_site.layer() &&
-           m_site.layer()->isEditable()) {
-    cels.push_back(m_site.cel());
+
+  switch (m_celsTarget) {
+
+    case CelsTarget::Selected: {
+      auto range = App::instance()->timeline()->range();
+      if (range.enabled())
+        cels = get_unlocked_unique_cels(m_site.sprite(), range);
+      else if (m_site.cel() &&
+               m_site.layer() &&
+               m_site.layer()->isEditable()) {
+        cels.push_back(m_site.cel());
+      }
+      break;
+    }
+
+    case CelsTarget::All: {
+      for (Cel* cel : m_site.sprite()->uniqueCels()) {
+        if (cel->layer()->isEditable())
+          cels.push_back(cel);
+      }
+      break;
+    }
   }
 
   if (cels.empty() && !paletteChange) {

src/app/commands/filters/filter_manager_impl.h
@@ -8,6 +8,7 @@
 #define APP_COMMANDS_FILTERS_FILTER_MANAGER_IMPL_H_INCLUDED
 #pragma once
 
+#include "app/commands/filters/cels_target.h"
 #include "base/exception.h"
 #include "base/unique_ptr.h"
 #include "doc/image_impl.h"
@@ -77,6 +78,7 @@ namespace app {
     doc::PixelFormat pixelFormat() const;
 
     void setTarget(Target target);
+    void setCelsTarget(CelsTarget celsTarget);
 
     void begin();
     void beginForPreview();
@@ -144,6 +146,7 @@ namespace app {
     doc::ImageBits<doc::BitmapTraits>::iterator m_maskIterator;
     Target m_targetOrig;          // Original targets
     Target m_target;              // Filtered targets
+    CelsTarget m_celsTarget;
     base::UniquePtr<doc::Palette> m_oldPalette;
     std::unique_ptr<Transaction> m_transaction;
 

src/app/commands/filters/filter_target_buttons.cpp
@@ -10,6 +10,7 @@
 
 #include "app/commands/filters/filter_target_buttons.h"
 
+#include "app/i18n/strings.h"
 #include "app/modules/gfx.h"
 #include "app/modules/gui.h"
 #include "app/ui/skin/skin_theme.h"
@@ -31,12 +32,14 @@ using namespace ui;
 FilterTargetButtons::FilterTargetButtons(int imgtype, bool withChannels)
   : ButtonSet(4)
   , m_target(0)
+  , m_celsTarget(CelsTarget::Selected)
   , m_red(nullptr)
   , m_green(nullptr)
   , m_blue(nullptr)
   , m_alpha(nullptr)
   , m_gray(nullptr)
   , m_index(nullptr)
+  , m_cels(nullptr)
 {
   setMultipleSelection(true);
   addChild(&m_tooltips);
@@ -61,9 +64,13 @@ FilterTargetButtons::FilterTargetButtons(int imgtype, bool withChannels)
         break;
     }
   }
+
+  // Create the button to select which cels will be modified by the
+  // filter.
+  m_cels = addItem(getCelsTargetText(), 4, 1);
 }
 
-void FilterTargetButtons::setTarget(int target)
+void FilterTargetButtons::setTarget(const int target)
 {
   selectTargetButton(m_red,   TARGET_RED_CHANNEL);
   selectTargetButton(m_green, TARGET_GREEN_CHANNEL);
@@ -71,8 +78,13 @@ void FilterTargetButtons::setTarget(int target)
   selectTargetButton(m_alpha, TARGET_ALPHA_CHANNEL);
   selectTargetButton(m_gray,  TARGET_GRAY_CHANNEL);
   selectTargetButton(m_index, TARGET_INDEX_CHANNEL);
-
   updateFromTarget();
+}
+
+void FilterTargetButtons::setCelsTarget(const CelsTarget celsTarget)
+{
+  m_celsTarget = celsTarget;
+  updateFromCelsTarget();
 }
 
 void FilterTargetButtons::selectTargetButton(Item* item, Target specificTarget)
@@ -91,6 +103,12 @@ void FilterTargetButtons::updateFromTarget()
   updateComponentTooltip(m_index, "Index", LEFT);
 }
 
+void FilterTargetButtons::updateFromCelsTarget()
+{
+  m_cels->setText(getCelsTargetText());
+  m_tooltips.addTooltipFor(m_cels, getCelsTargetTooltip(), LEFT);
+}
+
 void FilterTargetButtons::updateComponentTooltip(Item* item, const char* channelName, int align)
 {
   if (item) {
@@ -105,7 +123,8 @@ void FilterTargetButtons::updateComponentTooltip(Item* item, const char* channel
 void FilterTargetButtons::onItemChange(Item* item)
 {
   ButtonSet::onItemChange(item);
-  Target flags = m_target;
+  Target target = m_target;
+  CelsTarget celsTarget = m_celsTarget;
 
   if (m_index && item && item->isSelected()) {
     if (item == m_index) {
@@ -122,18 +141,51 @@ void FilterTargetButtons::onItemChange(Item* item)
     }
   }
 
-  if (m_red && m_red->isSelected()) flags |= TARGET_RED_CHANNEL;
-  if (m_green && m_green->isSelected()) flags |= TARGET_GREEN_CHANNEL;
-  if (m_blue && m_blue->isSelected()) flags |= TARGET_BLUE_CHANNEL;
-  if (m_gray && m_gray->isSelected()) flags |= TARGET_GRAY_CHANNEL;
-  if (m_index && m_index->isSelected()) flags |= TARGET_INDEX_CHANNEL;
-  if (m_alpha && m_alpha->isSelected()) flags |= TARGET_ALPHA_CHANNEL;
+  if (m_red && m_red->isSelected()) target |= TARGET_RED_CHANNEL;
+  if (m_green && m_green->isSelected()) target |= TARGET_GREEN_CHANNEL;
+  if (m_blue && m_blue->isSelected()) target |= TARGET_BLUE_CHANNEL;
+  if (m_gray && m_gray->isSelected()) target |= TARGET_GRAY_CHANNEL;
+  if (m_index && m_index->isSelected()) target |= TARGET_INDEX_CHANNEL;
+  if (m_alpha && m_alpha->isSelected()) target |= TARGET_ALPHA_CHANNEL;
 
-  if (m_target != flags) {
-    m_target = flags;
-    updateFromTarget();
+  if (m_cels->isSelected()) {
+    m_cels->setSelected(false);
+    celsTarget =              // Switch cels target
+      (m_celsTarget == CelsTarget::Selected ?
+       CelsTarget::All:
+       CelsTarget::Selected);
+  }
+
+  if (m_target != target ||
+      m_celsTarget != celsTarget) {
+    if (m_target != target) {
+      m_target = target;
+      updateFromTarget();
+    }
+    if (m_celsTarget != celsTarget) {
+      m_celsTarget = celsTarget;
+      updateFromCelsTarget();
+    }
     TargetChange();
   }
+}
+
+std::string FilterTargetButtons::getCelsTargetText() const
+{
+  switch (m_celsTarget) {
+    case CelsTarget::Selected: return Strings::filters_selected_cels();
+    case CelsTarget::All: return Strings::filters_all_cels();
+  }
+  return std::string();
+}
+
+std::string FilterTargetButtons::getCelsTargetTooltip() const
+{
+  switch (m_celsTarget) {
+    case CelsTarget::Selected: return Strings::filters_selected_cels_tooltip();
+    case CelsTarget::All: return Strings::filters_all_cels_tooltip();
+  }
+  return std::string();
 }
 
 } // namespace app

src/app/commands/filters/filter_target_buttons.h
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2001-2016, 2018  David Capello
+// Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
 // the End-User License Agreement for Aseprite.
@@ -8,6 +8,7 @@
 #define APP_COMMANDS_FILTERS_FILTER_TARGET_BUTTONS_H_INCLUDED
 #pragma once
 
+#include "app/commands/filters/cels_target.h"
 #include "app/ui/button_set.h"
 #include "app/ui/skin/skin_part.h"
 #include "filters/target.h"
@@ -27,8 +28,11 @@ namespace app {
     // the a sprite.
     FilterTargetButtons(int imgtype, bool withChannels);
 
-    Target getTarget() const { return m_target; }
-    void setTarget(Target target);
+    Target target() const { return m_target; }
+    CelsTarget celsTarget() const { return m_celsTarget; }
+
+    void setTarget(const Target target);
+    void setCelsTarget(const CelsTarget celsTarget);
 
     obs::signal<void()> TargetChange;
 
@@ -40,15 +44,20 @@ namespace app {
   private:
     void selectTargetButton(Item* item, Target specificTarget);
     void updateFromTarget();
+    void updateFromCelsTarget();
     void updateComponentTooltip(Item* item, const char* channelName, int align);
+    std::string getCelsTargetText() const;
+    std::string getCelsTargetTooltip() const;
 
     Target m_target;
+    CelsTarget m_celsTarget;
     Item* m_red;
     Item* m_green;
     Item* m_blue;
     Item* m_alpha;
     Item* m_gray;
     Item* m_index;
+    Item* m_cels;
     ui::TooltipManager m_tooltips;
   };
 

src/app/commands/filters/filter_window.cpp
@@ -15,6 +15,7 @@
 #include "app/ini_file.h"
 #include "app/modules/editors.h"
 #include "app/modules/gui.h"
+#include "app/pref/preferences.h"
 #include "app/ui/editor/editor.h"
 #include "base/bind.h"
 
@@ -47,7 +48,11 @@ FilterWindow::FilterWindow(const char* title, const char* cfgSection,
   if (m_tiledCheck)
     m_tiledCheck->processMnemonicFromText();
 
+  CelsTarget celsTarget = Preferences::instance().filters.celsTarget();
+  filterMgr->setCelsTarget(celsTarget);
+
   m_targetButton.setTarget(filterMgr->getTarget());
+  m_targetButton.setCelsTarget(celsTarget);
   m_targetButton.TargetChange.connect(&FilterWindow::onTargetButtonChange, this);
   m_okButton.Click.connect(&FilterWindow::onOk, this);
   m_cancelButton.Click.connect(&FilterWindow::onCancel, this);
@@ -87,6 +92,9 @@ FilterWindow::~FilterWindow()
 
   // Save "Preview" check status.
   set_config_bool(m_cfgSection, "Preview", m_showPreview.isSelected());
+
+  // Save cels target button
+  Preferences::instance().filters.celsTarget(m_targetButton.celsTarget());
 }
 
 bool FilterWindow::doModal()
@@ -166,7 +174,8 @@ void FilterWindow::onTargetButtonChange()
   stopPreview();
 
   // Change the targets in the filter manager and restart the filter preview.
-  m_filterMgr->setTarget(m_targetButton.getTarget());
+  m_filterMgr->setTarget(m_targetButton.target());
+  m_filterMgr->setCelsTarget(m_targetButton.celsTarget());
   restartPreview();
 }
 

src/app/pref/preferences.h
@@ -9,6 +9,7 @@
 #pragma once
 
 #include "app/color.h"
+#include "app/commands/filters/cels_target.h"
 #include "app/document_exporter.h"
 #include "app/pref/option.h"
 #include "app/sprite_sheet_type.h"