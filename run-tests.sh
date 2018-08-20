0dd41f69d5d48ad247ccd65463af806b81b5048d
David Capello <davidcapello@gmail.com>
2018-08-20 14:41:53 -0300

    Redir stderr

--- run-tests.sh
+++ run-tests.sh
@@ -18,12 +18,12 @@ $ASEPRITE --version
 
 echo ----------------------------------------------------------------------
 echo "Testing console..."
-$ASEPRITE -b --script scripts/console_assert.js > tmp
+$ASEPRITE -b --script scripts/console_assert.js >tmp 2>tmp_err
 ! grep -q Error tmp && fail
 ! grep -q "this should be in the output" tmp && fail
 grep -q "this should not be in the output" tmp && fail
 
-$ASEPRITE -b --script scripts/console_log.js > tmp
+$ASEPRITE -b --script scripts/console_log.js >tmp 2>tmp_err
 cat >tmp_expected <<EOF
 hello world
 1 2 3
@@ -37,9 +37,10 @@ for jsfile in scripts/*.js ; do
     [[ $jsfile =~ console ]] && continue
 
     echo "Running $jsfile"
-    if ! $ASEPRITE -b --script $jsfile > tmp ; then
-        cat tmp
+    if ! $ASEPRITE -b --script $jsfile >tmp 2>tmp_err ; then
         echo FAILED
+        echo STDOUT && cat tmp
+        echo STDERR && cat tmp_err
         result=1
     fi
 done
