4d7f3036d84a4c37bd02ee491447705fcf4a8141
David Capello <david@igarastudio.com>
2018-11-03 13:39:43 -0300

    Add app_active.lua

---
+++ scripts/app_active.lua
@@ -0,0 +1,15 @@
+-- Copyright (C) 2018  Igara Studio S.A.
+--
+-- This file is released under the terms of the MIT license.
+-- Read LICENSE.txt for more information.
+
+local s = Sprite(32, 64)
+assert(s == app.activeSprite)
+assert(s == app.activeCel.sprite)
+assert(s == app.activeFrame.sprite)
+assert(1 == app.activeFrame.frameNumber)
+assert(0.100 == app.activeFrame.duration) -- Default frame duration
+
+app.command.NewFrame()
+assert(2 == app.activeFrame.frameNumber)
+assert(0.100 == app.activeFrame.duration) -- Default frame duration
