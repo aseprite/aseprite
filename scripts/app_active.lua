567115dbc36e5fae0061ace8fef5993256f6e444
David Capello <david@igarastudio.com>
2018-11-22 14:45:26 -0300

    Add some app.site tests

--- scripts/app_active.lua
+++ scripts/app_active.lua
@@ -5,10 +5,15 @@
 
 local s = Sprite(32, 64)
 assert(s == app.activeSprite)
+assert(s == app.site.sprite)
 assert(s == app.activeCel.sprite)
+assert(s == app.site.cel.sprite)
 assert(s == app.activeFrame.sprite)
+assert(s == app.site.frame.sprite)
 assert(1 == app.activeFrame.frameNumber)
+assert(1 == app.site.frame.frameNumber)
 assert(0.100 == app.activeFrame.duration) -- Default frame duration
+assert(0.100 == app.site.frame.duration)
 
 -- TODO fix these tests when there is no UI
 --app.command.NewFrame()
