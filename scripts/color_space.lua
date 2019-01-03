1baca9c85ec598c00663e0a14d7d848eb2acfadc
David Capello <david@igarastudio.com>
2019-01-03 15:32:45 -0300

    Add new ColorSpace{ sRGB }

--- scripts/color_space.lua
+++ scripts/color_space.lua
@@ -3,10 +3,18 @@
 -- This file is released under the terms of the MIT license.
 -- Read LICENSE.txt for more information.
 
-local spr = Sprite(32, 32)
-local cs = spr.spec.colorSpace
-assert(cs.name == "sRGB")    -- Default color profile
+local none = ColorSpace()         -- None
+local srgb = ColorSpace{ sRGB }
+assert(none ~= srgb)
 
-local spr2 = Sprite(32, 32)
+local spr = Sprite(32, 32)
+local cs1 = spr.colorSpace
 local cs2 = spr.spec.colorSpace
-assert(cs == cs2)
+assert(cs1.name == "sRGB")    -- Default color profile: sRGB
+assert(cs1 == cs2)
+assert(cs1 ~= none)
+assert(cs1 == srgb)
+
+local spr3 = Sprite(32, 32)
+local cs3 = spr.spec.colorSpace
+assert(cs1 == cs3)
