34834792e3aeb02845466e48f0decf2706495a68
David Capello <david@igarastudio.com>
2019-01-04 16:30:28 -0300

    Add support to call PaletteSize without UI

--- src/app/commands/cmd_palette_size.cpp
+++ src/app/commands/cmd_palette_size.cpp
@@ -1,4 +1,5 @@
 // Aseprite
+// Copyright (C) 2019  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -53,25 +54,35 @@ void PaletteSizeCommand::onExecute(Context* context)
   ContextReader reader(context);
   frame_t frame = reader.frame();
   Palette palette(*reader.sprite()->palette(frame));
+  int ncolors = (m_size != 0 ? m_size: palette.size());
 
-  app::gen::PaletteSize window;
-  window.colors()->setTextf("%d", palette.size());
-  window.openWindowInForeground();
-  if (window.closer() == window.ok()) {
-    int ncolors = window.colors()->textInt();
-    if (ncolors == palette.size())
+#ifdef ENABLE_UI
+  if (m_size == 0 && context->isUIAvailable()) {
+    app::gen::PaletteSize window;
+    window.colors()->setTextf("%d", ncolors);
+    window.openWindowInForeground();
+    if (window.closer() != window.ok())
       return;
 
-    palette.resize(MID(1, ncolors, std::numeric_limits<int>::max()));
-
-    ContextWriter writer(reader);
-    Tx tx(context, "Palette Size", ModifyDocument);
-    tx(new cmd::SetPalette(writer.sprite(), frame, &palette));
-    tx.commit();
-
-    set_current_palette(&palette, false);
-    ui::Manager::getDefault()->invalidate();
+    ncolors = window.colors()->textInt();
   }
+#endif
+
+  if (ncolors == palette.size())
+    return;
+
+  palette.resize(MID(1, ncolors, std::numeric_limits<int>::max()));
+
+  ContextWriter writer(reader);
+  Tx tx(context, "Palette Size", ModifyDocument);
+  tx(new cmd::SetPalette(writer.sprite(), frame, &palette));
+  tx.commit();
+
+  set_current_palette(&palette, false);
+#ifdef ENABLE_UI
+  if (context->isUIAvailable())
+    ui::Manager::getDefault()->invalidate();
+#endif
 }
 
 Command* CommandFactory::createPaletteSizeCommand()
