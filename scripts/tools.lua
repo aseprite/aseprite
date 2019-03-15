0df118e4054daf495fb8c79ee0422f41d600bb09
David Capello <david@igarastudio.com>
2019-03-15 17:04:19 -0300

    Add some app.drawWithTool() basic tests

---
+++ scripts/tools.lua
@@ -0,0 +1,20 @@
+-- Copyright (C) 2019  Igara Studio S.A.
+--
+-- This file is released under the terms of the MIT license.
+-- Read LICENSE.txt for more information.
+
+local spr = Sprite(4, 4)
+local cel = spr.cels[1]
+assert(cel.bounds == Rectangle(0, 0, 4, 4))
+
+app.drawWithTool{
+  tool='pencil',
+  color=Color{ r=0, g=0, b=0 },
+  points={ Point(2, 2),
+           Point(3, 2) }}
+assert(cel.bounds == Rectangle(2, 2, 2, 1))
+
+app.drawWithTool{
+  tool='eraser',
+  points={ Point(2, 2) }}
+assert(cel.bounds == Rectangle(3, 2, 1, 1))
