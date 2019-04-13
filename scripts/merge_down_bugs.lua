de9f8beed9b1baaf95626a7ff7aeac9257161376
David Capello <david@igarastudio.com>
2019-04-13 17:19:11 -0300

    Add a test for https://community.aseprite.org/t/2894

---
+++ scripts/merge_down_bugs.lua
@@ -0,0 +1,27 @@
+-- Copyright (C) 2019  Igara Studio S.A.
+--
+-- This file is released under the terms of the MIT license.
+-- Read LICENSE.txt for more information.
+
+local red = Color{ r=255, g=0, b=0 }
+local blue = Color{ r=0, g=0, b=255 }
+
+-- Reproduces the bug reported in https://community.aseprite.org/t/2894
+do
+  local s = Sprite(32, 32)
+  local a = s.layers[1]
+  app.useTool{ color=red, layer=a, points={ Point(2, 2) }}
+
+  local b = s:newLayer()
+  app.useTool{ color=blue, layer=b, points={ Point(1, 1) }}
+
+  a.isContinuous = true
+  b.isContinuous = true
+
+  app.command.NewFrame()
+  app.activeLayer = b
+  app.command.MergeDownLayer()
+
+  assert(#s.cels == 2)
+  assert(s.cels[1].image:isEqual(s.cels[2].image))
+end
