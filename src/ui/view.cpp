aseprite/aseprite

commit b4eb7a0ccda38b91bd8fd253e54718c3a18d81b6

Add support to continue drawing while we're scrolling
This change also avoids three scroll events when we zoom in/out, so
there are no two extra scroll events with invalid mouse position <->
editor position conversions.

src/ui/view.cpp
@@ -1,5 +1,5 @@
 // Aseprite UI Library
-// Copyright (C) 2018  Igara Studio S.A.
+// Copyright (C) 2018-2019  Igara Studio S.A.
 // Copyright (C) 2001-2017  David Capello
 //
 // This file is released under the terms of the MIT license.
@@ -103,7 +103,8 @@ Size View::getScrollableSize()
               m_scrollbar_v.size());
 }
 
-void View::setScrollableSize(const Size& sz)
+void View::setScrollableSize(const gfx::Size& sz,
+                             const bool setScrollPos)
 {
   gfx::Rect viewportArea = childrenBounds();
 
@@ -122,11 +123,13 @@ void View::setScrollableSize(const Size& sz)
     m_scrollbar_h.setSize(sz.w);
     m_scrollbar_v.setSize(sz.h);
   }
+  m_viewport.setBoundsQuietly(viewportArea);
 
   // Setup viewport
-  invalidate();
-  m_viewport.setBounds(viewportArea);
-  setViewScroll(viewScroll()); // Setup the same scroll-point
+  if (setScrollPos) {
+    invalidate();
+    setViewScroll(viewScroll()); // Setup the same scroll-point
+  }
 }
 
 Size View::visibleSize() const
@@ -146,27 +149,31 @@ void View::setViewScroll(const Point& pt)
   onSetViewScroll(pt);
 }
 
-void View::updateView()
+void View::updateView(const bool restoreScrollPos)
 {
   Widget* vw = UI_FIRST_WIDGET(m_viewport.children());
   Point scroll = viewScroll();
 
   // Set minimum (remove scroll-bars)
-  setScrollableSize(Size(0, 0));
+  setScrollableSize(Size(0, 0), false);
 
   // Set needed size
-  setScrollableSize(m_viewport.calculateNeededSize());
+  setScrollableSize(m_viewport.calculateNeededSize(), false);
 
   // If there are scroll-bars, we have to setup the scrollable-size
   // again (because they remove visible space, maybe now we need a
   // vertical or horizontal bar too).
   if (hasChild(&m_scrollbar_h) || hasChild(&m_scrollbar_v))
-    setScrollableSize(m_viewport.calculateNeededSize());
+    setScrollableSize(m_viewport.calculateNeededSize(), false);
 
-  if (vw)
-    setViewScroll(scroll);
-  else
-    setViewScroll(Point(0, 0));
+  m_viewport.setBounds(m_viewport.bounds());
+  invalidate();
+  if (restoreScrollPos) {
+    if (vw)
+      setViewScroll(scroll);
+    else
+      setViewScroll(Point(0, 0));
+  }
 }
 
 Viewport* View::viewport()

src/ui/view.h
@@ -1,4 +1,5 @@
 // Aseprite UI Library
+// Copyright (C) 2019  Igara Studio S.A.
 // Copyright (C) 2001-2017  David Capello
 //
 // This file is released under the terms of the MIT license.
@@ -42,14 +43,15 @@ namespace ui {
     // Returns the maximum viewable size requested by the attached
     // widget in the viewport.
     gfx::Size getScrollableSize();
-    void setScrollableSize(const gfx::Size& sz);
+    void setScrollableSize(const gfx::Size& sz,
+                           const bool setScrollPos = true);
 
     // Returns the visible/available size to see the attached widget.
     gfx::Size visibleSize() const override;
     gfx::Point viewScroll() const override;
     void setViewScroll(const gfx::Point& pt) override;
 
-    void updateView();
+    void updateView(const bool restoreScrollPos = true);
 
     Viewport* viewport();
     gfx::Rect viewportBounds();

src/app/ui/editor/drawing_state.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2018  Igara Studio S.A.
+// Copyright (C) 2018-2019  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -26,6 +26,7 @@
 #include "app/ui/keyboard_shortcuts.h"
 #include "app/ui/skin/skin_theme.h"
 #include "app/ui_context.h"
+#include "base/scoped_value.h"
 #include "doc/layer.h"
 #include "ui/message.h"
 #include "ui/system.h"
@@ -50,6 +51,7 @@ DrawingState::DrawingState(Editor* editor,
   , m_toolLoopManager(new tools::ToolLoopManager(toolLoop))
   , m_mouseMoveReceived(false)
   , m_mousePressedReceived(false)
+  , m_processScrollChange(true)
 {
   m_beforeCmdConn =
     UIContext::instance()->BeforeCommandExecution.connect(
@@ -78,6 +80,7 @@ void DrawingState::initToolLoop(Editor* editor,
 
   ASSERT(!m_toolLoopManager->isCanceled());
 
+  m_lastPointer = pointer;
   m_toolLoopManager->prepareLoop(pointer);
   m_toolLoopManager->pressButton(pointer);
 
@@ -89,6 +92,7 @@ void DrawingState::initToolLoop(Editor* editor,
 void DrawingState::sendMovementToToolLoop(const tools::Pointer& pointer)
 {
   ASSERT(m_toolLoopManager);
+  m_lastPointer = pointer;
   m_toolLoopManager->movement(pointer);
 }
 
@@ -107,6 +111,7 @@ bool DrawingState::onMouseDown(Editor* editor, MouseMessage* msg)
     editor->captureMouse();
 
   tools::Pointer pointer = pointer_from_msg(editor, msg);
+  m_lastPointer = pointer;
 
   // Check if this drawing state was started with a Shift+Pencil tool
   // and now the user pressed the right button to draw the straight
@@ -156,10 +161,12 @@ bool DrawingState::onMouseUp(Editor* editor, MouseMessage* msg)
       m_mouseMoveReceived ||
       (editor->getToolLoopModifiers() != tools::ToolLoopModifiers::kReplaceSelection &&
        editor->getToolLoopModifiers() != tools::ToolLoopModifiers::kIntersectSelection)) {
+    m_lastPointer = pointer_from_msg(editor, msg);
+
     // Notify the release of the mouse button to the tool loop
     // manager. This is the correct way to say "the user finishes the
     // drawing trace correctly".
-    if (m_toolLoopManager->releaseButton(pointer_from_msg(editor, msg)))
+    if (m_toolLoopManager->releaseButton(m_lastPointer))
       return true;
   }
 
@@ -184,24 +191,23 @@ bool DrawingState::onMouseUp(Editor* editor, MouseMessage* msg)
 
 bool DrawingState::onMouseMove(Editor* editor, MouseMessage* msg)
 {
-  ASSERT(m_toolLoopManager != NULL);
-
-  m_mouseMoveReceived = true;
-
   // It's needed to avoid some glitches with brush boundaries.
   //
   // TODO we should be able to avoid this if we correctly invalidate
   // the BrushPreview::m_clippingRegion
   HideBrushPreview hide(editor->brushPreview());
 
-  // Infinite scroll
-  gfx::Point mousePos = editor->autoScroll(msg, AutoScroll::MouseDir);
-  tools::Pointer pointer(editor->screenToEditor(mousePos),
-                         button_from_msg(msg));
+  // Don't process onScrollChange() messages if autoScroll() changes
+  // the scroll.
+  base::ScopedValue<bool> disableScroll(m_processScrollChange,
+                                        false, m_processScrollChange);
 
-  // Notify mouse movement to the tool
-  ASSERT(m_toolLoopManager != NULL);
-  m_toolLoopManager->movement(pointer);
+  // The autoScroll() function controls the "infinite scroll" when we
+  // touch the viewport borders.
+  gfx::Point mousePos = editor->autoScroll(msg, AutoScroll::MouseDir);
+  handleMouseMovement(
+    tools::Pointer(editor->screenToEditor(mousePos),
+                   button_from_msg(msg)));
 
   return true;
 }
@@ -253,6 +259,17 @@ bool DrawingState::onKeyUp(Editor* editor, KeyMessage* msg)
   return true;
 }
 
+bool DrawingState::onScrollChange(Editor* editor)
+{
+  if (m_processScrollChange) {
+    gfx::Point mousePos = ui::get_mouse_position();
+    handleMouseMovement(
+      tools::Pointer(editor->screenToEditor(mousePos),
+                     m_lastPointer.button()));
+  }
+  return true;
+}
+
 bool DrawingState::onUpdateStatusBar(Editor* editor)
 {
   // The status bar is updated by ToolLoopImpl::updateStatusBar()
@@ -264,6 +281,16 @@ void DrawingState::onExposeSpritePixels(const gfx::Region& rgn)
 {
   if (m_toolLoop)
     m_toolLoop->validateDstImage(rgn);
+}
+
+void DrawingState::handleMouseMovement(const tools::Pointer& pointer)
+{
+  m_mouseMoveReceived = true;
+  m_lastPointer = pointer;
+
+  // Notify mouse movement to the tool
+  ASSERT(m_toolLoopManager);
+  m_toolLoopManager->movement(pointer);
 }
 
 bool DrawingState::canExecuteCommands()

src/app/ui/editor/drawing_state.h
@@ -1,4 +1,5 @@
 // Aseprite
+// Copyright (C) 2019  Igara Studio S.A.
 // Copyright (C) 2001-2017  David Capello
 //
 // This program is distributed under the terms of
@@ -33,6 +34,7 @@ namespace app {
     virtual bool onSetCursor(Editor* editor, const gfx::Point& mouseScreenPos) override;
     virtual bool onKeyDown(Editor* editor, ui::KeyMessage* msg) override;
     virtual bool onKeyUp(Editor* editor, ui::KeyMessage* msg) override;
+    virtual bool onScrollChange(Editor* editor) override;
     virtual bool onUpdateStatusBar(Editor* editor) override;
     virtual void onExposeSpritePixels(const gfx::Region& rgn) override;
 
@@ -50,6 +52,7 @@ namespace app {
     void notifyToolLoopModifiersChange(Editor* editor);
 
   private:
+    void handleMouseMovement(const tools::Pointer& pointer);
     bool canExecuteCommands();
     void onBeforeCommandExecution(CommandExecutionEvent& cmd);
     void destroyLoopIfCanceled(Editor* editor);
@@ -69,16 +72,18 @@ namespace app {
     // release the mouse button in the same location).
     bool m_mouseMoveReceived;
 
-    // Stores the last drawing position before we start this
-    // DrawingState. It's used to restore the last drawing position in
-    // case this stroke is canceled.
-    gfx::Point m_lastPoint;
+    // Stores the last mouse pointer, used to re-use the latest mouse
+    // button when onScrollChange() event is received.
+    tools::Pointer m_lastPointer;
 
     // Used to know if the button was pressed after the DrawingState
     // was initialized. In this way we can cancel the ToolLoop if the
     // Shift press is used to draw a line, but then released without a
     // mouse click.
     bool m_mousePressedReceived;
+
+    // Locks the scroll
+    bool m_processScrollChange;
 
     obs::scoped_connection m_beforeCmdConn;
   };

src/app/ui/editor/editor.cpp
@@ -491,7 +491,7 @@ void Editor::setEditorZoom(const render::Zoom& zoom)
 
 void Editor::updateEditor()
 {
-  View::getView(this)->updateView();
+  View::getView(this)->updateView(false);
 }
 
 void Editor::drawOneSpriteUnclippedRect(ui::Graphics* g, const gfx::Rect& spriteRectToDraw, int dx, int dy)
@@ -2331,6 +2331,10 @@ void Editor::startFlipTransformation(doc::algorithm::FlipType flipType)
 void Editor::notifyScrollChanged()
 {
   m_observers.notifyScrollChanged(this);
+
+  ASSERT(m_state);
+  if (m_state)
+    m_state->onScrollChange(this);
 }
 
 void Editor::notifyZoomChanged()

src/app/ui/editor/editor_state.h
@@ -1,4 +1,5 @@
 // Aseprite
+// Copyright (C) 2019  Igara Studio S.A.
 // Copyright (C) 2001-2016  David Capello
 //
 // This program is distributed under the terms of
@@ -104,6 +105,9 @@ namespace app {
 
     // Called when a key is released.
     virtual bool onKeyUp(Editor* editor, ui::KeyMessage* msg) { return false; }
+
+    // Called when the editor scroll is changed.
+    virtual bool onScrollChange(Editor* editor) { return false; }
 
     // Called when status bar needs to be updated.
     virtual bool onUpdateStatusBar(Editor* editor) { return false; }

commit 951fb7c35784d3e5b0aba86e340aed9edbd2456d

Fix invalid scroll on ui::View after changing required size of attached widget (regressiong introduced in b4eb7a0ccda38b91bd8fd253e54718c3a18d81b6)

src/ui/view.cpp
@@ -129,8 +129,8 @@ void View::setScrollableSize(const gfx::Size& sz,
 
   // Setup viewport
   if (setScrollPos) {
-    invalidate();
     setViewScroll(viewScroll()); // Setup the same scroll-point
+    invalidate();
   }
 }
 
@@ -169,13 +169,24 @@ void View::updateView(const bool restoreScrollPos)
     setScrollableSize(m_viewport.calculateNeededSize(), false);
 
   m_viewport.setBounds(m_viewport.bounds());
-  invalidate();
-  if (restoreScrollPos) {
+  if (restoreScrollPos  ||
+      // Force restoring the old scroll position if we are out of
+      // bounds in the viewport limits.
+      scroll != limitScrollPosToViewport(scroll)) {
     if (vw)
       setViewScroll(scroll);
     else
       setViewScroll(Point(0, 0));
   }
+  ASSERT(viewScroll() == limitScrollPosToViewport(viewScroll()));
+
+  if (Widget* child = attachedWidget()) {
+    updateAttachedWidgetBounds(viewScroll());
+    ASSERT(child->bounds().w >= viewportBounds().w);
+    ASSERT(child->bounds().h >= viewportBounds().h);
+  }
+
+  invalidate();
 }
 
 Viewport* View::viewport()
@@ -253,10 +264,6 @@ void View::onSetViewScroll(const gfx::Point& pt)
   if (newScroll == oldScroll)
     return;
 
-  // This is the movement for the scrolled region (which is inverse to
-  // the scroll position delta/movement).
-  Point delta = oldScroll - newScroll;
-
   // Visible viewport region that is not overlapped by windows
   Region drawableRegion;
   m_viewport.getDrawableRegion(
@@ -297,19 +304,8 @@ void View::onSetViewScroll(const gfx::Point& pt)
     onScrollRegion(ev);
   }
 
-  // Move viewport children
-  cpos.offset(-newScroll);
-  for (auto child : m_viewport.children()) {
-    Size reqSize = child->sizeHint();
-    cpos.w = MAX(reqSize.w, cpos.w);
-    cpos.h = MAX(reqSize.h, cpos.h);
-    if (cpos.w != child->bounds().w ||
-        cpos.h != child->bounds().h)
-      child->setBounds(cpos);
-    else
-      child->offsetWidgets(cpos.x - child->bounds().x,
-                           cpos.y - child->bounds().y);
-  }
+  // Move attached widget
+  updateAttachedWidgetBounds(newScroll);
 
   // Change scroll bar positions
   m_scrollbar_h.setPos(newScroll.x);
@@ -319,7 +315,10 @@ void View::onSetViewScroll(const gfx::Point& pt)
   Region invalidRegion(cpos);
   invalidRegion &= drawableRegion;
 
-  // Move the valid screen region.
+  // Move the valid screen region. "delta" is the movement for the
+  // scrolled region (which is inverse to the scroll position
+  // delta/movement).
+  const Point delta = oldScroll - newScroll;
   {
     // The movable region includes the given "validRegion"
     // intersecting itself when it's in the new position, so we don't
@@ -370,6 +369,23 @@ void View::onScrollRegion(ScrollRegionEvent& ev)
 void View::onScrollChange()
 {
   // Do nothing
+}
+
+void View::updateAttachedWidgetBounds(const gfx::Point& scrollPos)
+{
+  Rect cpos = m_viewport.childrenBounds();
+  cpos.offset(-scrollPos);
+  for (auto child : m_viewport.children()) {
+    Size reqSize = child->sizeHint();
+    cpos.w = MAX(reqSize.w, cpos.w);
+    cpos.h = MAX(reqSize.h, cpos.h);
+    if (cpos.w != child->bounds().w ||
+        cpos.h != child->bounds().h)
+      child->setBounds(cpos);
+    else
+      child->offsetWidgets(cpos.x - child->bounds().x,
+                           cpos.y - child->bounds().y);
+  }
 }
 
 gfx::Point View::limitScrollPosToViewport(const gfx::Point& pt) const

src/ui/view.h
@@ -71,6 +71,7 @@ namespace ui {
     virtual void onScrollChange();
 
   private:
+    void updateAttachedWidgetBounds(const gfx::Point& scrollPos);
     gfx::Point limitScrollPosToViewport(const gfx::Point& pt) const;
 
     bool m_hasBars;

src/ui/widget.cpp
@@ -1570,6 +1570,9 @@ double Widget::onGetTextDouble() const
 
 void Widget::offsetWidgets(int dx, int dy)
 {
+  if (dx == 0 && dy == 0)
+    return;
+
   m_updateRegion.offset(dx, dy);
   m_bounds.offset(dx, dy);
 