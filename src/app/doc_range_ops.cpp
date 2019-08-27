fe0563664db3cc7f41731c784de97bf7560c7a57
David Capello <david@igarastudio.com>
2019-08-27 20:01:23 -0300

    Remove unused #include

--- src/app/doc_range_ops.cpp
+++ src/app/doc_range_ops.cpp
@@ -1,4 +1,5 @@
 // Aseprite
+// Copyright (C) 2019  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -18,7 +19,6 @@
 #include "app/doc_api.h"
 #include "app/doc_range.h"
 #include "app/transaction.h"
-#include "app/ui/timeline/timeline.h"
 #include "doc/layer.h"
 #include "doc/sprite.h"
 
