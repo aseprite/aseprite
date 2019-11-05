aseprite/aseprite

commit 536a4c5d3aedccb4f24c5a5e28324ddad7638e18

Simplify Export Sprite Sheet with preview + changes to the UI
These changes include an option to split layers and tags
by rows (fix #1118)

src/ui/button.cpp
@@ -1,4 +1,5 @@
 // Aseprite UI Library
+// Copyright (C) 2019  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This file is released under the terms of the MIT license.
@@ -137,12 +138,23 @@ bool ButtonBase::onProcessMessage(Message* msg)
     }
 
     case kKeyUpMessage:
-      if (isEnabled()) {
-        if (m_behaviorType == kButtonWidget) {
-          if (isSelected()) {
-            generateButtonSelectSignal();
+      if (isEnabled() && hasFocus()) {
+        switch (m_behaviorType) {
+
+          case kButtonWidget:
+            if (isSelected()) {
+              generateButtonSelectSignal();
+              return true;
+            }
+            break;
+
+          case kCheckWidget: {
+            // Fire onClick() event
+            Event ev(this);
+            onClick(ev);
             return true;
           }
+
         }
       }
       break;

data/pref.xml
@@ -309,6 +309,7 @@
     <section id="sprite_sheet">
       <option id="show_overwrite_files_alert" type="bool" default="true" />
       <option id="default_extension" type="std::string" default="&quot;png&quot;" />
+      <option id="preview" type="bool" default="true" />
     </section>
     <section id="gif">
       <option id="show_alert" type="bool" default="true" />
@@ -447,13 +448,14 @@
       <option id="rows" type="int" default="0" />
       <option id="width" type="int" default="0" />
       <option id="height" type="int" default="0" />
-      <option id="best_fit" type="bool" default="false" />
       <option id="texture_filename" type="std::string" />
       <option id="data_filename" type="std::string" />
       <option id="data_format" type="SpriteSheetDataFormat" default="SpriteSheetDataFormat::Default" />
+      <option id="filename_format" type="std::string" />
       <option id="border_padding" type="int" default="0" />
       <option id="shape_padding" type="int" default="0" />
       <option id="inner_padding" type="int" default="0" />
+      <option id="trim_sprite" type="bool" default="false" />
       <option id="trim" type="bool" default="false" />
       <option id="trim_by_grid" type="bool" default="false" />
       <option id="extrude" type="bool" default="false" />
@@ -461,6 +463,7 @@
       <option id="layer" type="std::string" />
       <option id="frame_tag" type="std::string" />
       <option id="split_layers" type="bool" default="false" />
+      <option id="split_tags" type="bool" default="false" />
       <option id="list_layers" type="bool" default="true" />
       <option id="list_frame_tags" type="bool" default="true" />
       <option id="list_slices" type="bool" default="true" />

data/strings/en.ini
@@ -534,25 +534,56 @@ cancel = &Cancel
 
 [export_sprite_sheet]
 title = Export Sprite Sheet
+format = Format:
 sheet_type = Sheet Type:
-columns = # of Columns:
-rows = # of Rows:
-padding = Padding
-border = Border:
-shape = Shape:
-inner = Inner:
-trim = Trim
-trim_by_grid = by Grid
-extrude = Extrude
-width = Width:
-height = Height:
-best_fit = Best fit for texture
-layers = Layers:
-split_layers = Split
-split_layers_tooltip = <<<END
-Generates one sprite for each layer (and for each frame).
+sheet_type_tooltip = <<<END
+Indicates a specific way to layout sprites in the sprite sheet:
+* Horizontal: Each frame side by side.
+* Vertical: Each frame one below the other.
+* By Rows: Create one row for each layer or tag.
+* By Columns: Create one column for each layer or tag.
+* Packed: Try to fit all frames in the best possible way.
 END
+type_horz = Horizontal Strip
+type_vert = Vertical Strip
+type_rows = By Rows
+type_cols = By Columns
+type_pack = Packed
+constraints = Constraints
+constraints_tooltip = <<<END
+Special constraints for the sprite sheet.
+E.g. Fixed number of rows/columns or fixed
+number of pixels (width/height).
+END
+constraint_fixed_none = None
+constraint_fixed_cols = Fixed # of Columns
+constraint_fixed_rows = Fixed # of Rows
+constraint_fixed_width = Fixed Width
+constraint_fixed_height = Fixed Height
+constraint_fixed_size = Fixed Size
+padding = Padding
+padding_tooltip = Extra space to add between sprites
+border = Border:
+border_tooltip = Space between each frame and the edge of the sprite sheet
+shape = Spacing:
+shape_tooltip = Space between each frame in the sprite sheet
+inner = Inner:
+inner_tooltip = Extra space inside frame edges
+trim_sprite = Trim Sprite
+trim_sprite_tooltip = Trims the whole sprite before its frames are included in the sprite sheet
+trim = Trim Cels
+trim_tooltip = Trims each frame separately
+trim_by_grid = By Grid
+trim_by_grid_tooltip = Trims by grid boundaries instead of pixel by pixel
+extrude = Extrude
+extrude_tooltip = Adds a border to each frame duplicating the pixels of its edges
+layers = Layers:
+split_layers = Split Layers
+split_layers_tooltip = Generates one sprite for each layer
+split_tags = Split Tags
+split_tags_tooltip = Generates one sprite for each tag
 frames = Frames:
+output = Output:
 output_file = Output File
 json_data = JSON Data
 json_data_hash = Hash
@@ -561,9 +592,18 @@ meta = Meta:
 meta_layers = Layers
 meta_tags = Tags
 meta_slices = Slices
-open_sprite_sheet = Open generated sprite sheet
+data_filename_format = Item Filename:
+data_filename_format_tooltip = <<<END
+Each frame in the JSON data will contain a filename
+(string that identifies them), this field describes
+how to build each frame. You can use special marks
+like {layer}, {frame}, {tag}, {tagframe}, etc.
+END
+preview = Preview
+open_sprite_sheet = Open Generated Sprite Sheet
 export = &Export
 cancel = &Cancel
+generating = Generating...
 
 [file_selector]
 go_back_button_tooltip = Go back one folder

data/widgets/export_sprite_sheet.xml
@@ -3,51 +3,61 @@
 <!-- Copyright (C) 2001-2018  David Capello -->
 <gui>
   <window id="export_sprite_sheet" text="@.title">
-  <grid columns="4">
+  <grid columns="4" expansive="true">
+    <separator horizontal="true" text="@.format" cell_hspan="4" />
+
     <label text="@.sheet_type" />
-    <combobox id="sheet_type" cell_hspan="3" />
+    <combobox id="sheet_type" expansive="true" cell_hspan="2" cell_align="horizontal"
+	      tooltip="@.sheet_type_tooltip" />
+    <check id="show_constraints" text="@.constraints"
+	   tooltip="@.constraints_tooltip" />
 
-    <label id="columns_label" text="@.columns" />
-    <expr id="columns" text="" />
-    <boxfiller cell_hspan="2" />
-
-    <label id="rows_label" text="@.rows" />
-    <expr id="rows" text="" />
-    <boxfiller cell_hspan="2" />
-
-    <hbox />
-    <vbox>
-      <check id="padding_enabled" text="@.padding" />
-      <check id="trim_enabled" text="@.trim" />
-      <hbox id="trim_container">
-        <boxfiller />
-        <check id="grid_trim_enabled" text="@.trim_by_grid" />
-      </hbox>
-      <check id="extrude_enabled" text="@.extrude" />
-    </vbox>
-    <grid columns="2" id="padding_container" cell_hspan="2">
-      <label text="@.border" />
-      <expr id="border_padding" text="0" />
-      <label text="@.shape" />
-      <expr id="shape_padding" text="0" />
-      <label text="@.inner" />
-      <expr id="inner_padding" text="0" />
-    </grid>
-
-    <label id="fit_width_label" text="@.width" />
-    <combobox id="fit_width" text="" maxsize="5" editable="true" />
-    <label id="fit_height_label" text="@.height" />
-    <combobox id="fit_height" text="" maxsize="5" editable="true" />
-
-    <hbox id="best_fit_filler" />
-    <check cell_hspan="3" id="best_fit" text="@.best_fit" />
+    <label id="constraints_label" text="@.constraints" />
+    <hbox id="constraints_placeholder" cell_hspan="3">
+      <combobox id="constraint_type" />
+      <expr id="width_constraint" />
+      <expr id="height_constraint" />
+    </hbox>
 
     <label text="@.layers" />
     <combobox id="layers" text="" cell_hspan="2" />
     <check id="split_layers" text="@.split_layers" tooltip="@.split_layers_tooltip" />
 
     <label text="@.frames" />
-    <combobox id="frames" text="" cell_hspan="3" />
+    <combobox id="frames" text="" cell_hspan="2" />
+    <check id="split_tags" text="@.split_tags" tooltip="@.split_tags_tooltip" />
+
+    <hbox />
+    <hbox cell_hspan="3">
+      <vbox>
+	<check id="trim_sprite_enabled" text="@.trim_sprite" tooltip="@.trim_sprite_tooltip" />
+	<check id="trim_enabled" text="@.trim" tooltip="@.trim_tooltip" />
+	<vbox id="trim_container" cell_hspan="2">
+	  <check id="grid_trim_enabled"
+		 text="@.trim_by_grid"
+		 tooltip="@.trim_by_grid_tooltip" />
+	</vbox>
+      </vbox>
+      <separator vertical="true" />
+      <vbox>
+	<check id="padding_enabled" text="@.padding" tooltip="@.padding_tooltip" />
+	<check id="extrude_enabled"
+	       text="@.extrude"
+	       tooltip="@.extrude_tooltip" />
+      </vbox>
+      <vbox>
+	<grid columns="2" id="padding_container">
+          <label text="@.border" />
+          <expr id="border_padding" text="0" tooltip="@.border_tooltip" />
+          <label text="@.shape" />
+          <expr id="shape_padding" text="0" tooltip="@.shape_tooltip" />
+          <label text="@.inner" />
+          <expr id="inner_padding" text="0" tooltip="@.inner_tooltip" />
+	</grid>
+      </vbox>
+    </hbox>
+
+    <separator horizontal="true" text="@.output" cell_hspan="4" cell_align="horizontal" />
 
     <check id="image_enabled" text="@.output_file" />
     <button id="image_filename" cell_hspan="3" />
@@ -56,7 +66,7 @@
     <button id="data_filename" cell_hspan="3" />
 
     <hbox />
-    <hbox id="data_meta" cell_hspan="3">
+    <hbox id="data_meta" cell_hspan="3" cell_align="horizontal">
       <combobox id="data_format">
         <listitem text="@.json_data_hash" value="0" />
         <listitem text="@.json_data_array" value="1" />
@@ -67,9 +77,21 @@
       <check id="list_slices" text="@.meta_slices" />
     </hbox>
 
-    <check id="open_generated" text="@.open_sprite_sheet" cell_hspan="4" />
+    <hbox />
+    <hbox id="data_filename_format_placeholder" cell_hspan="3" cell_align="horizontal">
+      <label text="@.data_filename_format" />
+      <entry id="data_filename_format" maxsize="1024" maxwidth="256" expansive="true"
+             tooltip="@.data_filename_format_tooltip" />
+      <link text="(?)" url="https://www.aseprite.org/docs/cli/#filename-format" />
+    </hbox>
+
+    <separator horizontal="true" cell_hspan="4" />
 
     <hbox cell_hspan="4">
+      <hbox>
+	<check id="open_generated" text="@.open_sprite_sheet" cell_hspan="3" />
+        <check id="preview" text="@.preview" cell_hspan="1" />
+      </hbox>
       <boxfiller />
       <hbox homogeneous="true">
         <button text="@.export" minwidth="60" id="export_button" magnet="true" />

src/app/cli/app_options.cpp
@@ -55,6 +55,7 @@ AppOptions::AppOptions(int argc, const char* argv[])
   , m_tag(m_po.add("tag").alias("frame-tag").requiresValue("<name>").description("Include tagged frames in the sheet"))
   , m_frameRange(m_po.add("frame-range").requiresValue("from,to").description("Only export frames in the [from,to] range"))
   , m_ignoreEmpty(m_po.add("ignore-empty").description("Do not export empty frames/cels"))
+  , m_mergeDuplicates(m_po.add("merge-duplicates").description("Merge all duplicate frames into one in the sprite sheet"))
   , m_borderPadding(m_po.add("border-padding").requiresValue("<value>").description("Add padding on the texture borders"))
   , m_shapePadding(m_po.add("shape-padding").requiresValue("<value>").description("Add padding between frames"))
   , m_innerPadding(m_po.add("inner-padding").requiresValue("<value>").description("Add padding inside each frame"))

src/app/cli/app_options.h
@@ -69,6 +69,7 @@ public:
   const Option& tag() const { return m_tag; }
   const Option& frameRange() const { return m_frameRange; }
   const Option& ignoreEmpty() const { return m_ignoreEmpty; }
+  const Option& mergeDuplicates() const { return m_mergeDuplicates; }
   const Option& borderPadding() const { return m_borderPadding; }
   const Option& shapePadding() const { return m_shapePadding; }
   const Option& innerPadding() const { return m_innerPadding; }
@@ -129,6 +130,7 @@ private:
   Option& m_tag;
   Option& m_frameRange;
   Option& m_ignoreEmpty;
+  Option& m_mergeDuplicates;
   Option& m_borderPadding;
   Option& m_shapePadding;
   Option& m_innerPadding;

src/app/cli/cli_processor.cpp
@@ -305,6 +305,11 @@ void CliProcessor::process(Context* ctx)
           if (m_exporter)
             m_exporter->setIgnoreEmptyCels(true);
         }
+        // --merge-duplicates
+        else if (opt == &m_options.mergeDuplicates()) {
+          if (m_exporter)
+            m_exporter->setMergeDuplicates(true);
+        }
         // --border-padding
         else if (opt == &m_options.borderPadding()) {
           if (m_exporter)
@@ -625,37 +630,16 @@ bool CliProcessor::openFile(Context* ctx, CliOpenFile& cof)
         }
       }
 
-      if (cof.hasLayersFilter()) {
-        SelectedLayers filteredLayers;
+      SelectedLayers filteredLayers;
+      if (cof.hasLayersFilter())
         filterLayers(doc->sprite(), cof, filteredLayers);
 
-        if (cof.splitLayers) {
-          for (Layer* layer : filteredLayers.toAllLayersList()) {
-            SelectedLayers oneLayer;
-            oneLayer.insert(layer);
-
-            m_exporter->addDocument(doc, tag, &oneLayer,
-                                    (!selFrames.empty() ? &selFrames: nullptr));
-          }
-        }
-        else {
-          m_exporter->addDocument(doc, tag, &filteredLayers,
-                                  (!selFrames.empty() ? &selFrames: nullptr));
-        }
-      }
-      else if (cof.splitLayers) {
-        for (auto layer : doc->sprite()->allVisibleLayers()) {
-          SelectedLayers oneLayer;
-          oneLayer.insert(layer);
-
-          m_exporter->addDocument(doc, tag, &oneLayer,
-                                  (!selFrames.empty() ? &selFrames: nullptr));
-        }
-      }
-      else {
-        m_exporter->addDocument(doc, tag, nullptr,
-                                (!selFrames.empty() ? &selFrames: nullptr));
-      }
+      m_exporter->addDocumentSamples(
+        doc, tag,
+        cof.splitLayers,
+        cof.splitTags,
+        (cof.hasLayersFilter() ? &filteredLayers: nullptr),
+        (!selFrames.empty() ? &selFrames: nullptr));
     }
   }
 

src/app/cli/default_cli_delegate.cpp
@@ -121,7 +121,9 @@ void DefaultCliDelegate::exportFiles(Context* ctx, DocExporter& exporter)
 {
   LOG("APP: Exporting sheet...\n");
 
-  std::unique_ptr<Doc> spriteSheet(exporter.exportSheet(ctx));
+  base::task_token token;
+  std::unique_ptr<Doc> spriteSheet(
+    exporter.exportSheet(ctx, token));
 
   // Sprite sheet isn't used, we just delete it.
 

commit 5ac07c082308f4333498120b4362a9e721aa5c8c

Fix pressing buttons with Alt+mnemonic or Cmd+mnemonic (regression introduced in 536a4c5d3aedccb4f24c5a5e28324ddad7638e18)

src/ui/button.cpp
@@ -96,15 +96,12 @@ bool ButtonBase::onProcessMessage(Message* msg)
             }
           }
 
-          // Check if the user pressed mnemonic.
-          if (mnemonicPressed) {
-            setSelected(true);
-            return true;
-          }
-          // Magnetic widget catches ENTERs
-          else if (isFocusMagnet() &&
-                   ((scancode == kKeyEnter) ||
-                    (scancode == kKeyEnterPad))) {
+          if (// Check if the user pressed mnemonic
+              mnemonicPressed ||
+              // Magnetic widget catches ENTERs
+              (isFocusMagnet() &&
+               ((scancode == kKeyEnter) ||
+                (scancode == kKeyEnterPad)))) {
             manager()->setFocus(this);
 
             // Dispatch focus movement messages (because the buttons
@@ -117,8 +114,8 @@ bool ButtonBase::onProcessMessage(Message* msg)
         }
         // For kCheckWidget or kRadioWidget
         else {
-          /* if the widget has the focus and the user press space or
-             if the user press Alt+the underscored letter of the button */
+          // If the widget has the focus and the user press space or
+          // if the user press Alt+the underscored letter of the button
           if ((hasFocus() && (scancode == kKeySpace)) || mnemonicPressed) {
             if (m_behaviorType == kCheckWidget) {
               // Swap the select status