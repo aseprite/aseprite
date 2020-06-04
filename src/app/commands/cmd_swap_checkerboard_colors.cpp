1068926f7b7df41a5204a7149b81fbb55717d6ad
David Capello <david@igarastudio.com>
2020-06-04 16:58:30 -0300

    Reformat SwapCheckerboardColorsCommand file

--- src/app/commands/cmd_swap_checkerboard_colors.cpp
+++ src/app/commands/cmd_swap_checkerboard_colors.cpp
@@ -17,37 +17,37 @@
 
 namespace app {
 
-    class SwapCheckerboardColorsCommand : public Command {
-    public:
-        SwapCheckerboardColorsCommand();
+class SwapCheckerboardColorsCommand : public Command {
+public:
+  SwapCheckerboardColorsCommand();
 
-    protected:
-        bool onEnabled(Context* context) override;
-        void onExecute(Context* context) override;
-    };
+protected:
+  bool onEnabled(Context* context) override;
+  void onExecute(Context* context) override;
+};
 
-    SwapCheckerboardColorsCommand::SwapCheckerboardColorsCommand()
-        : Command(CommandId::SwapCheckerboardColors(), CmdUIOnlyFlag)
-    {
-    }
+SwapCheckerboardColorsCommand::SwapCheckerboardColorsCommand()
+  : Command(CommandId::SwapCheckerboardColors(), CmdUIOnlyFlag)
+{
+}
 
-    bool SwapCheckerboardColorsCommand::onEnabled(Context* context)
-    {
-        return true;
-    }
+bool SwapCheckerboardColorsCommand::onEnabled(Context* context)
+{
+  return true;
+}
 
-    void SwapCheckerboardColorsCommand::onExecute(Context* context)
-    {
-        DocumentPreferences& docPref = Preferences::instance().document(context->activeDocument());
-        app::Color c1 = docPref.bg.color1();
-        app::Color c2 = docPref.bg.color2();
+void SwapCheckerboardColorsCommand::onExecute(Context* context)
+{
+  DocumentPreferences& docPref = Preferences::instance().document(context->activeDocument());
+  app::Color c1 = docPref.bg.color1();
+  app::Color c2 = docPref.bg.color2();
 
-        docPref.bg.color1(c2);
-        docPref.bg.color2(c1);
-    }
-    Command* CommandFactory::createSwapCheckerboardColorsCommand()
-    {
-        return new SwapCheckerboardColorsCommand;
-    }
+  docPref.bg.color1(c2);
+  docPref.bg.color2(c1);
+}
+Command* CommandFactory::createSwapCheckerboardColorsCommand()
+{
+  return new SwapCheckerboardColorsCommand;
+}
 
 } // namespace app
