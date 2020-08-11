d701ef9ffd07fc2104c833ff2b553c7288e5e9a3
Gaspar Capello <gaspar@igarastudio.com>
2020-08-11 10:19:37 -0300

    Fix replace current pallete does not create a palette (fix #2480)

--- src/app/commands/cmd_color_quantization.cpp
+++ src/app/commands/cmd_color_quantization.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2019 Igara Studio S.A.
+// Copyright (C) 2019-2020 Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -114,8 +114,10 @@ void ColorQuantizationCommand::onExecute(Context* ctx)
     }
     else {
       createPal = false;
-      if (window.currentPalette()->isSelected())
+      if (window.currentPalette()->isSelected()) {
+        entries = PalettePicks(site.palette()->size());
         entries.all();
+      }
     }
   }
   else
