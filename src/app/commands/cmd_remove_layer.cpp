2de9a67ebcbfa9c53c554ee3b4c6dcf1bc05b2ff
Gaspar Capello <gaspar@igarastudio.com>
2021-01-15 16:40:48 -0300

    Fix deleting last nested group causes crash

--- src/app/commands/cmd_remove_layer.cpp
+++ src/app/commands/cmd_remove_layer.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2020  Igara Studio S.A.
+// Copyright (C) 2020-2021  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -78,7 +78,7 @@ void RemoveLayerCommand::onExecute(Context* context)
       }
     }
     else {
-      if (sprite->allLayersCount() == 1) {
+      if (sprite->root()->layersCount() == 1) {
         ui::Alert::show(Strings::alerts_cannot_delete_all_layers());
         return;
       }
