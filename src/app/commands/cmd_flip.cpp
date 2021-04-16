aseprite/aseprite

commit f3ab779bfded7710934cff6b9cc6b596756eccbb

Don't transform hidden layers (fix #2680)

src/app/commands/cmd_flip.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2019-2020  Igara Studio S.A.
+// Copyright (C) 2019-2021  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -96,8 +96,8 @@ void FlipCommand::onExecute(Context* ctx)
       cels = get_unlocked_unique_cels(site.sprite(), range);
     }
     else if (site.cel() &&
-              site.layer() &&
-              site.layer()->isEditable()) {
+             site.layer() &&
+             site.layer()->canEditPixels()) {
       cels.push_back(site.cel());
     }
 

src/app/commands/cmd_rotate.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2019-2020  Igara Studio S.A.
+// Copyright (C) 2019-2021  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -217,7 +217,7 @@ void RotateCommand::onExecute(Context* context)
         cels = get_unlocked_unique_cels(site.sprite(), range);
       else if (site.cel() &&
                site.layer() &&
-               site.layer()->isEditable()) {
+               site.layer()->canEditPixels()) {
         cels.push_back(site.cel());
       }
 

src/app/commands/filters/filter_manager_impl.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2019  Igara Studio S.A.
+// Copyright (C) 2019-2021  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -263,8 +263,7 @@ void FilterManagerImpl::applyToTarget()
       }
       else if (m_site.cel() &&
                m_site.layer() &&
-               m_site.layer()->isEditable() &&
-               !m_site.layer()->isReference()) {
+               m_site.layer()->canEditPixels()) {
         cels.push_back(m_site.cel());
       }
       break;
@@ -272,8 +271,7 @@ void FilterManagerImpl::applyToTarget()
 
     case CelsTarget::All: {
       for (Cel* cel : m_site.sprite()->uniqueCels()) {
-        if (cel->layer()->isEditable() &&
-            !cel->layer()->isReference())
+        if (cel->layer()->canEditPixels())
           cels.push_back(cel);
       }
       break;

src/app/ui/editor/pixels_movement.cpp
@@ -999,15 +999,17 @@ CelList PixelsMovement::getEditableCels()
     // TODO This case is used in paste too, where the cel() can be
     //      nullptr (e.g. we paste the clipboard image into an empty
     //      cel).
-    if (m_site.layer() && m_site.layer()->isEditableHierarchy())
+    if (m_site.layer() &&
+        m_site.layer()->canEditPixels()) {
       cels.push_back(m_site.cel());
+    }
     return cels;
   }
 
   // Current cel (m_site.cel()) can be nullptr when we paste in an
   // empty cel (Ctrl+V) and cut (Ctrl+X) the floating pixels.
   if (m_site.cel() &&
-      m_site.cel()->layer()->isEditableHierarchy()) {
+      m_site.cel()->layer()->canEditPixels()) {
     CelList::iterator it;
 
     // If we are in a linked cel, remove the cel that matches the

src/app/util/range_utils.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2020  Igara Studio S.A.
+// Copyright (C) 2020-2021  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -25,6 +25,7 @@ namespace app {
 using namespace doc;
 
 // TODO the DocRange should be "iteratable" to replace this function
+//      or we can wait to C++20 coroutines and co_yield
 static CelList get_cels_templ(const Sprite* sprite,
                               DocRange range,
                               const bool onlyUniqueCels,
@@ -39,8 +40,9 @@ static CelList get_cels_templ(const Sprite* sprite,
   for (Layer* layer : range.selectedLayers()) {
     if (!layer ||
         !layer->isImage() ||
-        (onlyUnlockedCel && !layer->isEditableHierarchy()))
+        (onlyUnlockedCel && !layer->canEditPixels())) {
       continue;
+    }
 
     LayerImage* layerImage = static_cast<LayerImage*>(layer);
     for (frame_t frame : range.selectedFrames()) {

src/doc/layer.cpp
@@ -1,5 +1,5 @@
 // Aseprite Document Library
-// Copyright (C) 2020  Igara Studio S.A.
+// Copyright (C) 2020-2021  Igara Studio S.A.
 // Copyright (C) 2001-2018 David Capello
 //
 // This file is released under the terms of the MIT license.
@@ -172,6 +172,24 @@ bool Layer::isEditableHierarchy() const
   while (layer) {
     if (!layer->isEditable())
       return false;
+    layer = layer->parent();
+  }
+  return true;
+}
+
+// It's like isVisibleHierarchy + isEditableHierarchy. Returns true if
+// the whole layer hierarchy is unlocked and visible, so the user can
+// edit its pixels without unexpected side-effects (e.g. editing
+// hidden layers).
+bool Layer::canEditPixels() const
+{
+  const Layer* layer = this;
+  while (layer) {
+    if (!layer->isVisible() ||
+        !layer->isEditable() ||
+        layer->isReference()) { // Cannot edit pixels from reference layers
+      return false;
+    }
     layer = layer->parent();
   }
   return true;

src/doc/layer.h
@@ -1,5 +1,5 @@
 // Aseprite Document Library
-// Copyright (C) 2020  Igara Studio S.A.
+// Copyright (C) 2020-2021  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This file is released under the terms of the MIT license.
@@ -89,6 +89,7 @@ namespace doc {
 
     bool isVisibleHierarchy() const;
     bool isEditableHierarchy() const;
+    bool canEditPixels() const;
     bool hasAncestor(const Layer* ancestor) const;
 
     void setBackground(bool state) { switchFlags(LayerFlags::Background, state); }

commit 4fe8c93c5c75646033ee0ab474d9a0900d6df885

Fix regression cannot move/resize reference layers
Regression introduced in f3ab779bfded7710934cff6b9cc6b596756eccbb

src/app/commands/cmd_flip.cpp
@@ -93,7 +93,7 @@ void FlipCommand::onExecute(Context* ctx)
 
     auto range = site.range();
     if (range.enabled()) {
-      cels = get_unlocked_unique_cels(site.sprite(), range);
+      cels = get_unique_cels_to_edit_pixels(site.sprite(), range);
     }
     else if (site.cel() &&
              site.layer() &&

src/app/commands/cmd_rotate.cpp
@@ -214,7 +214,7 @@ void RotateCommand::onExecute(Context* context)
 
       auto range = App::instance()->timeline()->range();
       if (range.enabled())
-        cels = get_unlocked_unique_cels(site.sprite(), range);
+        cels = get_unique_cels_to_edit_pixels(site.sprite(), range);
       else if (site.cel() &&
                site.layer() &&
                site.layer()->canEditPixels()) {

src/app/commands/filters/filter_manager_impl.cpp
@@ -256,10 +256,8 @@ void FilterManagerImpl::applyToTarget()
     case CelsTarget::Selected: {
       auto range = m_site.range();
       if (range.enabled()) {
-        for (Cel* cel : get_unlocked_unique_cels(m_site.sprite(), range)) {
-          if (!cel->layer()->isReference())
-            cels.push_back(cel);
-        }
+        for (Cel* cel : get_unique_cels_to_edit_pixels(m_site.sprite(), range))
+          cels.push_back(cel);
       }
       else if (m_site.cel() &&
                m_site.layer() &&

src/app/ui/doc_view.cpp
@@ -562,7 +562,7 @@ bool DocView::onClear(Context* ctx)
 
   CelList cels;
   if (site.range().enabled()) {
-    cels = get_unlocked_unique_cels(site.sprite(), site.range());
+    cels = get_unique_cels_to_edit_pixels(site.sprite(), site.range());
   }
   else if (site.cel()) {
     cels.push_back(site.cel());

src/app/ui/editor/moving_cel_state.cpp
@@ -70,7 +70,7 @@ MovingCelCollect::MovingCelCollect(Editor* editor, Layer* layer)
   }
 
   // Record start positions of all cels in selected range
-  for (Cel* cel : get_unlocked_unique_cels(editor->sprite(), range2)) {
+  for (Cel* cel : get_unique_cels_to_move_cel(editor->sprite(), range2)) {
     Layer* layer = cel->layer();
     ASSERT(layer);
 

src/app/ui/editor/pixels_movement.cpp
@@ -992,8 +992,8 @@ CelList PixelsMovement::getEditableCels()
   CelList cels;
 
   if (editMultipleCels()) {
-    cels = get_unlocked_unique_cels(
-      m_site.sprite(), m_site.range());
+    cels = get_unique_cels_to_edit_pixels(m_site.sprite(),
+                                          m_site.range());
   }
   else {
     // TODO This case is used in paste too, where the cel() can be

src/app/util/clipboard.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2019-2020  Igara Studio S.A.
+// Copyright (C) 2019-2021  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -283,7 +283,7 @@ void cut(ContextWriter& writer)
       Site site = writer.context()->activeSite();
       CelList cels;
       if (site.range().enabled()) {
-        cels = get_unlocked_unique_cels(site.sprite(), site.range());
+        cels = get_unique_cels_to_edit_pixels(site.sprite(), site.range());
       }
       else if (site.cel()) {
         cels.push_back(site.cel());

src/app/util/range_utils.cpp
@@ -24,23 +24,31 @@ namespace app {
 
 using namespace doc;
 
+enum class Target {
+  kAllCels,
+  kUniqueCels,
+  kUniqueCanMoveCels,
+  kUniqueCanEditPixelsCels,
+};
+
 // TODO the DocRange should be "iteratable" to replace this function
 //      or we can wait to C++20 coroutines and co_yield
 static CelList get_cels_templ(const Sprite* sprite,
                               DocRange range,
-                              const bool onlyUniqueCels,
-                              const bool onlyUnlockedCel)
+                              const Target target)
 {
   CelList cels;
   if (!range.convertToCels(sprite))
     return cels;
 
+  // Used to visit linked cels just once.
   std::set<ObjectId> visited;
 
   for (Layer* layer : range.selectedLayers()) {
     if (!layer ||
         !layer->isImage() ||
-        (onlyUnlockedCel && !layer->canEditPixels())) {
+        (target == Target::kUniqueCanMoveCels && !layer->isEditableHierarchy()) ||
+        (target == Target::kUniqueCanEditPixelsCels && !layer->canEditPixels())) {
       continue;
     }
 
@@ -50,9 +58,10 @@ static CelList get_cels_templ(const Sprite* sprite,
       if (!cel)
         continue;
 
-      if (!onlyUniqueCels ||
+      if (target == Target::kAllCels ||
           visited.find(cel->data()->id()) == visited.end()) {
-        if (onlyUniqueCels)
+        // Only unique cels (avoid visited cels)
+        if (target != Target::kAllCels)
           visited.insert(cel->data()->id());
 
         cels.push_back(cel);
@@ -64,17 +73,22 @@ static CelList get_cels_templ(const Sprite* sprite,
 
 CelList get_cels(const doc::Sprite* sprite, const DocRange& range)
 {
-  return get_cels_templ(sprite, range, false, false);
+  return get_cels_templ(sprite, range, Target::kAllCels);
 }
 
 CelList get_unique_cels(const Sprite* sprite, const DocRange& range)
 {
-  return get_cels_templ(sprite, range, true, false);
+  return get_cels_templ(sprite, range, Target::kUniqueCels);
 }
 
-CelList get_unlocked_unique_cels(const Sprite* sprite, const DocRange& range)
+CelList get_unique_cels_to_move_cel(const Sprite* sprite, const DocRange& range)
 {
-  return get_cels_templ(sprite, range, true, true);
+  return get_cels_templ(sprite, range, Target::kUniqueCanMoveCels);
+}
+
+CelList get_unique_cels_to_edit_pixels(const Sprite* sprite, const DocRange& range)
+{
+  return get_cels_templ(sprite, range, Target::kUniqueCanEditPixelsCels);
 }
 
 } // namespace app

src/app/util/range_utils.h
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2020  Igara Studio S.A.
+// Copyright (C) 2020-2021  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -24,7 +24,8 @@ namespace app {
 
   doc::CelList get_cels(const doc::Sprite* sprite, const DocRange& range);
   doc::CelList get_unique_cels(const doc::Sprite* sprite, const DocRange& range);
-  doc::CelList get_unlocked_unique_cels(const doc::Sprite* sprite, const DocRange& range);
+  doc::CelList get_unique_cels_to_edit_pixels(const doc::Sprite* sprite, const DocRange& range);
+  doc::CelList get_unique_cels_to_move_cel(const doc::Sprite* sprite, const DocRange& range);
 
 } // namespace app
 