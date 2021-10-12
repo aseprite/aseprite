52d0b94293299cbe626c478a08ce874ecf052085
Joshua Ogunyinka <ogunyinkajoshua@yahoo.com>
2021-10-12 11:28:37 +0400

    Refactor and removal of redundant parts of the code

--- src/app/file/psd_format.cpp
+++ src/app/file/psd_format.cpp
@@ -29,6 +29,49 @@ doc::PixelFormat psd_cmode_to_ase_format(const psd::ColorMode mode)
   }
 }
 
+doc::BlendMode psd_blendmode_to_ase(const psd::LayerBlendMode mode)
+{
+  switch (mode) {
+    case psd::LayerBlendMode::Multiply:
+      return doc::BlendMode::MULTIPLY;
+    case psd::LayerBlendMode::Darken:
+      return doc::BlendMode::DARKEN;
+    case psd::LayerBlendMode::ColorBurn:
+      return doc::BlendMode::COLOR_BURN;
+    case psd::LayerBlendMode::Lighten:
+      return doc::BlendMode::LIGHTEN;
+    case psd::LayerBlendMode::Screen:
+      return doc::BlendMode::SCREEN;
+    case psd::LayerBlendMode::ColorDodge:
+      return doc::BlendMode::COLOR_DODGE;
+    case psd::LayerBlendMode::Overlay:
+      return doc::BlendMode::OVERLAY;
+    case psd::LayerBlendMode::SoftLight:
+      return doc::BlendMode::SOFT_LIGHT;
+    case psd::LayerBlendMode::HardLight:
+      return doc::BlendMode::HARD_LIGHT;
+    case psd::LayerBlendMode::Difference:
+      return doc::BlendMode::DIFFERENCE;
+    case psd::LayerBlendMode::Exclusion:
+      return doc::BlendMode::EXCLUSION;
+    case psd::LayerBlendMode::Subtract:
+      return doc::BlendMode::SUBTRACT;
+    case psd::LayerBlendMode::Divide:
+      return doc::BlendMode::DIVIDE;
+    case psd::LayerBlendMode::Hue:
+      return doc::BlendMode::HSL_HUE;
+    case psd::LayerBlendMode::Saturation:
+      return doc::BlendMode::HSL_SATURATION;
+    case psd::LayerBlendMode::Color:
+      return doc::BlendMode::HSL_COLOR;
+    case psd::LayerBlendMode::Luminosity:
+      return doc::BlendMode::HSL_LUMINOSITY;
+    case psd::LayerBlendMode::Normal:
+    default:
+      return doc::BlendMode::NORMAL;
+  }
+}
+
 class PsdFormat : public FileFormat {
   const char* onGetName() const override { return "psd"; }
 
@@ -55,11 +98,14 @@ FileFormat* CreatePsdFormat()
 
 class PsdDecoderDelegate : public psd::DecoderDelegate {
 public:
-  PsdDecoderDelegate(FileOp* fop)
-    : m_fop(fop)
-    , m_currentLayer(nullptr)
+  PsdDecoderDelegate()
+    : m_currentLayer(nullptr)
     , m_currentImage(nullptr)
+#ifdef _DEBUG
+    , m_prevImage(nullptr)
+#endif  // _DEBUG
     , parentSprite(nullptr)
+    , m_pixelFormat(PixelFormat::IMAGE_INDEXED)
   { }
 
   Sprite* getSprite() { return assembleDocument(); }
@@ -68,7 +114,8 @@ public:
   {
     m_pixelFormat = psd_cmode_to_ase_format(header.colorMode);
     parentSprite = new Sprite(
-      ImageSpec(ColorMode(m_pixelFormat), header.width, header.width), 256);
+      ImageSpec(ColorMode(m_pixelFormat), header.width, header.width));
+    parentSprite->setTotalFrames(frame_t(1));
   }
 
   void onLayerSelected(const psd::LayerRecord& layerRecord) override
@@ -79,81 +126,69 @@ public:
       });
 
     if (findIter == layers.end()) {
-      if (m_currentLayer) {
-        m_currentLayer = new LayerImage(parentSprite);
-        m_currentLayer->setName(layerRecord.name);
-        parentSprite->root()->addLayer(m_currentLayer);
-        layers.push_back(m_currentLayer);
-        m_currentImage = nullptr;
-      }
-      else {
-        createSequenceImage(layerRecord.width(), layerRecord.height());
-        m_currentLayer = new LayerImage(parentSprite);
-        m_currentLayer->setName(layerRecord.name);
-        parentSprite->root()->addLayer(m_currentLayer);
-        static_cast<LayerImage*>(m_currentLayer)
-            ->addCel(new Cel(frame_t(0), ImageRef(m_currentImage)));
-        layers.push_back(m_currentLayer);
-      }
+      m_currentImage.reset();
+      createNewLayer(layerRecord.name);
     }
     else {
       m_currentLayer = *findIter;
-      m_currentImage = m_currentLayer->cel(frame_t(0))->image();
+      m_currentImage = m_currentLayer->cel(frame_t(0))->imageRef();
     }
   }
 
   void onColorModeData(const psd::ColorModeData& colorModeD) override
   {
     if (!colorModeD.colors.empty()) {
-      const uint8_t alpha = 255;
-      if (m_pixelFormat == PixelFormat::IMAGE_INDEXED) {
-        for (int i = 0; i < colorModeD.colors.size(); ++i) {
-          const psd::IndexColor iColor = colorModeD.colors[i];
-          const color_t color = rgba(iColor.r, iColor.g, iColor.b, alpha);
-          m_palette.setEntry(i, color);
-        }
-      }
-      else {
-        for (int i = 0; i < 255; ++i) {
-          const color_t color = rgba(i, i, i, alpha);
-          m_palette.setEntry(i, color);
-        }
+      for (int i = 0; i < colorModeD.colors.size(); ++i) {
+        const psd::IndexColor iColor = colorModeD.colors[i];
+        m_palette.setEntry(i, rgba(iColor.r, iColor.g, iColor.b, 255));
       }
     }
   }
 
   void onBeginImage(const psd::ImageData& imageData) override
   {
-    // just a simple image with no layer
+    // only occurs where there's an image with no layer
     if (layers.empty() && !m_currentImage) {
-      createSequenceImage(imageData.width, imageData.height);
-      m_currentLayer = new LayerImage(parentSprite);
-      parentSprite->root()->addLayer(m_currentLayer);
-      static_cast<LayerImage*>(m_currentLayer)
-        ->addCel(new Cel(frame_t(0), ImageRef(m_currentImage)));
-      layers.push_back(m_currentLayer);
+      createNewImage();
+      createNewLayer("Layer Y");
+      linkNewCel(m_currentLayer, m_currentImage);
     }
 
     else if (m_currentLayer && !m_currentImage) {
-      LayerImage* layerImage = static_cast<LayerImage*>(m_currentLayer);
-      layerImage->addCel(
-        new doc::Cel(frame_t(0),
-                     doc::ImageRef(doc::Image::create(
-                       m_pixelFormat, imageData.width, imageData.height))));
-      m_currentImage = m_currentLayer->cel(0)->image();
+      createNewImage();
+      linkNewCel(m_currentLayer, m_currentImage);
     }
   }
 
+  // emitted when a new layer's image is about to be worked on
+  void onLayersAndMask(const psd::LayersInformation& layersInfo) override
+  {
+    if (layersInfo.layers.size() == layers.size()) {
+      const std::vector<uint8_t>& globalMasks = layersInfo.globalMaskData;
+      for (int i = 0; i < layers.size(); ++i) {
+        const psd::LayerRecord& layerRecord = layersInfo.layers[i];
+
+        LayerImage* layer = static_cast<LayerImage*>(layers[i]);
+        layer->setBlendMode(psd_blendmode_to_ase(layerRecord.blendMode));
+
+        Cel* cel = layer->cel(frame_t(0));
+        cel->setOpacity(layerRecord.opacity);
+        // cel->setPosition(gfx::Point(layerRecord.left, layerRecord.top));
+        if (globalMasks.size() > i)
+          cel->image()->setMaskColor(globalMasks[i]);
+      }
+    }
+  }
   // this is called on images stored in raw data
   void onImageScanline(const psd::ImageData& imgData,
                        const int y,
                        const psd::ChannelID chanID,
-                       const std::vector<uint32_t>& data)
+                       const std::vector<uint32_t>& data) override
   {
     ASSERT(m_currentImage);
-    ASSERT(m_currentImage->width() == data.size());
+    ASSERT(m_currentImage->width() >= data.size());
 
-    for (int x = 0; x < m_currentImage->width(); ++x) {
+    for (int x = 0; x < data.size(); ++x) {
       const color_t c = m_currentImage->getPixel(x, y);
       putPixel(
         x, y, c, normalizeValue(data[x], imgData.depth), chanID, m_pixelFormat);
@@ -165,11 +200,10 @@ public:
                        const int y,
                        const psd::ChannelID chanID,
                        const uint8_t* data,
-                       const int bytes)
+                       const int bytes) override
   {
     ASSERT(m_currentImage);
     if (!m_currentImage) {
-      m_fop->setError("there was a problem on the image scanline");
       throw std::runtime_error("there was a problem in RLE image scanline");
     }
 
@@ -177,32 +211,33 @@ public:
       return;
 
     // uint32_t* dst_address = (uint32_t*)m_currentImage->getPixelAddress(0, y);
-    for (int x = 0; x < m_currentImage->width() && x < bytes; ++x) {
+    for (int x = 0; x < m_currentImage->width(); ++x) {
       const color_t c = m_currentImage->getPixel(x, y);
-      putPixel(x, y, c, normalizeValue(data[x], img.depth), chanID, m_pixelFormat);
+      putPixel(
+        x, y, c, normalizeValue(data[x], img.depth), chanID, m_pixelFormat);
     }
   }
 
 private:
-  // TODO: this is a sham, don't know what to do
-  std::uint8_t normalizeValue(const uint32_t value, const int depth)
+  // TODO: this is meant to convert values of a channel based on its depth 
+  std::uint32_t normalizeValue(const uint32_t value, const int depth)
   {
-    if (depth <= 8) {
-      return static_cast<uint8_t>(value);
-    }
-    else if (depth == 16) {
-      if (value > std::numeric_limits<uint8_t>::max()) {
-        return ((value >> 0) & 0xff);
-      }
-      else
-        return static_cast<uint8_t>(value);
-    }
-    else if (depth == 32) {
-      if (value <= std::numeric_limits<uint8_t>::max()) {
-        return 1;
-      }
-    }
-    return 0;
+    return value;
+  }
+
+  void linkNewCel(Layer* layer, doc::ImageRef image)
+  {
+    std::unique_ptr<Cel> cel(new doc::Cel(frame_t(0), image));
+    cel->setPosition(0, 0);
+    static_cast<LayerImage*>(layer)->addCel(cel.release());
+  }
+
+  void createNewLayer(const std::string& layerName)
+  {
+    m_currentLayer = new LayerImage(parentSprite);
+    parentSprite->root()->addLayer(m_currentLayer);
+    layers.push_back(m_currentLayer);
+    m_currentLayer->setName(layerName);
   }
 
   Sprite* assembleDocument()
@@ -211,6 +246,16 @@ private:
         m_pixelFormat == doc::PixelFormat::IMAGE_GRAYSCALE) {
       parentSprite->setPalette(&m_palette, true);
     }
+#ifdef _DEBUG
+    ASSERT(layers.size() >= 1);
+    for (int i = 0; i < layers.size(); ++i) {
+      LayerImage* currLayer = static_cast<LayerImage*>(layers[i]);
+      ASSERT(currLayer);
+      ASSERT(currLayer->getCelsCount() >= 1);
+      ASSERT(currLayer->cel(frame_t(0))->data());
+    }
+#endif  // _DEBUG
+
     return parentSprite;
   }
 
@@ -235,7 +280,8 @@ private:
       }
       else if (chanID == psd::ChannelID::Blue) {
         b = pixelValue;
-      } else if(chanID == psd::ChannelID::Alpha){
+      }
+      else if (chanID == psd::ChannelID::Alpha) {
         a = pixelValue;
       }
       m_currentImage->putPixel(x, y, rgba(r, g, b, a));
@@ -264,15 +310,27 @@ private:
     }
   }
 
-  void createSequenceImage(const int width, const int height)
+  void createNewImage()
   {
-    m_currentImage = Image::create(m_pixelFormat, width, height);
-    clear_image(m_currentImage, 0);
+    m_currentImage.reset(Image::create(
+      m_pixelFormat, parentSprite->width(), parentSprite->height()));
+    clear_image(m_currentImage.get(), 0);
+#ifdef _DEBUG
+    if (!m_prevImage) {
+      m_prevImage = m_currentImage;
+      return;
+    }
+    ASSERT(m_currentImage != m_prevImage);
+    m_prevImage = m_currentImage;
+#endif  // _DEBUG
   }
 
-  FileOp* m_fop;
   doc::Layer* m_currentLayer;
-  doc::Image* m_currentImage;
+  doc::ImageRef m_currentImage;
+#ifdef _DEBUG
+  doc::ImageRef m_prevImage;
+#endif  // _DEBUG
+
   Sprite* parentSprite;
   PixelFormat m_pixelFormat;
   std::vector<doc::Layer*> layers;
@@ -285,7 +343,7 @@ bool PsdFormat::onLoad(FileOp* fop)
     base::open_file_with_exception(fop->filename(), "rb");
   FILE* f = fileHandle.get();
   psd::StdioFileInterface fileInterface(f);
-  PsdDecoderDelegate pDelegate(fop);
+  PsdDecoderDelegate pDelegate;
   psd::Decoder decoder(&fileInterface, &pDelegate);
 
   if (!decoder.readFileHeader()) {
@@ -300,6 +358,12 @@ bool PsdFormat::onLoad(FileOp* fop)
       header.colorMode != psd::ColorMode::Grayscale) {
     fop->setError("this preliminary work only supports "
                   "RGB, Grayscale && Indexed\n");
+    return false;
+  }
+
+  //this would be removed when support for 32bit per channel is supported
+  if (header.depth >= 32) {
+    fop->setError("support for 32bit per channel isn't supported yet");
     return false;
   }
 
@@ -323,6 +387,7 @@ bool PsdFormat::onLoad(FileOp* fop)
     fop->setError("unable to decode the image data section\n");
     return false;
   }
+
   fop->createDocument(pDelegate.getSprite());
   return true;
 }
