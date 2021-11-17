aseprite/aseprite

commit f15589734f6d4400c79574d7142adce0c2dd53c8

Fix double-clicking a .aseprite file in macOS Finder doesn't open the file anymore (fix #2985)
More info in: #3006

src/app/app.cpp
@@ -325,17 +325,15 @@ int App::initialize(const AppOptions& options)
     // Show the main window (this is not modal, the code continues)
     m_mainWindow->openWindow();
 
-    // Redraw the whole screen.
-    manager->invalidate();
-
-    // Pump some messages so we receive the first
-    // Manager::onNewDisplayConfiguration() and we known the manager
-    // initial size. This is required so if the OpenFileCommand
-    // (called when we're processing the CLI with OpenBatchOfFiles)
-    // shows a dialog to open a sequence of files, the dialog is
-    // centered correctly to the manager bounds.
-    ui::MessageLoop loop(manager);
-    loop.pumpMessages();
+    // To know the initial manager size we call to
+    // Manager::updateAllDisplaysWithNewScale(...) so we receive a
+    // Manager::onNewDisplayConfiguration() (which will update the
+    // bounds of the manager for first time).  This is required so if
+    // the OpenFileCommand (called when we're processing the CLI with
+    // OpenBatchOfFiles) shows a dialog to open a sequence of files,
+    // the dialog is centered correctly to the manager bounds.
+    const int scale = Preferences::instance().general.screenScale();
+    manager->updateAllDisplaysWithNewScale(scale);
   }
 #endif  // ENABLE_UI
 

commit a713a8884572bec8dcd02f615f74a9815c219189

[x11] Fix initial black window on Linux
Possible regression introduced in f15589734f6d4400c79574d7142adce0c2dd53c8

src/app/app.cpp
@@ -325,6 +325,11 @@ int App::initialize(const AppOptions& options)
     // Show the main window (this is not modal, the code continues)
     m_mainWindow->openWindow();
 
+#if LAF_LINUX // TODO check why this is required and we cannot call
+              //      updateAllDisplaysWithNewScale() on Linux/X11
+    // Redraw the whole screen.
+    manager->invalidate();
+#else
     // To know the initial manager size we call to
     // Manager::updateAllDisplaysWithNewScale(...) so we receive a
     // Manager::onNewDisplayConfiguration() (which will update the
@@ -334,6 +339,7 @@ int App::initialize(const AppOptions& options)
     // the dialog is centered correctly to the manager bounds.
     const int scale = Preferences::instance().general.screenScale();
     manager->updateAllDisplaysWithNewScale(scale);
+#endif
   }
 #endif  // ENABLE_UI
 