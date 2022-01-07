7848a66c5ab7a4ff5dc7c8a0cda794f7507e72f8
Joshua Ogunyinka <ogunyinkajoshua@yahoo.com>
2022-01-07 12:29:48 +0400

    Add missing language strings in advanced sprite properties

--- src/app/commands/cmd_sprite_size.cpp
+++ src/app/commands/cmd_sprite_size.cpp
@@ -17,6 +17,7 @@
 #include "app/commands/params.h"
 #include "app/doc_api.h"
 #include "app/ini_file.h"
+#include "app/i18n/strings.h"
 #include "app/modules/gui.h"
 #include "app/modules/palettes.h"
 #include "app/sprite_job.h"
@@ -81,7 +82,7 @@ class SpriteSizeJob : public SpriteJob {
 public:
 
   SpriteSizeJob(const ContextReader& reader, int new_width, int new_height, ResizeMethod resize_method)
-    : SpriteJob(reader, "Sprite Size") {
+    : SpriteJob(reader, Strings::sprite_size_title().c_str()) {
     m_new_width = new_width;
     m_new_height = new_height;
     m_resize_method = resize_method;
@@ -264,9 +265,9 @@ public:
                   doc::algorithm::RESIZE_METHOD_BILINEAR == 1 &&
                   doc::algorithm::RESIZE_METHOD_ROTSPRITE == 2,
                   "ResizeMethod enum has changed");
-    method()->addItem("Nearest-neighbor");
-    method()->addItem("Bilinear");
-    method()->addItem("RotSprite");
+    method()->addItem(Strings::sprite_size_nearest_neighbor());
+    method()->addItem(Strings::sprite_size_bilinear());
+    method()->addItem(Strings::sprite_size_rotsprite());
     int resize_method;
     if (params.method.isSet())
       resize_method = (int)params.method();
