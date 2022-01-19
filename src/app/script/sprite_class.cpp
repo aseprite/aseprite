2c9e49b795280cffc1544268df7164ed26b7403c
Gaspar Capello <gaspar@igarastudio.com>
2022-01-19 10:25:05 -0300

    Add warning message to deleteLayer() lua function
    Before this fix, the deleteLayer() lua function didn't warn the user if its argument (a layer) didn't belong to the corresponding sprite.

--- src/app/script/sprite_class.cpp
+++ src/app/script/sprite_class.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2018-2021  Igara Studio S.A.
+// Copyright (C) 2018-2022  Igara Studio S.A.
 // Copyright (C) 2015-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -375,6 +375,8 @@ int Sprite_deleteLayer(lua_State* L)
     }
   }
   if (layer) {
+    if (sprite != layer->sprite())
+      return luaL_error(L, "the layer doesn't belong to the sprite");
     Tx tx;
     tx(new cmd::RemoveLayer(layer));
     tx.commit();
