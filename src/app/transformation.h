f7b66746802b0756b0d709f6a6fb5c939afa5e43
Gaspar Capello <gaspar@igarastudio.com>
2022-07-13 10:36:30 -0300

    Fix Marqueed selection gets some rows duplicated when moved left and right over left canvas edge with Ctrl still pressed #2891
    Prior to this fix, moving a marquee selection while holding down the Ctrl key outside the left/top area of the canvas results in a 1px distortion of the marquee image. This distortion is due to a false transformation when it is required to get the corner bounds, due to rounding the float coordinates towards 0 instead of the nearest left/top integer. The discrepancy occurs within the `getDraggedImageCopy`, `redrawExtraImage`, `drawImage` and `drawMask` functions.

--- src/app/transformation.h
+++ src/app/transformation.h
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2020-2021  Igara Studio S.A.
+// Copyright (C) 2020-2022  Igara Studio S.A.
 // Copyright (C) 2001-2016  David Capello
 //
 // This program is distributed under the terms of
@@ -63,7 +63,7 @@ public:
         gfx::RectF bounds;
         for (int i=0; i<Corners::NUM_OF_CORNERS; ++i)
           bounds |= gfx::RectF(m_corners[i].x, m_corners[i].y, cornerThick, cornerThick);
-        return bounds;
+        return bounds.floor();
       }
 
     private:
