aseprite/aseprite

commit dd0c29620974e56d9e62653804b996740a89e88a

Use tooltip_text style & color to paint tooltip windows (fix #2554)

data/extensions/aseprite-theme/theme.xml
@@ -496,9 +496,6 @@
         <style id="tooltip_window_arrow">
             <background part="tooltip_arrow" />
         </style>
-        <style id="tooltip_face">
-            <background color="tooltip_face" />
-        </style>
         <style id="tooltip_text">
             <background color="tooltip_face" />
             <text color="tooltip_text" align="left" />

src/app/ui/skin/skin_theme.cpp
@@ -1430,8 +1430,7 @@ void SkinTheme::paintTextBox(ui::PaintEvent& ev)
   Graphics* g = ev.graphics();
   Widget* widget = static_cast<Widget*>(ev.getSource());
 
-  Theme::drawTextBox(g, widget, nullptr, nullptr,
-                     BGCOLOR, colors.textboxText());
+  Theme::paintTextBoxWithStyle(g, widget);
 }
 
 void SkinTheme::paintViewViewport(PaintEvent& ev)

src/ui/theme.cpp
@@ -1,5 +1,5 @@
 // Aseprite UI Library
-// Copyright (C) 2019-2020  Igara Studio S.A.
+// Copyright (C) 2019-2022  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This file is released under the terms of the MIT license.
@@ -292,6 +292,28 @@ void Theme::paintTooltip(Graphics* g,
     if (intClip)
       paintWidget(g, widget, arrowStyle, rc);
   }
+}
+
+gfx::Size Theme::paintTextBoxWithStyle(Graphics* g,
+                                       const Widget* widget)
+{
+  gfx::Size size;
+  gfx::Color bg = gfx::ColorNone, fg = gfx::ColorNone;
+
+  for_each_layer(
+    PaintWidgetPartInfo::getStyleFlagsForWidget(widget),
+    widget->style(),
+    [&fg, &bg](const Style::Layer& layer) {
+      switch (layer.type()) {
+        case Style::Layer::Type::kBackground: bg = layer.color(); break;
+        case Style::Layer::Type::kText:       fg = layer.color(); break;
+      }
+    });
+
+  if (fg != gfx::ColorNone)
+    Theme::drawTextBox(g, widget, &size.w, &size.h, bg, fg);
+
+  return size;
 }
 
 void Theme::paintLayer(Graphics* g,
@@ -744,7 +766,7 @@ void Theme::drawSlices(Graphics* g, os::Surface* sheet,
 }
 
 // static
-void Theme::drawTextBox(Graphics* g, Widget* widget,
+void Theme::drawTextBox(Graphics* g, const Widget* widget,
                         int* w, int* h, gfx::Color bg, gfx::Color fg)
 {
   View* view = (g ? View::getView(widget): nullptr);

src/ui/theme.h
@@ -1,5 +1,5 @@
 // Aseprite UI Library
-// Copyright (C) 2020-2021  Igara Studio S.A.
+// Copyright (C) 2020-2022  Igara Studio S.A.
 // Copyright (C) 2001-2017  David Capello
 //
 // This file is released under the terms of the MIT license.
@@ -101,6 +101,9 @@ namespace ui {
                               const int arrowAlign,
                               const gfx::Rect& target);
 
+    gfx::Size paintTextBoxWithStyle(Graphics* g,
+                                    const Widget* widget);
+
     virtual gfx::Size calcSizeHint(const Widget* widget,
                                    const Style* style);
     virtual gfx::Border calcBorder(const Widget* widget,
@@ -125,8 +128,8 @@ namespace ui {
                            const gfx::Color color,
                            const bool drawCenter = true);
 
-    static  void drawTextBox(Graphics* g, Widget* textbox,
-                             int* w, int* h, gfx::Color bg, gfx::Color fg);
+    static void drawTextBox(Graphics* g, const Widget* textbox,
+                            int* w, int* h, gfx::Color bg, gfx::Color fg);
 
   protected:
     virtual void onRegenerateTheme() = 0;

commit 55885ee266fbb7d01894cf0a456aaceb8cc807c7

Fix regression: TextBox isn't rendering text (doesn't update the bounds area of the text box)
Regression in dd0c29620974e56d9e62653804b996740a89e88a (Use
tooltip_text style & color to paint tooltip windows #2554).

Prior this fix, text box does not update its bounds area because it
has not a style assigned.

Fix #3442, fix #3443

data/extensions/aseprite-theme/theme.xml
@@ -503,6 +503,10 @@
             <background color="tooltip_face" />
             <text color="tooltip_text" align="left" />
         </style>
+        <style id="textbox_text" align="left top wordwrap" border="4">
+            <background color="textbox_face" />
+            <text color="textbox_text" align="left" />
+        </style>
         <style id="label" padding="1">
             <text color="text" align="left" />
             <text color="disabled" align="left" state="disabled" />

src/app/ui/skin/skin_theme.cpp
@@ -913,9 +913,8 @@ void SkinTheme::initWidget(Widget* widget)
       break;
 
     case kTextBoxWidget:
-      BORDER(4*guiscale());
       widget->setChildSpacing(0);
-      widget->setBgColor(colors.textboxFace());
+      widget->setStyle(styles.textboxText());
       break;
 
     case kViewWidget:

src/ui/theme.cpp
@@ -294,10 +294,9 @@ void Theme::paintTooltip(Graphics* g,
   }
 }
 
-gfx::Size Theme::paintTextBoxWithStyle(Graphics* g,
-                                       const Widget* widget)
+void Theme::paintTextBoxWithStyle(Graphics* g,
+                                  const Widget* widget)
 {
-  gfx::Size size;
   gfx::Color bg = gfx::ColorNone, fg = gfx::ColorNone;
 
   for_each_layer(
@@ -311,9 +310,7 @@ gfx::Size Theme::paintTextBoxWithStyle(Graphics* g,
     });
 
   if (fg != gfx::ColorNone)
-    Theme::drawTextBox(g, widget, &size.w, &size.h, bg, fg);
-
-  return size;
+    Theme::drawTextBox(g, widget, nullptr, nullptr, bg, fg);
 }
 
 void Theme::paintLayer(Graphics* g,

src/ui/theme.h
@@ -101,8 +101,8 @@ namespace ui {
                               const int arrowAlign,
                               const gfx::Rect& target);
 
-    gfx::Size paintTextBoxWithStyle(Graphics* g,
-                                    const Widget* widget);
+    void paintTextBoxWithStyle(Graphics* g,
+                               const Widget* widget);
 
     virtual gfx::Size calcSizeHint(const Widget* widget,
                                    const Style* style);