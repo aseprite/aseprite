ec41e9fe66b2ff361760bf6c1e784041c70fb37c
David Capello <david@igara.com>
2022-08-25 18:12:15 -0300

    Include StatusBar in the set of customizable widgets in the layout

--- src/app/ui/main_window.cpp
+++ src/app/ui/main_window.cpp
@@ -160,6 +160,7 @@ void MainWindow::initialize()
   // constructor).
   m_colorBar->setId("colorbar");
   m_contextBar->setId("contextbar");
+  m_statusBar->setId("statusbar");
   m_timeline->setId("timeline");
   m_toolBar->setId("toolbar");
   m_workspace->setId("workspace");
@@ -181,7 +182,6 @@ void MainWindow::initialize()
   m_dock->top()->dock(ui::BOTTOM, m_tabsBar.get());
   m_dock->top()->dock(ui::CENTER, m_menuBar.get());
   m_dock->dock(ui::CENTER, m_customizableDockPlaceholder.get());
-  m_dock->dock(ui::BOTTOM, m_statusBar.get());
 
   setDefaultLayout();
 
@@ -398,6 +398,7 @@ void MainWindow::setDefaultLayout()
 
   m_customizableDock->resetDocks();
   m_customizableDock->dock(ui::LEFT, m_colorBar.get(), gfx::Size(colorBarWidth, 0));
+  m_customizableDock->dock(ui::BOTTOM, m_statusBar.get());
   m_customizableDock->center()->dock(ui::TOP, m_contextBar.get());
   m_customizableDock->center()->dock(ui::RIGHT, m_toolBar.get());
   m_customizableDock->center()->center()->dock(ui::BOTTOM,
@@ -418,6 +419,7 @@ void MainWindow::setDefaultMirrorLayout()
 
   m_customizableDock->resetDocks();
   m_customizableDock->dock(ui::RIGHT, m_colorBar.get(), gfx::Size(colorBarWidth, 0));
+  m_customizableDock->dock(ui::BOTTOM, m_statusBar.get());
   m_customizableDock->center()->dock(ui::TOP, m_contextBar.get());
   m_customizableDock->center()->dock(ui::LEFT, m_toolBar.get());
   m_customizableDock->center()->center()->dock(ui::BOTTOM,
