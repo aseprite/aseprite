8eae7c00ae36fa957e7c71c8118e25e85d56657d
Gaspar Capello <gaspar@igara.com>
2022-09-08 17:00:22 -0300

    When eyedropper tool picks a pixel of mask color, the FG color changes to entry 0 (fix #2813)

--- src/doc/palette.h
+++ src/doc/palette.h
@@ -1,4 +1,5 @@
 // Aseprite Document Library
+// Copyright (c) 2022 Igara Studio SA
 // Copyright (c) 2001-2018 David Capello
 //
 // This file is released under the terms of the MIT license.
@@ -91,6 +92,7 @@ namespace doc {
 
     int findExactMatch(int r, int g, int b, int a, int mask_index) const;
     int findBestfit(int r, int g, int b, int a, int mask_index) const;
+    int findMaskColor() const;
 
     void applyRemap(const Remap& remap);
 
