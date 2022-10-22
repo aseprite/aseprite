ac114046f89ebcbc70eb668f140330c8f29a3494
Gaspar Capello <gaspar@igara.com>
2022-10-21 21:38:20 -0300

    Fix 8 Connected Fill escapes grid with "Stop At Grid" checked (fix #3564)
    Prior to this fix with the setting: '8-Connected' + 'Stop at Grid', the 'Paint Bucket' and 'Magic Wand' tools went through the grid on the left side.

--- src/doc/algorithm/floodfill.cpp
+++ src/doc/algorithm/floodfill.cpp
@@ -274,7 +274,7 @@ static int flooder(const Image* image,
   right--;
 
   /* draw the line */
-  (*proc)(left, y, right, data);
+  (*proc)((left < bounds.x ? bounds.x : left), y, right, data);
 
   /* store it in the list of flooded segments */
   c = y;
