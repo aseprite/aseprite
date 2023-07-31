456113d015d57c2916e833ee5a1bfee09c0e8739
Gaspar Capello <gaspar@igara.com>
2023-07-31 13:39:03 -0300

    Fix after dropping a selection via Copy (Selection + Ctrl), handlers are not positioned correctly (fix #3977)
    This fix disables the fine control when the transformation doesn't
    include rotation, skew and/or fine scaling (so just translating doesn't
    allow fine control until we rotate/skew/scaling, this might change in
    the future if we add a new anti-aliasing rotation algorithm).
    Also this fix improves the regular movement of the selected image.

--- src/app/ui/editor/pixels_movement.cpp
+++ src/app/ui/editor/pixels_movement.cpp
@@ -129,6 +129,7 @@ PixelsMovement::PixelsMovement(
   , m_canHandleFrameChange(false)
   , m_fastMode(false)
   , m_needsRotSpriteRedraw(false)
+  , m_fineModificationApplied(false)
 {
   double cornerThick = (m_site.tilemapMode() == TilemapMode::Tiles) ?
                           CORNER_THICK_FOR_TILEMAP_MODE :
@@ -375,11 +376,15 @@ void PixelsMovement::moveImage(const gfx::PointF& pos, MoveModifier moveModifier
   switch (m_handle) {
 
     case MovePixelsHandle: {
-      double dx = (pos.x - m_catchPos.x);
-      double dy = (pos.y - m_catchPos.y);
-      if ((moveModifier & FineControl) == 0) {
-        if (dx >= 0.0) { dx = std::floor(dx); } else { dx = std::ceil(dx); }
-        if (dy >= 0.0) { dy = std::floor(dy); } else { dy = std::ceil(dy); }
+      double dx, dy;
+
+      if (m_fineModificationApplied) {
+        dx = (pos.x - m_catchPos.x);
+        dy = (pos.y - m_catchPos.y);
+      }
+      else {
+        dx = (std::floor(pos.x) - std::floor(m_catchPos.x));
+        dy = (std::floor(pos.y) - std::floor(m_catchPos.y));
       }
 
       if ((moveModifier & LockAxisMovement) == LockAxisMovement) {
@@ -475,6 +480,8 @@ void PixelsMovement::moveImage(const gfx::PointF& pos, MoveModifier moveModifier
           if (dx >= 0.0) { dx = std::floor(dx); } else { dx = std::ceil(dx); }
           if (dy >= 0.0) { dy = std::floor(dy); } else { dy = std::ceil(dy); }
         }
+        else
+          m_fineModificationApplied = true;
 
         if (m_handle == ScaleNHandle || m_handle == ScaleSHandle) {
           dx = 0.0;
@@ -550,6 +557,7 @@ void PixelsMovement::moveImage(const gfx::PointF& pos, MoveModifier moveModifier
       }
 
       newTransformation.angle(newAngle);
+      m_fineModificationApplied = true;
       break;
     }
 
@@ -694,6 +702,7 @@ void PixelsMovement::moveImage(const gfx::PointF& pos, MoveModifier moveModifier
       double newSkew = std::atan2(AC.x*AC0.y - AC.y*AC0.x, AC * AC0);
       newSkew = std::clamp(newSkew, -PI*85.0/180.0, PI*85.0/180.0);
       newTransformation.skew(newSkew);
+      m_fineModificationApplied = true;
       break;
     }
 
