aseprite/aseprite

commit 2cc15cda9e82611b3af7bb3cab07cf2fdc479b42

Add save dynamics options between sessions (fix #3933)

data/pref.xml
@@ -459,6 +459,21 @@
       <option id="refer_to" type="FillReferTo" default="FillReferTo::ACTIVE_LAYER" />
       <option id="pixel_connectivity" type="PixelConnectivity" default="PixelConnectivity::FOUR_CONNECTED" />
     </section>
+    <section id="dynamics">
+      <option id="stabilizer" type="bool" default="false" />
+      <option id="stabilizer_factor" type="int" default="0" />
+      <option id="size" type="app::tools::DynamicSensor" default="app::tools::DynamicSensor::Static" />
+      <option id="angle" type="app::tools::DynamicSensor" default="app::tools::DynamicSensor::Static" />
+      <option id="gradient" type="app::tools::DynamicSensor" default="app::tools::DynamicSensor::Static" />
+      <option id="min_size" type="int" default="1" />
+      <option id="min_angle" type="int" default="1" />
+      <option id="color_from_to" type="app::tools::ColorFromTo" default="app::tools::ColorFromTo::BgToFg" />
+      <option id="matrix_index" type="int" default="0" />
+      <option id="min_pressure_threshold" type="double" default="0.1" />
+      <option id="max_pressure_threshold" type="double" default="0.9" />
+      <option id="min_velocity_threshold" type="double" default="0.1" />
+      <option id="max_velocity_threshold" type="double" default="0.9" />
+    </section>
   </tool>
 
   <document>

src/app/ui/context_bar.cpp
@@ -1151,6 +1151,38 @@ public:
     : ButtonSet(1)
     , m_ctxBar(ctxBar) {
     addItem(SkinTheme::get(this)->parts.dynamics(), "dynamics_field");
+
+    // TODO: it would be better to initialize 'm_popup' at the time you
+    // need to display the dynamic options in the 'switchPopup()'
+    // function.
+    // However, initialization during construction of the DynamicField
+    // is an easy way to get the current dithering matrix given the
+    // index of the selected item of the "dithering matrix" comboBox.
+    m_popup.reset(new DynamicsPopup(this));
+    m_popup->loadDynamicPref(
+      &Preferences::instance().tool(App::instance()->activeTool()));
+    m_popup->setOptionsGridVisibility(m_optionsGridVisibility);
+    m_dynamics = m_popup->getDynamics();
+    m_popup->Close.connect(
+      [this](CloseEvent&){
+        deselectItems();
+        m_dynamics = m_popup->getDynamics();
+        auto& dynaPref = Preferences::instance().tool(
+          App::instance()->activeTool()).dynamics;
+        dynaPref.stabilizer(m_dynamics.stabilizer);
+        dynaPref.stabilizerFactor(m_dynamics.stabilizerFactor);
+        dynaPref.size(m_dynamics.size);
+        dynaPref.angle(m_dynamics.angle);
+        dynaPref.gradient(m_dynamics.gradient);
+        dynaPref.minSize.setValue(m_dynamics.minSize);
+        dynaPref.minAngle.setValue(m_dynamics.minAngle);
+        dynaPref.minPressureThreshold(m_dynamics.minPressureThreshold);
+        dynaPref.minVelocityThreshold(m_dynamics.minVelocityThreshold);
+        dynaPref.maxPressureThreshold(m_dynamics.maxPressureThreshold);
+        dynaPref.maxVelocityThreshold(m_dynamics.maxVelocityThreshold);
+        dynaPref.colorFromTo(m_dynamics.colorFromTo);
+        dynaPref.matrixIndex(m_popup->ditheringIndex());
+      });
   }
 
   void switchPopup() {
@@ -1158,16 +1190,6 @@ public:
         m_popup->isVisible()) {
       m_popup->closeWindow(nullptr);
       return;
-    }
-
-    if (!m_popup) {
-      m_popup.reset(new DynamicsPopup(this));
-      m_popup->setOptionsGridVisibility(m_optionsGridVisibility);
-      m_popup->Close.connect(
-        [this](CloseEvent&){
-          deselectItems();
-          m_dynamics = m_popup->getDynamics();
-        });
     }
 
     const gfx::Rect bounds = this->bounds();

commit 166210e0750daf649f32494b05921a0c6a8f8319

Add check box "Same in all tools" in the Dynamics options window (fix #4003)
Now each tool has independent 'dynamic options'. A global configuration
for all tools is also possible via the 'Same in all tools' option.

ALso this fix, solves a regression inserted in:
2cc15cda9e82611b3af7bb3cab07cf2fdc479b42
(bug inserted: stabilizer is always active)

data/gui.xml
@@ -1262,7 +1262,7 @@
         <param name="type" value="shading" />
       </item>
       <separator />
-      <item command="SetSameInk" text="@.same_in_all_tools" group="ink_options" />
+      <item command="SetSameInk" text="@general.same_in_all_tools" group="ink_options" />
     </menu>
 
   </menus>

data/pref.xml
@@ -317,6 +317,7 @@
       <option id="discard_brush" type="bool" default="false" />
     </section>
     <section id="shared">
+      <option id="share_dynamics" type="bool" default="false" />
       <option id="share_ink" type="bool" default="false" />
       <option id="ink" type="app::tools::InkType" default="app::tools::InkType::DEFAULT" />
     </section>

data/strings/en.ini
@@ -976,6 +976,7 @@ END
 reset = Reset
 advanced_options = Advanced Options
 unknown = Unknown
+same_in_all_tools = Same in all Tools
 
 [gif_options]
 title = GIF Options
@@ -1060,9 +1061,6 @@ alpha_compositing = Alpha Compositing
 copy_color = Copy Alpha+Color
 lock_alpha = Lock Alpha
 shading = Shading
-
-[ink_popup_menu]
-same_in_all_tools = Same in all Tools
 
 [jpeg_options]
 title = JPEG Options

data/widgets/dynamics.xml
@@ -1,5 +1,5 @@
 <!-- Aseprite -->
-<!-- Copyright (C) 2020-2022  Igara Studio S.A. -->
+<!-- Copyright (C) 2020-2023  Igara Studio S.A. -->
 <gui>
   <vbox id="dynamics">
     <hbox>
@@ -57,6 +57,8 @@
       <label id="velocity_label" text="@.velocity" style="mini_label" />
       <hbox id="velocity_placeholder" cell_align="horizontal" />
     </grid>
+    <separator horizontal="true" cell_hspan="2" />
+    <check id="same_in_all_tools" text="@general.same_in_all_tools" />
 
   </vbox>
 </gui>

src/app/script/dialog_class.cpp
@@ -18,6 +18,7 @@
 #include "app/script/graphics_context.h"
 #include "app/script/keys.h"
 #include "app/script/luacpp.h"
+#include "app/ui/button_set.h"
 #include "app/ui/color_button.h"
 #include "app/ui/color_shades.h"
 #include "app/ui/expr_entry.h"
@@ -64,13 +65,22 @@ std::vector<Dialog*> all_dialogs;
 
 struct Dialog {
   ui::Window window;
+  // Main grid that holds the dialog content.
   ui::Grid grid;
+  // Pointer to current grid (might be the main grid or a tab's grid).
+  ui::Grid* currentGrid;
   ui::HBox* hbox = nullptr;
   bool autoNewRow = false;
   WidgetsList mainWidgets;
   std::map<std::string, ui::Widget*> dataWidgets;
   std::map<std::string, ui::Widget*> labelWidgets;
   int currentRadioGroup = 0;
+
+  // Members used to hold current state about the creation of a group
+  // of tabs. After creation, these members are reset to their initial
+  // empty state to be ready for the next group of tabs.
+  std::vector<ui::Grid*> tabs;
+  HBox* tabsSelector = nullptr;
 
   // Used to create a new row when a different kind of widget is added
   // in the dialog.
@@ -89,7 +99,8 @@ struct Dialog {
   Dialog(const ui::Window::Type windowType,
          const std::string& title)
     : window(windowType, title),
-      grid(2, false) {
+      grid(2, false),
+      currentGrid(&grid) {
     window.addChild(&grid);
     all_dialogs.push_back(this);
   }
@@ -566,18 +577,18 @@ int Dialog_add_widget(lua_State* L, Widget* widget)
       if (!visible)
         labelWidget->setVisible(false);
 
-      dlg->grid.addChildInCell(labelWidget, 1, 1, ui::LEFT | ui::TOP);
+      dlg->currentGrid->addChildInCell(labelWidget, 1, 1, ui::LEFT | ui::TOP);
       if (!id.empty())
         dlg->labelWidgets[id] = labelWidget;
     }
     else
-      dlg->grid.addChildInCell(new ui::HBox, 1, 1, ui::LEFT | ui::TOP);
+      dlg->currentGrid->addChildInCell(new ui::HBox, 1, 1, ui::LEFT | ui::TOP);
 
     auto hbox = new ui::HBox;
     if (widget->type() == ui::kButtonWidget)
       hbox->enableFlags(ui::HOMOGENEOUS);
 
-    dlg->grid.addChildInCell(
+    dlg->currentGrid->addChildInCell(
       hbox, 1, 1,
       ui::HORIZONTAL | (vexpand ? ui::VERTICAL: ui::TOP));
 
@@ -1270,6 +1281,118 @@ int Dialog_canvas(lua_State* L)
   return Dialog_add_widget(L, widget);
 }
 
+int Dialog_tab(lua_State* L)
+{
+  auto dlg = get_obj<Dialog>(L, 1);
+
+  std::string text;
+  if (lua_istable(L, 2)) {
+    int type = lua_getfield(L, 2, "text");
+    if (type != LUA_TNIL) {
+      text = lua_tostring(L, -1);
+    }
+    lua_pop(L, 1);
+  }
+
+  auto tabContent = new ui::Grid(2, false);
+  tabContent->setVisible(false);
+  tabContent->setText(text);
+  dlg->tabs.push_back(tabContent);
+  dlg->currentGrid = tabContent;
+
+  // If this is the first tab create the tabs selector container.
+  if (dlg->tabs.size() == 1) {
+    dlg->tabsSelector = new HBox();
+    dlg->grid.addChildInCell(dlg->tabsSelector, 2, 1, ui::HORIZONTAL);
+  }
+
+  dlg->grid.addChildInCell(tabContent, 2, 1, ui::HORIZONTAL | ui::VERTICAL);
+  lua_pushvalue(L, 1);
+  return 1;
+}
+
+int Dialog_endtabs(lua_State* L)
+{
+  auto dlg = get_obj<Dialog>(L, 1);
+
+  // There is no starting :tab(), do nothing then.
+  if (dlg->tabs.empty()) {
+    lua_pushvalue(L, 1);
+    return 1;
+  }
+
+  int selectedTab = 0;
+  int align = ui::CENTER;
+  if (lua_istable(L, 2)) {
+    int type = lua_getfield(L, 2, "selected");
+    switch (type) {
+      case LUA_TSTRING: {
+        // Find the tab index with the specified text.
+        std::string selTabText = lua_tostring(L, -1);
+        for (int i=0; i<dlg->tabs.size(); ++i) {
+          if (dlg->tabs[i]->text() == selTabText) {
+            selectedTab = i;
+            break;
+          }
+        }
+        break;
+      }
+      case LUA_TNUMBER:
+        selectedTab = std::clamp<int>(lua_tointeger(L, -1), 1, dlg->tabs.size()) - 1;
+        break;
+    }
+    lua_pop(L, 1);
+
+    type = lua_getfield(L, 2, "align");
+    if(type != LUA_TNIL) {
+      int v = lua_tointeger(L, -1) & (ui::CENTER | ui::LEFT | ui::RIGHT);
+      // Set align only if it has a valid value.
+      if (v) align = v;
+    }
+    lua_pop(L, 1);
+  }
+
+  // Create the tabs selector's buttonset
+  auto widget = new app::ButtonSet(dlg->tabs.size());
+  for (int i=0; i<dlg->tabs.size(); ++i) {
+    auto item = widget->addItem(dlg->tabs[i]->text());
+    item->setSelected(i == selectedTab);
+    dlg->tabs[i]->setVisible(i == selectedTab);
+  }
+  auto tabs = dlg->tabs;
+  widget->ItemChange.connect([dlg, tabs](ButtonSet::Item* selItem) {
+    for (auto tab : tabs) {
+      tab->setVisible(selItem->text() ==  tab->text());
+    }
+    dlg->window.remapWindow();
+  });
+  widget->initTheme();
+  ui::Separator* sep;
+  if (align & ui::CENTER || align & ui::RIGHT) {
+    sep = new ui::Separator("", ui::HORIZONTAL);
+    sep->setExpansive(true);
+    dlg->tabsSelector->addChild(sep);
+  }
+  dlg->tabsSelector->addChild(widget);
+  if (align & ui::CENTER || align & ui::LEFT) {
+    sep = new ui::Separator("", ui::HORIZONTAL);
+    sep->setExpansive(true);
+    dlg->tabsSelector->addChild(sep);
+  }
+  // Add tab's bottom separator
+  sep = new ui::Separator("", ui::HORIZONTAL);
+  sep->setExpansive(true);
+  dlg->grid.addChildInCell(sep, 2, 1, ui::HORIZONTAL);
+
+  // Clear state to be ready for the next tabs creation
+  dlg->tabsSelector = nullptr;
+  dlg->tabs.clear();
+  dlg->currentGrid = &dlg->grid;
+
+  lua_pushvalue(L, 1);
+  return 1;
+}
+
 int Dialog_modify(lua_State* L)
 {
   auto dlg = get_obj<Dialog>(L, 1);
@@ -1711,6 +1834,8 @@ const luaL_Reg Dialog_methods[] = {
   { "shades", Dialog_shades },
   { "file", Dialog_file },
   { "canvas", Dialog_canvas },
+  { "tab", Dialog_tab },
+  { "endtabs", Dialog_endtabs },
   { "modify", Dialog_modify },
   { "repaint", Dialog_repaint },
   { nullptr, nullptr }

src/app/script/engine.cpp
@@ -32,6 +32,7 @@
 #include "doc/anidir.h"
 #include "doc/color_mode.h"
 #include "filters/target.h"
+#include "ui/base.h"
 #include "ui/cursor_type.h"
 #include "ui/mouse_button.h"
 
@@ -465,6 +466,14 @@ Engine::Engine()
   lua_setglobal(L, "FlipType");
   setfield_integer(L, "HORIZONTAL", doc::algorithm::FlipType::FlipHorizontal);
   setfield_integer(L, "VERTICAL",   doc::algorithm::FlipType::FlipVertical);
+  lua_pop(L, 1);
+
+  lua_newtable(L);
+  lua_pushvalue(L, -1);
+  lua_setglobal(L, "Align");
+  setfield_integer(L, "LEFT",   ui::LEFT);
+  setfield_integer(L, "CENTER", ui::CENTER);
+  setfield_integer(L, "RIGHT",  ui::RIGHT);
   lua_pop(L, 1);
 
   // Register classes/prototypes

src/app/tools/tool_loop_manager.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2019-2022  Igara Studio S.A.
+// Copyright (C) 2019-2023  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -169,7 +169,7 @@ bool ToolLoopManager::releaseButton(const Pointer& pointer)
 void ToolLoopManager::movement(Pointer pointer)
 {
   // Filter points with the stabilizer
-  if (m_dynamics.stabilizerFactor > 0) {
+  if (m_dynamics.stabilizer && m_dynamics.stabilizerFactor > 0) {
     const double f = m_dynamics.stabilizerFactor;
     const gfx::Point delta = (pointer.point() - m_stabilizerCenter);
     const double distance = std::sqrt(delta.x*delta.x + delta.y*delta.y);

src/app/ui/context_bar.cpp
@@ -1152,37 +1152,7 @@ public:
     , m_ctxBar(ctxBar) {
     addItem(SkinTheme::get(this)->parts.dynamics(), "dynamics_field");
 
-    // TODO: it would be better to initialize 'm_popup' at the time you
-    // need to display the dynamic options in the 'switchPopup()'
-    // function.
-    // However, initialization during construction of the DynamicField
-    // is an easy way to get the current dithering matrix given the
-    // index of the selected item of the "dithering matrix" comboBox.
-    m_popup.reset(new DynamicsPopup(this));
-    m_popup->loadDynamicPref(
-      &Preferences::instance().tool(App::instance()->activeTool()));
-    m_popup->setOptionsGridVisibility(m_optionsGridVisibility);
-    m_dynamics = m_popup->getDynamics();
-    m_popup->Close.connect(
-      [this](CloseEvent&){
-        deselectItems();
-        m_dynamics = m_popup->getDynamics();
-        auto& dynaPref = Preferences::instance().tool(
-          App::instance()->activeTool()).dynamics;
-        dynaPref.stabilizer(m_dynamics.stabilizer);
-        dynaPref.stabilizerFactor(m_dynamics.stabilizerFactor);
-        dynaPref.size(m_dynamics.size);
-        dynaPref.angle(m_dynamics.angle);
-        dynaPref.gradient(m_dynamics.gradient);
-        dynaPref.minSize.setValue(m_dynamics.minSize);
-        dynaPref.minAngle.setValue(m_dynamics.minAngle);
-        dynaPref.minPressureThreshold(m_dynamics.minPressureThreshold);
-        dynaPref.minVelocityThreshold(m_dynamics.minVelocityThreshold);
-        dynaPref.maxPressureThreshold(m_dynamics.maxPressureThreshold);
-        dynaPref.maxVelocityThreshold(m_dynamics.maxVelocityThreshold);
-        dynaPref.colorFromTo(m_dynamics.colorFromTo);
-        dynaPref.matrixIndex(m_popup->ditheringIndex());
-      });
+    loadDynamicsPref();
   }
 
   void switchPopup() {
@@ -1191,6 +1161,28 @@ public:
       m_popup->closeWindow(nullptr);
       return;
     }
+
+    if (!m_popup.get())
+      m_popup.reset(new DynamicsPopup(this));
+    auto activeTool = App::instance()->activeTool();
+    m_popup->loadDynamicsPref();
+    m_dynamics = m_popup->getDynamics();
+    m_sameInAllTools = m_popup->sharedSettings();
+    m_popup->Close.connect(
+      [this](CloseEvent&) {
+        deselectItems();
+        auto activeTool = App::instance()->activeTool();
+        m_dynamics = m_popup->getDynamics();
+        m_sameInAllTools = m_popup->sharedSettings();
+        if (m_sameInAllTools) {
+          for (Tool* tool : *App::instance()->toolBox())
+            saveDynamicsPref(tool);
+        }
+        else
+          saveDynamicsPref(activeTool);
+      });
+
+    m_popup->refreshVisibility();
 
     const gfx::Rect bounds = this->bounds();
     m_popup->remapWindow();
@@ -1212,6 +1204,48 @@ public:
     m_optionsGridVisibility = state;
     if (m_popup)
       m_popup->setOptionsGridVisibility(state);
+  }
+
+  void saveDynamicsPref(Tool* tool) {
+    auto& dynaPref = Preferences::instance().tool(tool).dynamics;
+    dynaPref.stabilizer(m_dynamics.stabilizer);
+    dynaPref.stabilizerFactor(m_dynamics.stabilizerFactor);
+    dynaPref.size(m_dynamics.size);
+    dynaPref.angle(m_dynamics.angle);
+    dynaPref.gradient(m_dynamics.gradient);
+    dynaPref.minSize.setValue(m_dynamics.minSize);
+    dynaPref.minAngle.setValue(m_dynamics.minAngle);
+    dynaPref.minPressureThreshold(m_dynamics.minPressureThreshold);
+    dynaPref.minVelocityThreshold(m_dynamics.minVelocityThreshold);
+    dynaPref.maxPressureThreshold(m_dynamics.maxPressureThreshold);
+    dynaPref.maxVelocityThreshold(m_dynamics.maxVelocityThreshold);
+    dynaPref.colorFromTo(m_dynamics.colorFromTo);
+    dynaPref.matrixIndex(m_popup->ditheringIndex());
+    Preferences::instance().shared.shareDynamics(m_sameInAllTools);
+  }
+
+  void loadDynamicsPref() {
+    auto& dynaPref = Preferences::instance()
+      .tool(App::instance()->activeTool()).dynamics;
+    m_dynamics.stabilizer = dynaPref.stabilizer();
+    m_dynamics.stabilizerFactor = dynaPref.stabilizerFactor();
+    m_dynamics.size = dynaPref.size();
+    m_dynamics.angle = dynaPref.angle();
+    m_dynamics.gradient = dynaPref.gradient();
+    m_dynamics.minSize = dynaPref.minSize();
+    m_dynamics.minAngle = dynaPref.minAngle();
+    m_dynamics.minPressureThreshold = dynaPref.minPressureThreshold();
+    m_dynamics.minVelocityThreshold = dynaPref.minVelocityThreshold();
+    m_dynamics.maxPressureThreshold = dynaPref.maxPressureThreshold();
+    m_dynamics.maxVelocityThreshold = dynaPref.maxVelocityThreshold();
+    m_dynamics.colorFromTo = dynaPref.colorFromTo();
+
+    DitheringSelector matrixSel(DitheringSelector::SelectMatrix);
+    matrixSel.setSelectedItemIndex(dynaPref.matrixIndex());
+    render::DitheringMatrix matrix(matrixSel.ditheringMatrix());
+    m_dynamics.ditheringMatrix = matrix;
+
+    m_sameInAllTools = Preferences::instance().shared.shareDynamics();
   }
 
 private:
@@ -1247,6 +1281,7 @@ private:
   ContextBar* m_ctxBar;
   mutable tools::DynamicsOptions m_dynamics;
   bool m_optionsGridVisibility = true;
+  bool m_sameInAllTools = false;
 };
 
 class ContextBar::FreehandAlgorithmField : public CheckBox {

src/app/ui/dynamics_popup.cpp
@@ -10,6 +10,8 @@
 
 #include "app/ui/dynamics_popup.h"
 
+#include "app/app.h"
+#include "app/tools/tool_box.h"
 #include "app/ui/dithering_selector.h"
 #include "app/ui/skin/skin_theme.h"
 #include "os/font.h"
@@ -293,37 +295,39 @@ int DynamicsPopup::ditheringIndex() const {
   return 0;
 }
 
-void DynamicsPopup::loadDynamicPref(ToolPreferences* toolPref) {
-  if (toolPref) {
-    auto& dynaPref = toolPref->dynamics;
-    m_dynamics->stabilizer()->setSelected(dynaPref.stabilizer());
-    m_stabilizerFactorBackup = dynaPref.stabilizerFactor();
-    m_dynamics->stabilizerFactor()->setValue(
-      dynaPref.stabilizer() ? m_stabilizerFactorBackup : 0);
-    m_dynamics->minSize()->setValue(dynaPref.minSize());
-    m_dynamics->minAngle()->setValue(dynaPref.minAngle());
-    m_pressureThreshold->minThreshold(dynaPref.minPressureThreshold());
-    m_pressureThreshold->maxThreshold(dynaPref.maxPressureThreshold());
-    m_velocityThreshold->minThreshold(dynaPref.minVelocityThreshold());
-    m_velocityThreshold->maxThreshold(dynaPref.maxVelocityThreshold());
-    m_fromTo = dynaPref.colorFromTo();
+void DynamicsPopup::loadDynamicsPref() {
+  auto& dynaPref = Preferences::instance().tool(
+    App::instance()->activeTool()).dynamics;
+  m_dynamics->stabilizer()->setSelected(dynaPref.stabilizer());
+  m_stabilizerFactorBackup = dynaPref.stabilizerFactor();
+  m_dynamics->stabilizerFactor()->setValue(
+    dynaPref.stabilizer() ? m_stabilizerFactorBackup : 0);
+  m_dynamics->minSize()->setValue(dynaPref.minSize());
+  m_dynamics->minAngle()->setValue(dynaPref.minAngle());
+  m_pressureThreshold->minThreshold(dynaPref.minPressureThreshold());
+  m_pressureThreshold->maxThreshold(dynaPref.maxPressureThreshold());
+  m_velocityThreshold->minThreshold(dynaPref.minVelocityThreshold());
+  m_velocityThreshold->maxThreshold(dynaPref.maxVelocityThreshold());
+  m_fromTo = dynaPref.colorFromTo();
 
-    setCheck(SIZE_WITH_PRESSURE,
-             dynaPref.size() == tools::DynamicSensor::Pressure);
-    setCheck(SIZE_WITH_VELOCITY,
-             dynaPref.size() == tools::DynamicSensor::Velocity);
-    setCheck(ANGLE_WITH_PRESSURE,
-             dynaPref.angle() == tools::DynamicSensor::Pressure);
-    setCheck(ANGLE_WITH_VELOCITY,
-             dynaPref.angle() == tools::DynamicSensor::Velocity);
-    setCheck(GRADIENT_WITH_PRESSURE,
-             dynaPref.gradient() == tools::DynamicSensor::Pressure);
-    setCheck(GRADIENT_WITH_VELOCITY,
-             dynaPref.gradient() == tools::DynamicSensor::Velocity);
+  setCheck(SIZE_WITH_PRESSURE,
+           dynaPref.size() == tools::DynamicSensor::Pressure);
+  setCheck(SIZE_WITH_VELOCITY,
+           dynaPref.size() == tools::DynamicSensor::Velocity);
+  setCheck(ANGLE_WITH_PRESSURE,
+           dynaPref.angle() == tools::DynamicSensor::Pressure);
+  setCheck(ANGLE_WITH_VELOCITY,
+           dynaPref.angle() == tools::DynamicSensor::Velocity);
+  setCheck(GRADIENT_WITH_PRESSURE,
+           dynaPref.gradient() == tools::DynamicSensor::Pressure);
+  setCheck(GRADIENT_WITH_VELOCITY,
+           dynaPref.gradient() == tools::DynamicSensor::Velocity);
 
-    if (m_ditheringSel)
-      m_ditheringSel->setSelectedItemIndex(dynaPref.matrixIndex());
-  }
+  if (m_ditheringSel)
+    m_ditheringSel->setSelectedItemIndex(dynaPref.matrixIndex());
+
+  m_dynamics->sameInAllTools()->setSelected(
+    Preferences::instance().shared.shareDynamics());
 }
 
 tools::DynamicsOptions DynamicsPopup::getDynamics() const
@@ -374,36 +378,12 @@ bool DynamicsPopup::isCheck(int i) const
           ->icon() == theme->parts.dropPixelsOk());
 }
 
-void DynamicsPopup::onValuesChange(ButtonSet::Item* item)
+// Update Pressure/Velocity/Gradient popup's variables visibility
+// according ButtonSet checks.
+// Used after a value change (onValuesChange) and when the popup is
+// displayed (on ContextBar::DynamicsField::switchPopup()).
+void DynamicsPopup::refreshVisibility()
 {
-  auto theme = SkinTheme::get(this);
-  const skin::SkinPartPtr& ok = theme->parts.dropPixelsOk();
-  const int i = (item ? m_dynamics->values()->getItemIndex(item): -1);
-
-  // Switch item off
-  if (item && item->icon().get() == ok.get()) {
-    item->setIcon(nullptr);
-  }
-  else {
-    switch (i) {
-      case SIZE_WITH_PRESSURE:
-      case SIZE_WITH_VELOCITY:
-        setCheck(SIZE_WITH_PRESSURE, i == SIZE_WITH_PRESSURE);
-        setCheck(SIZE_WITH_VELOCITY, i == SIZE_WITH_VELOCITY);
-        break;
-      case ANGLE_WITH_PRESSURE:
-      case ANGLE_WITH_VELOCITY:
-        setCheck(ANGLE_WITH_PRESSURE, i == ANGLE_WITH_PRESSURE);
-        setCheck(ANGLE_WITH_VELOCITY, i == ANGLE_WITH_VELOCITY);
-        break;
-      case GRADIENT_WITH_PRESSURE:
-      case GRADIENT_WITH_VELOCITY:
-        setCheck(GRADIENT_WITH_PRESSURE, i == GRADIENT_WITH_PRESSURE);
-        setCheck(GRADIENT_WITH_VELOCITY, i == GRADIENT_WITH_VELOCITY);
-        break;
-    }
-  }
-
   const bool hasPressure = (isCheck(SIZE_WITH_PRESSURE) ||
                             isCheck(ANGLE_WITH_PRESSURE) ||
                             isCheck(GRADIENT_WITH_PRESSURE));
@@ -457,6 +437,44 @@ void DynamicsPopup::onValuesChange(ButtonSet::Item* item)
 
   m_hotRegion |= gfx::Region(boundsOnScreen());
   setHotRegion(m_hotRegion);
+}
+
+bool DynamicsPopup::sharedSettings() const
+{
+  return m_dynamics->sameInAllTools()->isSelected();
+}
+
+void DynamicsPopup::onValuesChange(ButtonSet::Item* item)
+{
+  auto theme = SkinTheme::get(this);
+  const skin::SkinPartPtr& ok = theme->parts.dropPixelsOk();
+  const int i = (item ? m_dynamics->values()->getItemIndex(item): -1);
+
+  // Switch item off
+  if (item && item->icon().get() == ok.get()) {
+    item->setIcon(nullptr);
+  }
+  else {
+    switch (i) {
+      case SIZE_WITH_PRESSURE:
+      case SIZE_WITH_VELOCITY:
+        setCheck(SIZE_WITH_PRESSURE, i == SIZE_WITH_PRESSURE);
+        setCheck(SIZE_WITH_VELOCITY, i == SIZE_WITH_VELOCITY);
+        break;
+      case ANGLE_WITH_PRESSURE:
+      case ANGLE_WITH_VELOCITY:
+        setCheck(ANGLE_WITH_PRESSURE, i == ANGLE_WITH_PRESSURE);
+        setCheck(ANGLE_WITH_VELOCITY, i == ANGLE_WITH_VELOCITY);
+        break;
+      case GRADIENT_WITH_PRESSURE:
+      case GRADIENT_WITH_VELOCITY:
+        setCheck(GRADIENT_WITH_PRESSURE, i == GRADIENT_WITH_PRESSURE);
+        setCheck(GRADIENT_WITH_VELOCITY, i == GRADIENT_WITH_VELOCITY);
+        break;
+    }
+  }
+
+  refreshVisibility();
 }
 
 void DynamicsPopup::updateFromToText()

src/app/ui/dynamics_popup.h
@@ -37,8 +37,10 @@ namespace app {
 
     tools::DynamicsOptions getDynamics() const;
     void setOptionsGridVisibility(bool state);
-    void loadDynamicPref(ToolPreferences* toolPref);
+    void loadDynamicsPref();
     int ditheringIndex() const;
+    void refreshVisibility();
+    bool sharedSettings() const;
 
   private:
     class ThresholdSlider;