4387432ad33d0b1c3b3e0fe6ac2f5a6321ca42b7
David Capello <david@igara.com>
2023-10-25 11:59:28 -0300

    Take some tests out from a loop they don't depend on

--- src/doc/color_scales_tests.cpp
+++ src/doc/color_scales_tests.cpp
@@ -17,23 +17,18 @@
 
 using namespace doc;
 
-TEST(Scale, MatchValues)
+TEST(ColorScales, MatchValues)
 {
+  for (int v=0; v<8; ++v)
+    EXPECT_EQ(scale_3bits_to_8bits(v), scale_xbits_to_8bits(3, v));
+
+  for (int v=0; v<32; ++v)
+    EXPECT_EQ(scale_5bits_to_8bits(v), scale_xbits_to_8bits(5, v));
+
+  for (int v=0; v<64; ++v)
+    EXPECT_EQ(scale_6bits_to_8bits(v), scale_xbits_to_8bits(6, v));
+
   for (int x=1; x<=8; ++x) {
-    switch (x) {
-      case 3:
-        for (int v=0; v<8; ++v)
-          EXPECT_EQ(scale_3bits_to_8bits(v), scale_xbits_to_8bits(3, v));
-        break;
-      case 5:
-        for (int v=0; v<32; ++v)
-          EXPECT_EQ(scale_5bits_to_8bits(v), scale_xbits_to_8bits(5, v));
-        break;
-      case 6:
-        for (int v=0; v<64; ++v)
-          EXPECT_EQ(scale_6bits_to_8bits(v), scale_xbits_to_8bits(6, v));
-        break;
-    }
     for (int v=0; v<(1<<x); ++v)
       EXPECT_LE(std::abs((255 * v / ((1<<x)-1)) - scale_xbits_to_8bits(x, v)), 1);
   }
