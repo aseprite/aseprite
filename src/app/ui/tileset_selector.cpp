aseprite/aseprite

commit 242555ab06c31fb8f00e70e7b83456ff179a5848

Allow changing the tileset assigned to a tilemap layer

src/app/ui/tileset_selector.cpp
@@ -22,14 +22,18 @@ namespace app {
 using namespace ui;
 
 TilesetSelector::TilesetSelector(const doc::Sprite* sprite,
-                                 const TilesetSelector::Info& info)
+                                 const TilesetSelector::Info& info) : m_info(info)
 {
   initTheme();
 
-  name()->setText(info.name);
-  gridWidth()->setTextf("%d", info.grid.tileSize().w);
-  gridHeight()->setTextf("%d", info.grid.tileSize().h);
-  baseIndex()->setTextf("%d", info.baseIndex);
+  name()->setText(m_info.name);
+  gridWidth()->setTextf("%d", m_info.grid.tileSize().w);
+  gridHeight()->setTextf("%d", m_info.grid.tileSize().h);
+  baseIndex()->setTextf("%d", m_info.baseIndex);
+
+  if (!m_info.allowNewTileset) {
+    tilesets()->deleteAllItems();
+  }
 
   doc::tileset_index tsi = 0;
   for (doc::Tileset* tileset : *sprite->tilesets()) {
@@ -40,40 +44,47 @@ TilesetSelector::TilesetSelector(const doc::Sprite* sprite,
       fmt::format("Tileset #{0} ({1}x{2}): \"{3}\"",
                   tsi,
                   tileset->grid().tileSize().w,
-                  tileset->grid().tileSize().w,
+                  tileset->grid().tileSize().h,
                   tileset->name()));
     tilesets()->addItem(item);
 
-    if (info.tsi == tsi)
+    if (m_info.tsi == tsi)
       tilesets()->setSelectedItem(item);
 
     ++tsi;
   }
 
-  if (info.enabled) {
+  if (m_info.enabled) {
     tilesets()->Change.connect(
       [this, sprite]() {
-        int index = tilesets()->getSelectedItemIndex();
-        bool editable = (index == 0);
-
-        if (index == 0) {
-          name()->setText("");
-          baseIndex()->setTextf("%d", 1);
-        }
-        else {
-          doc::Tileset* ts = sprite->tilesets()->get(index-1);
-          doc::Grid grid = ts->grid();
-          name()->setText(ts->name());
-          gridWidth()->setTextf("%d", grid.tileSize().w);
-          gridHeight()->setTextf("%d", grid.tileSize().h);
-          baseIndex()->setTextf("%d", ts->baseIndex());
-        }
-
-        name()->setEnabled(editable);
-        gridWidth()->setEnabled(editable);
-        gridHeight()->setEnabled(editable);
-        baseIndex()->setEnabled(editable);
+        updateControlsState(sprite->tilesets());
       });
+  }
+  updateControlsState(sprite->tilesets());
+}
+
+void TilesetSelector::updateControlsState(const doc::Tilesets* spriteTilesets)
+{
+  if (m_info.enabled) {
+    int index = getSelectedItemIndex();
+    bool isNewTileset = (index == 0);
+    if (isNewTileset) {
+      name()->setText("");
+      baseIndex()->setTextf("%d", 1);
+    }
+    else {
+      doc::Tileset* ts = spriteTilesets->get(index-1);
+      doc::Grid grid = ts->grid();
+      name()->setText(ts->name());
+      gridWidth()->setTextf("%d", grid.tileSize().w);
+      gridHeight()->setTextf("%d", grid.tileSize().h);
+      baseIndex()->setTextf("%d", ts->baseIndex());
+    }
+
+    name()->setEnabled(isNewTileset || !m_info.allowNewTileset);
+    gridWidth()->setEnabled(isNewTileset);
+    gridHeight()->setEnabled(isNewTileset);
+    baseIndex()->setEnabled(isNewTileset);
   }
   else {
     tilesets()->setEnabled(false);
@@ -84,7 +95,7 @@ TilesetSelector::TilesetSelector(const doc::Sprite* sprite,
 
 TilesetSelector::Info TilesetSelector::getInfo()
 {
-  int itemIndex = tilesets()->getSelectedItemIndex();
+  int itemIndex = getSelectedItemIndex();
   Info info;
   if (itemIndex == 0) {
     gfx::Size sz(std::max(1, gridWidth()->textInt()),
@@ -100,6 +111,17 @@ TilesetSelector::Info TilesetSelector::getInfo()
   info.name = name()->text();
   info.baseIndex = baseIndex()->textInt();
   return info;
+}
+
+int TilesetSelector::getSelectedItemIndex()
+{
+  int index = tilesets()->getSelectedItemIndex();
+  // We have to compensate the selected index when allowNewTileset is false,
+  // because the "New Tileset" item was removed.
+  if (!m_info.allowNewTileset) {
+    index++;
+  }
+  return index;
 }
 
 } // namespace app

src/app/ui/tileset_selector.h
@@ -18,6 +18,7 @@
 namespace doc {
   class Sprite;
   class Tileset;
+  class Tilesets;
 }
 
 namespace app {
@@ -25,8 +26,14 @@ namespace app {
   class TilesetSelector : public app::gen::TilesetSelector {
   public:
     struct Info {
+      // Enables/disables the tileset selector combobox
       bool enabled = true;
-      bool newTileset = true;
+      // When false, removes the "New Tileset" option from the tileset selector combobox
+      bool allowNewTileset = true;
+
+      // Output members that are set when TilesetSelector.getInfo()
+      // is called.
+      bool newTileset = true;  // Set to true when the selected item is "New Tileset"
       std::string name;
       doc::Grid grid;
       int baseIndex = 1;
@@ -36,7 +43,18 @@ namespace app {
     TilesetSelector(const doc::Sprite* sprite,
                     const TilesetSelector::Info& info);
 
+    // Returns the data of this widget according to the user input
     Info getInfo();
+
+  private:
+    void updateControlsState(const doc::Tilesets* spriteTilesets);
+
+    // Returns the selected item index as if the combobox always has the "New Tileset"
+    // as its first item.
+    int getSelectedItemIndex();
+
+    // Holds the information used to create this widget
+    const TilesetSelector::Info m_info;
   };
 
 } // namespace app

src/app/cmd/set_layer_tileset.cpp
@@ -10,6 +10,7 @@
 
 #include "app/cmd/set_layer_tileset.h"
 
+#include "app/app.h"
 #include "app/doc.h"
 #include "doc/layer_tilemap.h"
 #include "doc/sprite.h"
@@ -37,6 +38,13 @@ void SetLayerTileset::onUndo()
   auto layer = static_cast<LayerTilemap*>(this->layer());
   layer->setTilesetIndex(m_oldTsi);
   layer->incrementVersion();
+}
+
+void SetLayerTileset::onFireNotifications()
+{
+  auto layer = static_cast<LayerTilemap*>(this->layer());
+  Doc* doc = static_cast<Doc*>(layer->sprite()->document());
+  doc->notifyTilesetChanged(layer->tileset());
 }
 
 } // namespace cmd

src/app/cmd/set_layer_tileset.h
@@ -29,6 +29,8 @@ namespace cmd {
   protected:
     void onExecute() override;
     void onUndo() override;
+    void onFireNotifications() override;
+
     size_t onMemSize() const override {
       return sizeof(*this);
     }

src/app/commands/cmd_layer_properties.cpp
@@ -13,6 +13,7 @@
 #include "app/cmd/set_layer_blend_mode.h"
 #include "app/cmd/set_layer_name.h"
 #include "app/cmd/set_layer_opacity.h"
+#include "app/cmd/set_layer_tileset.h"
 #include "app/cmd/set_tileset_base_index.h"
 #include "app/cmd/set_tileset_name.h"
 #include "app/cmd/set_user_data.h"
@@ -36,6 +37,7 @@
 #include "doc/layer_tilemap.h"
 #include "doc/sprite.h"
 #include "doc/tileset.h"
+#include "doc/tilesets.h"
 #include "doc/user_data.h"
 #include "ui/ui.h"
 
@@ -355,7 +357,7 @@ private:
 
     // Information about the tileset to be used for new tilemaps
     TilesetSelector::Info tilesetInfo;
-    tilesetInfo.enabled = false;
+    tilesetInfo.allowNewTileset = false;
     tilesetInfo.newTileset = false;
     tilesetInfo.grid = tileset->grid();
     tilesetInfo.name = tileset->name();
@@ -373,9 +375,15 @@ private:
       tilesetInfo = tilesetSel.getInfo();
 
       if (tileset->name() != tilesetInfo.name ||
-          tileset->baseIndex() != tilesetInfo.baseIndex) {
+          tileset->baseIndex() != tilesetInfo.baseIndex ||
+          tilesetInfo.tsi != tilemap->tilesetIndex()) {
         ContextWriter writer(UIContext::instance());
         Tx tx(writer.context(), "Set Tileset Properties");
+        // User changed tilemap's tileset
+        if (tilesetInfo.tsi != tilemap->tilesetIndex()) {
+          tileset = tilemap->sprite()->tilesets()->get(tilesetInfo.tsi);
+          tx(new cmd::SetLayerTileset(tilemap, tilesetInfo.tsi));
+        }
         if (tileset->name() != tilesetInfo.name)
           tx(new cmd::SetTilesetName(tileset, tilesetInfo.name));
         if (tileset->baseIndex() != tilesetInfo.baseIndex)

src/doc/tilesets.h
@@ -35,7 +35,7 @@ namespace doc {
 
     tileset_index add(Tileset* tileset);
 
-    Tileset* get(const tileset_index tsi) {
+    Tileset* get(const tileset_index tsi) const {
       if (tsi < size())
         return m_tilesets[tsi];
       else

commit aeb7157277b01c4b7035efd4eeb7599f95daf106

Fix possibility to edit tileset base index (fix #4163)
Regression from 242555ab06c31fb8f00e70e7b83456ff179a5848

src/app/ui/tileset_selector.cpp
@@ -111,10 +111,10 @@ void TilesetSelector::updateControlsState()
                    ts->matchFlags());
     }
 
-    name()->setEnabled(isNewTileset || !m_info.allowNewTileset);
+    name()->setEnabled(true);
     gridWidth()->setEnabled(isNewTileset);
     gridHeight()->setEnabled(isNewTileset);
-    baseIndex()->setEnabled(isNewTileset);
+    baseIndex()->setEnabled(true);
   }
   else {
     tilesets()->setEnabled(false);

src/app/ui/tileset_selector.h
@@ -26,11 +26,16 @@ namespace app {
   class TilesetSelector : public app::gen::TilesetSelector {
   public:
     struct Info {
-      // Enables/disables the tileset selector combobox
+      // Enables/disables the tileset selector combobox and fields to
+      // edit the tileset data.
       bool enabled = true;
-      // When false, removes the "New Tileset" option from the tileset selector combobox
+
+      // When false, removes the "New Tileset" option from the tileset
+      // selector combobox.
       bool allowNewTileset = true;
-      // When false, we only can select a "New Tileset" option or change the tileset properties
+
+      // When false, we only can select a "New Tileset" option or
+      // change the tileset properties.
       bool allowExistentTileset = true;
 
       // Output members that are set when TilesetSelector.getInfo()