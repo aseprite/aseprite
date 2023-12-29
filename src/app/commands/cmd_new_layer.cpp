aseprite/aseprite

commit 8722c8ec16ab68a69616f1e80362f170f9640494

Lock document in Tx() ctor (part of #2430)
This already fixes a lot of possible problems that can happen when a
script is running and modifying some part of a sprite that is being
backed up in a background thread.

We still need some work to being able to lock a sprite two or more
times in the same thread to write it. E.g. an app.transaction() should
lock the sprite for write access, but the script transaction function
could call a command, and that command could use a ContextWriter to
lock the sprite again. At the moment this is not possible because we
need a re-entrant RWLock implementation.

src/app/commands/cmd_new_layer.cpp
@@ -274,9 +274,8 @@ void NewLayerCommand::onExecute(Context* context)
 
   Layer* layer = nullptr;
   {
-    Tx tx(
-      writer.context(),
-      fmt::format(Strings::commands_NewLayer(), layerPrefix()));
+    Tx tx(writer,
+          fmt::format(Strings::commands_NewLayer(), layerPrefix()));
     DocApi api = document->getApi(tx);
     bool afterBackground = false;
 
@@ -428,7 +427,7 @@ void NewLayerCommand::onExecute(Context* context)
 #ifdef ENABLE_UI
     // Paste new layer from clipboard
     else if (params().fromClipboard() && layer->isImage()) {
-      context->clipboard()->paste(context, false);
+      context->clipboard()->paste(writer, false);
 
       if (layer->isReference()) {
         if (Cel* cel = layer->cel(site.frame())) {

src/app/ui/doc_view.cpp
@@ -594,7 +594,8 @@ bool DocView::onPaste(Context* ctx)
   auto clipboard = ctx->clipboard();
   if (clipboard->format() == ClipboardFormat::Image ||
       clipboard->format() == ClipboardFormat::Tilemap) {
-    clipboard->paste(ctx, true);
+    ContextWriter writer(ctx);
+    clipboard->paste(writer, true);
     return true;
   }
   else
@@ -630,7 +631,7 @@ bool DocView::onClear(Context* ctx)
 
   // TODO This code is similar to clipboard::cut()
   {
-    Tx tx(writer.context(), "Clear");
+    Tx tx(writer, "Clear");
     const bool deselectMask =
       (visibleMask &&
        !Preferences::instance().selection.keepSelectionAfterClear());

src/app/ui/timeline/timeline.cpp
@@ -1387,7 +1387,8 @@ bool Timeline::onProcessMessage(Message* msg)
             if (tag) {
               if ((m_state == STATE_RESIZING_TAG_LEFT && tag->fromFrame() != m_resizeTagData.from) ||
                   (m_state == STATE_RESIZING_TAG_RIGHT && tag->toFrame() != m_resizeTagData.to)) {
-                Tx tx(UIContext::instance(), Strings::commands_FrameTagProperties());
+                ContextWriter writer(UIContext::instance());
+                Tx tx(writer, Strings::commands_FrameTagProperties());
                 tx(new cmd::SetTagRange(
                      tag,
                      (m_state == STATE_RESIZING_TAG_LEFT ? m_resizeTagData.from: tag->fromFrame()),
@@ -4389,7 +4390,9 @@ bool Timeline::onPaste(Context* ctx)
       m_redrawMarchingAntsOnly = false;
       invalidate();
     }
-    clipboard->paste(ctx, true);
+
+    ContextWriter writer(ctx);
+    clipboard->paste(writer, true);
     return true;
   }
   else

src/app/util/clipboard.cpp
@@ -362,7 +362,7 @@ void Clipboard::cut(ContextWriter& writer)
   else {
     // TODO This code is similar to DocView::onClear()
     {
-      Tx tx(writer.context(), "Cut");
+      Tx tx(writer, "Cut");
       Site site = writer.context()->activeSite();
       CelList cels;
       if (site.range().enabled()) {
@@ -461,10 +461,10 @@ void Clipboard::copyPalette(const Palette* palette,
   m_data->picks = picks;
 }
 
-void Clipboard::paste(Context* ctx,
+void Clipboard::paste(ContextWriter& writer,
                       const bool interactive)
 {
-  Site site = ctx->activeSite();
+  const Site site = *writer.site();
   Doc* dstDoc = site.document();
   if (!dstDoc)
     return;
@@ -531,7 +531,7 @@ void Clipboard::paste(Context* ctx,
         if (!dstLayer || !dstLayer->isImage())
           return;
 
-        Tx tx(ctx, "Paste Image");
+        Tx tx(writer, "Paste Image");
         DocApi api = dstDoc->getApi(tx);
         Cel* dstCel = api.addCel(
           static_cast<LayerImage*>(dstLayer), site.frame(),
@@ -611,7 +611,7 @@ void Clipboard::paste(Context* ctx,
             break;
           }
 
-          Tx tx(ctx, "Paste Cels");
+          Tx tx(writer, "Paste Cels");
           DocApi api = dstDoc->getApi(tx);
 
           // Add extra frames if needed
@@ -671,7 +671,7 @@ void Clipboard::paste(Context* ctx,
             break;
           }
 
-          Tx tx(ctx, "Paste Frames");
+          Tx tx(writer, "Paste Frames");
           DocApi api = dstDoc->getApi(tx);
 
           auto srcLayers = srcSpr->allBrowsableLayers();
@@ -714,7 +714,7 @@ void Clipboard::paste(Context* ctx,
           if (srcDoc->colorMode() != dstDoc->colorMode())
             throw std::runtime_error("You cannot copy layers of document with different color modes");
 
-          Tx tx(ctx, "Paste Layers");
+          Tx tx(writer, "Paste Layers");
           DocApi api = dstDoc->getApi(tx);
 
           // Remove children if their parent is selected so we only

src/app/util/clipboard.h
@@ -74,7 +74,8 @@ namespace app {
                      const doc::Tileset* tileset);
     void copyPalette(const doc::Palette* palette,
                      const doc::PalettePicks& picks);
-    void paste(Context* ctx, const bool interactive);
+    void paste(ContextWriter& writer,
+               const bool interactive);
 
     doc::ImageRef getImage(doc::Palette* palette);
 

commit f178941f2cd799c400118e30343f5266b6d7c220

Fix crash copying, pasting, and transforming selection (fix #4249)
Regression introduced with 8722c8ec16ab68a69616f1e80362f170f9640494
and d3562b140ce12267693675f3f1f722a2670774ec.

Our re-entrant RWLock implementation is tricky because it doesn't
support to unlock in different order than it was locked.

E.g. If we create a DocWriter (e.g. paste command), and try to create
a DocReader (e.g. PixelsMovement) inside the DocWriter scope, the
reader will return a LockResult::Reentrant, but if we unlock the
write, then the reader cannot be used to upgradeToWrite() because the
lock will have just 0 locks (the re-entrant one didn't modify the
count of the RWLock). This is because it was thought that the
DocReader was going to be unlocked before the writer lock (in the
exact inverse order).

Basically these re-entrant RWLocks will not work fine if we mix up the
order of locks/unlocks in the same thread.

src/app/commands/cmd_new_layer.cpp
@@ -256,9 +256,8 @@ void NewLayerCommand::onExecute(Context* context)
   }
 #endif
 
-  ContextWriter writer(reader);
   LayerGroup* parent = sprite->root();
-  Layer* activeLayer = writer.layer();
+  Layer* activeLayer = reader.layer();
   SelectedLayers selLayers = site.selectedLayers();
   if (activeLayer) {
     if (activeLayer->isGroup() &&
@@ -274,6 +273,7 @@ void NewLayerCommand::onExecute(Context* context)
 
   Layer* layer = nullptr;
   {
+    ContextWriter writer(reader);
     Tx tx(writer,
           fmt::format(Strings::commands_NewLayer(), layerPrefix()));
     DocApi api = document->getApi(tx);
@@ -427,7 +427,7 @@ void NewLayerCommand::onExecute(Context* context)
 #ifdef ENABLE_UI
     // Paste new layer from clipboard
     else if (params().fromClipboard() && layer->isImage()) {
-      context->clipboard()->paste(writer, false);
+      context->clipboard()->paste(context, false);
 
       if (layer->isReference()) {
         if (Cel* cel = layer->cel(site.frame())) {

src/app/ui/doc_view.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2018-2022  Igara Studio S.A.
+// Copyright (C) 2018-2023  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -594,8 +594,7 @@ bool DocView::onPaste(Context* ctx)
   auto clipboard = ctx->clipboard();
   if (clipboard->format() == ClipboardFormat::Image ||
       clipboard->format() == ClipboardFormat::Tilemap) {
-    ContextWriter writer(ctx);
-    clipboard->paste(writer, true);
+    clipboard->paste(ctx, true);
     return true;
   }
   else

src/app/ui/timeline/timeline.cpp
@@ -4391,8 +4391,7 @@ bool Timeline::onPaste(Context* ctx)
       invalidate();
     }
 
-    ContextWriter writer(ctx);
-    clipboard->paste(writer, true);
+    clipboard->paste(ctx, true);
     return true;
   }
   else

src/app/util/clipboard.cpp
@@ -461,10 +461,10 @@ void Clipboard::copyPalette(const Palette* palette,
   m_data->picks = picks;
 }
 
-void Clipboard::paste(ContextWriter& writer,
+void Clipboard::paste(Context* ctx,
                       const bool interactive)
 {
-  const Site site = *writer.site();
+  const Site site = ctx->activeSite();
   Doc* dstDoc = site.document();
   if (!dstDoc)
     return;
@@ -531,6 +531,7 @@ void Clipboard::paste(ContextWriter& writer,
         if (!dstLayer || !dstLayer->isImage())
           return;
 
+        ContextWriter writer(ctx);
         Tx tx(writer, "Paste Image");
         DocApi api = dstDoc->getApi(tx);
         Cel* dstCel = api.addCel(
@@ -611,6 +612,7 @@ void Clipboard::paste(ContextWriter& writer,
             break;
           }
 
+          ContextWriter writer(ctx);
           Tx tx(writer, "Paste Cels");
           DocApi api = dstDoc->getApi(tx);
 
@@ -671,6 +673,7 @@ void Clipboard::paste(ContextWriter& writer,
             break;
           }
 
+          ContextWriter writer(ctx);
           Tx tx(writer, "Paste Frames");
           DocApi api = dstDoc->getApi(tx);
 
@@ -714,6 +717,7 @@ void Clipboard::paste(ContextWriter& writer,
           if (srcDoc->colorMode() != dstDoc->colorMode())
             throw std::runtime_error("You cannot copy layers of document with different color modes");
 
+          ContextWriter writer(ctx);
           Tx tx(writer, "Paste Layers");
           DocApi api = dstDoc->getApi(tx);
 

src/app/util/clipboard.h
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2019-2021  Igara Studio S.A.
+// Copyright (C) 2019-2023  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -74,7 +74,7 @@ namespace app {
                      const doc::Tileset* tileset);
     void copyPalette(const doc::Palette* palette,
                      const doc::PalettePicks& picks);
-    void paste(ContextWriter& writer,
+    void paste(Context* ctx,
                const bool interactive);
 
     doc::ImageRef getImage(doc::Palette* palette);