aseprite/aseprite

commit aeeef8e255c2d83a1b3e3cb2858939233d5a5d0b

Add suppor for doc::Image row stride size > width size
This patch solves several problems introducing the possibility to
specify a row stride bigger than the width (visible pixels) on each
image row. Useful in case that we want to align the initial pixel
address of each row (if DOC_USE_ALIGNED_PIXELS is defined).

This allows us to use some SIMD intrinsics (e.g. SSE2) for some image
functions in the future (right now implemented only in the new
is_same_image_simd_templ() for is_same_image()).

Anyway to avoid breaking some existing code, by default we'll still
keep the old behavior: row stride bytes = width bytes (so
DOC_USE_ALIGNED_PIXELS is undefined).

src/doc/image_buffer.h
@@ -1,5 +1,5 @@
 // Aseprite Document Library
-// Copyright (C) 2019  Igara Studio S.A.
+// Copyright (C) 2019-2023  Igara Studio S.A.
 // Copyright (C) 2001-2016  David Capello
 //
 // This file is released under the terms of the MIT license.
@@ -9,32 +9,52 @@
 #define DOC_IMAGE_BUFFER_H_INCLUDED
 #pragma once
 
+#include "base/disable_copying.h"
 #include "base/ints.h"
+#include "doc/aligned_memory.h"
 
+#include <algorithm>
 #include <cstddef>
+#include <cstdlib>
 #include <memory>
-#include <vector>
 
 namespace doc {
 
   class ImageBuffer {
   public:
-    ImageBuffer(std::size_t size = 1) : m_buffer(size) {
+    ImageBuffer(std::size_t size = 1)
+      : m_size(doc_align_size(size))
+      , m_buffer((uint8_t*)doc_aligned_alloc(m_size)) {
     }
 
-    std::size_t size() const { return m_buffer.size(); }
-    uint8_t* buffer() { return &m_buffer[0]; }
+    ~ImageBuffer() noexcept {
+      if (m_buffer)
+        doc_aligned_free(m_buffer);
+    }
+
+    std::size_t size() const { return m_size; }
+    uint8_t* buffer() { return (uint8_t*)m_buffer; }
 
     void resizeIfNecessary(std::size_t size) {
-      if (size > m_buffer.size())
-        m_buffer.resize(size);
+      if (size > m_size) {
+        if (m_buffer) {
+          doc_aligned_free(m_buffer);
+          m_buffer = nullptr;
+        }
+
+        m_size = doc_align_size(size);
+        m_buffer = (uint8_t*)doc_aligned_alloc(m_size);
+      }
     }
 
   private:
-    std::vector<uint8_t> m_buffer;
+    size_t m_size;
+    uint8_t* m_buffer;
+
+    DISABLE_COPYING(ImageBuffer);
   };
 
-  typedef std::shared_ptr<ImageBuffer> ImageBufferPtr;
+  using ImageBufferPtr = std::shared_ptr<ImageBuffer>;
 
 } // namespace doc
 

src/app/app_brushes.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2020-2022  Igara Studio S.A.
+// Copyright (C) 2020-2023  Igara Studio S.A.
 // Copyright (C) 2001-2016  David Capello
 //
 // This program is distributed under the terms of
@@ -128,7 +128,7 @@ void save_xml_image(TiXmlElement* imageElem, const Image* image)
     imageElem->SetAttribute("format", format.c_str());
 
   base::buffer data;
-  data.reserve(h * image->getRowStrideSize());
+  data.reserve(h * image->widthBytes());
   switch (image->pixelFormat()) {
     case IMAGE_RGB:{
       const LockImageBits<RgbTraits> pixels(image);

src/app/cmd/copy_rect.cpp
@@ -1,4 +1,5 @@
 // Aseprite
+// Copyright (C) 2023  Igara Studio S.A.
 // Copyright (C) 2001-2015  David Capello
 //
 // This program is distributed under the terms of
@@ -28,7 +29,7 @@ CopyRect::CopyRect(Image* dst, const Image* src, const gfx::Clip& clip)
 
   // Fill m_data with "src" data
 
-  int lineSize = src->getRowStrideSize(m_clip.size.w);
+  int lineSize = src->bytesPerPixel() * m_clip.size.w;
   m_data.resize(lineSize * m_clip.size.h);
 
   auto it = m_data.begin();
@@ -82,7 +83,7 @@ void CopyRect::swap()
 
 int CopyRect::lineSize()
 {
-  return image()->getRowStrideSize(m_clip.size.w);
+  return image()->bytesPerPixel() * m_clip.size.w;
 }
 
 } // namespace cmd

src/app/file/ase_format.cpp
@@ -110,9 +110,7 @@ public:
                      m_image->height());
   }
   int getScanlineSize() const override {
-    return doc::calculate_rowstride_bytes(
-      m_image->pixelFormat(),
-      m_image->width());
+    return m_image->widthBytes();
   }
   const uint8_t* getScanlineAddress(int y) const override {
     return m_image->getPixelAddress(0, y);
@@ -128,9 +126,8 @@ public:
                      m_tileset->grid().tileSize().h * m_tileset->size());
   }
   int getScanlineSize() const override {
-    return doc::calculate_rowstride_bytes(
-      m_tileset->sprite()->pixelFormat(),
-      m_tileset->grid().tileSize().w);
+    return bytes_per_pixel_for_colormode(m_tileset->sprite()->colorMode())
+      * m_tileset->grid().tileSize().w;
   }
   const uint8_t* getScanlineAddress(int y) const override {
     const int h = m_tileset->grid().tileSize().h;

src/app/file/fli_format.cpp
@@ -104,7 +104,7 @@ bool FliFormat::onLoad(FileOp* fop)
   flic::Frame fliFrame;
   flic::Colormap oldFliColormap;
   fliFrame.pixels = bmp->getPixelAddress(0, 0);
-  fliFrame.rowstride = IndexedTraits::getRowStrideBytes(bmp->width());
+  fliFrame.rowstride = bmp->rowBytes();
 
   frame_t frame_out = 0;
   for (frame_t frame_in=0;
@@ -229,7 +229,7 @@ bool FliFormat::onSave(FileOp* fop)
   // Write frame by frame
   flic::Frame fliFrame;
   fliFrame.pixels = bmp->getPixelAddress(0, 0);
-  fliFrame.rowstride = IndexedTraits::getRowStrideBytes(bmp->width());
+  fliFrame.rowstride = bmp->rowBytes();
 
   auto frame_beg = fop->roi().selectedFrames().begin();
   auto frame_end = fop->roi().selectedFrames().end();

src/app/file/tga_format.cpp
@@ -174,8 +174,8 @@ bool TgaFormat::onLoad(FileOp* fop)
 
   tga::Image tgaImage;
   tgaImage.pixels = image->getPixelAddress(0, 0);
-  tgaImage.rowstride = image->getRowStrideSize();
-  tgaImage.bytesPerPixel = image->getRowStrideSize(1);
+  tgaImage.rowstride = image->rowBytes();
+  tgaImage.bytesPerPixel = image->bytesPerPixel();
 
   // Read image
   TgaDelegate delegate(fop);
@@ -312,8 +312,8 @@ bool TgaFormat::onSave(FileOp* fop)
   doc::ImageRef image = img->getScaledImage();
   tga::Image tgaImage;
   tgaImage.pixels = image->getPixelAddress(0, 0);
-  tgaImage.rowstride = image->getRowStrideSize();
-  tgaImage.bytesPerPixel = image->getRowStrideSize(1);
+  tgaImage.rowstride = image->rowBytes();
+  tgaImage.bytesPerPixel = image->bytesPerPixel();
 
   TgaDelegate delegate(fop);
   encoder.writeImage(header, tgaImage);

src/app/file/webp_format.cpp
@@ -180,8 +180,11 @@ bool WebPFormat::onLoad(FileOp* fop)
 
     Cel* cel = layer->cel(f);
     if (cel) {
-      memcpy(cel->image()->getPixelAddress(0, 0),
-             frame_rgba, h*w*sizeof(uint32_t));
+      const uint32_t* src = (const uint32_t*)frame_rgba;
+      for (int y=0; y<h; ++y, src+=w) {
+        memcpy(cel->image()->getPixelAddress(0, y),
+               src, w*sizeof(uint32_t));
+      }
 
       if (!has_alpha) {
         const uint32_t* src = (const uint32_t*)frame_rgba;
@@ -310,7 +313,7 @@ bool WebPFormat::onSave(FileOp* fop)
   pic.height = h;
   pic.use_argb = true;
   pic.argb = (uint32_t*)image->getPixelAddress(0, 0);
-  pic.argb_stride = w;
+  pic.argb_stride = image->rowPixels(); // Stride in pixels (not bytes)
   pic.user_data = &wd;
   pic.progress_hook = progress_report;
 

src/app/render/shader_renderer.cpp
@@ -418,7 +418,7 @@ void ShaderRenderer::drawImage(SkCanvas* canvas,
 {
   auto skData = SkData::MakeWithoutCopy(
     (const void*)srcImage->getPixelAddress(0, 0),
-    srcImage->getMemSize());
+    srcImage->rowBytes() * srcImage->height());
 
   switch (srcImage->colorMode()) {
 
@@ -429,7 +429,7 @@ void ShaderRenderer::drawImage(SkCanvas* canvas,
                           kRGBA_8888_SkColorType,
                           kUnpremul_SkAlphaType),
         skData,
-        srcImage->getRowStrideSize());
+        srcImage->rowBytes());
 
       SkPaint p;
       p.setAlpha(opacity);
@@ -450,7 +450,7 @@ void ShaderRenderer::drawImage(SkCanvas* canvas,
                           kR8G8_unorm_SkColorType,
                           kOpaque_SkAlphaType),
         skData,
-        srcImage->getRowStrideSize());
+        srcImage->rowBytes());
 
       SkRuntimeShaderBuilder builder(m_grayscaleEffect);
       builder.child("iImg") = skImg->makeRawShader(SkSamplingOptions(SkFilterMode::kNearest));
@@ -478,7 +478,7 @@ void ShaderRenderer::drawImage(SkCanvas* canvas,
                           kAlpha_8_SkColorType,
                           kUnpremul_SkAlphaType),
         skData,
-        srcImage->getRowStrideSize());
+        srcImage->rowBytes());
 
       // Use the palette data as an "width x height" image where
       // width=number of palette colors, and height=1

commit d70801e88b997831abd16d7d4e58e9d39ee66aa3

Throw a bad_alloc when there is not enough memory for ImageBuffer (fix #4316)
Fix regression introduced in aeeef8e255c2d83a1b3e3cb2858939233d5a5d0b
when we replaced the std::vector with aligned memory allocations (or
just malloc/free).

src/doc/image_buffer.h
@@ -1,5 +1,5 @@
 // Aseprite Document Library
-// Copyright (C) 2019-2023  Igara Studio S.A.
+// Copyright (C) 2019-2024  Igara Studio S.A.
 // Copyright (C) 2001-2016  David Capello
 //
 // This file is released under the terms of the MIT license.
@@ -25,6 +25,8 @@ namespace doc {
     ImageBuffer(std::size_t size = 1)
       : m_size(doc_align_size(size))
       , m_buffer((uint8_t*)doc_aligned_alloc(m_size)) {
+      if (!m_buffer)
+        throw std::bad_alloc();
     }
 
     ~ImageBuffer() noexcept {
@@ -44,6 +46,8 @@ namespace doc {
 
         m_size = doc_align_size(size);
         m_buffer = (uint8_t*)doc_aligned_alloc(m_size);
+        if (!m_buffer)
+          throw std::bad_alloc();
       }
     }
 