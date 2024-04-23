a749595a928b08535ca3fcd69d4aef1c12fc10d6
David Capello <david@igara.com>
2024-04-23 16:06:54 -0300

    Update saving/loading dock layouts w/tinyxml2 library

--- src/app/ui/layout.h
+++ src/app/ui/layout.h
@@ -11,7 +11,7 @@
 #include <memory>
 #include <string>
 
-class TiXmlElement;
+#include "tinyxml2.h"
 
 namespace app {
 class Dock;
@@ -24,12 +24,12 @@ public:
   static constexpr const char* kDefault = "_default_";
   static constexpr const char* kMirroredDefault = "_mirrored_default_";
 
-  static LayoutPtr MakeFromXmlElement(const TiXmlElement* layoutElem);
+  static LayoutPtr MakeFromXmlElement(const tinyxml2::XMLElement* layoutElem);
   static LayoutPtr MakeFromDock(const std::string& id, const std::string& name, const Dock* dock);
 
   const std::string& id() const { return m_id; }
   const std::string& name() const { return m_name; }
-  const TiXmlElement* xmlElement() const { return m_elem.get(); }
+  const tinyxml2::XMLElement* xmlElement() const { return m_elem; }
 
   bool matchId(const std::string& id) const;
   bool loadLayout(Dock* dock) const;
@@ -37,7 +37,8 @@ public:
 private:
   std::string m_id;
   std::string m_name;
-  std::unique_ptr<TiXmlElement> m_elem;
+  tinyxml2::XMLDocument m_dummyDoc;
+  tinyxml2::XMLElement* m_elem = nullptr;
 };
 
 } // namespace app
