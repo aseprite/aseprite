d1f7c96788783c8c8cc94816a7365826014d605a
Gaspar Capello <gaspar@igara.com>
2024-04-26 09:37:04 -0300

    Add findBestFit function with FitCriteria to RgbMapBase

--- src/doc/rgbmap_base.h
+++ src/doc/rgbmap_base.h
@@ -8,12 +8,20 @@
 #define DOC_RGBMAP_BASE_H_INCLUDED
 #pragma once
 
+#include "doc/fit_criteria.h"
 #include "doc/palette.h"
 
 namespace doc {
 
 class RgbMapBase {
+public:
+  int findBestfit(int r, int g, int b, int a,
+                  int mask_index) const;
+private:
+  void rgbToOtherSpace(double& r, double& g, double& b) const;
+
 protected:
+  FitCriteria m_fitCriteria;
   const Palette* m_palette = nullptr;
   int m_modifications = 0;
   int m_maskIndex = 0;
