aseprite/aseprite

commit db639072f77daf724f9eeb0642d7de61924db973

Show relative path when exporting sprite (fix #4059)
When exporting a file that was already saved, shows the relative path
to the original path instead of only the filename.

src/app/ui/export_file_window.cpp
@@ -192,8 +192,14 @@ void ExportFileWindow::setAniDir(const doc::AniDir aniDir)
 
 void ExportFileWindow::setOutputFilename(const std::string& pathAndFilename)
 {
-  m_outputPath = base::get_file_path(pathAndFilename);
-  m_outputFilename = base::get_file_name(pathAndFilename);
+  if (base::get_file_path(m_doc->filename()).empty()) {
+    m_outputPath = base::get_file_path(pathAndFilename);
+    m_outputFilename = base::get_file_name(pathAndFilename);
+  }
+  else {
+    m_outputPath = base::get_file_path(m_doc->filename());
+    m_outputFilename = base::get_relative_path(pathAndFilename, base::get_file_path(m_doc->filename()));
+  }
 
   updateOutputFilenameEntry();
 }

commit 1ca4306f9d93573e8fe5063be11a4c5b00da66bc

[win] Fix File > Export > Export As to other drive (fix #4607)
This only affects the Windows platform.
Regression introduced in db639072f77daf724f9eeb0642d7de61924db973

src/app/ui/export_file_window.cpp
@@ -199,6 +199,12 @@ void ExportFileWindow::setOutputFilename(const std::string& pathAndFilename)
   else {
     m_outputPath = base::get_file_path(m_doc->filename());
     m_outputFilename = base::get_relative_path(pathAndFilename, base::get_file_path(m_doc->filename()));
+
+    // Cannot find a relative path (e.g. we selected other drive)
+    if (m_outputFilename == pathAndFilename) {
+      m_outputPath = base::get_file_path(pathAndFilename);
+      m_outputFilename = base::get_file_name(pathAndFilename);
+    }
   }
 
   updateOutputFilenameEntry();