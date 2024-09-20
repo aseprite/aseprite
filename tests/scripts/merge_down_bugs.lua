b40614ca36251ee711982b51a94dfa7608f96608
دانتي باولا <38820379+Liebranca@users.noreply.github.com>
2024-09-20 17:25:09 -0300

    Use FlattenLayers for MergeDownLayerCommand (#4643)
    Rework of the cmd::FlattenLayers implementation to accommodates the
    'Merge Down' command as a special case.

--- tests/scripts/merge_down_bugs.lua
+++ tests/scripts/merge_down_bugs.lua
@@ -25,3 +25,24 @@ do
   assert(#s.cels == 2)
   assert(s.cels[1].image:isEqual(s.cels[2].image))
 end
+
+-- Checks that merge down accounts for z-index (issue #4468)
+do
+  local s = Sprite(32, 32)
+  local a = s.layers[1]
+  app.useTool{ color=red, layer=a, points={ Point(1, 1) }}
+
+  local b = s:newLayer()
+  app.useTool{ color=blue, layer=b, points={ Point(1, 1) }}
+
+  a.cels[1].zIndex = 1
+  b.cels[1].zIndex = 0
+
+  local before = s.cels[1].image:clone()
+
+  app.activeLayer = b
+  app.command.MergeDownLayer()
+
+  local after = s.cels[1].image
+  assert(before:isEqual(after))
+end
