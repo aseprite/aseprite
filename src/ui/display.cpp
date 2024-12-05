aseprite/aseprite

commit 2e13559bbb211e79f5a031daa7b241a98b75895d

Fix crash displaying the Preview window in multiple windows (fix #4864)
We were using a backLayer with a nullptr surface.

src/ui/display.cpp
@@ -44,6 +44,9 @@ Display::Display(Display* parentDisplay,
          m_containedWidget->type() == kWindowWidget);
 #endif
   m_dirtyRegion = bounds();
+
+  // Configure the back layer surface.
+  configureBackLayer();
 }
 
 os::SurfaceRef Display::nativeSurface() const

src/ui/view.cpp
@@ -353,7 +353,7 @@ void View::onSetViewScroll(const gfx::Point& pt)
   // scrolled region (which is inverse to the scroll position
   // delta/movement).
   const Point delta = oldScroll - newScroll;
-  if (!display->nativeWindow()->gpuAcceleration()) {
+  if (display && !display->nativeWindow()->gpuAcceleration()) {
     // The movable region includes the given "validRegion"
     // intersecting itself when it's in the new position, so we don't
     // overlap regions outside the "validRegion".

commit 890e27b0a13ee384e9474b0d04d3d838ff4c568c

Fix UI tests
Fixed a crash accessing a nullptr native window running UI tests from CLI.
Regression from 2e13559bbb211e79f5a031daa7b241a98b75895d

src/ui/display.cpp
@@ -51,7 +51,10 @@ Display::Display(Display* parentDisplay,
 
 os::SurfaceRef Display::nativeSurface() const
 {
-  return base::AddRef(m_nativeWindow->surface());
+  if (m_nativeWindow)
+    return base::AddRef(m_nativeWindow->surface());
+  else
+    return nullptr;
 }
 
 void Display::addLayer(const UILayerRef& layer)
@@ -76,6 +79,9 @@ void Display::removeLayer(const UILayerRef& layer)
 void Display::configureBackLayer()
 {
   const os::SurfaceRef displaySurface = nativeSurface();
+  if (!displaySurface)
+    return;
+
   UILayerRef layer = backLayer();
 
   os::SurfaceRef layerSurface = layer->surface();