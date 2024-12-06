6c45e7b441eec5c0380bc36ff60de7faa8647ef9
Liebranca <dantepaola25@gmail.com>
2024-12-06 16:00:34 -0300

    Move 'OutputFile' logic to FilenameEntry class

--- src/app/ui/filename_field.h
+++ src/app/ui/filename_field.h
@@ -23,10 +23,16 @@ public:
 
   FilenameField(const Type type, const std::string& pathAndFilename);
 
-  std::string filename() const;
-  void setFilename(const std::string& fn);
+  std::string filepath() const { return m_path; };
+  std::string filename() const { return m_file; };
+  std::string fullFilename() const;
+  std::string displayedFilename() const;
+  void setFilename(const std::string& pathAndFilename);
+  void setFilenameQuiet(const std::string& fn) { m_file = fn; };
+  void setShowFullPath(const bool fullPath);
+  void setDocFilename(const std::string& fn) { m_docFilename = fn; };
 
-  obs::signal<std::string()> SelectFile;
+  obs::signal<std::string()> SelectOutputFile;
   obs::signal<void()> Change;
 
 protected:
@@ -34,10 +40,15 @@ protected:
   void onInitTheme(ui::InitThemeEvent& ev) override;
 
 private:
+  void onBrowse();
   void updateWidgets();
+  const std::string updatedFilename() const;
 
+  bool m_showFullPath;
   std::string m_path;
+  std::string m_pathBase;
   std::string m_file;
+  std::string m_docFilename;
   ui::Entry* m_entry;
   ui::Button m_button;
 };
