c8e9b33ad34df6effb518fb8e9365c09d5f6263c
David Capello <david@igara.com>
2024-12-17 11:05:43 -0300

    Fix bug where the ToolBar popup wasn't being opened
    This was a new issue with combination of
    9b90159d1bdd019e24ef41f6fc76650843d99c2c, probably we should close the
    popup when the window is resized (instead of onSizeHint()).

--- src/app/ui/toolbar.cpp
+++ src/app/ui/toolbar.cpp
@@ -308,10 +308,14 @@ void ToolBar::onSizeHint(SizeHintEvent& ev)
   iconsize.h += border().height();
   ev.setSizeHint(iconsize);
 
+#if 0 // The Dock widget will ask for sizeHint() of this widget when
+      // we open the popup, so we cannot close the recently closed
+      // popup.
   if (m_popupWindow) {
     closePopupWindow();
     closeTipWindow();
   }
+#endif
 }
 
 void ToolBar::onResize(ui::ResizeEvent& ev)
