aseprite/aseprite

commit db639072f77daf724f9eeb0642d7de61924db973

Show relative path when exporting sprite (fix #4059)
When exporting a file that was already saved, shows the relative path
to the original path instead of only the filename.

src/app/ui/export_file_window.cpp
@@ -192,8 +192,14 @@ void ExportFileWindow::setAniDir(const doc::AniDir aniDir)
 
 void ExportFileWindow::setOutputFilename(const std::string& pathAndFilename)
 {
-  m_outputPath = base::get_file_path(pathAndFilename);
-  m_outputFilename = base::get_file_name(pathAndFilename);
+  if (base::get_file_path(m_doc->filename()).empty()) {
+    m_outputPath = base::get_file_path(pathAndFilename);
+    m_outputFilename = base::get_file_name(pathAndFilename);
+  }
+  else {
+    m_outputPath = base::get_file_path(m_doc->filename());
+    m_outputFilename = base::get_relative_path(pathAndFilename, base::get_file_path(m_doc->filename()));
+  }
 
   updateOutputFilenameEntry();
 }

commit c04ddc667540bd76d3364a9ba48af6fd6f5e9a87

Fix recent files/folders repeating items (fix #4851)
Prior to this fix, duplicate folders and files were displayed on
the Home tab. Additionally, paths were sometimes not well-formed and
included "/.." strings.

Regression introduced in #4389

src/app/ui/export_file_window.cpp
@@ -123,7 +123,7 @@ void ExportFileWindow::savePref()
 
 std::string ExportFileWindow::outputFilenameValue() const
 {
-  return base::join_path(m_outputPath, m_outputFilename);
+  return base::get_absolute_path(base::join_path(m_outputPath, m_outputFilename));
 }
 
 double ExportFileWindow::resizeValue() const