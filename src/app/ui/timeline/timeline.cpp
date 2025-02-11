5b64f01c7643f721227f704de706a4378abe2ddd
Gaspar Capello <gaspar@igara.com>
2025-02-11 13:46:13 -0300

    Fix onionskin visiblity is not updated if cel is empty (fix #4969)

--- src/app/ui/timeline/timeline.cpp
+++ src/app/ui/timeline/timeline.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2018-2024  Igara Studio S.A.
+// Copyright (C) 2018-2025  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -1953,6 +1953,8 @@ void Timeline::onAfterLayerVisibilityChange(DocEvent& ev)
   layer_t layerIdx = getLayerIndex(ev.layer());
   if (layerIdx >= 0)
     invalidateRect(getPartBounds(Hit(PART_ROW_EYE_ICON, layerIdx)).offset(origin()));
+  if (docPref().onionskin.active())
+    m_document->notifyGeneralUpdate();
 }
 
 void Timeline::onStateChanged(Editor* editor)
