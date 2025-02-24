1cb4a8189d7d9d36c3bc6cf6a2bb70e04515498d
Gaspar Capello <gaspar@igara.com>
2025-02-24 13:27:33 -0300

    Fix cel.image:clear() cannot be undone (fix #5015)

--- src/app/cmd/clear_rect.h
+++ src/app/cmd/clear_rect.h
@@ -1,4 +1,5 @@
 // Aseprite
+// Copyright (C)      2025  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -25,7 +26,9 @@ using namespace doc;
 
 class ClearRect : public Cmd {
 public:
+  // rgba_rgb_mask chosen as an improbable color
   ClearRect(Cel* cel, const gfx::Rect& bounds);
+  ClearRect(Cel* cel, const gfx::Rect& bounds, color_t color);
 
 protected:
   void onExecute() override;
@@ -37,6 +40,7 @@ protected:
   }
 
 private:
+  void initialize(Cel* cel, const gfx::Rect& bounds, color_t color);
   void clear();
   void restore();
 
