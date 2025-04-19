92b472ae4ba66a2f42353509f2f7d4f59af24697
Christian Kaiser <info@ckaiser.com.ar>
2025-04-19 12:51:32 -0300

    Re-add os.rename and os.remove support

--- tests/scripts/os.lua
+++ tests/scripts/os.lua
@@ -12,3 +12,13 @@ print("Start ", start_clock)
 local end_clock = os.clock()
 print("End ", end_clock, " Elapsed ", end_clock - start_clock)
 assert(start_clock < end_clock)
+
+-- os.rename & remove
+assert(app.fs.makeDirectory("_os_tmp"))
+assert(os.rename("_os_tmp", "_os_tmp_renamed"))
+assert(not os.rename("_os_tmp", "_os_tmp_shouldfail"))
+assert(not os.remove("_os_tmp_shouldfail"))
+assert(not os.rename("_os_tmp_shouldfail", "_test1.png")) -- Should fail, already exists
+assert(os.remove("_os_tmp_renamed"))
+assert(not app.fs.isDirectory("_os_tmp"))
+assert(not app.fs.isDirectory("_os_tmp_renamed"))
