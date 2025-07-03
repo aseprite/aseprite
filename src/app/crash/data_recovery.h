b7bfc5b83c57d3c60783f14f7c8e0c8b1910e1c1
Gaspar Capello <gaspar@igara.com>
2025-07-03 17:45:14 -0300

    Fix session 'isRunning' detection (fix #5252)
    Before this fix, sometimes files available for recovery aren't
    displayed due to the coincidence of the pid number of a crashed
    session with the current pid number of the current session. This
    coincidence caused Aseprite to falsely detect that it was
    the current session.

--- src/app/crash/data_recovery.h
+++ src/app/crash/data_recovery.h
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2019-2021  Igara Studio S.A.
+// Copyright (C) 2019-2025  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -44,6 +44,8 @@ public:
 
   // Returns a copy of the list of sessions that can be recovered.
   Sessions sessions();
+
+  bool isRunningSession(const SessionPtr& session) const;
 
   // Triggered in the UI-thread from the m_thread using an
   // ui::execute_from_ui_thread() when the list of sessions is ready
