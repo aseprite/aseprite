aseprite/aseprite

commit 1a6a39700e680e15c430d1aaa330202846bc8b62

Fix suffix separation in Entry fields w/TTF fonts (fix #5261)

src/app/ui/skin/skin_theme.cpp
@@ -1350,6 +1350,7 @@ public:
     , m_h(h)
   {
     m_widget->getEntryThemeInfo(&m_index, &m_caret, &m_state, &m_range);
+    m_suffixIndex = m_widget->text().size();
   }
 
   int index() const { return m_index; }
@@ -1368,6 +1369,11 @@ public:
     auto& colors = theme->colors;
     bg = ColorNone;
     fg = colors.text();
+
+    // Suffix text
+    if (m_index >= m_suffixIndex) {
+      fg = colors.entrySuffix();
+    }
 
     // Selected
     if ((m_index >= m_range.from) && (m_index < m_range.to)) {
@@ -1433,6 +1439,7 @@ private:
   int m_lastX; // Last position used to fill the background
   int m_y, m_h;
   int m_charStartX;
+  int m_suffixIndex;
 };
 
 } // anonymous namespace
@@ -1445,8 +1452,10 @@ void SkinTheme::drawEntryText(ui::Graphics* g, ui::Entry* widget)
   DrawEntryTextDelegate delegate(widget, g, bounds.origin(), widget->textHeight());
   int scroll = delegate.index();
 
-  if (!widget->text().empty()) {
-    const std::string& textString = widget->text();
+  // Full text to paint: widget text + suffix
+  const std::string textString = widget->text() + widget->getSuffix();
+
+  if (!textString.empty()) {
     base::utf8_decode dec(textString);
     auto pos = dec.pos();
     for (int i = 0; i < scroll && dec.next(); ++i)
@@ -1454,33 +1463,21 @@ void SkinTheme::drawEntryText(ui::Graphics* g, ui::Entry* widget)
 
     IntersectClip clip(g, bounds);
     if (clip) {
-      g->drawTextWithDelegate(
-        std::string(pos, textString.end()), // TODO use a string_view()
-        colors.text(),
-        ColorNone,
-        gfx::Point(bounds.x, widget->textBaseline() - widget->textBlob()->baseline()),
-        &delegate);
-    }
-  }
+      int baselineAdjustment = widget->textBaseline();
+      if (auto blob = widget->textBlob()) {
+        baselineAdjustment -= blob->baseline();
+      }
+      else {
+        text::FontMetrics metrics;
+        widget->font()->metrics(&metrics);
+        baselineAdjustment += metrics.ascent;
+      }
 
-  bounds.x += delegate.textBounds().w;
-
-  // Draw suffix if there is enough space
-  if (!widget->getSuffix().empty()) {
-    Rect sufBounds(bounds.x,
-                   bounds.y,
-                   bounds.x2() - widget->childSpacing() - bounds.x,
-                   widget->textHeight());
-    IntersectClip clip(g, sufBounds & widget->clientChildrenBounds());
-    if (clip) {
-      drawText(g,
-               widget->getSuffix().c_str(),
-               colors.entrySuffix(),
-               ColorNone,
-               widget,
-               sufBounds,
-               widget->align(),
-               0);
+      g->drawTextWithDelegate(std::string(pos, textString.end()), // TODO use a string_view()
+                              colors.text(),
+                              ColorNone,
+                              gfx::Point(bounds.x, baselineAdjustment),
+                              &delegate);
     }
   }
 

commit 2d87a7b1843720902b2db52694b16aa2f91c20b7

[ui] Fix drawing cursor at the end of the Entry/Combobox field
Regression introduced in 1a6a39700e680e15c430d1aaa330202846bc8b62

src/app/ui/skin/skin_theme.cpp
@@ -1483,6 +1483,8 @@ void SkinTheme::drawEntryText(ui::Graphics* g, ui::Entry* widget)
 
   // Draw caret at the end of the text
   if (!delegate.caretDrawn()) {
+    bounds.x += delegate.textBounds().w;
+
     gfx::Rect charBounds(bounds.x + widget->bounds().x,
                          bounds.y + widget->bounds().y,
                          0,