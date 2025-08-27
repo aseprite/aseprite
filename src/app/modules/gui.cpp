aseprite/aseprite

commit f61c2c39500e9a9d5b2790a0557beb7d59779922

Revert Esc key behavior to drop+deselect (fix #5102)
This adds a new button in the context bar so we have the three
available options to handle a transformation/drop pixels:

* Drop pixels and deselect (Esc key)
* Drop pixels but keep the selection (Enter key), new "Apply" command
* Discard changes/undo (Ctrl+Z)

This adds a new key context (Transformation) and also fixes tooltip
shortcuts on context bar buttons to show the current configured
shortcut for each action.

Reverts debab653fa106c4df5c6392a82edcf07902979ed and 194f8424a80fc89d5bc8109817e4dd70b5d1b601

src/app/modules/gui.cpp
@@ -681,81 +681,89 @@ void CustomizedGuiManager::onNewDisplayConfiguration(Display* display)
 bool CustomizedGuiManager::processKey(Message* msg)
 {
   App* app = App::instance();
+  const KeyContext currentCtx = KeyboardShortcuts::getCurrentKeyContext();
   const KeyboardShortcuts* keys = KeyboardShortcuts::instance();
-  const KeyContext contexts[] = { KeyboardShortcuts::getCurrentKeyContext(), KeyContext::Normal };
+  const KeyContext contexts[] = { currentCtx, KeyContext::Normal };
   int n = (contexts[0] != contexts[1] ? 2 : 1);
+
+  // Find best match (prefer the shortcut that matches the context first)
+  KeyPtr key = nullptr;
   for (int i = 0; i < n; ++i) {
-    for (const KeyPtr& key : *keys) {
-      if (key->isPressed(msg, contexts[i])) {
-        // Cancel menu-bar loops (to close any popup menu)
-        app->mainWindow()->getMenuBar()->cancelMenuLoop();
-
-        switch (key->type()) {
-          case KeyType::Tool: {
-            tools::Tool* current_tool = app->activeTool();
-            tools::Tool* select_this_tool = key->tool();
-            tools::ToolBox* toolbox = app->toolBox();
-            std::vector<tools::Tool*> possibles;
-
-            // Collect all tools with the pressed keyboard-shortcut
-            for (tools::Tool* tool : *toolbox) {
-              const KeyPtr key = keys->tool(tool);
-              if (key && key->isPressed(msg))
-                possibles.push_back(tool);
-            }
-
-            if (possibles.size() >= 2) {
-              bool done = false;
-
-              for (size_t i = 0; i < possibles.size(); ++i) {
-                if (possibles[i] != current_tool &&
-                    ToolBar::instance()->isToolVisible(possibles[i])) {
-                  select_this_tool = possibles[i];
-                  done = true;
-                  break;
-                }
-              }
-
-              if (!done) {
-                for (size_t i = 0; i < possibles.size(); ++i) {
-                  // If one of the possibilities is the current tool
-                  if (possibles[i] == current_tool) {
-                    // We select the next tool in the possibilities
-                    select_this_tool = possibles[(i + 1) % possibles.size()];
-                    break;
-                  }
-                }
-              }
-            }
-
-            ToolBar::instance()->selectTool(select_this_tool);
-            return true;
-          }
-
-          case KeyType::Command: {
-            Command* command = key->command();
-
-            // Commands are executed only when the main window is
-            // the current window running.
-            if (getForegroundWindow() == app->mainWindow()) {
-              // OK, so we can execute the command represented
-              // by the pressed-key in the message...
-              UIContext::instance()->executeCommandFromMenuOrShortcut(command, key->params());
-              return true;
-            }
-            break;
-          }
-
-          case KeyType::Quicktool: {
-            // Do nothing, it is used in the editor through the
-            // KeyboardShortcuts::getCurrentQuicktool() function.
-            break;
-          }
-        }
-        break;
+    for (const KeyPtr& k : *keys) {
+      if (k->isPressed(msg, contexts[i]) &&
+          (!key || (key->keycontext() != currentCtx && k->keycontext() == currentCtx))) {
+        key = k;
       }
     }
   }
+  if (!key)
+    return false;
+
+  // Cancel menu-bar loops (to close any popup menu)
+  app->mainWindow()->getMenuBar()->cancelMenuLoop();
+
+  switch (key->type()) {
+    case KeyType::Tool: {
+      tools::Tool* current_tool = app->activeTool();
+      tools::Tool* select_this_tool = key->tool();
+      tools::ToolBox* toolbox = app->toolBox();
+      std::vector<tools::Tool*> possibles;
+
+      // Collect all tools with the pressed keyboard-shortcut
+      for (tools::Tool* tool : *toolbox) {
+        const KeyPtr key = keys->tool(tool);
+        if (key && key->isPressed(msg))
+          possibles.push_back(tool);
+      }
+
+      if (possibles.size() >= 2) {
+        bool done = false;
+
+        for (size_t i = 0; i < possibles.size(); ++i) {
+          if (possibles[i] != current_tool && ToolBar::instance()->isToolVisible(possibles[i])) {
+            select_this_tool = possibles[i];
+            done = true;
+            break;
+          }
+        }
+
+        if (!done) {
+          for (size_t i = 0; i < possibles.size(); ++i) {
+            // If one of the possibilities is the current tool
+            if (possibles[i] == current_tool) {
+              // We select the next tool in the possibilities
+              select_this_tool = possibles[(i + 1) % possibles.size()];
+              break;
+            }
+          }
+        }
+      }
+
+      ToolBar::instance()->selectTool(select_this_tool);
+      return true;
+    }
+
+    case KeyType::Command: {
+      Command* command = key->command();
+
+      // Commands are executed only when the main window is
+      // the current window running.
+      if (getForegroundWindow() == app->mainWindow()) {
+        // OK, so we can execute the command represented
+        // by the pressed-key in the message...
+        UIContext::instance()->executeCommandFromMenuOrShortcut(command, key->params());
+        return true;
+      }
+      break;
+    }
+
+    case KeyType::Quicktool: {
+      // Do nothing, it is used in the editor through the
+      // KeyboardShortcuts::getCurrentQuicktool() function.
+      break;
+    }
+  }
+
   return false;
 }
 

src/app/ui/key_context.h
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2023  Igara Studio S.A.
+// Copyright (C) 2023-2025  Igara Studio S.A.
 // Copyright (C) 2017  David Capello
 //
 // This program is distributed under the terms of
@@ -23,6 +23,7 @@ enum class KeyContext {
   ShapeTool,
   MouseWheel,
   FramesSelection,
+  Transformation,
 };
 
 } // namespace app

src/app/ui/keyboard_shortcuts.cpp
@@ -22,6 +22,7 @@
 #include "app/tools/ink.h"
 #include "app/tools/tool.h"
 #include "app/tools/tool_box.h"
+#include "app/ui/editor/editor.h"
 #include "app/ui/key.h"
 #include "app/ui/timeline/timeline.h"
 #include "app/ui_context.h"
@@ -157,6 +158,7 @@ static struct {
   { "FreehandTool",         app::KeyContext::FreehandTool         },
   { "ShapeTool",            app::KeyContext::ShapeTool            },
   { "FramesSelection",      app::KeyContext::FramesSelection      },
+  { "Transformation",       app::KeyContext::Transformation       },
   { NULL,                   app::KeyContext::Any                  }
 };
 
@@ -1078,6 +1080,12 @@ void KeyboardShortcuts::disableShortcut(const ui::Shortcut& shortcut,
 // static
 KeyContext KeyboardShortcuts::getCurrentKeyContext()
 {
+  // For shortcuts to Apply/Cancel transformation/moving pixels state.
+  auto* editor = Editor::activeEditor();
+  if (editor && editor->isMovingPixels()) {
+    return KeyContext::Transformation;
+  }
+
   auto* ctx = UIContext::instance();
   Doc* doc = ctx->activeDocument();
   if (doc && doc->isMaskVisible() &&
@@ -1335,6 +1343,7 @@ std::string convertKeyContextToUserFriendlyString(KeyContext keyContext)
     case KeyContext::FreehandTool:         return I18N_KEY(key_context_freehand_tool);
     case KeyContext::ShapeTool:            return I18N_KEY(key_context_shape_tool);
     case KeyContext::FramesSelection:      return I18N_KEY(key_context_frames_selection);
+    case KeyContext::Transformation:       return I18N_KEY(key_context_transformation);
   }
   return std::string();
 }

data/extensions/aseprite-theme/dark/theme.xml
@@ -331,6 +331,8 @@
         <part id="flag_highlight" x="16" y="240" w="16" h="10"/>
         <part id="drop_pixels_ok" x="176" y="176" w="7" h="8"/>
         <part id="drop_pixels_ok_selected" x="176" y="184" w="7" h="8"/>
+        <part id="drop_pixels_drop" x="184" y="176" w="7" h="8"/>
+        <part id="drop_pixels_drop_selected" x="184" y="184" w="7" h="8"/>
         <part id="drop_pixels_cancel" x="192" y="176" w="7" h="8"/>
         <part id="drop_pixels_cancel_selected" x="192" y="184" w="7" h="8"/>
         <part id="warning_box" x="112" y="80" w="9" h="10"/>

data/extensions/aseprite-theme/theme.xml
@@ -327,6 +327,8 @@
         <part id="flag_highlight" x="16" y="240" w="16" h="10"/>
         <part id="drop_pixels_ok" x="176" y="176" w="7" h="8"/>
         <part id="drop_pixels_ok_selected" x="176" y="184" w="7" h="8"/>
+        <part id="drop_pixels_drop" x="184" y="176" w="7" h="8"/>
+        <part id="drop_pixels_drop_selected" x="184" y="184" w="7" h="8"/>
         <part id="drop_pixels_cancel" x="192" y="176" w="7" h="8"/>
         <part id="drop_pixels_cancel_selected" x="192" y="184" w="7" h="8"/>
         <part id="warning_box" x="112" y="80" w="9" h="10"/>

data/gui.xml
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!-- Aseprite -->
-<!-- Copyright (C) 2018-2024  Igara Studio S.A. -->
+<!-- Copyright (C) 2018-2025  Igara Studio S.A. -->
 <!-- Copyright (C) 2001-2018  David Capello -->
 <gui>
   <!-- Keyboard shortcuts -->
@@ -370,6 +370,12 @@
         <param name="units" value="zoomed-tile-height" />
         <param name="quantity" value="1" />
       </key>
+
+      <!-- Main selection actions (apply transformation / undo) -->
+      <key command="DeselectMask" shortcut="Esc" context="Transformation" />
+      <key command="Apply" shortcut="Enter" context="Transformation" />
+      <key command="Apply" shortcut="Enter Pad" context="Transformation" />
+      <key command="Undo" shortcut="Ctrl+Z" mac="Cmd+Z" context="Transformation" />
 
       <!-- Move selection with arrows -->
       <key command="MoveMask" shortcut="Left" context="Selection">

commit 9a1e92da3525037599d895ae02855322d802b059

Fix arrow keys to move selection (fix #5385)
Regression introduced in f61c2c39500e9a9d5b2790a0557beb7d59779922

src/app/modules/gui.cpp
@@ -691,7 +691,8 @@ bool CustomizedGuiManager::processKey(Message* msg)
   for (int i = 0; i < n; ++i) {
     for (const KeyPtr& k : *keys) {
       if (k->isPressed(msg, contexts[i]) &&
-          (!key || (key->keycontext() != currentCtx && k->keycontext() == currentCtx))) {
+          (!key ||
+           (key->keycontext() != currentCtx && match_key_context(k->keycontext(), currentCtx)))) {
         key = k;
       }
     }

src/app/ui/key_context.h
@@ -26,6 +26,13 @@ enum class KeyContext {
   Transformation,
 };
 
+inline bool match_key_context(const KeyContext a, const KeyContext b)
+{
+  return (a == b) || (a == KeyContext::Any || b == KeyContext::Any) ||
+         ((a == KeyContext::SelectionTool && b == KeyContext::Transformation) ||
+          (a == KeyContext::Transformation && b == KeyContext::SelectionTool));
+}
+
 } // namespace app
 
 #endif

src/app/ui/keyboard_shortcuts.cpp
@@ -493,7 +493,7 @@ const ui::Shortcut* Key::isPressed(const Message* msg, const KeyContext keyConte
   if (const auto* keyMsg = dynamic_cast<const KeyMessage*>(msg)) {
     for (const Shortcut& shortcut : shortcuts()) {
       if (shortcut.isPressed(keyMsg->modifiers(), keyMsg->scancode(), keyMsg->unicodeChar()) &&
-          (m_keycontext == KeyContext::Any || m_keycontext == keyContext)) {
+          (m_keycontext == KeyContext::Any || match_key_context(m_keycontext, keyContext))) {
         return &shortcut;
       }
     }