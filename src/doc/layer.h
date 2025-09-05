afe47d3986e2f1674de7869cd0b5afee78ac0430
David Capello <david@igara.com>
2025-09-05 16:52:38 -0300

    Make serialization/crash data more generic for future layer types

--- src/doc/layer.h
+++ src/doc/layer.h
@@ -159,6 +159,7 @@ public:
   void moveCel(Cel* cel, frame_t frame);
 
   Cel* cel(frame_t frame) const;
+  const CelList& cels() const { return m_cels; }
   virtual void getCels(CelList& cels) const;
   virtual void displaceFrames(frame_t fromThis, frame_t delta);
 
