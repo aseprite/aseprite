6563a47e11a2f9ad0b80b772b158a21023cbe617
Christian Kaiser <info@ckaiser.com.ar>
2025-10-31 03:55:44 -0300

    Add AppTooltipManager with preferences-based delay

---
+++ src/app/ui/app_tooltips.h
@@ -0,0 +1,33 @@
+// Aseprite
+// Copyright (C) 2019-2025  Igara Studio S.A.
+//
+// This program is distributed under the terms of
+// the End-User License Agreement for Aseprite.
+
+#ifndef APP_UI_APP_TOOLTIPS_H_INCLUDED
+#define APP_UI_APP_TOOLTIPS_H_INCLUDED
+#pragma once
+
+#include "app/app.h"
+#include "app/pref/preferences.h"
+#include "ui/tooltips.h"
+
+namespace app {
+
+class AppTooltipManager : public ui::TooltipManager {
+public:
+  explicit AppTooltipManager()
+  {
+    int delay = App::instance()->preferences().editor.tooltipDelay();
+    m_delayConn = App::instance()->preferences().editor.tooltipDelay.AfterChange.connect(
+      [this] { setDelay(App::instance()->preferences().editor.tooltipDelay()); });
+    setDelay(delay);
+  }
+
+private:
+  obs::scoped_connection m_delayConn;
+};
+
+} // namespace app
+
+#endif
