aseprite/aseprite

commit 6bdfdec840c935f458ed5dd56c3710dca788be3c

Add key bindable "Hide other layers" command (fix #5291)

src/app/ui/timeline/timeline.cpp
@@ -791,53 +791,23 @@ bool Timeline::onProcessMessage(Message* msg)
           if (validLayer(m_clk.layer)) {
             Row& row = m_rows[m_clk.layer];
             Layer* layer = row.layer();
-            ASSERT(layer)
+            ASSERT(layer);
 
-            // Hide everything or restore alternative state
-            bool oneWithInternalState = false;
             if (msg->altPressed()) {
-              for (const Row& row : m_rows) {
-                const Layer* l = row.layer();
-                if (l->hasFlags(LayerFlags::Internal_WasVisible)) {
-                  oneWithInternalState = true;
-                  break;
-                }
-              }
-
-              // If there is one layer with the internal state, restore the previous visible state
-              if (oneWithInternalState) {
-                for (Row& row : m_rows) {
-                  Layer* l = row.layer();
-                  if (l->hasFlags(LayerFlags::Internal_WasVisible)) {
-                    m_document->setLayerVisibilityWithNotifications(l, true);
-                    l->switchFlags(LayerFlags::Internal_WasVisible, false);
-                  }
-                  else {
-                    m_document->setLayerVisibilityWithNotifications(l, false);
-                  }
-                }
-              }
-              // In other case, hide everything
-              else {
-                for (Row& row : m_rows) {
-                  Layer* l = row.layer();
-                  l->switchFlags(LayerFlags::Internal_WasVisible, l->isVisible());
-                  m_document->setLayerVisibilityWithNotifications(l, false);
-                }
-              }
-
-              regenerateRows();
-              invalidate();
-
-              m_document->notifyGeneralUpdate();
+              Command* command = Commands::instance()->byId(
+                CommandId::ToggleOtherLayersVisibility());
+              Params params;
+              params.set("layerId", base::convert_to<std::string>(layer->id()).c_str());
+              m_context->executeCommand(command, params);
             }
+            else {
+              if (layer->isVisible())
+                m_state = STATE_HIDING_LAYERS;
+              else
+                m_state = STATE_SHOWING_LAYERS;
 
-            if (layer->isVisible() && !oneWithInternalState)
-              m_state = STATE_HIDING_LAYERS;
-            else
-              m_state = STATE_SHOWING_LAYERS;
-
-            setLayerVisibleFlag(m_clk.layer, m_state == STATE_SHOWING_LAYERS);
+              setLayerVisibleFlag(m_clk.layer, m_state == STATE_SHOWING_LAYERS);
+            }
           }
           break;
 

data/gui.xml
@@ -318,6 +318,8 @@
       <key command="LayerOpacity"><param name="opacity" value="224" /></key>
       <key command="LayerOpacity" shortcut="Shift+4"><param name="opacity" value="255" /></key>
 
+      <key command="ToggleOtherLayersVisibility" />
+
       <key command="ToggleOtherLayersOpacity" />
       <key command="ToggleOtherLayersOpacity" shortcut="Shift+F7">
         <param name="preview" value="true" />

data/strings/en.ini
@@ -474,6 +474,7 @@ TiledMode_Y = Y Axis
 Timeline = Switch Timeline
 ToggleOtherLayersOpacity = Toggle Other Layers Opacity
 ToggleOtherLayersOpacity_PreviewEditor = Toggle Other Layers Opacity in Preview
+ToggleOtherLayersVisibility = Toggle Other Layers Visibility
 TogglePlayAll = Play All Frames (Ignore Tags)
 TogglePlayOnce = Play Once
 TogglePlaySubtags = Play Subtags & Repetitions

src/app/CMakeLists.txt
@@ -521,6 +521,7 @@ target_sources(app-lib PRIVATE
   commands/show_menu.cpp
   commands/tileset_mode.cpp
   commands/toggle_other_layers_opacity.cpp
+  commands/toggle_other_layers_visibility.cpp
   commands/toggle_play_option.cpp
   console.cpp
   context.cpp

src/app/commands/commands_list.h
@@ -169,6 +169,7 @@ FOR_EACH_COMMAND(TiledMode)
 FOR_EACH_COMMAND(TilesetMode)
 FOR_EACH_COMMAND(Timeline)
 FOR_EACH_COMMAND(ToggleOtherLayersOpacity)
+FOR_EACH_COMMAND(ToggleOtherLayersVisibility)
 FOR_EACH_COMMAND(TogglePlayAll)
 FOR_EACH_COMMAND(TogglePlayOnce)
 FOR_EACH_COMMAND(TogglePlaySubtags)

src/app/commands/toggle_other_layers_visibility.cpp
@@ -1,0 +1,135 @@
+// Aseprite
+// Copyright (C) 2025  Igara Studio S.A.
+//
+// This program is distributed under the terms of
+// the End-User License Agreement for Aseprite.
+
+#include "app/commands/command.h"
+#include "app/commands/new_params.h"
+#include "app/context_access.h"
+#include "app/doc.h"
+#include "app/i18n/strings.h"
+#include "doc/layer.h"
+
+namespace app {
+
+struct ToggleOtherLayersVisibilityParams : public NewParams {
+  Param<int> layerId{ this, 0, "layerId" };
+};
+
+class ToggleOtherLayersVisibilityCommand
+  : public CommandWithNewParams<ToggleOtherLayersVisibilityParams> {
+public:
+  ToggleOtherLayersVisibilityCommand();
+
+private:
+  void onExecute(Context* ctx) override;
+  std::string onGetFriendlyName() const override;
+};
+
+ToggleOtherLayersVisibilityCommand::ToggleOtherLayersVisibilityCommand()
+  : CommandWithNewParams<ToggleOtherLayersVisibilityParams>(
+      CommandId::ToggleOtherLayersVisibility(),
+      CmdUIOnlyFlag)
+{
+}
+
+static void show_children_layers(const LayerGroup* layerGroup, Doc* doc)
+{
+  for (Layer* child : layerGroup->layers()) {
+    if (child->isGroup() && static_cast<const LayerGroup*>(child)->layersCount() > 0)
+      show_children_layers(static_cast<const LayerGroup*>(child), doc);
+    doc->setLayerVisibilityWithNotifications(child, true);
+  }
+}
+
+void ToggleOtherLayersVisibilityCommand::onExecute(Context* ctx)
+{
+  ContextReader reader(ctx);
+  if (!reader.document() || !reader.document()->sprite())
+    return;
+  Doc* doc = reader.document();
+  const Sprite* sprite = doc->sprite();
+  SelectedLayers selLayers;
+  auto range = ctx->range();
+  // Case of Alt+Click eye pick. In this case just one layer is referred by object id via
+  // command parmameter.
+  int layerId = params().layerId();
+  if (layerId > 0) {
+    for (Layer* layer : sprite->allLayers())
+      if (layer->id() == layerId) {
+        selLayers.insert(layer);
+        break;
+      }
+  }
+  // No parameters means 'command via keyboard shortcut' (i.e. no eye pick cases)
+  else {
+    if (range.enabled())
+      selLayers = range.selectedLayers();
+    else
+      selLayers.insert(reader.layer());
+  }
+
+  // Hide everything or restore alternative state
+  bool oneWithInternalState = false;
+  for (Layer* layer : sprite->allLayers()) {
+    if (layer->hasFlags(LayerFlags::Internal_WasVisible)) {
+      oneWithInternalState = true;
+      break;
+    }
+  }
+  // If there is one layer with the internal state, restore the previous visible state
+  if (oneWithInternalState) {
+    for (Layer* layer : sprite->allLayers()) {
+      if (layer->hasFlags(LayerFlags::Internal_WasVisible)) {
+        doc->setLayerVisibilityWithNotifications(layer, true);
+        layer->switchFlags(LayerFlags::Internal_WasVisible, false);
+      }
+      else
+        doc->setLayerVisibilityWithNotifications(layer, false);
+    }
+    // Keep the layer clicked with Alt+click, or the selected layers visible.
+    if (layerId > 0)
+      doc->setLayerVisibilityWithNotifications(*(selLayers.begin()), true);
+    else
+      for (Layer* layer : selLayers)
+        doc->setLayerVisibilityWithNotifications(layer, true);
+  }
+  // In other case, hide everything
+  else {
+    for (Layer* layer : sprite->allLayers()) {
+      layer->switchFlags(LayerFlags::Internal_WasVisible, layer->isVisible());
+      doc->setLayerVisibilityWithNotifications(layer, false);
+    }
+    // Keep the selected layers visible.
+    for (Layer* layer : selLayers) {
+      doc->setLayerVisibilityWithNotifications(layer, true);
+
+      // Show children layers if it's a Group
+      if (layer->isGroup() && static_cast<const LayerGroup*>(layer)->layersCount() > 0)
+        show_children_layers(static_cast<const LayerGroup*>(layer), doc);
+
+      // Show parents too
+      if (layer->parent() && !layer->parent()->isVisible()) {
+        Layer* parentLayer = layer->parent();
+        while (parentLayer) {
+          if (!parentLayer->isVisible())
+            doc->setLayerVisibilityWithNotifications(parentLayer, true);
+          parentLayer = parentLayer->parent();
+        }
+      }
+    }
+  }
+}
+
+std::string ToggleOtherLayersVisibilityCommand::onGetFriendlyName() const
+{
+  return Strings::commands_ToggleOtherLayersVisibility();
+}
+
+Command* CommandFactory::createToggleOtherLayersVisibilityCommand()
+{
+  return new ToggleOtherLayersVisibilityCommand;
+}
+
+} // namespace app

commit 3731253483fe48e2b5c6d3e0d92e5db1bf5b566c

Restore possibility to soloing several layers w/Alt+drag mouse in timeline eye icons
Minor regression from #5527

src/app/ui/timeline/timeline.cpp
@@ -793,20 +793,33 @@ bool Timeline::onProcessMessage(Message* msg)
             Layer* layer = row.layer();
             ASSERT(layer);
 
+            // Hide everything or restore alternative state
+            bool oneWithInternalState = false;
             if (msg->altPressed()) {
+              oneWithInternalState =
+                std::any_of(m_rows.begin(), m_rows.end(), [](const Row& row) -> bool {
+                  return row.layer()->hasFlags(LayerFlags::Internal_WasVisible);
+                });
+
               Command* command = Commands::instance()->byId(CommandId::SoloLayer());
               Params params;
               params.set("layerId", base::convert_to<std::string>(layer->id()).c_str());
               m_context->executeCommand(command, params);
+
+              regenerateRows();
+              invalidate();
             }
             else {
-              if (layer->isVisible())
-                m_state = STATE_HIDING_LAYERS;
-              else
-                m_state = STATE_SHOWING_LAYERS;
-
-              setLayerVisibleFlag(m_clk.layer, m_state == STATE_SHOWING_LAYERS);
+              setLayerVisibleFlag(m_clk.layer, !layer->isVisible());
             }
+
+            // Set the internal m_state to continue with this action
+            // (hiding layers or showing layers) if we drag the mouse
+            // over other eye icons.
+            if (!layer->isVisible() && !oneWithInternalState)
+              m_state = STATE_HIDING_LAYERS;
+            else
+              m_state = STATE_SHOWING_LAYERS;
           }
           break;
 