34f20b9093b7c48950b2d3a0159388fbec8d0af7
Sebastien Major <seb.major@live.com>
2026-02-23 06:15:10 -0500

    Split animated slice commands

--- src/app/doc_api.cpp
+++ src/app/doc_api.cpp
@@ -40,7 +40,7 @@
 #include "app/cmd/set_tag_range.h"
 #include "app/cmd/set_total_frames.h"
 #include "app/cmd/set_transparent_color.h"
-#include "app/cmd/move_slices.h"
+#include "app/cmd/adjust_slice_keys.h"
 #include "app/color_target.h"
 #include "app/color_utils.h"
 #include "app/context.h"
@@ -330,7 +330,7 @@ void DocApi::addEmptyFrame(Sprite* sprite, frame_t newFrame)
 {
   m_transaction.execute(new cmd::AddFrame(sprite, newFrame));
   adjustTags(sprite, newFrame, +1, kDropBeforeFrame, kDefaultTagsAdjustment);
-  m_transaction.execute(new cmd::MoveSlices(sprite, +1, newFrame, newFrame));
+  m_transaction.execute(new cmd::InsertSliceKeys(sprite, newFrame));
 }
 
 void DocApi::addEmptyFramesTo(Sprite* sprite, frame_t newFrame)
@@ -358,7 +358,7 @@ void DocApi::copyFrame(Sprite* sprite,
     copyCel(layer, fromFrame, layer, newFrame);
 
   adjustTags(sprite, newFrame0, +1, dropFramePlace, tagsHandling);
-  m_transaction.execute(new cmd::MoveSlices(sprite, +1, newFrame0, newFrame0));
+  m_transaction.execute(new cmd::InsertSliceKeys(sprite, newFrame0));
 }
 
 void DocApi::removeFrame(Sprite* sprite, frame_t frame)
@@ -366,7 +366,7 @@ void DocApi::removeFrame(Sprite* sprite, frame_t frame)
   ASSERT(frame >= 0);
   m_transaction.execute(new cmd::RemoveFrame(sprite, frame));
   adjustTags(sprite, frame, -1, kDropBeforeFrame, kDefaultTagsAdjustment);
-  m_transaction.execute(new cmd::MoveSlices(sprite, -1, frame, frame));
+  m_transaction.execute(new cmd::DeleteSliceKeys(sprite, frame));
 }
 
 void DocApi::setTotalFrames(Sprite* sprite, frame_t frames)
@@ -423,7 +423,7 @@ void DocApi::moveFrame(Sprite* sprite,
     if (dropFramePlace == kDropAfterFrame)
       ++sliceFrame;
 
-    m_transaction.execute(new cmd::MoveSlices(sprite, 0, frame, sliceFrame));
+    m_transaction.execute(new cmd::MoveSliceKeys(sprite, frame, sliceFrame));
       
     if (tagsHandling != kDontAdjustTags) {
       adjustTags(sprite, frame, -1, dropFramePlace, tagsHandling);
