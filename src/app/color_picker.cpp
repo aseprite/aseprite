efbb6951264fc26c98ecb6984ba28d8c49fbe3a8
Christian Kaiser <info@ckaiser.com.ar>
2026-03-04 04:56:59 -0300

    Fix eyedropper behavior without a reference layer (fix #5685)

--- src/app/color_picker.cpp
+++ src/app/color_picker.cpp
@@ -82,7 +82,7 @@ ColorPicker::ColorPicker() : m_tile(doc::notile), m_alpha(0), m_layer(nullptr)
 void ColorPicker::pickColor(const Site& site,
                             const gfx::PointF& _pos,
                             const render::Projection& proj,
-                            const Mode mode)
+                            Mode mode)
 {
   const doc::Sprite* sprite = site.sprite();
   gfx::PointF pos = _pos;
@@ -99,6 +99,9 @@ void ColorPicker::pickColor(const Site& site,
 
     pos = wrap_pointF(docPref.tiled.mode(), site.sprite()->size(), pos);
   }
+
+  if (mode == FromFirstReferenceLayer && sprite && !sprite->hasVisibleReferenceLayers())
+    mode = FromComposition;
 
   // Get the color from the image
   switch (mode) {
