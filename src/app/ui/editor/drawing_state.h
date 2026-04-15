fbaf91619938f27cc061863f2351d239474765ba
Gaspar Capello <gaspar@igara.com>
2026-04-15 14:39:40 -0300

    Add option to change auto-scroll speed on editor edges (fix #5617)

--- src/app/ui/editor/drawing_state.h
+++ src/app/ui/editor/drawing_state.h
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2019-2025  Igara Studio S.A.
+// Copyright (C) 2019-present  Igara Studio S.A.
 // Copyright (C) 2001-2017  David Capello
 //
 // This program is distributed under the terms of
@@ -39,6 +39,7 @@ public:
   virtual bool onKeyDown(Editor* editor, ui::KeyMessage* msg) override;
   virtual bool onKeyUp(Editor* editor, ui::KeyMessage* msg) override;
   virtual bool onScrollChange(Editor* editor) override;
+  virtual void setIgnoreScrollChange(bool ignore) override;
   virtual bool onUpdateStatusBar(Editor* editor) override;
   virtual void onExposeSpritePixels(const gfx::Region& rgn) override;
 
