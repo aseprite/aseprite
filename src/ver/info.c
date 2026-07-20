ff95de79ea9d4ced2b6b86ffc6d9ec7dec42a05a
David Capello <david@igara.com>
2026-07-20 20:06:40 -0300

    Add mbedtls library to use libcurl+mbedtls for HTTPS requests

--- src/ver/info.c
+++ src/ver/info.c
@@ -1,5 +1,5 @@
 /* Aseprite
-   Copyright (C) 2020-2024  Igara Studio S.A.
+   Copyright (C) 2020-present  Igara Studio S.A.
 
    This program is distributed under the terms of
    the End-User License Agreement for Aseprite.  */
@@ -8,22 +8,16 @@
 #include "generated_version.h" /* It defines the VERSION macro */
 
 #define PACKAGE   "Aseprite"
-#define COPYRIGHT "Copyright (C) 2001-2025 Igara Studio S.A."
-
-#if defined(_WIN32) || defined(__APPLE__)
-  #define HTTP "https"
-#else
-  #define HTTP "http"
-#endif
+#define COPYRIGHT "Copyright (C) 2001-2026 Igara Studio S.A."
 
 #ifdef CUSTOM_WEBSITE_URL
   #define WEBSITE CUSTOM_WEBSITE_URL /* To test web server */
 #else
-  #define WEBSITE HTTP "://www.aseprite.org/"
+  #define WEBSITE "https://www.aseprite.org/"
 #endif
 #define WEBSITE_DOWNLOAD     WEBSITE "download/"
 #define WEBSITE_CONTRIBUTORS WEBSITE "contributors/"
-#define WEBSITE_NEWS_RSS     HTTP "://blog.aseprite.org/rss"
+#define WEBSITE_NEWS_RSS     "https://blog.aseprite.org/rss"
 #define WEBSITE_UPDATE       WEBSITE "update/?xml=1"
 
 const char* get_app_name()
