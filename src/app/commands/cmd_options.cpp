aseprite/aseprite

commit 60bd3378fcea11ab18a2b0d3ad2bc27b55ced40a

New UILayers to composite floating elements (fix #4825)
* Replaced ui::Overlays with ui::UILayer
* Refactored BrushPreview to use its own UILayer (+ new blender shader
  for the negative black & white effect).
* Add a default back UILayer to each ui::Display
* Use ui::move_region() in this "back layer" when GPU is disabled.

Although we should completely remove ui::move_region in a future, for
CPU rendering sometimes it's faster to just move memory instead of
redrawing the widgets.

src/app/ui/editor/brush_preview.cpp
@@ -28,6 +28,7 @@
 #include "app/ui/editor/editor.h"
 #include "app/ui/editor/tool_loop_impl.h"
 #include "app/ui_context.h"
+#include "app/util/shader_helpers.h"
 #include "app/util/wrap_value.h"
 #include "base/debug.h"
 #include "base/scoped_value.h"
@@ -42,6 +43,7 @@
 #include "os/system.h"
 #include "os/window.h"
 #include "render/render.h"
+#include "ui/layer.h"
 #include "ui/manager.h"
 #include "ui/system.h"
 
@@ -88,6 +90,19 @@ static int g_cacheCursorScale = 0;
 // without dot at the center, crosshair with dot in the center)
 static std::array<os::CursorRef, 3> g_solidCursors;
 static gfx::Color g_solidCursorColor = gfx::ColorNone;
+
+// Shader used as a blender for the UILayer to draw the Eraser or
+// selection cursors. Based on color_utils::blackandwhite_neg()
+// function.
+static const char* kNegativeBlackAndWhiteShaderCode = R"(
+half4 main(half4 src, half4 dst) {
+ if (src.a > 0) {
+  return ((dst.r*0.30 + dst.g*0.59 + dst.b*0.11) < 0.5*dst.a) ?
+    half4(1): half4(0, 0, 0, 1);
+ }
+ return dst;
+}
+)";
 
 // static
 void BrushPreview::destroyInternals()
@@ -297,7 +312,7 @@ void BrushPreview::show(const gfx::Point& screenPos)
       m_type &= ~BRUSH_BOUNDARIES;
   }
   if (m_type & BRUSH_BOUNDARIES)
-    generateBoundaries(site, spritePos);
+    createBoundaries(site, spritePos);
 
   // Draw pixel/brush preview
   if (showPreview) {
@@ -415,25 +430,95 @@ void BrushPreview::show(const gfx::Point& screenPos)
   // Save area and draw the cursor
   if (!(m_type & NATIVE_CROSSHAIR) ||
       (m_type & BRUSH_BOUNDARIES)) {
-    ui::ScreenGraphics g(m_editor->display());
-    ui::SetClip clip(&g);
+    ui::Display* display = m_editor->display();
     gfx::Color uiCursorColor = color_utils::color_for_ui(appCursorColor);
 
     if (!(m_type & NATIVE_CROSSHAIR)) {
+      // We use the Display back layer to get pixels in createCrosshairCursor().
+      ui::Graphics g(display, display->backLayer()->surface(), 0, 0);
       createCrosshairCursor(&g, uiCursorColor);
     }
 
-    forEachBrushPixel(&g, spritePos, uiCursorColor, &BrushPreview::savePixelDelegate);
-    forEachBrushPixel(&g, spritePos, uiCursorColor, &BrushPreview::drawPixelDelegate);
-    m_withModifiedPixels = true;
+    // Create (or re-use) the UILayer
+    if ((m_type & SELECTION_CROSSHAIR) || (m_type & BRUSH_BOUNDARIES)) {
+      bool cached = createUILayer(brushBounds);
+      const render::Projection& proj = m_editor->projection();
+
+      if (m_uiLayer->surface()) {
+        gfx::Rect layerBounds = m_uiLayer->surface()->bounds();
+
+        if (m_type & SELECTION_CROSSHAIR) {
+          layerBounds.setOrigin(
+            m_editor->editorToScreen(spritePos)
+            - gfx::Point(layerBounds.size()/2)
+            + gfx::Point(proj.applyX(1)/2,
+                         proj.applyY(1)/2));
+        }
+        else if (m_type & BRUSH_BOUNDARIES) {
+          layerBounds.setOrigin(
+            m_editor->editorToScreen(spritePos)
+            - gfx::Point(layerBounds.w/2 - proj.applyX(layerBounds.w%2),
+                         layerBounds.h/2 - proj.applyY(layerBounds.h%2)));
+        }
+
+        m_uiLayer->setPosition(layerBounds.origin());
+        m_uiLayer->setClipRegion(m_clippingRegion);
+      }
+
+      display->addLayer(m_uiLayer);
+      m_layerAdded = true;
+
+      // Check if the cached brush painted in the m_uiLayer is still valid
+      if (m_cachedType != m_type ||
+          m_cachedBrushGen != m_brushGen) {
+        cached = false;
+      }
+
+      // Here we re-use the cached surface
+      if (!cached && m_uiLayer->surface()) {
+        gfx::Rect layerBounds = m_uiLayer->surface()->bounds();
+        ui::Graphics g(display, m_uiLayer->surface(), 0, 0);
+
+        os::Paint paint;
+        paint.style(os::Paint::Stroke);
+        if (m_blackAndWhiteNegative)
+          paint.color(gfx::rgba(255, 255, 255, 255));
+        else
+          paint.color(uiCursorColor);
+
+        if (m_type & SELECTION_CROSSHAIR) {
+          gfx::Point pos(3, 3);
+          strokeSelectionCrossPixels(&g, pos, paint, 1);
+        }
+        else if (m_type & BRUSH_BOUNDARIES) {
+          gfx::Point pos(layerBounds.w/2 - proj.applyX(layerBounds.w%2),
+                         layerBounds.h/2 - proj.applyY(layerBounds.h%2));
+          strokeBrushBoundaries(&g, pos, paint);
+        }
+
+#if LAF_SKIA
+        if (m_blackAndWhiteNegative) {
+          static sk_sp<SkBlender> blender;
+          if (!blender) {
+            SkRuntimeBlendBuilder builder(make_blender(kNegativeBlackAndWhiteShaderCode));
+            blender = builder.makeBlender();
+          }
+          m_uiLayer->paint().skPaint().setBlender(blender);
+        }
+        else {
+          m_uiLayer->paint().skPaint().setBlender(nullptr);
+        }
+#endif // LAF_SKIA
+
+        m_cachedType = m_type;
+        m_cachedBrushGen = m_brushGen;
+      }
+    }
   }
 
   // Cursor in the editor (model)
   m_onScreen = true;
   m_editorPosition = spritePos;
-
-  // Save the clipping-region to know where to clean the pixels
-  m_oldClippingRegion = m_clippingRegion;
 
   if (m_type & NATIVE_CROSSHAIR)
     ui::set_mouse_cursor(ui::kCrosshairCursor);
@@ -458,19 +543,12 @@ void BrushPreview::hide()
   //if (m_cursor)
   //  m_editor->display()->nativeWindow()->setCursor(os::NativeCursor::Hidden);
 
-  // Get drawable region
-  m_editor->getDrawableRegion(m_clippingRegion, ui::Widget::kCutTopWindows);
+  if (m_layerAdded) {
+    ui::Display* display = m_editor->display();
 
-  // Remove the invalidated region in the editor.
-  m_clippingRegion.createSubtraction(m_clippingRegion,
-                                     m_editor->getUpdateRegion());
-
-  if (m_withModifiedPixels) {
-    // Restore pixels
-    ui::ScreenGraphics g(m_editor->display());
-    ui::SetClip clip(&g);
-    forEachBrushPixel(&g, m_editorPosition, gfx::ColorNone,
-                      &BrushPreview::clearPixelDelegate);
+    // Remove the UI layer to hide the brush preview
+    display->removeLayer(m_uiLayer);
+    m_layerAdded = false;
   }
 
   // Clean pixel/brush preview
@@ -491,9 +569,7 @@ void BrushPreview::hide()
   }
 
   m_onScreen = false;
-
   m_clippingRegion.clear();
-  m_oldClippingRegion.clear();
 }
 
 void BrushPreview::discardBrushPreview()
@@ -530,8 +606,43 @@ void BrushPreview::calculateTileBoundariesOrigin(const doc::Grid& grid,
                            canvasPos.y - spritePos.y);
 }
 
-void BrushPreview::generateBoundaries(const Site& site,
-                                      const gfx::Point& spritePos)
+bool BrushPreview::createUILayer(const gfx::Rect& brushBounds)
+{
+  if (!m_uiLayer)
+    m_uiLayer = ui::UILayer::Make();
+
+  gfx::Size sizeHint(0, 0);
+  const render::Projection& proj = m_editor->projection();
+
+  if (m_type & SELECTION_CROSSHAIR) {
+    gfx::Size pixelSize(6+std::max(1, proj.applyX(1)),
+                        6+std::max(1, proj.applyY(1)));
+    sizeHint = pixelSize;
+  }
+  else if (m_type & BRUSH_BOUNDARIES) {
+    gfx::Size brushSize(proj.applyX(brushBounds.w+2),
+                        proj.applyY(brushBounds.h+2));
+    sizeHint = brushSize;
+  }
+
+  if (sizeHint.w == 0 || sizeHint.h == 0)
+    return false;
+
+  os::SurfaceRef surface = m_uiLayer->surface();
+  if (!surface ||
+      surface->width() != sizeHint.w ||
+      surface->height() != sizeHint.h) {
+    surface = os::System::instance()->makeRgbaSurface(sizeHint.w, sizeHint.h);
+    m_uiLayer->setSurface(surface);
+  }
+  else
+    return true;                // We can use the cached UILayer
+
+  return false;
+}
+
+void BrushPreview::createBoundaries(const Site& site,
+                                    const gfx::Point& spritePos)
 {
   BrushRef brush = getCurrentBrush();
   Layer* currentLayer = site.layer();
@@ -761,38 +872,12 @@ void BrushPreview::createCrosshairCursor(ui::Graphics* g,
   }
 }
 
-void BrushPreview::forEachBrushPixel(
-  ui::Graphics* g,
-  const gfx::Point& spritePos,
-  gfx::Color color,
-  PixelDelegate pixelDelegate)
+// Thick crosshair used for selection tools
+void BrushPreview::strokeSelectionCrossPixels(ui::Graphics* g,
+                                              gfx::Point pos,
+                                              const ui::Paint& paint,
+                                              const int thickness)
 {
-  m_savedPixelsIterator = 0;
-
-  if (m_type & SELECTION_CROSSHAIR)
-    traceSelectionCrossPixels(g, spritePos, color, 1, pixelDelegate);
-
-  if (m_type & BRUSH_BOUNDARIES)
-    traceBrushBoundaries(g, spritePos, color, pixelDelegate);
-
-  m_savedPixelsLimit = m_savedPixelsIterator;
-}
-
-// Old thick cross (used for selection tools)
-void BrushPreview::traceSelectionCrossPixels(
-  ui::Graphics* g,
-  const gfx::Point& pt, gfx::Color color,
-  int thickness, PixelDelegate pixelDelegate)
-{
-  static int cross[6*6] = {
-    0, 0, 1, 1, 0, 0,
-    0, 0, 1, 1, 0, 0,
-    1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1,
-    0, 0, 1, 1, 0, 0,
-    0, 0, 1, 1, 0, 0,
-  };
-  gfx::Point out, outpt = m_editor->editorToScreen(pt);
   const render::Projection& proj = m_editor->projection();
   gfx::Size size(proj.applyX(thickness/2),
                  proj.applyY(thickness/2));
@@ -801,100 +886,39 @@ void BrushPreview::traceSelectionCrossPixels(
   if (size2.w == 0) size2.w = 1;
   if (size2.h == 0) size2.h = 1;
 
-  for (int v=0; v<6; v++) {
-    for (int u=0; u<6; u++) {
-      if (!cross[v*6+u])
-        continue;
+  int u0 = pos.x-size.w-3;
+  int v0 = pos.y-size.h-1;
+  int u1 = pos.x-size.w+size2.w;
+  int v1 = pos.y-size.h+size2.h;
+  g->drawHLine(u0, v0, 3, paint);
+  g->drawHLine(u1, v0, 3, paint);
+  g->drawHLine(u0, v1, 3, paint);
+  g->drawHLine(u1, v1, 3, paint);
 
-      out = outpt;
-      out.x += ((u<3) ? u-size.w-3: u-size.w-3+size2.w);
-      out.y += ((v<3) ? v-size.h-3: v-size.h-3+size2.h);
-
-      (this->*pixelDelegate)(g, out, color);
-    }
-  }
+  u0 = pos.x-size.w-1;
+  v0 = pos.y-size.h-3;
+  g->drawVLine(u0, v0, 3, paint);
+  g->drawVLine(u1, v0, 3, paint);
+  g->drawVLine(u0, v1, 3, paint);
+  g->drawVLine(u1, v1, 3, paint);
 }
 
-// Current brush edges
-void BrushPreview::traceBrushBoundaries(ui::Graphics* g,
-                                        gfx::Point pos,
-                                        gfx::Color color,
-                                        PixelDelegate pixelDelegate)
+// Brush edges used for Eraser tool
+void BrushPreview::strokeBrushBoundaries(ui::Graphics* g,
+                                         gfx::Point pos,
+                                         const os::Paint& paint)
 {
-  for (const auto& seg : m_brushBoundaries) {
-    gfx::Rect bounds = seg.bounds();
-    bounds.offset(pos);
-    bounds = m_editor->editorToScreen(bounds);
+  auto& segs = m_brushBoundaries;
+  segs.createPathIfNeeeded();
 
-    if (seg.open()) {
-      if (seg.vertical()) --bounds.x;
-      else --bounds.y;
-    }
+  const render::Projection& proj = m_editor->projection();
 
-    gfx::Point pt(bounds.x, bounds.y);
-    if (seg.vertical()) {
-      for (; pt.y<bounds.y+bounds.h; ++pt.y)
-        (this->*pixelDelegate)(g, pt, color);
-    }
-    else {
-      for (; pt.x<bounds.x+bounds.w; ++pt.x)
-        (this->*pixelDelegate)(g, pt, color);
-    }
-  }
-}
+  gfx::Path path;
+  segs.path().transform(proj.scaleMatrix(), &path);
 
-//////////////////////////////////////////////////////////////////////
-// Pixel delegates
+  path.offset(pos.x, pos.y);
 
-void BrushPreview::savePixelDelegate(ui::Graphics* g, const gfx::Point& pt, gfx::Color color)
-{
-  if (m_clippingRegion.contains(pt)) {
-    color_t c = g->getPixel(pt.x, pt.y);
-
-    if (m_savedPixelsIterator < (int)m_savedPixels.size())
-      m_savedPixels[m_savedPixelsIterator] = c;
-    else
-      m_savedPixels.push_back(c);
-
-    ++m_savedPixelsIterator;
-  }
-}
-
-void BrushPreview::drawPixelDelegate(ui::Graphics* gfx, const gfx::Point& pt, gfx::Color color)
-{
-  if (m_savedPixelsIterator < (int)m_savedPixels.size() &&
-      m_clippingRegion.contains(pt)) {
-    if (m_blackAndWhiteNegative) {
-      int c = m_savedPixels[m_savedPixelsIterator];
-      int r = gfx::getr(c);
-      int g = gfx::getg(c);
-      int b = gfx::getb(c);
-
-      gfx->putPixel(color_utils::blackandwhite_neg(gfx::rgba(r, g, b)), pt.x, pt.y);
-    }
-    else {
-      gfx->putPixel(color, pt.x, pt.y);
-    }
-    ++m_savedPixelsIterator;
-  }
-}
-
-void BrushPreview::clearPixelDelegate(ui::Graphics* g, const gfx::Point& pt, gfx::Color color)
-{
-  if (m_savedPixelsIterator < (int)m_savedPixels.size()) {
-    if (m_oldClippingRegion.contains(pt)) {
-      if (m_clippingRegion.contains(pt))
-        g->putPixel(m_savedPixels[m_savedPixelsIterator], pt.x, pt.y);
-      ++m_savedPixelsIterator;
-    }
-  }
-
-#if _DEBUG
-  if (!(m_savedPixelsIterator <= m_savedPixelsLimit)) {
-    TRACE("m_savedPixelsIterator <= m_savedPixelsLimit: %d <= %d failed\n",
-          m_savedPixelsIterator, m_savedPixelsLimit);
-  }
-#endif
+  g->drawPath(path, paint);
 }
 
 } // namespace app

src/app/ui/editor/brush_preview.h
@@ -20,6 +20,7 @@
 #include "gfx/region.h"
 #include "os/surface.h"
 #include "ui/cursor.h"
+#include "ui/layer.h"
 
 #include <vector>
 
@@ -86,8 +87,6 @@ namespace app {
     void invalidateRegion(const gfx::Region& region);
 
   private:
-    typedef void (BrushPreview::*PixelDelegate)(ui::Graphics*, const gfx::Point&, gfx::Color);
-
     doc::BrushRef getCurrentBrush();
     static doc::color_t getBrushColor(doc::Sprite* sprite, doc::Layer* layer);
 
@@ -96,24 +95,20 @@ namespace app {
     void calculateTileBoundariesOrigin(const doc::Grid& grid,
                                        const gfx::Point& spritePos);
 
-    void generateBoundaries(const Site& site,
-                            const gfx::Point& spritePos);
+    bool createUILayer(const gfx::Rect& brushBounds);
+    void createBoundaries(const Site& site,
+                          const gfx::Point& spritePos);
 
     // Creates a little native cursor to draw the CROSSHAIR
     void createCrosshairCursor(ui::Graphics* g, const gfx::Color cursorColor);
 
-    void forEachBrushPixel(
-      ui::Graphics* g,
-      const gfx::Point& spritePos,
-      gfx::Color color,
-      PixelDelegate pixelDelegate);
-
-    void traceSelectionCrossPixels(ui::Graphics* g, const gfx::Point& pt, gfx::Color color, int thickness, PixelDelegate pixel);
-    void traceBrushBoundaries(ui::Graphics* g, gfx::Point pos, gfx::Color color, PixelDelegate pixel);
-
-    void savePixelDelegate(ui::Graphics* g, const gfx::Point& pt, gfx::Color color);
-    void drawPixelDelegate(ui::Graphics* g, const gfx::Point& pt, gfx::Color color);
-    void clearPixelDelegate(ui::Graphics* g, const gfx::Point& pt, gfx::Color color);
+    void strokeSelectionCrossPixels(ui::Graphics* g,
+                                    gfx::Point pos,
+                                    const os::Paint& paint,
+                                    int thickness);
+    void strokeBrushBoundaries(ui::Graphics* g,
+                               gfx::Point pos,
+                               const os::Paint& paint);
 
     Editor* m_editor;
     int m_type = CROSSHAIR;
@@ -129,19 +124,17 @@ namespace app {
     gfx::Point m_screenPosition; // Position in the screen (view)
     gfx::Point m_editorPosition; // Position in the editor (model)
 
+    // UI layer to draw (and move) the brush boundaries
+    ui::UILayerRef m_uiLayer;
+    bool m_layerAdded = false;
+    int m_cachedType = 0;
+    int m_cachedBrushGen = 0;
+
     // Information about current brush
     doc::MaskBoundaries m_brushBoundaries;
     int m_brushGen;
 
-    // True if we've modified pixels in the display surface
-    // (e.g. drawing the selection crosshair or the brush edges).
-    bool m_withModifiedPixels = false;
-    std::vector<gfx::Color> m_savedPixels;
-    int m_savedPixelsIterator;
-    int m_savedPixelsLimit;
-
     gfx::Region m_clippingRegion;
-    gfx::Region m_oldClippingRegion;
 
     // Information stored in show() and used in hide() to clear the
     // brush preview in the exact same place.

commit 3ceec26156b0d6c1b2161e582456d1ecd734d7cc

Fix "edges" preview for tiles (fix #5045)
This regression was introduced with
60bd3378fcea11ab18a2b0d3ad2bc27b55ced40a were an auxiliary UILayer is
created to paint the edges once, and then we move this layer to the
correct position. With this patch we move the UILayer with the "tile
preview" correctly.

We've also added a new "Tile Preview" option to differentiate the
"Brush Preview" vs "Tile Preview". And now by default we show the tile
edges with the eraser or when painting with tile = 0 (the empty tile).

data/pref.xml
@@ -228,6 +228,7 @@
       <option id="painting_cursor_type" type="PaintingCursorType" default="PaintingCursorType::CROSSHAIR_ON_SPRITE" />
       <option id="brush_preview" type="BrushPreview" default="BrushPreview::FULL" />
       <option id="brush_preview_in_preview" type="bool" default="false" />
+      <option id="tile_preview" type="BrushPreview" default="BrushPreview::FULL" />
       <option id="snap_to_grid" type="bool" default="false" />
     </section>
     <section id="preview">

data/strings/en.ini
@@ -1538,6 +1538,7 @@ brush_preview_full = Full Preview
 brush_preview_fullall = Full Preview with All Tools
 brush_preview_fullnedges = Full Preview and Edges
 brush_preview_in_preview = Brush Preview in Preview
+tile_preview = Tile Preview:
 cursor_color_type = Crosshair && Brush Edges Color:
 cursor_neg_bw = Negative Black and White
 cursor_specific_color = Specific Color

data/widgets/options.xml
@@ -1,5 +1,5 @@
 <!-- Aseprite -->
-<!-- Copyright (C) 2018-2026  Igara Studio S.A. -->
+<!-- Copyright (C) 2018-present  Igara Studio S.A. -->
 <!-- Copyright (C) 2001-2018  David Capello -->
 <gui>
   <window id="options" text="@.title" help="preferences">
@@ -341,6 +341,15 @@
 
             <label text="@.brush_preview" for="brush_preview" />
             <combobox id="brush_preview">
+              <listitem text="@.brush_preview_none" value="0" />
+              <listitem text="@.brush_preview_edges" value="1" />
+              <listitem text="@.brush_preview_full" value="2" />
+              <listitem text="@.brush_preview_fullall" value="3" />
+              <listitem text="@.brush_preview_fullnedges" value="4" />
+            </combobox>
+
+            <label text="@.tile_preview" for="tile_preview" />
+            <combobox id="tile_preview">
               <listitem text="@.brush_preview_none" value="0" />
               <listitem text="@.brush_preview_edges" value="1" />
               <listitem text="@.brush_preview_full" value="2" />

src/app/commands/cmd_options.cpp
@@ -662,6 +662,7 @@ public:
 
     // Brush preview
     brushPreview()->setSelectedItemIndex((int)m_pref.cursor.brushPreview());
+    tilePreview()->setSelectedItemIndex((int)m_pref.cursor.tilePreview());
 
     // Guide colors
     layerEdgesColor()->setColor(m_pref.guides.layerEdgesColor());
@@ -876,6 +877,8 @@ public:
     m_pref.cursor.cursorColor(cursorColor()->getColor());
     m_pref.cursor.brushPreview(
       static_cast<app::gen::BrushPreview>(brushPreview()->getSelectedItemIndex()));
+    m_pref.cursor.tilePreview(
+      static_cast<app::gen::BrushPreview>(tilePreview()->getSelectedItemIndex()));
     m_pref.cursor.useNativeCursor(nativeCursor()->isSelected());
     m_pref.cursor.cursorScale(base::convert_to<int>(cursorScale()->getValue()));
     m_pref.selection.autoOpaque(autoOpaque()->isSelected());

src/app/ui/editor/brush_preview.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2019-2025  Igara Studio S.A.
+// Copyright (C) 2019-present  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -155,8 +155,9 @@ void BrushPreview::show(const gfx::Point& screenPos)
 
   Doc* document = m_editor->document();
   Sprite* sprite = m_editor->sprite();
-  Site site = m_editor->getSite();
+  const Site site = m_editor->getSite();
   Layer* layer = (m_editor->layer() && m_editor->layer()->isImage() ? m_editor->layer() : nullptr);
+  const doc::Grid grid = site.grid();
   ASSERT(sprite);
 
   // Get drawable region
@@ -178,10 +179,13 @@ void BrushPreview::show(const gfx::Point& screenPos)
   // Get current tilemap mode
   const TilemapMode tilemapMode = site.tilemapMode();
 
-  gfx::Rect origBrushBounds = ((isFloodfill && brush->type() != BrushType::kImageBrushType) ||
-                                   tilemapMode == TilemapMode::Tiles ?
-                                 gfx::Rect(0, 0, 1, 1) :
-                                 brush->bounds());
+  gfx::Rect origBrushBounds;
+  if (isFloodfill && brush->type() != BrushType::kImageBrushType)
+    origBrushBounds = gfx::Rect(0, 0, 1, 1);
+  else if (tilemapMode == TilemapMode::Tiles)
+    origBrushBounds = gfx::Rect(grid.tileSize());
+  else
+    origBrushBounds = brush->bounds();
   gfx::Rect brushBounds = origBrushBounds;
 
   // Cursor in the screen (view)
@@ -220,16 +224,20 @@ void BrushPreview::show(const gfx::Point& screenPos)
   if (ink->isSelection() || ink->isSlice()) {
     m_type = SELECTION_CROSSHAIR;
   }
-  else if ((tilemapMode == TilemapMode::Pixels) &&
-           (brush->type() == kImageBrushType ||
-            ((isFloodfill ? 1 : brush->size()) > (1.0 / m_editor->zoom().scale()))) &&
-           ( // Use cursor bounds for inks that are effects (eraser, blur, etc.)
-             (ink->isEffect()) ||
-             // or when the brush color is transparent and we are not in the background layer
-             (!ink->isShading() && (layer && layer->isTransparent()) &&
-              ((sprite->pixelFormat() == IMAGE_INDEXED && brush_color == mask_index) ||
-               (sprite->pixelFormat() == IMAGE_RGB && rgba_geta(brush_color) == 0) ||
-               (sprite->pixelFormat() == IMAGE_GRAYSCALE && graya_geta(brush_color) == 0))))) {
+  else if (((tilemapMode == TilemapMode::Pixels) &&
+            (brush->type() == kImageBrushType ||
+             ((isFloodfill ? 1 : brush->size()) > (1.0 / m_editor->zoom().scale()))) &&
+            ( // Use cursor bounds for inks that are effects (eraser, blur, etc.)
+              (ink->isEffect()) ||
+              // or when the brush color is transparent and we are not in the background layer
+              (!ink->isShading() && (layer && layer->isTransparent()) &&
+               ((sprite->pixelFormat() == IMAGE_INDEXED && brush_color == mask_index) ||
+                (sprite->pixelFormat() == IMAGE_RGB && rgba_geta(brush_color) == 0) ||
+                (sprite->pixelFormat() == IMAGE_GRAYSCALE && graya_geta(brush_color) == 0))))) ||
+           // Painting with tile 0 or Eraser tool in Tiles mode should show tile boundaries
+           ((tilemapMode == TilemapMode::Tiles) &&
+            ((ink->isPaint() && Preferences::instance().colorBar.fgTile() == 0) ||
+             ink->isEffect()))) {
     m_type = BRUSH_BOUNDARIES;
   }
   else {
@@ -239,7 +247,9 @@ void BrushPreview::show(const gfx::Point& screenPos)
   bool showPreview = false;
   bool showPreviewWithEdges = false;
   bool cancelEdges = false;
-  auto brushPreview = pref.cursor.brushPreview();
+
+  auto brushPreview = (tilemapMode == TilemapMode::Tiles ? pref.cursor.tilePreview() :
+                                                           pref.cursor.brushPreview());
   if (!m_editor->docPref().show.brushPreview())
     brushPreview = app::gen::BrushPreview::NONE;
 
@@ -301,6 +311,9 @@ void BrushPreview::show(const gfx::Point& screenPos)
   // Draw pixel/brush preview
   if (showPreview) {
     brushBounds.offset(spritePos);
+    if (tilemapMode == TilemapMode::Tiles)
+      brushBounds.setOrigin(grid.tileToCanvas(grid.canvasToTile(brushBounds.origin())));
+
     gfx::Rect extraCelBoundsInCanvas = brushBounds;
 
     // Tiled mode might require a bigger extra cel (to show the tiled)
@@ -328,7 +341,6 @@ void BrushPreview::show(const gfx::Point& screenPos)
     gfx::Rect extraCelBounds;
     if (tilemapMode == TilemapMode::Tiles) {
       ASSERT(layer->isTilemap());
-      doc::Grid grid = site.grid();
       extraCelBounds = grid.canvasToTile(extraCelBoundsInCanvas);
       extraCelBoundsInCanvas = grid.tileToCanvas(extraCelBounds);
     }
@@ -438,9 +450,15 @@ void BrushPreview::show(const gfx::Point& screenPos)
                                 gfx::Point(proj.applyX(1) / 2, proj.applyY(1) / 2));
         }
         else if (m_type & BRUSH_BOUNDARIES) {
-          layerBounds.setOrigin(m_editor->editorToScreen(spritePos) -
-                                gfx::Point(layerBounds.w / 2 - proj.applyX(layerBounds.w % 2),
-                                           layerBounds.h / 2 - proj.applyY(layerBounds.h % 2)));
+          if (tilemapMode == TilemapMode::Tiles) {
+            layerBounds.setOrigin(
+              m_editor->editorToScreen(grid.tileToCanvas(grid.canvasToTile(spritePos))));
+          }
+          else {
+            layerBounds.setOrigin(m_editor->editorToScreen(spritePos) -
+                                  gfx::Point(layerBounds.w / 2 - proj.applyX(layerBounds.w % 2),
+                                             layerBounds.h / 2 - proj.applyY(layerBounds.h % 2)));
+          }
         }
 
         m_uiLayer->setPosition(layerBounds.origin());
@@ -474,8 +492,11 @@ void BrushPreview::show(const gfx::Point& screenPos)
           strokeSelectionCrossPixels(&g, pos, paint, 1);
         }
         else if (m_type & BRUSH_BOUNDARIES) {
-          gfx::Point pos(layerBounds.w / 2 - proj.applyX(layerBounds.w % 2),
-                         layerBounds.h / 2 - proj.applyY(layerBounds.h % 2));
+          gfx::Point pos;
+          if (tilemapMode == TilemapMode::Pixels) {
+            pos = gfx::Point(layerBounds.w / 2 - proj.applyX(layerBounds.w % 2),
+                             layerBounds.h / 2 - proj.applyY(layerBounds.h % 2));
+          }
           strokeBrushBoundaries(&g, pos, paint);
         }
 
@@ -578,14 +599,6 @@ void BrushPreview::invalidateRegion(const gfx::Region& region)
   m_clippingRegion.createSubtraction(m_clippingRegion, region);
 }
 
-void BrushPreview::calculateTileBoundariesOrigin(const doc::Grid& grid, const gfx::Point& spritePos)
-{
-  m_brushBoundaries.offset(-m_brushBoundaries.begin()[0].bounds().x,
-                           -m_brushBoundaries.begin()[0].bounds().y);
-  gfx::Point canvasPos = grid.tileToCanvas(grid.canvasToTile(spritePos));
-  m_brushBoundaries.offset(canvasPos.x - spritePos.x, canvasPos.y - spritePos.y);
-}
-
 bool BrushPreview::createUILayer(const gfx::Rect& brushBounds)
 {
   if (!m_uiLayer)
@@ -623,16 +636,11 @@ void BrushPreview::createBoundaries(const Site& site, const gfx::Point& spritePo
   Layer* currentLayer = site.layer();
   TilemapMode tilemapMode = site.tilemapMode();
 
-  if (tilemapMode == TilemapMode::Pixels && tilemapMode == m_lastTilemapMode &&
-      !m_brushBoundaries.isEmpty() && m_brushGen == brush->gen()) {
-    return;
-  }
-  else if (tilemapMode == TilemapMode::Tiles && tilemapMode == m_lastTilemapMode &&
-           m_lastLayer == currentLayer) {
-    // When tilemapMode is 'Tiles' is needed an offset
-    // re-calculation of the brush boundaries, even
-    // if it's no need to update the mask.
-    calculateTileBoundariesOrigin(site.grid(), spritePos);
+  // Re-use current boundaries if they are still valid.
+  if ((tilemapMode == TilemapMode::Pixels ||
+       (tilemapMode == TilemapMode::Tiles && m_lastLayer == currentLayer)) &&
+      tilemapMode == m_lastTilemapMode && !m_brushBoundaries.isEmpty() &&
+      m_brushGen == brush->gen()) {
     return;
   }
 
@@ -662,8 +670,6 @@ void BrushPreview::createBoundaries(const Site& site, const gfx::Point& spritePo
     if (!isOnePixel)
       m_brushBoundaries.offset(-brush->center().x, -brush->center().y);
   }
-  else
-    calculateTileBoundariesOrigin(site.grid(), spritePos);
 
   if (deleteMask)
     delete mask;

src/app/ui/editor/brush_preview.h
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2019-2024  Igara Studio S.A.
+// Copyright (C) 2019-present  Igara Studio S.A.
 // Copyright (C) 2001-2016  David Capello
 //
 // This program is distributed under the terms of
@@ -89,10 +89,6 @@ public:
 private:
   doc::BrushRef getCurrentBrush();
   static doc::color_t getBrushColor(doc::Sprite* sprite, doc::Layer* layer);
-
-  // Offset re-calculation of brush boundaries only for TilemapMode::Tiles.
-  // Used within 'generateBoundaries' function.
-  void calculateTileBoundariesOrigin(const doc::Grid& grid, const gfx::Point& spritePos);
 
   bool createUILayer(const gfx::Rect& brushBounds);
   void createBoundaries(const Site& site, const gfx::Point& spritePos);