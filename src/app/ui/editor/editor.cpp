aseprite/aseprite

commit a522d2b46de31cb4ca0b65f685aa1e488bd09586

Avoid deleting a DocumentPreferences before its connections (fix #6025)
This might not be the final fix for #6025 crash, but at least will
give us the chance to get the real exception behind it.

We've added a new onDeleteDocumentPreferences() to delete the
preferences of the document as the last step after all
onRemoveDocument() connections are called.

src/app/ui/editor/editor.cpp
@@ -232,6 +232,15 @@ Editor::~Editor()
   setCustomizationDelegate(NULL);
 
   m_antsTimer.stop();
+
+  // Delete all EditorStates so we onBeforePopState() each state, and
+  // disconnect all observers from each state. This might be needed
+  // mainly when an unhandled exception is thrown and we don't want to
+  // create another crash from the stack unwinding keeping invalid
+  // observer connections/slots.
+  while (!m_statesHistory.empty())
+    backToPreviousState();
+  m_deletedStates.clear();
 }
 
 void Editor::destroyEditorSharedInternals()
@@ -317,7 +326,9 @@ void Editor::setStateInternal(const EditorStatePtr& newState)
     m_state = m_statesHistory.top();
   }
 
-  ASSERT(m_state);
+  // This can happen from ~Editor() when all states are be deleted.
+  if (!m_state)
+    return;
 
   // Change to the new state.
   m_state->onEnterState(this);

src/app/app.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2018-2025  Igara Studio S.A.
+// Copyright (C) 2018-present  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -72,6 +72,10 @@
 #include "ui/ui.h"
 #include "updater/user_agent.h"
 #include "ver/info.h"
+
+#if ENABLE_SENTRY
+  #include "app/sentry_wrapper.h"
+#endif
 
 #if LAF_MACOS
   #include "os/osx/system.h"
@@ -455,17 +459,14 @@ struct DeleteAllDocs {
       docs.push_back(doc);
 
     for (Doc* doc : docs) {
-      // First we close the document. In this way we receive recent
-      // notifications related to the document as a app::Doc. If
-      // we delete the document directly, we destroy the app::Doc
-      // too early, and then doc::~Document() call
-      // DocsObserver::onRemoveDocument(). In this way, observers
-      // could think that they have a fully created app::Doc when
-      // in reality it's a doc::Document (in the middle of a
-      // destruction process).
-      //
-      // TODO: This problem is because we're extending doc::Document,
-      // in the future, we should remove app::Doc.
+      // First we close the document and then we can delete it. In
+      // this way we receive recent notifications related to the
+      // document as a app::Doc. If we delete the document directly,
+      // we destroy the app::Doc too early, then doc::~Document()
+      // calls DocsObserver::onRemoveDocument(), and observers could
+      // think that they have a fully created app::Doc when in reality
+      // it's just a doc::Document (in the middle of a destruction
+      // process).
       doc->close();
       delete doc;
     }
@@ -479,132 +480,144 @@ void App::run(const bool runGuiManager)
   CloseMainWindow closeMainWindow(m_mainWindow);
   DeleteAllDocs deleteAllDocsAtExit(context());
 
-  // Run the GUI
-  if (isGui()) {
-    auto manager = ui::Manager::getDefault();
+  try {
+    // Run the GUI
+    if (isGui()) {
+      auto manager = ui::Manager::getDefault();
 #if LAF_WINDOWS
-    // How to interpret one finger on Windows tablets.
-    manager->display()->nativeWindow()->setInterpretOneFingerGestureAsMouseMovement(
-      preferences().experimental.oneFingerAsMouseMovement());
+      // How to interpret one finger on Windows tablets.
+      manager->display()->nativeWindow()->setInterpretOneFingerGestureAsMouseMovement(
+        preferences().experimental.oneFingerAsMouseMovement());
   #if ENABLE_WEBP && LAF_SKIA
-    // In Windows we use a custom webp decoder for drag & drop operations.
-    os::set_decode_webp(util::decode_webp);
+      // In Windows we use a custom webp decoder for drag & drop operations.
+      os::set_decode_webp(util::decode_webp);
   #endif
 #endif
 
 #if LAF_LINUX
-    // Setup app icon for Linux window managers
-    try {
-      os::Window* window = os::System::instance()->defaultWindow();
-      os::SurfaceList icons;
+      // Setup app icon for Linux window managers
+      try {
+        os::Window* window = os::System::instance()->defaultWindow();
+        os::SurfaceList icons;
 
-      for (const int size : { 32, 64, 128 }) {
-        ResourceFinder rf;
-        rf.includeDataDir(fmt::format("icons/ase{0}.png", size).c_str());
-        if (rf.findFirst()) {
-          os::SurfaceRef surf = os::System::instance()->loadRgbaSurface(rf.filename().c_str());
-          if (surf) {
-            surf->setImmutable();
-            icons.push_back(surf);
+        for (const int size : { 32, 64, 128 }) {
+          ResourceFinder rf;
+          rf.includeDataDir(fmt::format("icons/ase{0}.png", size).c_str());
+          if (rf.findFirst()) {
+            os::SurfaceRef surf = os::System::instance()->loadRgbaSurface(rf.filename().c_str());
+            if (surf) {
+              surf->setImmutable();
+              icons.push_back(surf);
+            }
           }
         }
-      }
 
-      window->setIcons(icons);
-    }
-    catch (const std::exception&) {
-      // Just ignore the exception, we couldn't change the app icon, no
-      // big deal.
-    }
+        window->setIcons(icons);
+      }
+      catch (const std::exception&) {
+        // Just ignore the exception, we couldn't change the app icon, no
+        // big deal.
+      }
 #endif
 
-    // Initialize Steam API
+      // Initialize Steam API
 #ifdef ENABLE_STEAM
-    std::unique_ptr<steam::SteamAPI> steam;
-    if (m_inAppSteam) {
-      steam = std::make_unique<steam::SteamAPI>();
-      if (steam->isInitialized())
-        os::System::instance()->activateApp();
-    }
-    else {
-      // We tried to load the Steam SDK without calling
-      // SteamAPI_InitSafe() to check if we could run the program
-      // without "in game" indication but still capturing screenshots
-      // on Steam, and that wasn't the case.
-    }
+      std::unique_ptr<steam::SteamAPI> steam;
+      if (m_inAppSteam) {
+        steam = std::make_unique<steam::SteamAPI>();
+        if (steam->isInitialized())
+          os::System::instance()->activateApp();
+      }
+      else {
+        // We tried to load the Steam SDK without calling
+        // SteamAPI_InitSafe() to check if we could run the program
+        // without "in game" indication but still capturing screenshots
+        // on Steam, and that wasn't the case.
+      }
 #endif
 
 #if defined(_DEBUG) || defined(ENABLE_DEVMODE)
-    // On OS X, when we compile Aseprite on devmode, we're using it
-    // outside an app bundle, so we must active the app explicitly.
-    os::System::instance()->activateApp();
+      // On OS X, when we compile Aseprite on devmode, we're using it
+      // outside an app bundle, so we must active the app explicitly.
+      os::System::instance()->activateApp();
 #endif
 
 #ifdef ENABLE_UPDATER
-    // Launch the thread to check for updates.
-    app::CheckUpdateThreadLauncher checkUpdate(m_mainWindow->getCheckUpdateDelegate());
-    checkUpdate.launch();
+      // Launch the thread to check for updates.
+      app::CheckUpdateThreadLauncher checkUpdate(m_mainWindow->getCheckUpdateDelegate());
+      checkUpdate.launch();
 #endif
 
 #if !ENABLE_SENTRY
-    app::SendCrash sendCrash;
-    sendCrash.search();
+      app::SendCrash sendCrash;
+      sendCrash.search();
 #endif
 
-    // Keep the console alive the whole program execute (just in case
-    // we've to print errors).
-    Console console;
+      // Keep the console alive the whole program execute (just in case
+      // we've to print errors).
+      Console console;
 #ifdef ENABLE_SCRIPTING
-    // Use the app::Console() for script errors
-    ConsoleEngineDelegate delegate(console);
-    script::ScopedEngineDelegate setEngineDelegate(m_engine.get(), &delegate);
+      // Use the app::Console() for script errors
+      ConsoleEngineDelegate delegate(console);
+      script::ScopedEngineDelegate setEngineDelegate(m_engine.get(), &delegate);
 #endif
 
-    // Run the GUI main message loop
-    if (runGuiManager) {
-      try {
-        manager->run();
-        set_app_state(AppState::kClosing);
-      }
-      catch (...) {
-        set_app_state(AppState::kClosingWithException);
-        throw;
+      // Run the GUI main message loop
+      if (runGuiManager) {
+        try {
+          manager->run();
+          set_app_state(AppState::kClosing);
+        }
+        catch (...) {
+          set_app_state(AppState::kClosingWithException);
+          throw;
+        }
       }
     }
-  }
 
 #ifdef ENABLE_SCRIPTING
-  // Start shell to execute scripts.
-  if (m_isShell) {
-    m_engine->printLastResult(); // TODO is this needed?
-    Shell shell;
-    shell.run(*m_engine);
-  }
+    // Start shell to execute scripts.
+    if (m_isShell) {
+      m_engine->printLastResult(); // TODO is this needed?
+      Shell shell;
+      shell.run(*m_engine);
+    }
 #endif // ENABLE_SCRIPTING
 
-  // ----------------------------------------------------------------------
+    // ----------------------------------------------------------------------
 
 #ifdef ENABLE_SCRIPTING
-  // Call the exit() function from all plugins
-  extensions().executeExitActions();
+    // Call the exit() function from all plugins
+    extensions().executeExitActions();
 #endif
 
-  if (isGui()) {
-    ExitGui();
+    if (isGui()) {
+      ExitGui();
 
-    // Select no document
-    static_cast<UIContext*>(context())->setActiveView(nullptr);
+      // Select no document
+      static_cast<UIContext*>(context())->setActiveView(nullptr);
 
-    // Delete backups (this is a normal shutdown, we are not handling
-    // exceptions, and we are not in a destructor).
-    m_modules->deleteDataRecovery();
-  }
+      // Delete backups (this is a normal shutdown, we are not handling
+      // exceptions, and we are not in a destructor).
+      m_modules->deleteDataRecovery();
+    }
 #if !LAF_SKIA
-  else if (m_showCliOnlyWarning) {
-    std::printf("You have a CLI-only Aseprite version\n"
-                "To enable GUI support build with LAF_BACKEND=skia\n");
-  }
+    else if (m_showCliOnlyWarning) {
+      std::printf("You have a CLI-only Aseprite version\n"
+                  "To enable GUI support build with LAF_BACKEND=skia\n");
+    }
 #endif
+  }
+  // This is here just in case ~DeleteAllDocs or ~CloseMainWindow
+  // throw another exception when we are unwiding this one. In other
+  // case main() will handle the exception and log it with Sentry too.
+  catch (const std::exception& ex) {
+    LOG(ERROR, "APP::run: Unhandled exception: %s\n", ex.what());
+#if ENABLE_SENTRY
+    Sentry::addBreadcrumb(ex.what());
+#endif
+    throw;
+  }
 }
 
 // Finishes the Aseprite application.

src/app/docs.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2018-2019  Igara Studio S.A.
+// Copyright (C) 2018-present  Igara Studio S.A.
 // Copyright (c) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -68,6 +68,7 @@ void Docs::remove(Doc* doc)
   m_docs.erase(it);
 
   notify_observers(&DocsObserver::onRemoveDocument, doc);
+  notify_observers(&DocsObserver::onDeleteDocumentPreferences, doc);
 
   doc->setContext(NULL);
 }

src/app/docs_observer.h
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2018  Igara Studio S.A.
+// Copyright (C) 2018-present  Igara Studio S.A.
 // Copyright (c) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -29,6 +29,11 @@ public:
   virtual void onAddDocument(Doc* doc) {}
   virtual void onBeforeRemoveDocument(Doc* doc) {}
   virtual void onRemoveDocument(Doc* doc) {}
+
+  // Called to delete the preferences of the document (could be in
+  // onRemoveDocument, but sometimes observers of onRemoveDocument
+  // needs these preferences, so the preferences must be the last thing to delete).
+  virtual void onDeleteDocumentPreferences(Doc* doc) {}
 };
 
 } // namespace app

src/app/pref/preferences.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2018-2024  Igara Studio S.A.
+// Copyright (C) 2018-present  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -234,7 +234,7 @@ void Preferences::removeDocument(Doc* doc)
   }
 }
 
-void Preferences::onRemoveDocument(Doc* doc)
+void Preferences::onDeleteDocumentPreferences(Doc* doc)
 {
   removeDocument(doc);
 }

src/app/pref/preferences.h
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2018-2023  Igara Studio S.A.
+// Copyright (C) 2018-present  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -79,7 +79,7 @@ public:
 
 protected:
   // DocsObserver impl
-  void onRemoveDocument(Doc* doc) override;
+  void onDeleteDocumentPreferences(Doc* doc) override;
 
 private:
   void load();

src/app/ui/doc_view.cpp
@@ -1,5 +1,5 @@
 // Aseprite
-// Copyright (C) 2018-2025  Igara Studio S.A.
+// Copyright (C) 2018-present  Igara Studio S.A.
 // Copyright (C) 2001-2018  David Capello
 //
 // This program is distributed under the terms of
@@ -50,6 +50,10 @@
 #include "ui/shortcut.h"
 #include "ui/system.h"
 #include "ui/view.h"
+
+#if ENABLE_SENTRY
+  #include "app/sentry_wrapper.h"
+#endif
 
 #include <typeinfo>
 
@@ -177,6 +181,10 @@ protected:
       return Editor::onProcessMessage(msg);
     }
     catch (const std::exception& ex) {
+      LOG(ERROR, "EDITOR: Unhandled exception: %s\n", ex.what());
+#if ENABLE_SENTRY
+      Sentry::addBreadcrumb(ex.what());
+#endif
       showUnhandledException(ex, msg);
       return false;
     }

commit 375989a61c3425cd4e8cdedfcfcca4bdfef7e1d9

Fix crash using Export Sprite Sheet from a sprite w/selection (fix #6031)
Regression introduced in a522d2b46de31cb4ca0b65f685aa1e488bd09586

src/app/ui/editor/editor.cpp
@@ -2689,7 +2689,9 @@ bool Editor::isInsideSelection()
   gfx::Point spritePos = screenToEditor(mousePosInDisplay());
   spritePos -= mainTilePosition();
 
-  KeyAction action = m_customizationDelegate->getPressedKeyAction(KeyContext::SelectionTool);
+  const KeyAction action = (m_customizationDelegate ? m_customizationDelegate->getPressedKeyAction(
+                                                        KeyContext::SelectionTool) :
+                                                      KeyAction::None);
   return (action == KeyAction::None) && m_document && m_document->isMaskVisible() &&
          m_document->mask()->containsPoint(spritePos.x, spritePos.y);
 }