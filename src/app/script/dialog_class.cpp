2559d136d32767f3404ac994a3bb12985d6d92e7
Christian Kaiser <info@ckaiser.com.ar>
2026-09-14 18:49:38 -0300

    [lua] Fix dialog relayout not invalidating display and contents (fix ##5766)

--- src/app/script/dialog_class.cpp
+++ src/app/script/dialog_class.cpp
@@ -1722,6 +1722,7 @@ int Dialog_modify(lua_State* L)
     // to modify dialog's properties (which might generate a relayout)
     // during an ongoing resize event.
     if (relayout && !dlg->window.isResizing()) {
+      gfx::Rect prelayoutBounds = dlg->window.bounds();
       dlg->window.layout();
 
       if (dlg->autofit > 0) {
@@ -1790,6 +1791,10 @@ int Dialog_modify(lua_State* L)
                      }
                    });
       }
+
+      dlg->window.invalidate();
+      if (dlg->window.manager()->display())
+        dlg->window.manager()->display()->invalidateRect(prelayoutBounds);
     }
   }
   lua_pushvalue(L, 1);
