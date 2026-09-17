46738dcb0ae7a4cc6c22698267c68a4a788f34d1
David Capello <david@igara.com>
2026-09-17 14:31:23 -0300

    Remove --version / app.version tests because they're used to fail (#2175)

--- tests/scripts/version.lua
+++ tests/scripts/version.lua
@@ -1,16 +1,8 @@
--- Copyright (C) 2019-2021  Igara Studio S.A.
+-- Copyright (C) 2019-present  Igara Studio S.A.
 -- Copyright (C) 2018  David Capello
 --
 -- This file is released under the terms of the MIT license.
 -- Read LICENSE.txt for more information.
-
-assert(string.sub(tostring(app.version), 1, 1) == "1")
-assert(string.sub(tostring(app.version), 2, 2) == ".")
-assert(app.version.major == 1)
-
--- We cannot test the specific app.version from the "main" branch
--- because it's "1.x-dev" (which is converted to "1.0-dev" as Version object)
---assert(app.version > Version("1.2.10-beta4"))
 
 assert(Version("1") == Version("1"))
 assert(Version("1.1") > Version("1"))
