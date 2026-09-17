86052b24794a8fbfe2be59c4f708f67b077fdd10
David Capello <david@igara.com>
2026-09-17 08:23:15 -0300

    Remove dead code from ImageImpl
    clip_rects() member function was replaced by gfx::Clip a long time
    ago.

--- src/doc/image_impl.h
+++ src/doc/image_impl.h
@@ -196,61 +196,6 @@ private:
       addr += m_rowBytes;
     }
   }
-
-  bool clip_rects(const Image* src, int& dst_x, int& dst_y, int& src_x, int& src_y, int& w, int& h)
-    const
-  {
-    // Clip with destionation image
-    if (dst_x < 0) {
-      w += dst_x;
-      src_x -= dst_x;
-      dst_x = 0;
-    }
-    if (dst_y < 0) {
-      h += dst_y;
-      src_y -= dst_y;
-      dst_y = 0;
-    }
-    if (dst_x + w > width()) {
-      w = width() - dst_x;
-    }
-    if (dst_y + h > height()) {
-      h = height() - dst_y;
-    }
-
-    // Clip with source image
-    if (src_x < 0) {
-      w += src_x;
-      dst_x -= src_x;
-      src_x = 0;
-    }
-    if (src_y < 0) {
-      h += src_y;
-      dst_y -= src_y;
-      src_y = 0;
-    }
-    if (src_x + w > src->width()) {
-      w = src->width() - src_x;
-    }
-    if (src_y + h > src->height()) {
-      h = src->height() - src_y;
-    }
-
-    // Empty cases
-    if (w < 1 || h < 1)
-      return false;
-
-    if ((src_x + w <= 0) || (src_x >= src->width()) || (src_y + h <= 0) || (src_y >= src->height()))
-      return false;
-
-    if ((dst_x + w <= 0) || (dst_x >= width()) || (dst_y + h <= 0) || (dst_y >= height()))
-      return false;
-
-    // Check this function is working correctly
-    ASSERT(src->bounds().contains(gfx::Rect(src_x, src_y, w, h)));
-    ASSERT(bounds().contains(gfx::Rect(dst_x, dst_y, w, h)));
-    return true;
-  }
 };
 
 //////////////////////////////////////////////////////////////////////
